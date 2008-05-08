1f6c5cccd269ad70257cacb5d6f13e421d6cfb4f
Steven Barth <steven@midlink.org>
2008-05-08 16:12:37 +0000

    * Readded themes

---
+++ themes/fledermaus/root/www/ffluci/media/cbi.js
@@ -0,0 +1,42 @@
+var cbi_d = {};
+
+function cbi_d_add(field, target, value) {
+	if (!cbi_d[target]) {
+		cbi_d[target] = {};
+	}
+	if (!cbi_d[target][value]) {
+		cbi_d[target][value] = [];
+	}
+	cbi_d[target][value].push(field);
+}
+
+function cbi_d_update(target) {
+	if (!cbi_d[target]) {
+		return;
+	}
+	
+	for (var x in cbi_d[target]) {
+		for (var i=0; i<cbi_d[target][x].length; i++) {	
+			var y = document.getElementById(cbi_d[target][x][i])	
+			y.style.display = "none";
+		}
+	}
+	
+	var t = document.getElementById(target);
+	if (t && t.value && cbi_d[target][t.value]) {
+		for (var i=0; i<cbi_d[target][t.value].length; i++) {			
+			var y = document.getElementById(cbi_d[target][t.value][i])
+			if (!y.value) {	
+				y.style.display = "table-row";
+			} else {
+				y.style.display = "block";
+			}
+		}
+	}
+}
+
+function cbi_d_init() {
+	for (var x in cbi_d) {
+		cbi_d_update(x);
+	}
+}
