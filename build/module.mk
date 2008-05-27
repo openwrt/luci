5df565faba5eb0c26a7ce0e2d6e9092bf71cb1e8
Steven Barth <steven@midlink.org>
2008-05-27 12:13:19 +0000

    add some indirection around make targets of module.mk, so you can combine it more easily with other build instructions

--- build/module.mk
+++ build/module.mk
@@ -1,15 +1,19 @@
-.PHONY: all compile source clean
+.PHONY: all compile compile-module source source-module clean clean-module
 
 all: compile
+compile: compile-module
+clean: clean-module
+source: source-module
 
-source:
+source-module:
 	mkdir -p dist$(LUCI_INSTALLDIR)
 	cp root/* dist -R 2>/dev/null || true
 	cp src/* dist$(LUCI_INSTALLDIR) -R 2>/dev/null || true
 	for i in $$(find dist -name .svn); do rm $$i -rf; done  
 	
-compile: source
+compile-module: source-module
 	for i in $$(find dist -name *.lua -not -name debug.lua); do $(LUAC) $(LUAC_OPTIONS) -o $$i $$i; done
-	
-clean:
+
+clean-module:
 	rm -rf dist
+
