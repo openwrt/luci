52c9166a7abe947662fe03d1051e2d05e64f4f18
Steven Barth <steven@midlink.org>
2008-09-09 21:55:54 +0000

    uvldoc: Added preliminary section support, optimized menu generating

--- libs/uvldoc/luasrc/uvldoc/renderer.lua
+++ libs/uvldoc/luasrc/uvldoc/renderer.lua
@@ -20,7 +20,8 @@ local util = require "luci.util"
 local ltn12 = require "luci.ltn12"
 local template = require "luci.template"
 
-local ipairs = ipairs
+local ipairs, getfenv, pairs, require = ipairs, getfenv, pairs, require
+local luci = luci
 
 module "luci.uvldoc.renderer"
 
@@ -52,11 +53,19 @@ function Generator.make(self)
 
 	template.compiler_mode = "memory"
 	template.viewdir = self.sourcedir
+	template.context.viewns = {
+		include = function(name) template.Template(name):render(getfenv(2)) end,
+		luci = luci,
+		require = require
+	}
 
 	self:_make_index()
 
-	for scheme, file in util.kspairs(self.schemes) do
+	for scheme, package in pairs(self.schemes) do
 		self:_make_package(scheme)
+		for type, section in pairs(package.sections) do
+			self:_make_section(scheme, type)
+		end
 	end
 end
 
@@ -78,10 +87,26 @@ function Generator._make_package(self, scheme)
 	sink()
 end
 
+function Generator._make_section(self, scheme, section)
+	local t = template.Template("section.xml")
+	local sink = ltn12.sink.file(
+		io.open(self.output .. "/" .. self:_section_filename(scheme, section), "w")
+	)
+	local pkg = self.schemes[scheme]
+	t:render({self = self, package = pkg,
+		scheme = scheme, type=section, section=pkg.sections[section],
+		write = sink})
+	sink()
+end
+
 function Generator._index_filename(self)
 	return "index%s" % self.extension
 end
 
 function Generator._scheme_filename(self, scheme)
 	return "scheme.%s%s" % {scheme, self.extension}
+end
+
+function Generator._section_filename(self, scheme, section)
+	return "section.%s.%s%s" % {scheme, section, self.extension}
 end
