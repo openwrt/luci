a175a1776157de619954077e0548fbd0b5ed29a5
Jo-Philipp Wich <jow@openwrt.org>
2008-09-16 11:23:56 +0000

    * luci-0.8: backport p910nd application

---
+++ applications/luci-p910nd/luasrc/controller/p910nd.lua
@@ -0,0 +1,28 @@
+--[[
+
+LuCI UVC Streamer
+(c) 2008 Yanira <forum-2008@email.de>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+
+]]--
+
+module("luci.controller.p910nd", package.seeall)
+
+function index()
+       require("luci.i18n")
+       luci.i18n.loadc("p910nd")
+       if not luci.fs.isfile("/etc/config/p910nd") then
+               return
+       end
+
+       local page = entry({"admin", "services", "p910nd"}, cbi("p910nd"), luci.i18n.translate("p910nd", "Printer server"), 60)
+       page.i18n = "p910nd"
+       page.dependent = true
+end
---
+++ applications/luci-p910nd/luasrc/model/cbi/p910nd.lua
@@ -0,0 +1,33 @@
+--[[
+
+LuCI p910nd
+(c) 2008 Yanira <forum-2008@email.de>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+
+]]--
+
+local uci = luci.model.uci.cursor_state()
+
+m = Map("p910nd", translate("p910nd", "p910nd"),
+        translatef("p910nd_desc"))
+
+s = m:section(TypedSection, "p910nd", translate("settings", "settings"))
+s.addremove = true
+s.anonymous = true
+
+s:option(Flag, "enabled", translate("enabled"))
+
+s:option(Value, "device", translate("device", "Device")).rmempty = true
+
+s:option(Value, "port", translate("port"), translate("port_help")).rmempty = true
+
+s:option(Flag, "bidirectional", translate("bidirectional"))
+
+return m
