75070567629eb140f6f3438cb4a7a11852375f13
Jo-Philipp Wich <jow@openwrt.org>
2008-09-24 21:13:42 +0000

    * luci-0.8: backport ushare updates

---
+++ applications/luci-ushare/luasrc/i18n/ushare.de.lua
@@ -0,0 +1,8 @@
+ushare = 'uShare'
+ushare_desc = 'uShare'
+settings = 'Einstellungen'
+disable_webif = 'Webinterface deaktivieren'
+disable_telnet = 'Telnet-Konsole deaktivieren'
+servername = 'Servername'
+content_directories = 'Freigabeverzeichnisse'
+options = 'Optionen'
