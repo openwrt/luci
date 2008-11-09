6114c472698644076bd26afd69b8a99f4f456b1a
Jo-Philipp Wich <jow@openwrt.org>
2008-11-09 22:42:36 +0000

    * luci/i18n/french: refresh and fix french translation, thanks! (fixes #4)

--- i18n/french/luasrc/i18n/sysauth.fr.lua
+++ i18n/french/luasrc/i18n/sysauth.fr.lua
@@ -1,3 +1,3 @@
 sysauth_head = 'Authorisation requise'
 sysauth_prompt = 'Saisissez votre nom d&#39;utilisateur et mot de passe.'
-sysauth_failed = 'Nom d&#39;utilisateur et/ou mot de passe erroné ! Réessayes s&#39;il vous plaît.'
+sysauth_failed = 'Nom d&#39;utilisateur et/ou mot de passe erroné ! Réessayez s&#39;il vous plaît.'
