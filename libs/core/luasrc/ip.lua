18b4c1f6b6298ccdbbc4be0f0b01bbec8cad7902
Jo-Philipp Wich <jow@openwrt.org>
2008-11-26 22:57:26 +0000

    libs/core/ip: fix host() method

--- libs/core/luasrc/ip.lua
+++ libs/core/luasrc/ip.lua
@@ -481,7 +481,7 @@ end
 -- @see			cidr.broadcast
 -- @see			cidr.mask
 function cidr.host( self )
-	return __bless({ self[1], data, __maxlen(self[1]) })
+	return __bless({ self[1], self[2], __maxlen(self[1]) })
 end
 
 --- Return a corresponding CIDR representing the netmask of this instance.
