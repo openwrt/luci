4ef86cacadfed121d2ea07f4580003c33a371bb8
Steven Barth <steven@midlink.org>
2008-12-05 15:37:39 +0000

    Reload luci.config for each thread

--- libs/web/luasrc/config.lua
+++ libs/web/luasrc/config.lua
@@ -25,9 +25,18 @@ limitations under the License.
 
 ]]--
 
+local util = require "luci.util"
 module("luci.config",
 		function(m)
 			if pcall(require, "luci.model.uci") then
-				setmetatable(m, {__index = luci.model.uci.cursor():get_all("luci")})
+				local config = util.threadlocal()
+				setmetatable(m, {
+					__index = function(tbl, key)
+						if not config[key] then
+							config[key] = luci.model.uci.cursor():get_all("luci", key)
+						end
+						return config[key]
+					end
+				})
 			end
 		end)
