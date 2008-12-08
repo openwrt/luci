e96f31f5a68a6c8c0841946ccbae9d1b09826adc
Jo-Philipp Wich <jow@openwrt.org>
2008-12-08 00:50:57 +0000

    tags/v0.8.2: add missing polipo

---
+++ applications/luci-polipo/luasrc/controller/polipo.lua
@@ -0,0 +1,28 @@
+--[[
+LuCI - Lua Configuration Interface
+
+Copyright 2008 Aleksandar Krsteski <alekrsteski@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+]]--
+module("luci.controller.polipo", package.seeall)
+
+function index()
+	if not luci.fs.isfile("/etc/config/polipo") then
+		return
+	end
+	
+	require("luci.i18n")
+	luci.i18n.loadc("polipo")
+	local i18n = luci.i18n.translate
+	
+	local p = entry({"admin", "services", "polipo"}, cbi("polipo"), i18n("polipo", "Polipo"))
+	p.dependent = true
+	p.i18n = "polipo"
+end
