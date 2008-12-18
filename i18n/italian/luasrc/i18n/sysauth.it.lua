2fa1cc30c5ce23a614cfad148775872606bf593a
Steven Barth <steven@midlink.org>
2008-12-18 12:29:37 +0000

    Merge italian translation

---
+++ i18n/italian/luasrc/i18n/sysauth.it.lua
@@ -0,0 +1,3 @@
+sysauth_head = 'Autorizzazione richiesta'
+sysauth_prompt = 'Per favore inserisci i tuoi username e password.'
+sysauth_failed = 'Username o password non validi! Per favore riprova.'
