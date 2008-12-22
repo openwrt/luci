4364423cd44421199b58e4f3788969a8f30073fd
Jo-Philipp Wich <jow@openwrt.org>
2008-12-22 22:01:15 +0000

    luci/i18n: apply french translations - thanks! (#14, #15, #16)

---
+++ applications/luci-uvc_streamer/luasrc/i18n/uvc_streamer.fr.lua
@@ -0,0 +1,5 @@
+framespersecond = 'Images par seconde'
+resolution = 'Résolution'
+settings = 'Paramètres'
+uvc_streamer = 'Webcam streaming'
+uvc_streamer_desc = 'Configurez le pilote linux UVC-webcam pour votre webcam. Pointez votre navigateur par exemple sur<a href="http://%s:%i/">http://%s:%i/</a>'
