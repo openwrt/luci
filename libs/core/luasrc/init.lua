89c8f98b12f28caf95989d99da6b6cb411f979c5
Jo-Philipp Wich <jow@openwrt.org>
2009-01-17 16:35:02 +0000

    build: add mkversion.sh helper script contrib: (re-)generate version.lua at build time libs/core: use version.lua themes: adept header to version.lua, fix css for openwrt.org

--- libs/core/luasrc/init.lua
+++ libs/core/luasrc/init.lua
@@ -12,9 +12,9 @@ Copyright 2008 Steven Barth <steven@midlink.org>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
-You may obtain a copy of the License at 
+You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0 
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,7 +23,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 ]]--
+
+local require = require
+
 module "luci"
 
-__version__ = "0.9"
-__appname__ = "LuCI"
+local v = require "luci.version"
+
+__version__ = v.luciversion or "0.9"
+__appname__ = v.luciname    or "LuCI"
