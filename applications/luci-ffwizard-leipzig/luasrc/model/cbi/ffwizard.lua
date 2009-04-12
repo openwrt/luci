8dac35f7cd4a7a4f61ca4ce620a7d4e85b20c33a
Jo-Philipp Wich <jow@openwrt.org>
2009-04-12 19:30:57 +0000

    application/luci-ffwizard-leipzig: don't write system hostname to olsrd config, will be taken from system config

--- applications/luci-ffwizard-leipzig/luasrc/model/cbi/ffwizard.lua
+++ applications/luci-ffwizard-leipzig/luasrc/model/cbi/ffwizard.lua
@@ -262,19 +262,6 @@ function olsr.write(self, section, value)
 	local community = net:formvalue(section)
 	local external  = community and uci:get("freifunk", community, "external") or ""
 
-	-- Configure nameservice
-	local hostname
-	uci:foreach("system", "system", function(s) hostname = s.hostname end)
-
-	if hostname then
-		uci:foreach("olsrd", "LoadPlugin",
-			function(s)
-				if s.library == "olsrd_nameservice.so.0.3" then
-					uci:set("olsrd", s['.name'], "name", hostname)
-				end
-			end)
-	end
-
 	-- Delete old interface
 	uci:delete_all("olsrd", "Interface", {interface=device})
 
