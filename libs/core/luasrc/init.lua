62fade570951964115136f2cd47487633cd529be
Jo-Philipp Wich <jow@openwrt.org>
2009-05-17 01:56:15 +0000

    libs/core: make sure that bitlib is loaded, fixes some sdk problems

--- libs/core/luasrc/init.lua
+++ libs/core/luasrc/init.lua
@@ -26,6 +26,11 @@ limitations under the License.
 
 local require = require
 
+-- Make sure that bitlib is loaded
+if not _G.bit then
+	_G.bit = require "bit"
+end
+
 module "luci"
 
 local v = require "luci.version"
