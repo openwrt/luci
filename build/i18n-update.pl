1f3666f7225eb33d932a7ae067dbca05e57ad9ca
Jo-Philipp Wich <jow@openwrt.org>
2009-05-19 17:21:07 +0000

    build: add i18n-update.pl to refresh *.po files from *.pot templates

---
+++ build/i18n-update.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+@ARGV >= 1 || die "Usage: $0 <po directory> [<file pattern>]\n";
+
+my $source  = shift @ARGV;
+my $pattern = shift @ARGV || '*.po';
+
+if( open F, "find $source -type f -name '$pattern' |" )
+{
+	while( chomp( my $file = readline F ) )
+	{
+		my ( $basename ) = $file =~ m{.+/([^/]+)\.po$};
+		
+		if( -f "$source/templates/$basename.pot" )
+		{
+			printf "Updating %-40s", $file;
+			system("msgmerge", "-U", $file, "$source/templates/$basename.pot");
+		}
+	}
+
+	close F;
+}
