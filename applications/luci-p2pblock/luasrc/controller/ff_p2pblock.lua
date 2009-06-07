a44de71eda36b130405d0759b191e474d8e5b5c2
Jo-Philipp Wich <jow@openwrt.org>
2009-06-07 20:24:15 +0000

    luci-0.8: merge r4793

---
+++ applications/luci-p2pblock/luasrc/controller/ff_p2pblock.lua
@@ -0,0 +1,21 @@
+--[[
+LuCI - Lua Configuration Interface
+
+Copyright 2009 Jo-Philipp Wich <xm@subsignal.org>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+]]--
+
+module("luci.controller.ff_p2pblock", package.seeall)
+
+function index()
+	require("luci.i18n").loadc("freifunk")
+	entry({"admin", "network", "firewall", "p2pblock"}, cbi("luci_fw/p2pblock"),
+		luci.i18n.translate("ff_p2pblock", "P2P-Block"), 40)
+end
---
+++ applications/luci-p2pblock/luasrc/model/cbi/luci_fw/p2pblock.lua
@@ -0,0 +1,77 @@
+--[[
+LuCI - Lua Configuration Interface
+
+Copyright 2009 Jo-Philipp Wich <xm@subsignal.org>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+]]--
+
+local sys = require "luci.sys"
+
+m = Map("freifunk_p2pblock", translate("ff_p2pblock", "P2P-Block"),
+	translate("ff_p2pblock_desc", "P2P-Block is a greylisting mechanism to block various peer-to-peer protocols for non-whitelisted clients."))
+
+s = m:section(NamedSection, "p2pblock", "settings", "Settings")
+s.anonymous = true
+s.addremove = false
+
+en = s:option(Flag, "_enabled", translate("ff_p2pblock_enabled", "Enable P2P-Block"))
+en.rmempty = false
+
+function en.cfgvalue()
+	return ( sys.init.enabled("freifunk-p2pblock") and "1" or "0" )
+end
+
+function en.write(self, section, val)
+	if val == "1" then
+		sys.init.enable("freifunk-p2pblock")
+	else
+		sys.init.disable("freifunk-p2pblock")
+	end
+end
+
+s:option(Value, "portrange", translate("ff_p2pblock_portrange", "Portrange"))
+
+s:option(Value, "blocktime", translate("ff_p2pblock_blocktime", "Block Time"),
+	translate("ff_p2pblock_blocktime_desc", "seconds"))
+
+s:option(DynamicList, "whitelist", translate("ff_p2pblock_whitelist", "Whitelisted IPs"))
+
+l7 = s:option(MultiValue, "layer7", translate("ff_p2pblock_layer7", "Layer7-Protokolle"))
+l7.widget = "checkbox"
+l7:value("aim", "AIM Chat")
+l7:value("bittorrent", "Bittorrent")
+l7:value("edonkey", "eDonkey, eMule, Kademlia")
+l7:value("fasttrack", "Fasttrack Protocol")
+l7:value("ftp", "File Transfer Protocol")
+l7:value("gnutella", "Gnutella")
+l7:value("http", "Hypertext Transfer Protocol")
+l7:value("ident", "Ident Protocol")
+l7:value("irc", "Internet Relay Chat")
+l7:value("jabber", "Jabber/XMPP")
+l7:value("msnmessenger", "MSN Messenger")
+l7:value("ntp", "Network Time Protocol")
+l7:value("pop3", "POP3 Protocol")
+l7:value("smtp", "SMTP Protocol")
+l7:value("ssl", "SSL Protocol")
+l7:value("vnc", "VNC Protocol")
+
+ipp2p = s:option(MultiValue, "ipp2p", translate("ff_p2pblock_ipp2p", "IP-P2P"))
+ipp2p.widget = "checkbox"
+ipp2p:value("edk", "eDonkey, eMule, Kademlia")
+ipp2p:value("kazaa", "KaZaA, FastTrack")
+ipp2p:value("gnu", "Gnutella")
+ipp2p:value("dc", "Direct Connect")
+ipp2p:value("bit", "BitTorrent, extended BT")
+ipp2p:value("apple", "AppleJuice")
+ipp2p:value("winmx", "WinMX")
+ipp2p:value("soul", "SoulSeek")
+ipp2p:value("ares", "AresLite")
+
+return m
