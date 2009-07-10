6ea0b5eb454457c3328ae002b3d32bb1555f5891
Jo-Philipp Wich <jow@openwrt.org>
2009-07-10 13:15:43 +0000

    libs/core: make memory tracer work with c and r events, avoid string allocations for debug output

--- libs/core/luasrc/debug.lua
+++ libs/core/luasrc/debug.lua
@@ -6,13 +6,20 @@ module "luci.debug"
 __file__ = debug.getinfo(1, 'S').source:sub(2)
 
 -- Enables the memory tracer with given flags and returns a function to disable the tracer again
-function trap_memtrace(flags)
-	flags = flags or "l"
-	local tracefile = io.open("/tmp/memtrace", "w")
+function trap_memtrace(flags, dest)
+	flags = flags or "clr"
+	local tracefile = io.open(dest or "/tmp/memtrace", "w")
 
 	local function trap(what, line)
 		local info = debug.getinfo(2, "Sn")
-		tracefile:write(info.source..":"..line.."\t"..(info.namewhat or "").."\t"..(info.name or "").."\t"..collectgarbage("count").."\n")
+		if tracefile then
+			tracefile:write(
+				"[", what, "] ", info.source, ":", (line or "?"), "\t",
+				(info.namewhat or ""), "\t",
+				(info.name or ""), "\t",
+				collectgarbage("count"), "\n"
+			)
+		end
 	end
 
 	debug.sethook(trap, flags)
