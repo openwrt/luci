e1802315458fc26a23c891a6f6f3252962f37a3a
Jo-Philipp Wich <jow@openwrt.org>
2009-07-31 23:36:15 +0000

    luci-0.9: merge r5184-r5189

--- libs/lucid-http/luasrc/lucid/http/handler/luci.lua
+++ libs/lucid-http/luasrc/lucid/http/handler/luci.lua
@@ -26,6 +26,7 @@ Luci = util.class(srv.Handler)
 function Luci.__init__(self, name, prefix)
 	srv.Handler.__init__(self, name)
 	self.prefix = prefix
+	dsp.indexcache = "/tmp/luci-indexcache"
 end
 
 function Luci.handle_HEAD(self, ...)
