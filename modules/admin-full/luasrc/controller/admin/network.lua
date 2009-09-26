4d3ddb03a308629abc4f9c37686c5d2351ce5950
Jo-Philipp Wich <jow@openwrt.org>
2009-09-26 11:10:01 +0000

    modules/admin-full: initial work on site survey and network join

--- modules/admin-full/luasrc/controller/admin/network.lua
+++ modules/admin-full/luasrc/controller/admin/network.lua
@@ -29,7 +29,7 @@ function index()
 	page.title  = i18n("a_n_switch")
 	page.order  = 20
 
-	local page = entry({"admin", "network", "wireless"}, arcombine(cbi("admin_network/wireless"), cbi("admin_network/wifi")), i18n("wifi"), 15)
+	local page = entry({"admin", "network", "wireless"}, arcombine(template("admin_network/wifi_overview"), cbi("admin_network/wifi")), i18n("wifi"), 15)
 	page.i18n   = "wifi"
 	page.leaf = true
 	page.subindex = true
@@ -42,6 +42,10 @@ function index()
 				 ifc:upper()).i18n = "wifi"
 		end
 	)
+
+	local page = entry({"admin", "network", "wireless_join"}, call("wifi_join"), nil, 16)
+	page.i18n = "wifi"
+	page.leaf = true
 
 	local page = entry({"admin", "network", "network"}, arcombine(cbi("admin_network/network"), cbi("admin_network/ifaces")), i18n("interfaces", "Schnittstellen"), 10)
 	page.leaf   = true
@@ -80,4 +84,90 @@ function index()
 	page.title  = i18n("a_n_routes_static")
 	page.order  = 50
 
+end
+
+function wifi_join()
+	local function param(x)
+		return luci.http.formvalue(x)
+	end
+
+	local function ptable(x)
+		x = param(x)
+		return x and (type(x) ~= "table" and { x } or x) or {}
+	end
+
+	local dev  = param("device")
+	local ssid = param("join")
+
+	if dev and ssid then
+		local wep     = (tonumber(param("wep")) == 1)
+		local wpa     = tonumber(param("wpa_version")) or 0
+		local channel = tonumber(param("channel"))
+		local mode    = param("mode")
+		local bssid   = param("bssid")
+
+		local confirm = (param("confirm") == "1")
+		local cancel  = param("cancel") and true or false
+
+		if confirm and not cancel then
+			local fixed_bssid = (param("fixed_bssid") == "1")
+			local replace_net = (param("replace_net") == "1")
+			local autoconnect = (param("autoconnect") == "1")
+			local attach_intf = param("attach_intf")
+
+			local uci = require "luci.model.uci".cursor()
+
+			if replace_net then
+				uci:delete_all("wireless", "wifi-iface")
+			end
+
+			local wificonf = {
+				device  = dev,
+				mode    = (mode == "Ad-Hoc" and "adhoc" or "sta"),
+				ssid    = ssid
+			}
+
+			if attach_intf and uci:get("network", attach_intf, "ifname") then
+				-- target network already has a interface, make it a bridge
+				uci:set("network", attach_intf, "type", "bridge")
+				uci:save("network")
+				uci:commit("network")
+
+				if autoconnect then
+					require "luci.sys".call("/sbin/ifup " .. attach_intf)
+				end
+			end
+
+			if fixed_bssid then
+				wificonf.bssid = bssid
+			end
+
+			if wep then
+				wificonf.encryption = "wep"
+				wificonf.key = param("key")
+			elseif wpa > 0 then
+				wificonf.encryption = param("wpa_suite")
+				wificonf.key = param("key")
+			end
+
+			uci:section("wireless", "wifi-iface", nil, wificonf)
+			uci:delete("wireless", dev, "disabled")
+			uci:set("wireless", dev, "channel", channel)
+
+			uci:save("wireless")
+			uci:commit("wireless")
+
+			if autoconnect then
+				require "luci.sys".call("/sbin/wifi")
+			end
+
+			luci.http.redirect(luci.dispatcher.build_url("admin/network/wireless", dev))
+		elseif cancel then
+			luci.http.redirect(luci.dispatcher.build_url("admin/network/wireless_join?device=" .. dev))
+		else
+			luci.template.render("admin_network/wifi_join_settings")
+		end
+	else
+		luci.template.render("admin_network/wifi_join")
+	end
 end
