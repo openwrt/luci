86e00cb771b81dce8d9629326a5c25b92a024d04
Steven Barth <steven@midlink.org>
2009-11-02 15:59:44 +0000

    LuCId: Watch threads in debug mode

--- libs/lucid/luasrc/lucid.lua
+++ libs/lucid/luasrc/lucid.lua
@@ -145,6 +145,7 @@ function run()
 		
 		local pid, stat, code = nixio.wait(-1, "nohang")
 		while pid and pid > 0 do
+			nixio.syslog("info", "Buried thread: " .. pid)
 			if tpids[pid] then
 				tcount = tcount - 1
 				if tpids[pid] ~= true then
@@ -233,6 +234,7 @@ function create_process(threadcb, waitcb)
 	end
 	local pid, code, err = nixio.fork()
 	if pid and pid ~= 0 then
+		nixio.syslog("info", "Created thread: " .. pid)
 		tpids[pid] = waitcb or true
 		tcount = tcount + 1
 	elseif pid == 0 then
