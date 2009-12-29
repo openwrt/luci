7d0134fedc14794d68c9e9fe9dba60744d8ed8af
Jo-Philipp Wich <jow@openwrt.org>
2009-12-29 18:09:17 +0000

    build: emulate nixio.sysinfo() on Darwin

--- build/setup.lua
+++ build/setup.lua
@@ -26,3 +26,22 @@ end
 -- allow any password in local sdk
 local sys = require "luci.sys"
 sys.user.checkpasswd = function() return true end
+
+-- dummy sysinfo on Darwin
+require "nixio"
+
+if not nixio.sysinfo then
+	function nixio.sysinfo()
+		return {
+			bufferram = 0,
+			freehigh = 0,
+			freeram = 0,
+			freeswap = 0,
+			loads = { 0.0, 0.0, 0.0 },
+			mem_unit = 1024,
+			procs = 0,
+			sharedram = 0,
+			totalhigh = 0
+		}
+	end
+end
