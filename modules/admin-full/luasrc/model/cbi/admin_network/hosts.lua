95691ca53c7756e452954da35e2522335b2a727c
Jo-Philipp Wich <jow@openwrt.org>
2010-03-08 01:18:22 +0000

    trunk: drop luci_hosts, now covered by /etc/config/dhcp

--- modules/admin-full/luasrc/model/cbi/admin_network/hosts.lua
+++ modules/admin-full/luasrc/model/cbi/admin_network/hosts.lua
@@ -2,6 +2,7 @@
 LuCI - Lua Configuration Interface
 
 Copyright 2008 Steven Barth <steven@midlink.org>
+Copyright 2010 Jo-Philipp Wich <xm@subsignal.org>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,15 +15,15 @@ $Id$
 
 require("luci.sys")
 require("luci.util")
-m = Map("luci_hosts", translate("Hostnames"))
+m = Map("dhcp", translate("Hostnames"))
 
-s = m:section(TypedSection, "host", translate("Host entries"))
+s = m:section(TypedSection, "domain", translate("Host entries"))
 s.addremove = true
 s.anonymous = true
 s.template = "cbi/tblsection"
 
-hn = s:option(Value, "hostname", translate("Hostname"))
-ip = s:option(Value, "ipaddr", translate("IP address"))
+hn = s:option(Value, "name", translate("Hostname"))
+ip = s:option(Value, "ip", translate("IP address"))
 for i, dataset in ipairs(luci.sys.net.arptable()) do
 	ip:value(
 		dataset["IP address"],
