e47ac7cabc225eae353fd7c347f123a76897ef4d
Jo-Philipp Wich <jow@openwrt.org>
2010-03-25 14:50:26 +0000

    applications/luci-samba: forgot to commit a change of #117

--- applications/luci-samba/luasrc/model/cbi/samba.lua
+++ applications/luci-samba/luasrc/model/cbi/samba.lua
@@ -13,42 +13,49 @@ You may obtain a copy of the License at
 $Id$
 ]]--
 
-m = Map("samba")
+m = Map("samba", translate("Network Shares"))
 
 s = m:section(TypedSection, "samba", "Samba")
 s.anonymous = true
 
-s:option(Value, "name")
-s:option(Value, "description")
-s:option(Value, "workgroup")
-s:option(Flag, "homes")
+s:option(Value, "name", translate("Hostname"))
+s:option(Value, "description", translate("Description"))
+s:option(Value, "workgroup", translate("Workgroup"))
+s:option(Flag, "homes", translate("Share home-directories"),
+        translate("Allow system users to reach their home directories via " ..
+                "network shares"))
 
 
-s = m:section(TypedSection, "sambashare")
+s = m:section(TypedSection, "sambashare", translate("Shared Directories"))
 s.anonymous = true
 s.addremove = true
 s.template = "cbi/tblsection"
 
 s:option(Value, "name", translate("Name"))
-s:option(Value, "path").titleref = luci.dispatcher.build_url("admin", "system", "fstab")
+pth = s:option(Value, "path", translate("Path"))
+if nixio.fs.access("/etc/config/fstab") then
+        pth.titleref = luci.dispatcher.build_url("admin", "system", "fstab")
+end
 
-s:option(Value, "users").rmempty = true
+s:option(Value, "users", translate("Allowed users")).rmempty = true
 
-ro = s:option(Flag, "read_only")
+ro = s:option(Flag, "read_only", translate("Read-only"))
 ro.rmempty = false
 ro.enabled = "yes"
 ro.disabled = "no"
 
-go = s:option(Flag, "guest_ok")
+go = s:option(Flag, "guest_ok", translate("Allow guests"))
 go.rmempty = false
 go.enabled = "yes"
 go.disabled = "no"
 
-cm = s:option(Value, "create_mask")
+cm = s:option(Value, "create_mask", translate("Create mask"),
+        translate("Mask for new files"))
 cm.rmempty = true
 cm.size = 4
 
-dm = s:option(Value, "dir_mask")
+dm = s:option(Value, "dir_mask", translate("Directory mask"),
+        translate("Mask for new directories"))
 dm.rmempty = true
 dm.size = 4
 
