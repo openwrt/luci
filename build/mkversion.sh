119b6d898f0bca9874fa91268d4984be84e92dfc
Jo-Philipp Wich <jow@openwrt.org>
2010-04-03 19:31:40 +0000

    luci-0.9: merge r6016

--- build/mkversion.sh
+++ build/mkversion.sh
@@ -11,10 +11,17 @@ else
 fi
 
 cat <<EOF > $1
+local pcall, dofile, _G = pcall, dofile, _G
+
 module "luci.version"
 
-distname    = "${2:-OpenWrt}"
-distversion = "${3:-Development Snapshot}"
+if pcall(dofile, "/etc/openwrt_release") and _G.DISTRIB_DESCRIPTION then
+	distname    = ""
+	distversion = _G.DISTRIB_DESCRIPTION
+else
+	distname    = "${2:-OpenWrt}"
+	distversion = "${3:-Development Snapshot}"
+end
 
 luciname    = "$variant"
 luciversion = "${5:-svn}"
