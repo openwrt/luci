74f118ab5c48dce01f2ac97c5739c3e1740a101e
Jo-Philipp Wich <jow@openwrt.org>
2010-04-05 17:38:05 +0000

    libs/nixio: save uhttpd certs as well

--- libs/nixio/root/lib/upgrade/luci-add-conffiles.sh
+++ libs/nixio/root/lib/upgrade/luci-add-conffiles.sh
@@ -4,8 +4,12 @@ add_luci_conffiles()
 
 	# save ssl certs
 	if [ -d /etc/nixio ]; then
-		find /etc/nixio >> $filelist
+		find /etc/nixio -type f >> $filelist
 	fi
+
+	# save uhttpd certs
+	[ -f "/etc/uhttpd.key" ] && echo /etc/uhttpd.key >> $filelist
+	[ -f "/etc/uhttpd.crt" ] && echo /etc/uhttpd.crt >> $filelist
 }
 
 sysupgrade_init_conffiles="$sysupgrade_init_conffiles add_luci_conffiles"
