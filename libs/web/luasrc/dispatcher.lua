57dc871f5a90634ea087f0cd3d232046a992cb93
Jo-Philipp Wich <jow@openwrt.org>
2010-04-16 19:05:48 +0000

    luci-0.9: merge r6081

--- libs/web/luasrc/dispatcher.lua
+++ libs/web/luasrc/dispatcher.lua
@@ -146,7 +146,7 @@ function dispatch(request)
 	assert(conf.main,
 		"/etc/config/luci seems to be corrupt, unable to find section 'main'")
 
-	local lang = conf.main.lang
+	local lang = conf.main.lang or "auto"
 	if lang == "auto" then
 		local aclang = http.getenv("HTTP_ACCEPT_LANGUAGE") or ""
 		for lpat in aclang:gmatch("[%w-]+") do
@@ -156,7 +156,7 @@ function dispatch(request)
 				break
 			end
 		end
-        end
+	end
 	require "luci.i18n".setlanguage(lang)
 
 	local c = ctx.tree
