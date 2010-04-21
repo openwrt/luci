d902b228344030aa20f62b5294596726924e2be1
Jo-Philipp Wich <jow@openwrt.org>
2010-04-21 23:04:58 +0000

    applications/luci-vnstat: fix menu label

--- applications/luci-vnstat/luasrc/controller/vnstat.lua
+++ applications/luci-vnstat/luasrc/controller/vnstat.lua
@@ -6,9 +6,9 @@ function index()
 
 	entry({"admin", "network", "vnstat"}, alias("admin", "network", "vnstat", "graphs"), i18n("VnStat Traffic Monitor"), 90).i18n = "vnstat"
 	entry({"admin", "network", "vnstat", "graphs"}, template("vnstat"), i18n("Graphs"), 1)
-	entry({"admin", "network", "vnstat", "config"}, cbi("vnstat"), i18n("VnStat Traffic Monitor"), 2)
+	entry({"admin", "network", "vnstat", "config"}, cbi("vnstat"), i18n("Configuration"), 2)
 
 	entry({"mini", "network", "vnstat"}, alias("mini", "network", "vnstat", "graphs"), i18n("VnStat Traffic Monitor"), 90).i18n = "vnstat"
 	entry({"mini", "network", "vnstat", "graphs"}, template("vnstat"), i18n("Graphs"), 1)
-	entry({"mini", "network", "vnstat", "config"}, cbi("vnstat"), i18n("VnStat Traffic Monitor"), 2)
+	entry({"mini", "network", "vnstat", "config"}, cbi("vnstat"), i18n("Configuration"), 2)
 end
