98d1bef6816c5a7562e625b6e65b42f4342f1132
Jo-Philipp Wich <jow@openwrt.org>
2010-10-20 21:48:44 +0000

    modules/admin-full: add configuration support for ATM bridges

--- modules/admin-full/luasrc/model/cbi/admin_network/network.lua
+++ modules/admin-full/luasrc/model/cbi/admin_network/network.lua
@@ -143,4 +143,53 @@ function errors.cfgvalue(self, section)
 	return string.format("%s / %s", tx, rx)
 end
 
+-- Show ATM bridge section if we have the capabilities
+if fs.access("/usr/sbin/br2684ctl") then
+	atm = m:section(TypedSection, "atm-bridge", translate("ATM Bridges"),
+		translate("ATM bridges expose encapsulated ethernet in AAL5 " ..
+			"connections as virtual Linux network interfaces which can " ..
+			"be used in conjunction with DHCP or PPP to dial into the " ..
+			"provider network."))
+
+	atm.addremove = true
+	atm.anonymous = true
+
+	atm.create = function(self, section)
+		local sid = TypedSection.create(self, section)
+		local max_unit = -1
+
+		m.uci:foreach("network", "atm-bridge",
+			function(s)
+				local u = tonumber(s.unit)
+				if u ~= nil and u > max_unit then
+					max_unit = u
+				end
+			end)
+
+		m.uci:set("network", sid, "unit", max_unit + 1)
+		m.uci:set("network", sid, "atmdev", 0)
+		m.uci:set("network", sid, "encaps", "llc")
+		m.uci:set("network", sid, "payload", "bridged")
+		m.uci:set("network", sid, "vci", 35)
+		m.uci:set("network", sid, "vpi", 8)
+
+		return sid
+	end
+
+	atm:tab("general", translate("General Setup"))
+	atm:tab("advanced", translate("Advanced Settings"))
+
+	vci    = atm:taboption("general", Value, "vci", translate("ATM Virtual Channel Identifier (VCI)"))
+	vpi    = atm:taboption("general", Value, "vpi", translate("ATM Virtual Path Identifier (VPI)"))
+	encaps = atm:taboption("general", ListValue, "encaps", translate("Encapsulation mode"))
+	encaps:value("llc", translate("LLC"))
+	encaps:value("vc", translate("VC-Mux"))
+
+	atmdev  = atm:taboption("advanced", Value, "atmdev", translate("ATM device number"))
+	unit    = atm:taboption("advanced", Value, "unit", translate("Bridge unit number"))
+	payload = atm:taboption("advanced", ListValue, "payload", translate("Forwarding mode"))
+	payload:value("bridged", translate("bridged"))
+	payload:value("routed", translate("routed"))
+end
+
 return m
