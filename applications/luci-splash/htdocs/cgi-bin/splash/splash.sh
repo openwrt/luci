75e1a4cfbcedaa0185c087b3fae35e847500c3fa
Manuel Munz <freifunk@somakoma.de>
2010-12-11 03:41:08 +0000

    applications/luci-splash: Fix typo

--- applications/luci-splash/htdocs/cgi-bin/splash/splash.sh
+++ applications/luci-splash/htdocs/cgi-bin/splash/splash.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo -en "Cache-Control: no-cache, max-age=0, no-store, must-revalidate\r\n"
 echo -en "Pragma: no-cache\r\n"
-echo -en "expires: -1\r\n"
+echo -en "Expires: -1\r\n"
 echo -en "Status: 307 Temporary Redirect\r\n"
 echo -en "Location: http://$SERVER_ADDR/cgi-bin/luci/splash\r\n\r\n" 
