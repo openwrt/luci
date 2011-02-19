1a87c7b347da6dfdec72df261140c5b46088fdc5
Manuel Munz <freifunk@somakoma.de>
2011-02-19 14:54:16 +0000

    modules/freifunk: Change default netmask to 255.255.0.0, change non-working dns, show error when user tries to edit profiles but there is no profile set yet

--- modules/freifunk/luasrc/model/cbi/freifunk/profile_expert.lua
+++ modules/freifunk/luasrc/model/cbi/freifunk/profile_expert.lua
@@ -12,26 +12,30 @@ You may obtain a copy of the License at
 
 local fs = require "nixio.fs"
 local uci = require "luci.model.uci".cursor()
-local community = "/etc/config/profile_" .. uci:get("freifunk", "community", "name")
+local community = uci:get("freifunk", "community", "name")
 
-f = SimpleForm("community", translate("Community profile"), 
-	translate("You can manually edit the selected community profile here."))
+if community == nil then
+        luci.http.redirect(luci.dispatcher.build_url("admin", "freifunk", "profile_error"))
+        return
+else
+        community = "/etc/config/profile_" .. community
+	f = SimpleForm("community", translate("Community profile"), translate("You can manually edit the selected community profile here."))
 
-t = f:field(TextValue, "cop")
-t.rmempty = true
-t.rows = 30
-function t.cfgvalue()
-	return fs.readfile(community) or ""
-end
-
-function f.handle(self, state, data)
-	if state == FORM_VALID then
-		if data.cop then
-			fs.writefile(cop, data.rcs:gsub("\r\n", "\n"))
-		end
+	t = f:field(TextValue, "cop")
+	t.rmempty = true
+	t.rows = 30
+	function t.cfgvalue()
+		return fs.readfile(community) or ""
 	end
-	return true
-end
 
-return f
+	function f.handle(self, state, data)
+		if state == FORM_VALID then
+			if data.cop then
+				fs.writefile(cop, data.rcs:gsub("\r\n", "\n"))
+			end
+		end
+		return true
+	end
+	return f
+end
 
