a3850aa3c7ffbee487cfc9a7903b0561e532f335
Jo-Philipp Wich <jow@openwrt.org>
2011-05-03 21:52:19 +0000

    applications/luci-vnstat: move to status menu

--- applications/luci-vnstat/luasrc/controller/vnstat.lua
+++ applications/luci-vnstat/luasrc/controller/vnstat.lua
@@ -4,9 +4,9 @@ function index()
 	require("luci.i18n").loadc("vnstat")
 	local i18n = luci.i18n.translate
 
-	entry({"admin", "network", "vnstat"}, alias("admin", "network", "vnstat", "graphs"), i18n("VnStat Traffic Monitor"), 90).i18n = "vnstat"
-	entry({"admin", "network", "vnstat", "graphs"}, template("vnstat"), i18n("Graphs"), 1)
-	entry({"admin", "network", "vnstat", "config"}, cbi("vnstat"), i18n("Configuration"), 2)
+	entry({"admin", "status", "vnstat"}, alias("admin", "status", "vnstat", "graphs"), i18n("VnStat Traffic Monitor"), 90).i18n = "vnstat"
+	entry({"admin", "status", "vnstat", "graphs"}, template("vnstat"), i18n("Graphs"), 1)
+	entry({"admin", "status", "vnstat", "config"}, cbi("vnstat"), i18n("Configuration"), 2)
 
 	entry({"mini", "network", "vnstat"}, alias("mini", "network", "vnstat", "graphs"), i18n("VnStat Traffic Monitor"), 90).i18n = "vnstat"
 	entry({"mini", "network", "vnstat", "graphs"}, template("vnstat"), i18n("Graphs"), 1)
