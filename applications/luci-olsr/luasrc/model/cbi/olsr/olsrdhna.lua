623b7d368863da0eecd3469bf044f8106966866f
Manuel Munz <freifunk@somakoma.de>
2011-05-17 17:02:07 +0000

    luci-0.10: Merge r7068

--- applications/luci-olsr/luasrc/model/cbi/olsr/olsrdhna.lua
+++ applications/luci-olsr/luasrc/model/cbi/olsr/olsrdhna.lua
@@ -15,22 +15,30 @@ $Id$
 mh = Map("olsrd", translate("OLSR - HNA-Announcements"))
 
 
-hna4 = mh:section(TypedSection, "Hna4", "Hna4")
+hna4 = mh:section(TypedSection, "Hna4", translate("Hna4"), translate("Both values must use the dotted decimal notation."))
 hna4.addremove = true
 hna4.anonymous = true
 hna4.template  = "cbi/tblsection"
 
 net4 = hna4:option(Value, "netaddr", translate("Network address"))
+net4.datatype = "ip4addr"
+net4.placeholder = "15.15.0.0"
 msk4 = hna4:option(Value, "netmask", translate("Netmask"))
+msk4.datatype = "ip4addr"
+msk4.placeholder = "255.255.255.0"
 
-
-hna6 = mh:section(TypedSection, "Hna6", "Hna6")
+hna6 = mh:section(TypedSection, "Hna6", translate("Hna6"), translate("IPv6 network must be given in full notation, " ..
+	"prefix must be in CIDR notation."))
 hna6.addremove = true
 hna6.anonymous = true
 hna6.template  = "cbi/tblsection"
 
 net6 = hna6:option(Value, "netaddr", translate("Network address"))
+net6.datatype = "ip6addr"
+net6.placeholder = "fec0:2200:106:0:0:0:0:0"
 msk6 = hna6:option(Value, "prefix", translate("Prefix"))
+msk6.datatype = "integer"
+msk6.placeholder = "48"
 
 
 return mh
