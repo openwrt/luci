7dbb07ff6f6c6ef26af7c02c26edb507f88d5471
Jo-Philipp Wich <jow@openwrt.org>
2011-06-17 00:28:52 +0000

    applications/luci-firewall: relax masq_{src,dest} validation to allow for interface names

--- applications/luci-firewall/luasrc/model/cbi/luci_fw/zone.lua
+++ applications/luci-firewall/luasrc/model/cbi/luci_fw/zone.lua
@@ -129,7 +129,7 @@ msrc = s:taboption("advanced", DynamicList, "masq_src",
 	translate("Restrict Masquerading to given source subnets"))
 
 msrc.optional = true
-msrc.datatype = "neg_ip4addr"
+msrc.datatype = "host" -- XXX: ipaddr & uciname
 msrc.placeholder = "0.0.0.0/0"
 msrc:depends("family", "")
 msrc:depends("family", "ipv4")
@@ -138,7 +138,7 @@ mdest = s:taboption("advanced", DynamicList, "masq_dest",
 	translate("Restrict Masquerading to given destination subnets"))
 
 mdest.optional = true
-mdest.datatype = "neg_ip4addr"
+mdest.datatype = "host" -- XXX: ipaddr & uciname
 mdest.placeholder = "0.0.0.0/0"
 mdest:depends("family", "")
 mdest:depends("family", "ipv4")
