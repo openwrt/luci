f09ffc2850d43672d036f3e710a9880444fcd678
Jo-Philipp Wich <jow@openwrt.org>
2011-11-28 04:50:54 +0000

    applications/luci-statistics: add support for conntrack plugin

---
+++ applications/luci-statistics/luasrc/statistics/rrdtool/definitions/conntrack.lua
@@ -0,0 +1,35 @@
+--[[
+
+Luci statistics - conntrack plugin diagram definition
+(c) 2011 Jo-Philipp Wich <xm@subsignal.org>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+
+]]--
+
+module("luci.statistics.rrdtool.definitions.conntrack",package.seeall)
+
+function rrdargs( graph, host, plugin, plugin_instance, dtype )
+	return {
+		title = "%H: Conntrack entries",
+		vlabel = "Count",
+		number_format = "%5.0lf",
+		data = {
+			sources = {
+				conntrack = { "entropy" }
+			},
+			options = {
+				conntrack = { 
+					color = "0000ff",
+					title = "Tracked connections"
+				}
+			}
+		}
+	}
+end
