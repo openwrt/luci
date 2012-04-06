0231c256554f51267bf82c34f142da5f341c6833
Jo-Philipp Wich <jow@openwrt.org>
2012-04-06 09:44:46 +0000

    build/i18n-init.sh: add git support
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>

--- build/i18n-init.sh
+++ build/i18n-init.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
 PATTERN=$1
+SCM=
 
-[ -z "$PATTERN" ] && PATTERN='*.pot'
+[ -d .svn ] && SCM="svn"
+[ -d .git ] && SCM="git"
+
+[ -z "$SCM" ] && {
+	echo "Unsupported SCM tool" >&2
+	exit 1
+}
+
+[ -z "$PATTERN" ] && PATTERN="*.pot"
 
 for lang in $(cd po; echo ?? ??_??); do
 	for file in $(cd po/templates; echo $PATTERN); do
 		if [ -f po/templates/$file -a ! -f "po/$lang/${file%.pot}.po" ]; then
 			msginit --no-translator -l "$lang" -i "po/templates/$file" -o "po/$lang/${file%.pot}.po"
-			svn add "po/$lang/${file%.pot}.po"
+			$SCM add "po/$lang/${file%.pot}.po"
 		fi
 	done
 done
