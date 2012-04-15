84dadb80ab8b2af0f761c44d1cdc012ebc6d8a5b
Manuel Munz <freifunk@somakoma.de>
2012-04-15 20:26:10 +0000

    applications/transmission: Add support for transmission. Patch by Gabor Varga. #392

---
+++ applications/luci-transmission/luasrc/controller/transmission.lua
@@ -0,0 +1,28 @@
+--[[
+LuCI - Lua Configuration Interface - Transmission support
+
+Copyright 2012 Gabor Varga <vargagab@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+]]--
+
+module("luci.controller.transmission", package.seeall)
+
+function index()
+	require("luci.i18n")
+	luci.i18n.loadc("transmission")
+	if not nixio.fs.access("/etc/config/transmission") then
+		return
+	end
+
+	local page = entry({"admin", "services", "transmission"}, cbi("transmission"), _("Transmission"))
+	page.i18n = "transmission"
+	page.dependent = true
+
+end
