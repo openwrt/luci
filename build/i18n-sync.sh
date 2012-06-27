3944cdaac4d145b67668da055c15b931fd117e80
Jo-Philipp Wich <jow@openwrt.org>
2012-06-27 18:02:50 +0000

    build: add i18n-sync.sh, a script to automatically sync most translations

---
+++ build/i18n-sync.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+for m in */*/Makefile; do
+	if grep -qE '^PO *=' $m; then
+		p="${m%/Makefile}"
+		t="$(sed -ne 's/^PO *= *//p' $m)"
+
+		case "$t" in
+			*\ *)
+				echo "WARNING: Cannot handle $p" >&2
+				continue
+			;;
+			*base*)
+				continue
+			;;
+		esac
+
+		if [ -f "po/templates/$t.pot" ]; then
+			./build/i18n-scan.pl "$p" > "po/templates/$t.pot"
+		fi
+	fi
+done
+
+./build/i18n-update.pl po
