86e346f0cdd5ba11809532c14bcb76ae89cb28c8
Manuel Munz <freifunk@somakoma.de>
2012-08-11 08:43:48 +0000

    meshkit: do not accept ra's on wan when using auto-ipv6

--- contrib/package/meshwizard/files/usr/bin/meshwizard/helpers/setup_auto-ipv6.sh
+++ contrib/package/meshwizard/files/usr/bin/meshwizard/helpers/setup_auto-ipv6.sh
@@ -2,7 +2,6 @@
 
 . $dir/functions.sh
 
-echo $profile_ipv6_config
 if [ "$profile_ipv6_config" = "auto-ipv6-fromv4" ]; then
 	mode="fromv4"
 else
@@ -12,3 +11,7 @@ fi
 uci set autoipv6.olsr_node.enabled=1
 uci set autoipv6.olsr_node.mode="$mode"
 uci_commitverbose "Setup auto-ipv6" autoipv6
+
+uci set network.wan.accept_ra=0
+uci_commitverbose "Do not accept ra's on wan when using auto-ipv6" network
+
