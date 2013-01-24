095adf1354f860617ef7fb927ef055151000c4e0
Patrick Grimm <patrick@lunatiki.de>
2013-01-24 09:45:56 +0000

    contrib/package/freifunk-common: add ipv6 support for neigh.sh

--- contrib/package/freifunk-common/files/usr/bin/neigh.sh
+++ contrib/package/freifunk-common/files/usr/bin/neigh.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 wget -q -O - http://localhost:2006/neighbours|sed -e's/LinkQuality/LQ/;s/Hysteresis/Hyst./;s/Willingness/Will./'
+wget -q -O - http://[::1]:2006/neighbours|sed -e's/LinkQuality/LQ/;s/Hysteresis/Hyst./;s/Willingness/Will./'
