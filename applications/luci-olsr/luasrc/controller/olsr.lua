97f0cf2996be5ce5ad58e22c35ea81336727db08
Manuel Munz <freifunk@somakoma.de>
2013-03-16 18:08:13 +0000

    applications/luci-olsr: Use jsoninfo instead of txtinfo as data source.

--- applications/luci-olsr/luasrc/controller/olsr.lua
+++ applications/luci-olsr/luasrc/controller/olsr.lua
@@ -5,10 +5,22 @@ function index()
 		return
 	end
 
+	require("luci.model.uci")
+	local uci = luci.model.uci.cursor_state()
+
+	uci:foreach("olsrd", "olsrd", function(s)
+        	if s.SmartGateway and s.SmartGateway == "yes" then has_smartgw  = true end
+	end)
+
 	local page  = node("admin", "status", "olsr")
 	page.target = template("status-olsr/overview")
 	page.title  = _("OLSR")
 	page.subindex = true
+
+	local page  = node("admin", "status", "olsr", "json")
+	page.target = call("action_json")
+	page.title = nil
+	page.leaf = true
 
 	local page  = node("admin", "status", "olsr", "neighbors")
 	page.target = call("action_neigh")
@@ -36,10 +48,12 @@ function index()
 	page.title  = _("MID")
 	page.order  = 50
 
-	local page  = node("admin", "status", "olsr", "smartgw")
-	page.target = call("action_smartgw")
-	page.title  = _("SmartGW")
-	page.order  = 60
+	if has_smartgw then
+		local page  = node("admin", "status", "olsr", "smartgw")
+		page.target = call("action_smartgw")
+		page.title  = _("SmartGW")
+		page.order  = 60
+	end
 
 	local page  = node("admin", "status", "olsr", "interfaces")
         page.target = call("action_interfaces")
@@ -88,286 +102,231 @@ function index()
 	)
 end
 
-local function compare_links(a, b)
-	local c = tonumber(a.Cost)
-	local d = tonumber(b.Cost)
+function action_json()
+	local http = require "luci.http"
+	local utl = require "luci.util"
 
-	if not c or c == 0 then
-		return false
-	end
+        local jsonreq4 = utl.exec("echo /status | nc 127.0.0.1 9090")
+        local jsonreq6 = utl.exec("echo /status | nc ::1 9090")
 
-	if not d or d == 0 then
-		return true
-	end
-	return c < d
+	http.prepare_content("application/json")
+	http.write("{v4:" .. jsonreq4 .. ", v6:" .. jsonreq6 .. "}")
 end
 
 function action_neigh(json)
-	local data = fetch_txtinfo("links")
+	local data, has_v4, has_v6, error = fetch_jsoninfo('links')
 
-	if not data or not data.Links then
-		luci.template.render("status-olsr/error_olsr")
-		return nil
+	if error then
+		return
 	end
 
-	table.sort(data.Links, compare_links)
+	local uci = require "luci.model.uci".cursor_state()
+	local resolve = uci:get("luci_olsr", "general", "resolve")
+	luci.sys.net.routes(function(r) if r.dest:prefix() == 0 then defaultgw = r.gateway:string() end end)
 
-	luci.template.render("status-olsr/neighbors", {links=data.Links})
+	local function compare(a,b)
+		if a.proto == b.proto then
+			return a.linkCost < b.linkCost
+		else
+			return a.proto < b.proto
+		end
+	end
+
+	for k, v in ipairs(data) do
+		if resolve == "1" then
+			hostname = nixio.getnameinfo(v.remoteIP)
+			if hostname then
+				v.hostname = hostname
+			end
+		end
+		if defaultgw == v.remoteIP then
+			v.defaultgw = 1
+		end
+	end
+
+	table.sort(data, compare)
+	luci.template.render("status-olsr/neighbors", {links=data, has_v4=has_v4, has_v6=has_v6})
 end
 
 function action_routes()
-	local data = fetch_txtinfo("routes")
-
-	if not data or not data.Routes then
-		luci.template.render("status-olsr/error_olsr")
-		return nil
+	local data, has_v4, has_v6, error = fetch_jsoninfo('routes')
+	if error then
+		return
 	end
 
-	local function compare(a, b)
-		local c = tonumber(a.ETX)
-		local d = tonumber(b.ETX)
+	local uci = require "luci.model.uci".cursor_state()
+	local resolve = uci:get("luci_olsr", "general", "resolve")
 
-		if not c or c == 0 then
-			return false
+	for k, v in ipairs(data) do
+		if resolve == "1" then
+			local hostname = nixio.getnameinfo(v.gateway)
+			if hostname then
+				v.hostname = hostname
+			end
 		end
-
-		if not d or d == 0 then
-			return true
-		end
-
-		return c < d
 	end
 
-	table.sort(data.Routes, compare)
+	local function compare(a,b)
+		if a.proto == b.proto then
+			return a.rtpMetricCost < b.rtpMetricCost
+		else
+			return a.proto < b.proto
+		end
+	end
 
-	luci.template.render("status-olsr/routes", {routes=data.Routes})
+	table.sort(data, compare)
+	luci.template.render("status-olsr/routes", {routes=data, has_v4=has_v4, has_v6=has_v6})
 end
 
 function action_topology()
-	local data = fetch_txtinfo("topology")
-
-	if not data or not data.Topology then
-		luci.template.render("status-olsr/error_olsr")
-		return nil
+	local data, has_v4, has_v6, error = fetch_jsoninfo('topology')
+	if error then
+		return
 	end
 
-	local function compare(a, b)
-		return a["Dest. IP"] < b["Dest. IP"]
+	local function compare(a,b)
+		if a.proto == b.proto then
+			return a.tcEdgeCost < b.tcEdgeCost
+		else
+			return a.proto < b.proto
+		end
 	end
 
-	table.sort(data.Topology, compare)
-
-	luci.template.render("status-olsr/topology", {routes=data.Topology})
+	table.sort(data, compare)
+	luci.template.render("status-olsr/topology", {routes=data, has_v4=has_v4, has_v6=has_v6})
 end
 
 function action_hna()
-	local data = fetch_txtinfo("hna")
-
-	if not data or not data.HNA then
-		luci.template.render("status-olsr/error_olsr")
-		return nil
+	local data, has_v4, has_v6, error = fetch_jsoninfo('hna')
+	if error then
+		return
 	end
 
-	local function compare(a, b)
-		return a.Destination < b.Destination
+	local uci = require "luci.model.uci".cursor_state()
+	local resolve = uci:get("luci_olsr", "general", "resolve")
+
+	local function compare(a,b)
+		if a.proto == b.proto then
+			return a.genmask < b.genmask
+		else
+			return a.proto < b.proto
+		end
 	end
 
-	table.sort(data.HNA, compare)
+	for k, v in ipairs(data) do
+		if resolve == "1" then
+			hostname = nixio.getnameinfo(v.gateway)
+			if hostname then
+				v.hostname = hostname
+			end
+		end
+		if v.validityTime then
+	                v.validityTime = tonumber(string.format("%.0f", v.validityTime / 1000))
+		end
+	end
 
-	luci.template.render("status-olsr/hna", {routes=data.HNA})
+	table.sort(data, compare)
+	luci.template.render("status-olsr/hna", {hna=data, has_v4=has_v4, has_v6=has_v6})
 end
 
 function action_mid()
-	local data = fetch_txtinfo("mid")
-
-	if not data or not data.MID then
-		luci.template.render("status-olsr/error_olsr")
-		return nil
+	local data, has_v4, has_v6, error = fetch_jsoninfo('mid')
+	if error then
+		return
 	end
 
-	local function compare(a, b)
-		return a["IP address"] < b["IP address"]
+	local function compare(a,b)
+		if a.proto == b.proto then
+			return a.ipAddress < b.ipAddress
+		else
+			return a.proto < b.proto
+		end
 	end
 
-	table.sort(data.MID, compare)
-
-	luci.template.render("status-olsr/mid", {mids=data.MID})
+	table.sort(data, compare)
+	luci.template.render("status-olsr/mid", {mids=data, has_v4=has_v4, has_v6=has_v6})
 end
 
 function action_smartgw()
-        local data = fetch_txtinfo("gateways")
+	local data, has_v4, has_v6, error = fetch_jsoninfo('gateways')
+	if error then
+		return
+	end
 
-        if not data or not data.Gateways then
-                luci.template.render("status-olsr/error_olsr")
-                return nil
-        end
+	local function compare(a,b)
+		if a.proto == b.proto then
+			return a.tcPathCost < b.tcPathCost
+		else
+			return a.proto < b.proto
+		end
+	end
 
-        local function compare(a, b)
-                return a["ETX"] < b["ETX"]
-        end
-
-        table.sort(data.Gateways, compare)
-
-        luci.template.render("status-olsr/smartgw", {gws=data.Gateways})
+	table.sort(data, compare)
+	luci.template.render("status-olsr/smartgw", {gws=data, has_v4=has_v4, has_v6=has_v6})
 end
 
 function action_interfaces()
-        local data = fetch_txtinfo("interfaces")
+	local data, has_v4, has_v6, error = fetch_jsoninfo('interfaces')
+	if error then
+		return
+	end
 
-        if not data or not data.Interfaces then
-                luci.template.render("status-olsr/error_olsr")
-                return nil
-        end
+	local function compare(a,b)
+		return a.proto < b.proto
+	end
 
-        luci.template.render("status-olsr/interfaces", {iface=data.Interfaces})
+	table.sort(data, compare)
+	luci.template.render("status-olsr/interfaces", {iface=data, has_v4=has_v4, has_v6=has_v6})
 end
 
 -- Internal
-function fetch_txtinfo(otable)
-	require("luci.sys")
-	local uci = require "luci.model.uci".cursor_state()
-	local resolve = uci:get("luci_olsr", "general", "resolve")
-	otable = otable or ""
- 	local rawdata = luci.sys.httpget("http://127.0.0.1:2006/"..otable)
- 	local rawdatav6 = luci.sys.httpget("http://[::1]:2006/"..otable)
-	local data = {}
-	local dataindex = 0
-	local name = ""
-	local defaultgw
+function fetch_jsoninfo(otable)
+	local utl = require "luci.util"
+	local json = require "luci.json"
+        local jsonreq4 = utl.exec("echo /" .. otable .. " | nc 127.0.0.1 9090")
+        local jsondata4 = {}
+        local jsonreq6 = utl.exec("echo /" .. otable .. " | nc ::1 9090")
+        local jsondata6 = {}
+	local data4 = {}
+	local data6 = {}
+	local has_v4 = False
+	local has_v6 = False
 
-	if #rawdata ~= 0 then
-		local tables = luci.util.split(luci.util.trim(rawdata), "\r?\n\r?\n", nil, true)
-
-		if otable == "links" then
-			local route = {}
-			luci.sys.net.routes(function(r) if r.dest:prefix() == 0 then defaultgw = r.gateway:string() end end)
-		end
-
-		for i, tbl in ipairs(tables) do
-			local lines = luci.util.split(tbl, "\r?\n", nil, true)
-			name  = table.remove(lines, 1):sub(8)
-			local keys  = luci.util.split(table.remove(lines, 1), "\t")
-			local split = #keys - 1
-			if not data[name] then
-				data[name] = {}
-			end
-
-			for j, line in ipairs(lines) do
-				dataindex = ( dataindex + 1 )
-				di = dataindex
-				local fields = luci.util.split(line, "\t", split)
-				data[name][di] = {}
-				for k, key in pairs(keys) do
-					if key == "Remote IP" or key == "Dest. IP" or key == "Gateway IP" or key == "Gateway" then
-						data[name][di][key] = fields[k]
-						if resolve == "1" then
-							hostname = nixio.getnameinfo(fields[k], "inet")
-							if hostname then
-								data[name][di]["Hostname"] = hostname
-							end
-						end
-						if key == "Remote IP" and defaultgw then
-							if defaultgw == fields[k] then
-								data[name][di]["defaultgw"] = 1
-							end
-						end
-					elseif key == "Local IP" then
-						data[name][di][key] = fields[k]
-						data[name][di]['Local Device'] = fields[k]
-						uci:foreach("network", "interface",
-							function(s)
-								localip = string.gsub(fields[k], '	', '')
-								if s.ipaddr == localip then
-									data[name][di]['Local Device'] = s['.name'] or interface
-								end
-							end)
-					elseif key == "Interface" then
-						data[name][di][key] = fields[k]
-						uci:foreach("network", "interface",
-						function(s)
-							interface = string.gsub(fields[k], '	', '')
-							if s.ifname == interface then
-								data[name][di][key] = s['.name'] or interface
-							end
-						end)
-					else
-					    data[name][di][key] = fields[k]
-			        end
-				end
-				if data[name][di].Linkcost then
-					data[name][di].LinkQuality,
-					data[name][di].NLQ,
-					data[name][di].ETX =
-					data[name][di].Linkcost:match("([%w.]+)/([%w.]+)[%s]+([%w.]+)")
-				end
-			end
-		end
+	if jsonreq4 == '' and jsonreq6 == '' then
+		luci.template.render("status-olsr/error_olsr")
+		return nil, 0, 0, true
 	end
 
-	if #rawdatav6 ~= 0 then
-		local tables = luci.util.split(luci.util.trim(rawdatav6), "\r?\n\r?\n", nil, true)
-		for i, tbl in ipairs(tables) do
-			local lines = luci.util.split(tbl, "\r?\n", nil, true)
-			name  = table.remove(lines, 1):sub(8)
-			local keys  = luci.util.split(table.remove(lines, 1), "\t")
-			local split = #keys - 1
-			if not data[name] then
-				data[name] = {}
-			end
-			for j, line in ipairs(lines) do
-				dataindex = ( dataindex + 1 )
-				di = dataindex
-				local fields = luci.util.split(line, "\t", split)
-				data[name][di] = {}
-				for k, key in pairs(keys) do
-					if key == "Remote IP" then
-						data[name][di][key] = "[" .. fields[k] .. "]"
-						if resolve == "1" then
-							hostname = nixio.getnameinfo(fields[k], "inet6")
-							if hostname then
-								data[name][di]["Hostname"] = hostname
-							end
-						end
-					elseif key == "Local IP" then
-						data[name][di][key] = fields[k]
-						data[name][di]['Local Device'] = fields[k]
-						uci:foreach("network", "interface",
-						function(s)
-							local localip = string.gsub(fields[k], '	', ''):upper()
-							localip = luci.ip.IPv6(localip):string()
-							if s.ip6addr then
-								s.ip6addr = luci.ip.IPv6(s.ip6addr):string()
-								local ip6addr = string.gsub(s.ip6addr, '\/.*', ''):upper()
-								if ip6addr == localip then
-									data[name][di]['Local Device'] = s['.name'] or s.interface
-								end
-							end
-						end)
-					elseif key == "Dest. IP" then
-						data[name][di][key] = "[" .. fields[k] .. "]"
-					elseif key == "Last hop IP" then
-						data[name][di][key] = "[" .. fields[k] .. "]"
-					elseif key == "IP address" then
-						data[name][di][key] = "[" .. fields[k] .. "]"
-					elseif key == "Gateway" then
-						data[name][di][key] = "[" .. fields[k] .. "]"
-					else
-						data[name][di][key] = fields[k]
-					end
-				end
-
-				if data[name][di].Linkcost then
-					data[name][di].LinkQuality,
-					data[name][di].NLQ,
-					data[name][di].ETX =
-					data[name][di].Linkcost:match("([%w.]+)/([%w.]+)[%s]+([%w.]+)")
-				end
-			end
+        if #jsonreq4 ~= 0 then
+		has_v4 = 1
+                jsondata4 = json.decode(jsonreq4)
+		if otable == 'status' then
+			data4 = jsondata4
+		else
+			data4 = jsondata4[otable]
 		end
+
+                for k, v in ipairs(data4) do
+                    data4[k]['proto'] = '4'
+		end
+        end
+        if #jsonreq6 ~= 0 then
+		has_v6 = 1
+                jsondata6 = json.decode(jsonreq6)
+		if otable == 'status' then
+	                data6 = jsondata6
+		else
+	                data6 = jsondata6[otable]
+		end
+                for k, v in ipairs(data6) do
+                    data6[k]['proto'] = '6'
+                end
+        end
+
+	for k, v in ipairs(data6) do
+		table.insert(data4, v)
 	end
 
-
-	if data then
-	    return data
-	end
+        return data4, has_v4, has_v6, false
 end
+
