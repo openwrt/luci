28abb7b85be42c975ae7eeb5bcc5ca09e4b5906e
Manuel Munz <freifunk@somakoma.de>
2013-03-28 23:25:02 +0000

    contrib/freifunk-common: Check for ipv6 availability in neigh.sh

--- contrib/package/freifunk-common/files/usr/bin/neigh.sh
+++ contrib/package/freifunk-common/files/usr/bin/neigh.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 wget -q -O - http://localhost:2006/neighbours|sed -e's/LinkQuality/LQ/;s/Hysteresis/Hyst./;s/Willingness/Will./'
-wget -q -O - http://[::1]:2006/neighbours|sed -e's/LinkQuality/LQ/;s/Hysteresis/Hyst./;s/Willingness/Will./'
+[ -f /proc/net/ipv6_route ] && wget -q -O - http://[::1]:2006/neighbours|sed -e's/LinkQuality/LQ/;s/Hysteresis/Hyst./;s/Willingness/Will./'
