a4c083ecef6256e56a5f2809a3cebf908787afcb
Manuel Munz <freifunk@somakoma.de>
2013-09-16 19:51:25 +0000

    applications/luci-ddns: Add more datatype checks and defaults

--- applications/luci-ddns/luasrc/model/cbi/ddns/ddns.lua
+++ applications/luci-ddns/luasrc/model/cbi/ddns/ddns.lua
@@ -3,6 +3,7 @@ LuCI - Lua Configuration Interface
 
 Copyright 2008 Steven Barth <steven@midlink.org>
 Copyright 2008 Jo-Philipp Wich <xm@leipzig.freifunk.net>
+Copyright 2013 Manuel Munz <freifunk at somakoma dot de>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -10,13 +11,9 @@ You may obtain a copy of the License at
 
 	http://www.apache.org/licenses/LICENSE-2.0
 
-$Id$
 ]]--
 
 require("luci.tools.webadmin")
-
-local is_mini = (luci.dispatcher.context.path[1] == "mini")
-
 
 m = Map("ddns", translate("Dynamic DNS"),
 	translate("Dynamic DNS allows that your router can be reached with " ..
@@ -29,12 +26,9 @@ s.anonymous = false
 
 s:option(Flag, "enabled", translate("Enable"))
 
-interface = s:option(ListValue, "interface", translate("Event interface"), translate("On which interface up should start the ddns script process."))
-luci.tools.webadmin.cbi_add_networks(interface)
-interface.default = "wan"
-
 svc = s:option(ListValue, "service_name", translate("Service"))
 svc.rmempty = false
+svc.default = "dyndns.org"
 
 local services = { }
 local fd = io.open("/usr/lib/ddns/services", "r")
@@ -72,60 +66,63 @@ end
 
 svc:value("-", "-- "..translate("custom").." --")
 
-url = s:option(Value, "update_url", translate("Custom update-URL"))
+local url = s:option(Value, "update_url", translate("Custom update-URL"))
 url:depends("service_name", "-")
 url.rmempty = true
 
-s:option(Value, "domain", translate("Hostname")).rmempty = true
-s:option(Value, "username", translate("Username")).rmempty = true
-pw = s:option(Value, "password", translate("Password"))
+local hostname = s:option(Value, "domain", translate("Hostname"))
+hostname.rmempty = true
+hostname.default = "mypersonaldomain.dyndns.org"
+hostname.datatype = "host"
+
+local username = s:option(Value, "username", translate("Username"))
+username.rmempty = true
+
+local pw = s:option(Value, "password", translate("Password"))
 pw.rmempty = true
 pw.password = true
 
+require("luci.tools.webadmin")
 
-if is_mini then
-	s.defaults.ip_source = "network"
-	s.defaults.ip_network = "wan"
-else
+local src = s:option(ListValue, "ip_source",
+	translate("Source of IP address"))
+src.default = "network"
+src:value("network", translate("network"))
+src:value("interface", translate("interface"))
+src:value("web", translate("URL"))
 
-	src = s:option(ListValue, "ip_source",
-		translate("Source of IP address"))
-	src:value("network", translate("network"))
-	src:value("interface", translate("interface"))
-	src:value("web", translate("URL"))
-
-	iface = s:option(ListValue, "ip_network", translate("Network"))
-	iface:depends("ip_source", "network")
-	iface.rmempty = true
-	luci.tools.webadmin.cbi_add_networks(iface)
-
-	iface = s:option(ListValue, "ip_interface", translate("Interface"))
-	iface:depends("ip_source", "interface")
-	iface.rmempty = true
-	for k, v in pairs(luci.sys.net.devices()) do
-		iface:value(v)
-	end
-
-	web = s:option(Value, "ip_url", translate("URL"))
-	web:depends("ip_source", "web")
-	web.rmempty = true
+local iface = s:option(ListValue, "ip_network", translate("Network"))
+iface:depends("ip_source", "network")
+iface.rmempty = true
+iface.default = "wan"
+luci.tools.webadmin.cbi_add_networks(iface)
+iface = s:option(ListValue, "ip_interface", translate("Interface"))
+iface:depends("ip_source", "interface")
+iface.rmempty = true
+for k, v in pairs(luci.sys.net.devices()) do
+	iface:value(v)
 end
 
+local web = s:option(Value, "ip_url", translate("URL"))
+web:depends("ip_source", "web")
+web.default = "http://checkip.dyndns.com/"
+web.rmempty = true
 
-ci = s:option(Value, "check_interval", translate("Check for changed IP every"))
-ci.datatype = "and(uinteger,min(1))"
-ci.default = 10
 
-unit = s:option(ListValue, "check_unit", translate("Check-time unit"))
+local ci = s:option(Value, "check_interval", translate("Check for changed IP every")) 
+ci.datatype = "and(uinteger,min(1))" 
+ci.default = 10 
+
+local unit = s:option(ListValue, "check_unit", translate("Check-time unit"))
 unit.default = "minutes"
 unit:value("minutes", translate("min"))
 unit:value("hours", translate("h"))
 
-fi = s:option(Value, "force_interval", translate("Force update every"))
-fi.datatype = "and(uinteger,min(1))"
-fi.default = 72
+fi = s:option(Value, "force_interval", translate("Force update every")) 
+fi.datatype = "and(uinteger,min(1))" 
+fi.default = 72 
 
-unit = s:option(ListValue, "force_unit", translate("Force-time unit"))
+local unit = s:option(ListValue, "force_unit", translate("Force-time unit"))
 unit.default = "hours"
 unit:value("minutes", translate("min"))
 unit:value("hours", translate("h"))
