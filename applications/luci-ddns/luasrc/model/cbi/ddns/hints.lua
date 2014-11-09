ee9fdba61744111d8f5d7f7d616992ac7135ac09
Christian Schoenebeck <christian.schoenebeck@gmail.com>
2014-11-09 18:37:15 +0100

    luci-app-ddns: Update to support ddns-scripts 2.1.0-1
    fix verify of entry for DNS server Issue #244
    	https://github.com/openwrt/luci/issues/244
    add support for option 'update_script'
    add display of version information when click on "Dynamic DNS" on
    overview page
    add verify of installed ddns-scripts version and show as hint if not
    correct version
    modified epoch to date conversation
    cbi object Flag did not set section.changed state, fixed in
    tools.flag_parse function
    ucitrack entry no longer needed and removed
    minor fixes
    
    Signed-off-by: Christian Schoenebeck <christian.schoenebeck@gmail.com>

--- applications/luci-ddns/luasrc/model/cbi/ddns/hints.lua
+++ applications/luci-ddns/luasrc/model/cbi/ddns/hints.lua
@@ -12,56 +12,74 @@ You may obtain a copy of the License at
 $Id$
 ]]--
 
-require "luci.sys"
-require "luci.dispatcher"
-require "luci.tools.ddns"
+local CTRL = require "luci.controller.ddns"	-- this application's controller
+local DISP = require "luci.dispatcher"
+local SYS  = require "luci.sys"
+local DDNS = require "luci.tools.ddns"		-- ddns multiused functions
 
--- check supported options
+-- check supported options -- ##################################################
 -- saved to local vars here because doing multiple os calls slow down the system
-has_ssl    = luci.tools.ddns.check_ssl()	-- HTTPS support
-has_proxy  = luci.tools.ddns.check_proxy()	-- Proxy support
-has_dnstcp = luci.tools.ddns.check_bind_host()	-- DNS TCP support
+has_ssl     = DDNS.check_ssl()		-- HTTPS support
+has_proxy   = DDNS.check_proxy()	-- Proxy support
+has_dnstcp  = DDNS.check_bind_host()	-- DNS TCP support
+need_update = CTRL.update_needed()	-- correct ddns-scripts version
 
 -- html constants
+font_red = [[<font color="red">]]
+font_off = [[</font>]]
 bold_on  = [[<strong>]]
 bold_off = [[</strong>]]
 
--- cbi-map definition
+-- cbi-map definition -- #######################################################
 m = Map("ddns")
 
-m.title = [[<a href="]] .. luci.dispatcher.build_url("admin", "services", "ddns") .. [[">]] .. 
-		translate("Dynamic DNS") .. [[</a>]]
+-- first need to close <a> from cbi map template our <a> closed by template
+m.title = [[</a><a href="]] .. DISP.build_url("admin", "services", "ddns") .. [[">]] ..
+		translate("Dynamic DNS")
 
 m.description = translate("Dynamic DNS allows that your router can be reached with " ..
 			"a fixed hostname while having a dynamically changing " ..
 			"IP address.")
 
-m.redirect = luci.dispatcher.build_url("admin", "services", "ddns")
+m.redirect = DISP.build_url("admin", "services", "ddns")
 
--- SimpleSection definition
+-- SimpleSection definition -- #################################################
 -- show Hints to optimize installation and script usage
-s = m:section( SimpleSection, 
-	translate("Hints"), 
+s = m:section( SimpleSection,
+	translate("Hints"),
 	translate("Below a list of configuration tips for your system to run Dynamic DNS updates without limitations") )
--- DDNS Service disabled
-if not luci.sys.init.enabled("ddns") then
-	local dv = s:option(DummyValue, "_not_enabled")
-	dv.titleref = luci.dispatcher.build_url("admin", "system", "startup")
+
+-- ddns_scripts needs to be updated for full functionality
+if need_update then
+	local dv = s:option(DummyValue, "_update_needed")
+	dv.titleref = DISP.build_url("admin", "system", "packages")
 	dv.rawhtml  = true
-	dv.title = bold_on .. 
+	dv.title = font_red .. bold_on ..
+		translate("Software update required") .. bold_off .. font_off
+	dv.value = translate("The currently installed 'ddns-scripts' package did not support all available settings.") ..
+			"<br />" ..
+			translate("Please update to the current version!")
+end
+
+-- DDNS Service disabled
+if not SYS.init.enabled("ddns") then
+	local dv = s:option(DummyValue, "_not_enabled")
+	dv.titleref = DISP.build_url("admin", "system", "startup")
+	dv.rawhtml  = true
+	dv.title = bold_on ..
 		translate("DDNS Autostart disabled") .. bold_off
-	dv.value = translate("Currently DDNS updates are not started at boot or on interface events." .. "<br />" .. 
+	dv.value = translate("Currently DDNS updates are not started at boot or on interface events." .. "<br />" ..
 			"This is the default if you run DDNS scripts by yourself (i.e. via cron with force_interval set to '0')" )
 end
 
 -- No IPv6 support
-if not luci.tools.ddns.check_ipv6() then
+if not DDNS.check_ipv6() then
 	local dv = s:option(DummyValue, "_no_ipv6")
 	dv.titleref = 'http://www.openwrt.org" target="_blank'
 	dv.rawhtml  = true
-	dv.title = bold_on .. 
+	dv.title = bold_on ..
 		translate("IPv6 not supported") .. bold_off
-	dv.value = translate("IPv6 is currently not (fully) supported by this system" .. "<br />" .. 
+	dv.value = translate("IPv6 is currently not (fully) supported by this system" .. "<br />" ..
 			"Please follow the instructions on OpenWrt's homepage to enable IPv6 support" .. "<br />" ..
 			"or update your system to the latest OpenWrt Release")
 end
@@ -69,13 +87,13 @@ end
 -- No HTTPS support
 if not has_ssl then
 	local dv = s:option(DummyValue, "_no_https")
-	dv.titleref = luci.dispatcher.build_url("admin", "system", "packages")
+	dv.titleref = DISP.build_url("admin", "system", "packages")
 	dv.rawhtml  = true
-	dv.title = bold_on .. 
+	dv.title = bold_on ..
 		translate("HTTPS not supported") .. bold_off
-	dv.value = translate("Neither GNU Wget with SSL nor cURL installed to support updates via HTTPS protocol.") .. 
-			"<br />- " .. 
-			translate("You should install GNU Wget with SSL (prefered) or cURL package.") .. 
+	dv.value = translate("Neither GNU Wget with SSL nor cURL installed to support updates via HTTPS protocol.") ..
+			"<br />- " ..
+			translate("You should install GNU Wget with SSL (prefered) or cURL package.") ..
 			"<br />- " ..
 			translate("In some versions cURL/libcurl in OpenWrt is compiled without proxy support.")
 end
@@ -83,13 +101,13 @@ end
 -- cURL without proxy support
 if has_ssl and not has_proxy then
 	local dv = s:option(DummyValue, "_no_proxy")
-	dv.titleref = luci.dispatcher.build_url("admin", "system", "packages")
+	dv.titleref = DISP.build_url("admin", "system", "packages")
 	dv.rawhtml  = true
-	dv.title = bold_on .. 
+	dv.title = bold_on ..
 		translate("cURL without Proxy Support") .. bold_off
-	dv.value = translate("cURL is installed, but libcurl was compiled without proxy support.") .. 
-			"<br />- " .. 
-			translate("You should install GNU Wget with SSL or replace libcurl.") .. 
+	dv.value = translate("cURL is installed, but libcurl was compiled without proxy support.") ..
+			"<br />- " ..
+			translate("You should install GNU Wget with SSL or replace libcurl.") ..
 			"<br />- " ..
 			translate("In some versions cURL/libcurl in OpenWrt is compiled without proxy support.")
 end
@@ -97,12 +115,12 @@ end
 -- "Force IP Version not supported"
 if not (has_ssl and has_dnstcp) then
 	local dv = s:option(DummyValue, "_no_force_ip")
-	dv.titleref = luci.dispatcher.build_url("admin", "system", "packages")
+	dv.titleref = DISP.build_url("admin", "system", "packages")
 	dv.rawhtml  = true
-	dv.title = bold_on .. 
+	dv.title = bold_on ..
 		translate("Force IP Version not supported") .. bold_off
 	local value = translate("BusyBox's nslookup and Wget do not support to specify " ..
-			"the IP version to use for communication with DDNS Provider.") 
+			"the IP version to use for communication with DDNS Provider.")
 	if not has_ssl then
 		value = value .. "<br />- " ..
 			translate("You should install GNU Wget with SSL (prefered) or cURL package.")
@@ -117,11 +135,11 @@ end
 -- "DNS requests via TCP not supported"
 if not has_dnstcp then
 	local dv = s:option(DummyValue, "_no_dnstcp")
-	dv.titleref = luci.dispatcher.build_url("admin", "system", "packages")
+	dv.titleref = DISP.build_url("admin", "system", "packages")
 	dv.rawhtml  = true
-	dv.title = bold_on .. 
+	dv.title = bold_on ..
 		translate("DNS requests via TCP not supported") .. bold_off
-	dv.value = translate("BusyBox's nslookup does not support to specify to use TCP instead of default UDP when requesting DNS server") .. 
+	dv.value = translate("BusyBox's nslookup does not support to specify to use TCP instead of default UDP when requesting DNS server") ..
 			"<br />- " ..
 			translate("You should install BIND host package for DNS requests.")
 end
