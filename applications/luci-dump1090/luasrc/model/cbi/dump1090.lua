c5e1037b4068a73e48e2ab5b279f5b25f44a0096
Álvaro Fernández Rojas <noltari@gmail.com>
2014-11-10 13:31:17 +0100

    applications/luci-dump1090: update config to latest version
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- applications/luci-dump1090/luasrc/model/cbi/dump1090.lua
+++ applications/luci-dump1090/luasrc/model/cbi/dump1090.lua
@@ -58,6 +58,10 @@ net_beast.default = false
 net_only=s:option(Flag, "net_only", translate("Enable just networking, no RTL device or file used"))
 net_only.default = false
 
+net_bind_address=s:option(Value, "net_bind_address", translate("IP address to bind to"))
+net_bind_address.rmempty = true
+net_bind_address.datatype = "ipaddr"
+
 net_http_port=s:option(Value, "net_http_port", translate("HTTP server port"))
 net_http_port.rmempty = true
 net_http_port.datatype = "port"
