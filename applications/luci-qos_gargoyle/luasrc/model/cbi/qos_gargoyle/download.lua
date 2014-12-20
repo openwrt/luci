c445917f88ddc982c8d0d0750c10a9edd534ee32
郭传鈜 <gch981213@gmail.com>
2014-12-20 20:27:51 +0800

    添加石像鬼QoS Luci界面

---
+++ applications/luci-qos_gargoyle/luasrc/model/cbi/qos_gargoyle/download.lua
@@ -0,0 +1,113 @@
+--[[
+
+]]--
+
+local wa = require "luci.tools.webadmin"
+local fs = require "nixio.fs"
+
+m = Map("qos_gargoyle", translate("Download"),translate("Download set"))
+
+s = m:section(TypedSection, "download_class", translate("download_class"))
+s.addremove = true
+s.template = "cbi/tblsection"
+
+
+name = s:option(Value, "name", translate("name"))
+
+pb = s:option(Value, "percent_bandwidth", translate("percent_bandwidth"), translate("percent of total bandwidth to use"))
+
+minb = s:option(Value, "min_bandwidth", translate("min_bandwidth"), translate("min bandwidth useage in absolute speed (kbit/s)"))
+minb.datatype = "and(uinteger,min(0))"
+
+maxb = s:option(Value, "max_bandwidth", translate("max_bandwidth"), translate("max bandwidth useage in absolute speed (kbit/s)"))
+maxb.datatype = "and(uinteger,min(0))"
+
+minRTT = s:option(ListValue, "minRTT", translate("minRTT"))
+minRTT:value("Yes")
+minRTT:value("No")
+minRTT.default = "No"
+
+s = m:section(TypedSection, "download_rule", translate("download_rule"))
+s.addremove = true
+--s.sortable = true
+--s.anonymous = true
+s.template = "cbi/tblsection"
+
+class = s:option(Value, "class", translate("class"), translate("<abbr title=\"name of bandwidth class to use if rule matches, this is required in each rule section\">Help</abbr>"))
+class.default = "dclass_2"
+for l in io.lines("/etc/config/qos_gargoyle") do
+	local s = l
+	l = string.gsub(l, "config ['\"]*download_class['\"]* ", "")
+	if s ~= l then
+		l = string.gsub(l, "^'", "")
+		l = string.gsub(l, "^\"", "")
+		l = string.gsub(l, "'$", "")
+		l = string.gsub(l, "\"$", "")
+		class:value(l, translate(m.uci:get("qos_gargoyle", l, "name")))
+	end
+end
+
+to = s:option(Value, "test_order", translate("test_order"), translate("<abbr title=\"an integer that specifies the order in which the rule should be checked for a match (lower numbers are checked first)\">Help</abbr>"))
+to.rmempty = "true"
+to:value(100)
+to:value(200)
+to:value(300)
+to:value(400)
+to:value(500)
+to:value(600)
+to:value(700)
+to:value(800)
+to:value(900)
+
+pr = s:option(Value, "proto", translate("proto"), translate("<abbr title=\"check that packet has this protocol (tcp, udp, both)\">Help</abbr>"))
+pr:value("tcp")
+pr:value("udp")
+pr:value("icmp")
+pr:value("gre")
+pr.rmempty = "true"
+
+sip = s:option(Value, "source", translate("source ip"), translate("<abbr title=\"check that packet has this source ip, can optionally have /[mask] after it (see -s option in iptables man page)\">Help</abbr>"))
+wa.cbi_add_knownips(sip)
+sip.datatype = "and(ipaddr)"
+
+dip = s:option(Value, "destination", translate("destination ip"), translate("<abbr title=\"check that packet has this destination ip, can optionally have /[mask] after it (see -d option in iptables man page)\">Help</abbr>"))
+wa.cbi_add_knownips(dip)
+dip.datatype = "and(ipaddr)"
+
+dport = s:option(Value, "dstport", translate("destination port"), translate("<abbr title=\"check that packet has this destination port\">Help</abbr>"))
+dport.datatype = "and(uinteger,max(65536),min(1))"
+
+sport = s:option(Value, "srcport", translate("source port"), translate("<abbr title=\"check that packet has this source port\">Help</abbr>"))
+sport.datatype = "and(uinteger,max(65536),min(1))"
+
+min_pkt_size = s:option(Value, "min_pkt_size", translate("min_pkt_size"), translate("<abbr title=\"check that packet is at least this size (in bytes)\">Help</abbr>"))
+min_pkt_size.datatype = "and(uinteger,min(1))"
+
+max_pkt_size = s:option(Value, "max_pkt_size", translate("max_pkt_size"), translate("<abbr title=\"check that packet is no larger than this size (in bytes)\">Help</abbr>"))
+max_pkt_size.datatype = "and(uinteger,min(1))"
+
+connbytes_kb = s:option(Value, "connbytes_kb", translate("connbytes_kbyte"), translate("<abbr title=\"kbyte\">Help</abbr>"))
+connbytes_kb.datatype = "and(uinteger,min(0))"
+
+layer7 = s:option(Value, "layer7", translate("layer7"), translate("<abbr title=\"check whether packet matches layer7 specification\">Help</abbr>"))
+local pats = io.popen("find /etc/l7-protocols/ -type f -name '*.pat'")
+if pats then
+	local l
+	while true do
+		l = pats:read("*l")
+		if not l then break end
+
+		l = l:match("([^/]+)%.pat$")
+		if l then
+			layer7:value(l)
+		end
+	end
+	pats:close()
+end
+
+ipp2p = s:option(Value, "ipp2p", translate("ipp2p"), translate("<abbr title=\"check wither packet matches ipp2p specification (used to recognize p2p protocols),ipp2p or all will match any of the specified p2p protocols, you can also specifically match any protocol listed in the documentation here: http://ipp2p.org/docu_en.html\">Help</abbr>"))
+ipp2p:value("ipp2p")
+ipp2p:value("all")
+
+return m
+
