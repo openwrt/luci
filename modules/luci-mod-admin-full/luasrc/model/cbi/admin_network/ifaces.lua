9132d83461c7f64f11b8dabdf8b71646c5369ef8
Christian Schoenebeck <christian.schoenebeck@gmail.com>
2015-02-08 19:17:09 +0100

    admin_network: interface details, implement back to overview
    Implements "Back to Overview" on interface details page
    
    Signed-off-by: Christian Schoenebeck <christian.schoenebeck@gmail.com>

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/ifaces.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/ifaces.lua
@@ -14,6 +14,7 @@ local has_dnsmasq  = fs.access("/etc/config/dhcp")
 local has_firewall = fs.access("/etc/config/firewall")
 
 m = Map("network", translate("Interfaces") .. " - " .. arg[1]:upper(), translate("On this page you can configure the network interfaces. You can bridge several interfaces by ticking the \"bridge interfaces\" field and enter the names of several network interfaces separated by spaces. You can also use <abbr title=\"Virtual Local Area Network\">VLAN</abbr> notation <samp>INTERFACE.VLANNR</samp> (<abbr title=\"for example\">e.g.</abbr>: <samp>eth0.1</samp>)."))
+m.redirect = luci.dispatcher.build_url("admin", "network", "network")
 m:chain("wireless")
 
 if has_firewall then
