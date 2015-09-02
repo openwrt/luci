6c72dec7e520bf9bef18b5db8656532278bcac26
Jo-Philipp Wich <jow@openwrt.org>
2015-09-02 09:38:28 +0200

    build/makedocs.sh: handle relative output directories
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>

--- build/makedocs.sh
+++ build/makedocs.sh
@@ -7,8 +7,15 @@ topdir=$(pwd)
 	exit 1
 }
 
+outdir=$(readlink -f "$1")
+
+mkdir -p "$outdir" || {
+	echo "Unable to mkdir '$outdir'" >&2
+	exit 1
+}
+
 (
 	cd "$topdir/build/luadoc/"
 	find "$topdir/libs/" "$topdir/modules/" -type f -name '*.lua' -or -name '*.luadoc' | \
-		xargs grep -l '@return' | xargs ./doc.lua --no-files -d "$1"
+		xargs grep -l '@return' | xargs ./doc.lua --no-files -d "$outdir"
 )
