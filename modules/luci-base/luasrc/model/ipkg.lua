c8b12e7fa925148a765f589df1af41797727db46
Hannu Nyman <hannu.nyman@iki.fi>
2015-10-24 09:40:55 +0300

    Backport luci-base: filter invalid opkg status lines
    Backport of commit 6b3f804 from trunk.
    
    Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

--- modules/luci-base/luasrc/model/ipkg.lua
+++ modules/luci-base/luasrc/model/ipkg.lua
@@ -139,11 +139,13 @@ function _list(action, pat, cb)
 				desc = ""
 			end
 
-			if #version > 26 then
-				version = version:sub(1,21) .. ".." .. version:sub(-3,-1)
-			end
+			if name and version then
+				if #version > 26 then
+					version = version:sub(1,21) .. ".." .. version:sub(-3,-1)
+				end
 
-			cb(name, version, sz, desc)
+				cb(name, version, sz, desc)
+			end
 
 			name    = nil
 			version = nil
