3622b45b65dbe97003c9a83c675a9f290e91c201
Chuanhong Guo <gch981213@gmail.com>
2016-02-18 12:23:40 +0800

    跟进hackpascal的luci-app-vsftpd

--- applications/luci-app-vsftpd/luasrc/model/cbi/vsftpd/item.lua
+++ applications/luci-app-vsftpd/luasrc/model/cbi/vsftpd/item.lua
@@ -1,8 +1,7 @@
 --[[
 LuCI - Lua Configuration Interface
 
-Copyright 2008 Steven Barth <steven@midlink.org>
-Copyright 2014 HackPascal <hackpascal@gmail.com>
+Copyright 2016 Weijie Gao <hackpascal@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
