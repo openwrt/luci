3749c45f80a8613b7d0d1f7fc4f82531de5db8ce
Hannu Nyman <hannu.nyman@iki.fi>
2016-02-19 12:13:44 +0200

    luci-app-adblock: Beautify blocklist selection table and reorganise
    Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

--- applications/luci-app-adblock/luasrc/model/cbi/adblock.lua
+++ applications/luci-app-adblock/luasrc/model/cbi/adblock.lua
@@ -8,7 +8,7 @@ m = Map("adblock", translate("Adblock"),
 
 s = m:section(NamedSection, "global", "adblock", translate("Global options"))
 
-o1 = s:option(Flag, "adb_enabled", translate("Enable"))
+o1 = s:option(Flag, "adb_enabled", translate("Enable adblock"))
 o1.rmempty = false
 o1.default = 0
 
@@ -22,6 +22,21 @@ o3 = s:option(Value, "adb_whitelist", translate("Whitelist file"),
 o3.rmempty = false
 o3.datatype = "file"
 
+-- Blocklist options
+
+bl = m:section(TypedSection, "source", translate("Blocklist sources"),
+	translate("Available blocklist sources (")
+	.. [[<a href="https://github.com/openwrt/packages/blob/master/net/adblock/files/README.md#main-features" target="_blank">]]
+	.. translate("see list details")
+	.. [[</a>]]
+	.. translate("). Note that list URLs and Shallalist category selections are not configurable via Luci."))
+bl.template = "cbi/tblsection"
+
+name = bl:option(Flag, "enabled", translate("Enabled"))
+name.rmempty  = false
+
+-- Additional options
+
 s2 = m:section(NamedSection, "backup", "service", translate("Backup options"))
 
 o4 = s2:option(Flag, "enabled", translate("Enable blocklist backup"))
@@ -32,8 +47,7 @@ o5 = s2:option(Value, "adb_backupdir", translate("Backup directory"))
 o5.rmempty = false
 o5.datatype = "directory"
 
-s3 = m:section(NamedSection, "debuglog", "service", translate("Debug log options"),
-	translate("Verbose log for debuging purposes."))
+s3 = m:section(NamedSection, "debuglog", "service", translate("Debug log options"))
 
 o6 = s3:option(Flag, "enabled", translate("Enable debug log"))
 o6.rmempty = false
@@ -42,19 +56,6 @@ o6.default = 0
 o7 = s3:option(Value, "adb_logfile", translate("Debug log file"))
 o7.rmempty = false
 o7.datatype = "string"
-
--- Blocklist options
-
-s3 = m:section(TypedSection, "source", translate("Blocklist sources"),
-	translate("Available blocklist sources (")
-	.. [[<a href="https://github.com/openwrt/packages/blob/master/net/adblock/files/README.md#main-features" target="_blank">]]
-	.. translate("see list details")
-	.. [[</a>]]
-	.. translate("). Note that list URLs and Shallalist category selections are not configurable via Luci."))
-
-name = s3:option(Flag, "enabled", translate("Enabled"))
-name.rmempty  = false
-
 
 return m
 
