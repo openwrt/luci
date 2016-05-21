a64448b16bcb741bf872409b3d051035a28dd3f8
Chuanhong Guo <gch981213@gmail.com>
2016-05-21 11:14:31 +0300

    luci-base: Turn off pattern matching when generating distversion
    Otherwize the distrevision may be displayed twice
    if it contains some special matching patterns.
    (For example I am using "git-$hash" in my own OpenWrt branch.)
    Signed-off-by: Chuanhong Guo <gch981213@gmail.com>
    
    (cherry picked from commit c1f0a9971a0b75515996cd59dfaf70f3ff6cc624)

--- modules/luci-base/src/mkversion.sh
+++ modules/luci-base/src/mkversion.sh
@@ -10,7 +10,7 @@ if pcall(dofile, "/etc/openwrt_release") and _G.DISTRIB_DESCRIPTION then
 	distversion = _G.DISTRIB_DESCRIPTION
 	if _G.DISTRIB_REVISION then
 		distrevision = _G.DISTRIB_REVISION
-		if not distversion:find(distrevision) then
+		if not distversion:find(distrevision,1,true) then
 			distversion = distversion .. " " .. distrevision
 		end
 	end
