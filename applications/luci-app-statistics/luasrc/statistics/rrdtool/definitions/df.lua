a21bcb51137f787eaa12bdb63a1082d26d4a790f
Artur Stepniewski <artur.stepniewski@gmail.com>
2016-08-10 22:09:32 +0200

    Fix missing title in df graphs
    It used data source instance name instead of plugin instance name.
    
    Signed-off-by: Artur Stepniewski <artur.stepniewski+openwrt@gmail.com>

--- applications/luci-app-statistics/luasrc/statistics/rrdtool/definitions/df.lua
+++ applications/luci-app-statistics/luasrc/statistics/rrdtool/definitions/df.lua
@@ -6,7 +6,7 @@ module("luci.statistics.rrdtool.definitions.df", package.seeall)
 function rrdargs( graph, plugin, plugin_instance, dtype )
 
 	return {
-		title = "%H: Disk space usage on %di",
+		title = "%H: Disk space usage on %pi",
 		vlabel = "Bytes",
 		per_instance  = true,
 		number_format = "%5.1lf%sB",
