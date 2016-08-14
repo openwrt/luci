d5e5508aeff879de18fb2099c09c12433f96480e
Daniel Dickinson <lede@cshore.thecshore.com>
2016-08-13 20:37:37 -0400

    luci-app-statistics: Add support for encrypted networking
    Add configuration for encrypted networking, when enabled.
    
    Signed-off-by: Daniel Dickinson <lede@cshore.thecshore.com>

--- applications/luci-app-statistics/luasrc/model/cbi/luci_statistics/network.lua
+++ applications/luci-app-statistics/luasrc/model/cbi/luci_statistics/network.lua
@@ -1,6 +1,9 @@
 -- Copyright 2008 Freifunk Leipzig / Jo-Philipp Wich <jow@openwrt.org>
 -- Licensed to the public under the Apache License 2.0.
 
+require "luci.util"
+local fs = require "nixio.fs"
+
 m = Map("luci_statistics",
 	translate("Network Plugin Configuration"),
 	translate(
@@ -18,6 +21,8 @@ s = m:section( NamedSection, "collectd_network", "luci_statistics" )
 enable = s:option( Flag, "enable", translate("Enable this plugin") )
 enable.default = 0
 
+local have_encrypted_network_support = luci.util.checklib("/usr/lib/collectd/network.so", "libgcrypt.so")
+local f
 
 -- collectd_network_listen config section (Listen)
 listen = m:section( TypedSection, "collectd_network_listen",
@@ -39,6 +44,41 @@ listen_port.default   = 25826
 listen_port.isinteger = true
 listen_port.optional  = true
 
+if have_encrypted_network_support then
+	securitylevel = listen:option(ListValue, "security_level", translate("Security Level"))
+	securitylevel:value("None", translate("No encryption"))
+	securitylevel:value("Encrypt", translate("Encrypted data only"))
+	securitylevel:value("Sign", translate("Signed and encrypted data only"))
+	securitylevel.default = "None"
+	authfile = listen:option(Value, "auth_file", translate("Auth file"), translate("File containing username:password pairs to allow"))
+	authfile:depends("security_level", "Encrypt")
+	authfile:depends("security_level", "Sign")
+	authfile.placeholder = "/etc/collectd/authfile"
+
+	t = listen:option(TextValue, "auths", translate("Auth File"), translate("username:password pairs allowed by this listener"))
+	t:depends("security_level", "Encrypt")
+	t:depends("security_level", "Sign")
+	t.rmempty = true
+	t.rows = 10
+	function t.cfgvalue(self, section)
+		local afile = authfile:cfgvalue(section) or "/etc/collectd/authfile"
+		return fs.readfile(afile) or ""
+	end
+
+	function t.write(self, section, value)
+		local afile = authfile:cfgvalue(section) or "/etc/collectd/authfile"
+		if value then
+                        fs.writefile(afile, value:gsub("\r\n", "\n"))
+		else
+			fs.writefile(afile, "")
+		end
+	end
+
+	function t.remove(self, section)
+		local afile = authfile:cfgvalue(section) or "/etc/collectd/authfile"
+		fs.writefile(afile, "")
+	end
+end
 
 -- collectd_network_server config section (Server)
 server = m:section( TypedSection, "collectd_network_server",
@@ -59,6 +99,23 @@ server_port = server:option( Value, "port", translate("Server port") )
 server_port.default   = 25826
 server_port.isinteger = true
 server_port.optional  = true
+
+if have_encrypted_network_support then
+	securitylevel = server:option(ListValue, "security_level", translate("Security Level"))
+	securitylevel:value("None", translate("No encryption"))
+	securitylevel:value("Encrypt", translate("Encrypted data only"))
+	securitylevel:value("Sign", translate("Signed and encrypted data only"))
+	username = server:option(Value, "username", translate("Username"))
+	username:depends("security_level", "Encrypt")
+	username:depends("security_level", "Sign")
+	password = server:option(Value, "password", translate("Password"))
+	password:depends("security_level", "Encrypt")
+	password:depends("security_level", "Sign")
+	password.password = true
+	resolveinterval = server:option(Value, "resolve_interval", translate("Resolve Interval"), translate("Interval (in seconds) in which to redo resolution of DNS names."))
+	resolveinterval.datatype = "uinteger"
+	resolveinterval.optional = true
+end
 
 -- collectd_network.timetolive (TimeToLive)
 ttl = s:option( Value, "TimeToLive", translate("TTL for network packets") )
@@ -82,4 +139,4 @@ cacheflush.optional  = true
 cacheflush:depends( "enable", 1 )
 
 
-return m
+return m, f
