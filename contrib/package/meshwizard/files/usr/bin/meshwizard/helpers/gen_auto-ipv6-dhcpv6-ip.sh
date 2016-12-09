5852affbbc286e7d11218d990f8ecba0f525b91b
Hannu Nyman <hannu.nyman@iki.fi>
2016-12-09 17:51:50 +0200

    meshwizard: remove local variable definitions outside functions
    Adapted from PR #819 by FreifunkUFO
    
    Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

--- contrib/package/meshwizard/files/usr/bin/meshwizard/helpers/gen_auto-ipv6-dhcpv6-ip.sh
+++ contrib/package/meshwizard/files/usr/bin/meshwizard/helpers/gen_auto-ipv6-dhcpv6-ip.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 netrenamed=$1
 
-local PREFIX="$(echo $ipv6_prefix| cut -d "/" -f 1| sed 's/::/:/')"
+PREFIX="$(echo $ipv6_prefix| cut -d "/" -f 1| sed 's/::/:/')"
 
 # Get the devices mac address
-local device="$(uci -p/var/state -q get network.$1.ifname)"
+device="$(uci -p/var/state -q get network.$1.ifname)"
 if [ -n "$device" ]; then
-	local MAC="$(ifconfig $netrenamed |grep HWaddr | awk '{ print $5 '})"
+	MAC="$(ifconfig $netrenamed |grep HWaddr | awk '{ print $5 '})"
 else 
-	local MAC="$(cat /sys/class/net/$1/address)"
-	local IPV6_UNIQ="$(echo $MAC | awk -F: '{ print $1$2":"$3$4":"$5$6 }')"
+	MAC="$(cat /sys/class/net/$1/address)"
+	IPV6_UNIQ="$(echo $MAC | awk -F: '{ print $1$2":"$3$4":"$5$6 }')"
 fi
 
 echo "${PREFIX}${IPV6_UNIQ}:1"
