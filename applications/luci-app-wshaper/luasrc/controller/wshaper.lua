100ece993e56854ebd449fb6b96e38ac258b457a
Hannu Nyman <hannu.nyman@iki.fi>
2017-02-07 11:49:10 +0200

    luci-app-wshaper: remove package
    wshaper was removed from the Packages repo
    so remove also the corresponding LuCI app.
    
    Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

--- applications/luci-app-wshaper/luasrc/controller/wshaper.lua
+++
@@ -1,9 +0,0 @@
--- Copyright 2011 Manuel Munz <freifunk somakoma de>
--- Licensed to the public under the Apache License 2.0.
-
-module "luci.controller.wshaper"
-
-function index()
-	entry({"admin", "network", "wshaper"}, cbi("wshaper"), _("Wondershaper"), 80)
-end
-
