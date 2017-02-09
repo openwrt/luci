cd6944b6daefa2920a8f60a2bfbc30683cacf583
muddyfeet <warrenandjoy@linton.id.au>
2017-02-09 10:13:01 +0000

    Add 802.11r fast transition support to wifi.lua
    Adds support for 802.11r fast transition to luci.  
    
    Corrects the deletion on nasid in issue #844
    
    The R0KH & R1KH options are not perfect as it would be better to separate out each option and do some sanity checking on the values, but that is beyond my capabilities.
    
    Signed-off-by: Warren Linton warren@linton.id.au

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/wifi.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/wifi.lua
@@ -903,6 +903,7 @@ if hwtype == "atheros" or hwtype == "mac80211" or hwtype == "prism2" then
 	local hostapd = fs.access("/usr/sbin/hostapd")
 
 	-- Probe EAP support as a proxy for determining if 802.11r support is present
+	
 	local has_ap_eap  = (os.execute("hostapd -veap >/dev/null 2>/dev/null") == 0)
 
 	ieee80211r = s:taboption("encryption", Flag, "ieee80211r", translate("802.11r Fast Transition"))
