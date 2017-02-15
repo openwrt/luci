b48d7d42b64a074f2efffde6552779aea131b38a
danrl <mail@danrl.com>
2017-02-15 07:44:23 +0100

    fixup! mod-admin-full: Allow odhcpd to be configured via LuCI

--- modules/luci-base/luasrc/cbi/datatypes.lua
+++ modules/luci-base/luasrc/cbi/datatypes.lua
@@ -200,7 +200,15 @@ function macaddr(val)
 end
 
 function duid(val)
-	return (val and val:match("^[a-fA-F0-9]{16}$"))
+	return (val and val:match("^[a-fA-F0-9]+$") and (#val == 28))
+end
+
+function hostid(val)
+	if val and val:match("^[a-fA-F0-9:]+$") and (#val > 2) then
+		return (ip6addr("2001:db8:0:0" .. val) or ip6addr("2001:db8:0:0:" .. val))
+	end
+
+	return false
 end
 
 function hostname(val)
@@ -449,4 +457,3 @@ function dateyyyymmdd(val)
 	end
 	return false
 end
-
