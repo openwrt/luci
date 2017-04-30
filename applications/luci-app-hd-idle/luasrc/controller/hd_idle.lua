d2ce55bc1ef594bbc7411fb5486afb7573a20528
fantom-x <fantom-x@users.noreply.github.com>
2017-04-30 15:42:55 -0400

    luci-app-hd-idle: removed dashes in menu items and text labels
    "hd-idle" replaced with "HDD Idle" in the menu and on the page. All translation files updated accordingly.
    
    Signed-off-by: Marc Benoit marcb6218@gmail.com

--- applications/luci-app-hd-idle/luasrc/controller/hd_idle.lua
+++ applications/luci-app-hd-idle/luasrc/controller/hd_idle.lua
@@ -10,6 +10,6 @@ function index()
 
 	local page
 
-	page = entry({"admin", "services", "hd_idle"}, cbi("hd_idle"), _("hd-idle"), 60)
+	page = entry({"admin", "services", "hd_idle"}, cbi("hd_idle"), _("HDD Idle"), 60)
 	page.dependent = true
 end
