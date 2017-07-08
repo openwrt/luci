48b5ccfad54d6d21468f68a2ae8c52c377bc5c5e
Yousong Zhou <yszhou4tech@gmail.com>
2017-07-09 03:18:15 +0800

    luci-app-shadowsocks-libev: rewrite for shadowsocks-libev 3.0.6-2
    Signed-off-by: Yousong Zhou <yszhou4tech@gmail.com>

--- applications/luci-app-shadowsocks-libev/luasrc/controller/shadowsocks-libev.lua
+++ applications/luci-app-shadowsocks-libev/luasrc/controller/shadowsocks-libev.lua
@@ -1,12 +1,22 @@
--- Copyright 2015 Jian Chang <aa65535@live.com>
+-- Copyright 2017 Yousong Zhou <yszhou4tech@gmail.com>
 -- Licensed to the public under the Apache License 2.0.
-
+--
 module("luci.controller.shadowsocks-libev", package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/shadowsocks-libev") then
-		return
-	end
+	entry({"admin", "services", "shadowsocks-libev"},
+		alias("admin", "services", "shadowsocks-libev", "instances"),
+		_("Shadowsocks-libev"), 59)
 
-	entry({"admin", "services", "shadowsocks-libev"}, cbi("shadowsocks-libev"), _("ShadowSocks-libev"), 74).dependent = true
+	entry({"admin", "services", "shadowsocks-libev", "instances"},
+		arcombine(cbi("shadowsocks-libev/instances"), cbi("shadowsocks-libev/instance-details")),
+		_("Local Instances"), 10).leaf = true
+
+	entry({"admin", "services", "shadowsocks-libev", "servers"},
+		cbi("shadowsocks-libev/servers"),
+		_("Remote Servers"), 20).leaf = true
+
+	entry({"admin", "services", "shadowsocks-libev", "rules"},
+		cbi("shadowsocks-libev/rules"),
+		_("Redir Rules"), 30).leaf = true
 end
