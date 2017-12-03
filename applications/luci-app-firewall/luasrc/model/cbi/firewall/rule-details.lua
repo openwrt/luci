5d0547e613f5081d6c9cdebde3f67485d9bfb75c
Venca B Spam <vbspam@centrum.cz>
2017-12-03 21:26:44 +0100

    Fix #1457: luci-app-firewall: It is not possible to create 'Firewall - Traffic Rule' for OUTPUT chain
    Fix the issue by adding 'None' source zone type which will instruct the
    backend not to create 'src' record in the UCI config file.
    
    Although it is Copy&Paste type fix, with little knowledge of LUCI
    framework, I believe it may help someone.
    
    The idea is taken from how the 'INPUT' chain is implemented.

--- applications/luci-app-firewall/luasrc/model/cbi/firewall/rule-details.lua
+++ applications/luci-app-firewall/luasrc/model/cbi/firewall/rule-details.lua
@@ -93,6 +93,7 @@ elseif rule_type == "redirect" then
 
 	o = s:option(Value, "src", translate("Source zone"))
 	o.nocreate = true
+	o.allownone = true
 	o.default = "wan"
 	o.template = "cbi/firewall_zonelist"
 
@@ -118,6 +119,7 @@ elseif rule_type == "redirect" then
 
 	o = s:option(Value, "dest", translate("Destination zone"))
 	o.nocreate = true
+	o.allownone = false
 	o.default = "lan"
 	o.template = "cbi/firewall_zonelist"
 
@@ -255,6 +257,7 @@ else
 	o = s:option(Value, "src", translate("Source zone"))
 	o.nocreate = true
 	o.allowany = true
+	o.allownone = true
 	o.default = "wan"
 	o.template = "cbi/firewall_zonelist"
 
@@ -286,6 +289,7 @@ else
 	o.nocreate = true
 	o.allowany = true
 	o.allowlocal = true
+	o.allownone = false
 	o.template = "cbi/firewall_zonelist"
 
 
