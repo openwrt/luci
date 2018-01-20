7edc6f03f04f386c680d419afc5bf50c91c819a3
Hannu Nyman <hannu.nyman@iki.fi>
2018-01-20 15:45:45 +0200

    freifunk-p2pblock / luci-app-p2pblock: remove from repo
    Remove freifunk-p2pblock and luci-app-p2pblock as they have been BROKEN
    since 2016. Packages depend on the removed l7-protocols as well as the
    deprecated l7 support in the iptables kernel modules.
    
    Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

--- applications/luci-app-p2pblock/luasrc/controller/ff_p2pblock.lua
+++
@@ -1,9 +0,0 @@
--- Copyright 2009 Jo-Philipp Wich <jow@openwrt.org>
--- Licensed to the public under the Apache License 2.0.
-
-module("luci.controller.ff_p2pblock", package.seeall)
-
-function index()
-	entry({"admin", "network", "firewall", "p2pblock"}, cbi("luci_fw/p2pblock"),
-		_("P2P-Block"), 40)
-end
