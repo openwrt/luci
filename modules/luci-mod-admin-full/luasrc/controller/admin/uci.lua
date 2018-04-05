731ed77c0bbee7004a6b5645d9a8592a76748a1c
Jo-Philipp Wich <jo@mein.io>
2018-04-05 22:37:37 +0200

    treewide: improve handling of page redirections in uci change views
    Instead of passing the full LuCI request url, pass the relative resolved
    request path instead and filter the received value through the lookup()
    dispatcher function to only allow paths to actual internal pages.
    
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-mod-admin-full/luasrc/controller/admin/uci.lua
+++ modules/luci-mod-admin-full/luasrc/controller/admin/uci.lua
@@ -5,8 +5,7 @@
 module("luci.controller.admin.uci", package.seeall)
 
 function index()
-	local redir = luci.http.formvalue("redir", true) or
-	  luci.dispatcher.build_url(unpack(luci.dispatcher.context.request))
+	local redir = luci.http.formvalue("redir", true) or table.concat(disp.context.request, "/")
 
 	entry({"admin", "uci"}, nil, _("Configuration"))
 	entry({"admin", "uci", "changes"}, call("action_changes"), _("Changes"), 40).query = {redir=redir}
