dacf88216d93919d8e988a08902c9f87779874ee
Jo-Philipp Wich <jo@mein.io>
2018-04-06 11:53:59 +0200

    luci-app-wol: dispatch SimpleForm model using the form() action
    This fixes issues dicovered by check-controllers.sh
    
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- applications/luci-app-wol/luasrc/controller/wol.lua
+++ applications/luci-app-wol/luasrc/controller/wol.lua
@@ -1,6 +1,6 @@
 module("luci.controller.wol", package.seeall)
 
 function index()
-	entry({"admin", "services", "wol"}, cbi("wol"), _("Wake on LAN"), 90)
-	entry({"mini", "services", "wol"}, cbi("wol"), _("Wake on LAN"), 90)
+	entry({"admin", "services", "wol"}, form("wol"), _("Wake on LAN"), 90)
+	entry({"mini", "services", "wol"}, form("wol"), _("Wake on LAN"), 90)
 end
