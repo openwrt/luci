c91ba4babc63fcc94771b608ae497a7a2db32b86
Yousong Zhou <yszhou4tech@gmail.com>
2018-05-07 21:17:42 +0800

    luci-mod-admin-full: use fs.glob instead nixio.fs.glob
    It's how the module was named when "require"d and used by other code in
    the same function
    
    Signed-off-by: Yousong Zhou <yszhou4tech@gmail.com>

--- modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
+++ modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
@@ -27,7 +27,7 @@ function index()
 		entry({"admin", "system", "fstab", "swap"},  cbi("admin_system/fstab/swap"),  nil).leaf = true
 	end
 
-	local nodes, number = nixio.fs.glob("/sys/class/leds/*")
+	local nodes, number = fs.glob("/sys/class/leds/*")
 	if number > 0 then
 		entry({"admin", "system", "leds"}, cbi("admin_system/leds"), _("<abbr title=\"Light Emitting Diode\">LED</abbr> Configuration"), 60)
 	end
