5e1afd06ccd209e659724ba3c6df55b9314e17ba
Qin Wei <Vonger@users.noreply.github.com>
2018-06-27 01:02:08 +0800

    luci-mod-admin-full: ralink support wpa/wpa2 encryption

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/wifi.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/wifi.lua
@@ -660,7 +660,7 @@ if hwtype == "mac80211" or hwtype == "prism2" then
 			"and ad-hoc mode) to be installed."
 		)
 	end
-elseif hwtype == "broadcom" then
+elseif hwtype == "broadcom" or hwtype == "ralink" then
 	encr:value("psk", "WPA-PSK")
 	encr:value("psk2", "WPA2-PSK")
 	encr:value("psk+psk2", "WPA-PSK/WPA2-PSK Mixed Mode")
