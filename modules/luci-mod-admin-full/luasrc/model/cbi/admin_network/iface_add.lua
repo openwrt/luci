9bdd097f53160a9d7c7ee536ee61f5282eaa7955
Jo-Philipp Wich <jo@mein.io>
2018-07-18 10:11:45 +0200

    luci-mod-admin-full: offer "Cancel" button in iface add dialog
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/iface_add.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/iface_add.lua
@@ -10,6 +10,10 @@ m = SimpleForm("network", translate("Create Interface"))
 m.redirect = luci.dispatcher.build_url("admin/network/network")
 m.reset = false
 
+function m.on_cancel()
+	luci.http.redirect(luci.dispatcher.build_url("admin/network/network"))
+end
+
 newnet = m:field(Value, "_netname", translate("Name of the new interface"),
 	translate("The allowed characters are: <code>A-Z</code>, <code>a-z</code>, " ..
 		"<code>0-9</code> and <code>_</code>"
