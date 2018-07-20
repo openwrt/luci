5609121c1128081ec15433b81a6f01e1ab439a66
Stan Grishin <stangri@melmac.net>
2018-07-20 16:50:08 -0700

    luci-app-simple-adblock: remove extra controller file
    Signed-off-by: Stan Grishin <stangri@melmac.net>

--- applications/luci-app-simple-adblock/luasrc/controller/simpleadblock.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.simpleadblock", package.seeall)
-function index()
-	if not nixio.fs.access("/etc/config/simple-adblock") then
-		return
-	end
-	entry({"admin", "services", "simpleadblock"}, cbi("simpleadblock"), _("Simple AdBlock"))
-end
