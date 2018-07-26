aee33a1abdd29539cdd46b76cd9916a7b3f24916
Eric Luehrsen <ericluehrsen@gmail.com>
2018-07-26 02:05:33 -0400

    luci-app-unbound: add LuCI for forward stub and auth zone clauses
    With growing interest, DNS over TLS can be setup in Unbounds foward-zone:
    clause. New section 'zone' is available for forward-, stub-, and auth-
    zone cluses. This LuCI application will show the 'zone' section and
    permit changing 'enabled' and 'fallback' options. Detailed changes to
    'zone' secitons will need to use the Edit:UCI tab (text editor).
    
    Signed-off-by: Eric Luehrsen <ericluehrsen@gmail.com>
    (cherry pick commit: 394658a1141b630c0f5d7078f2b41c6c89a40bd1)

--- applications/luci-app-unbound/luasrc/model/cbi/unbound/server.lua
+++ applications/luci-app-unbound/luasrc/model/cbi/unbound/server.lua
@@ -1,28 +1,31 @@
--- Copyright 2016 Eric Luehrsen <ericluehrsen@hotmail.com>
+-- Copyright 2016 Eric Luehrsen <ericluehrsen@gmail.com>
 -- Licensed to the public under the Apache License 2.0.
 
 local m3, s3, frm
 local filename = "/etc/unbound/unbound_srv.conf"
-local description =  translatef("Here you may edit the 'server:' clause in an internal 'include:'")
-description = description .. " (" .. filename .. ")"
+local fs = require "nixio.fs"
+local ut = require "luci.util"
 
 m3 = SimpleForm("editing", nil)
 m3:append(Template("unbound/css-editing"))
 m3.submit = translate("Save")
 m3.reset = false
-s3 = m3:section(SimpleSection, "Unbound Server Conf", description)
+s3 = m3:section(SimpleSection, "",
+    translatef(
+    "Edit 'server:' clause options for 'include: " .. filename .. "'"))
+
 frm = s3:option(TextValue, "data")
 frm.datatype = "string"
 frm.rows = 20
 
 
 function frm.cfgvalue()
-  return nixio.fs.readfile(filename) or ""
+    return fs.readfile(filename) or ""
 end
 
 
 function frm.write(self, section, data)
-  return nixio.fs.writefile(filename, luci.util.trim(data:gsub("\r\n", "\n")))
+    return fs.writefile(filename, ut.trim(data:gsub("\r\n", "\n")))
 end
 
 
