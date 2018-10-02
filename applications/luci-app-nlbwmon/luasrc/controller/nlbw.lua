openwrt/luci

commit b3642f476267e60aa1fcd7b0c3fac9512f3754ad

luci-app-nlbwmon: add proper file names for json and csv exports.
Currently, the json and csv exports are just named `data` which is not
intuitive. This adds proper file name extensions using the
Content-disposition HTTP header.

Suggested-by: Ameer Dawood <ameer1234567890@gmail.com>
[slightly reword commit message, squash commits]
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

applications/luci-app-nlbwmon/luasrc/controller/nlbw.lua
@@ -57,6 +57,8 @@ function action_data()
 		json = "application/json"
 	}
 
+	local filename = "data." .. mtype
+
 	local args = { }
 	local mtype = http.formvalue("type") or "json"
 	local delim = http.formvalue("delim") or ";"
@@ -92,6 +94,7 @@ function action_data()
 	end
 
 	http.prepare_content(types[mtype])
+	http.header("Content-Disposition", "attachment; filename=\"%s\"" % filename)
 	exec("/usr/sbin/nlbw", args, http.write)
 end
 

commit fb859e602b1254c5d199d7f641f6b6717edd01cf

luci-app-nlbwmon: fix nil value concat
Do not concatenate the yet undefined mtime variable to avoid a controller
error with backtrace.

Fixes: b3642f476 ("luci-app-nlbwmon: add proper file names for json and csv exports.")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

applications/luci-app-nlbwmon/luasrc/controller/nlbw.lua
@@ -57,8 +57,6 @@ function action_data()
 		json = "application/json"
 	}
 
-	local filename = "data." .. mtype
-
 	local args = { }
 	local mtype = http.formvalue("type") or "json"
 	local delim = http.formvalue("delim") or ","
@@ -94,7 +92,7 @@ function action_data()
 	end
 
 	http.prepare_content(types[mtype])
-	http.header("Content-Disposition", "attachment; filename=\"%s\"" % filename)
+	http.header("Content-Disposition", "attachment; filename=\"data.%s\"" % mtype)
 	exec("/usr/sbin/nlbw", args, http.write)
 end
 