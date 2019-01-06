53e20c4925f9f3429c46ffd31efd3442af1e1c03
Jo-Philipp Wich <jo@mein.io>
2019-01-06 16:37:05 +0100

    luci-base: luci.js: add Object.assign polyfill
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-base/htdocs/luci-static/resources/luci.js
+++ modules/luci-base/htdocs/luci-static/resources/luci.js
@@ -1,4 +1,26 @@
 (function(window, document, undefined) {
+	/* Object.assign polyfill for IE */
+	if (typeof Object.assign !== 'function') {
+		Object.defineProperty(Object, 'assign', {
+			value: function assign(target, varArgs) {
+				if (target == null)
+					throw new TypeError('Cannot convert undefined or null to object');
+
+				var to = Object(target);
+
+				for (var index = 1; index < arguments.length; index++)
+					if (arguments[index] != null)
+						for (var nextKey in arguments[index])
+							if (Object.prototype.hasOwnProperty.call(arguments[index], nextKey))
+								to[nextKey] = arguments[index][nextKey];
+
+				return to;
+			},
+			writable: true,
+			configurable: true
+		});
+	}
+
 	var modalDiv = null,
 	    tooltipDiv = null,
 	    tooltipTimeout = null,
