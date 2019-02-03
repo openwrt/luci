4654ba92c3999d84b08211f05d2ccb5ea9da731b
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-02-03 14:13:35 +0100

    treewide: move freifunk-related packages to separate repo
    Even Freifunk was one of the major factory to create the LuCI-system, it's
    now only a very small part of LuCI. LuCI has become a much bigger thing
    and it seems that it's time to move the packages only relating to Freifunk
    into it's own feed.
    On the mailinglist it was discussed [1] and a repo below the general
    Freifunk team on github was created.
    
    This commit removes all packages that will be hosted in the new repo [2]
    
    1 - http://lists.freifunk.net/pipermail/wlannews-freifunk.net/2019-February/004818.html
    2 - https://github.com/freifunk/openwrt-packages
    
    Signed-off-by: Sven Roederer <freifunk@it-solutions.geroedel.de>

--- applications/luci-app-freifunk-policyrouting/luasrc/controller/freifunk/policy-routing.lua
+++
@@ -1,9 +0,0 @@
--- Copyright 2011 Manuel Munz <freifunk at somakoma de>
--- Licensed to the public under the Apache License 2.0.
-
-module "luci.controller.freifunk.policy-routing"
-
-function index()
-	entry({"admin", "freifunk", "policyrouting"}, cbi("freifunk/policyrouting"),
-		_("Policy Routing"), 60)
-end
--- applications/luci-app-meshwizard/luasrc/controller/meshwizard.lua
+++
@@ -1,9 +0,0 @@
--- Copyright 2011 Manuel Munz <freifunk somakoma de>
--- Licensed to the public under the Apache License 2.0.
-
-module "luci.controller.meshwizard"
-
-function index()
-	entry({"admin", "freifunk", "meshwizard"}, cbi("freifunk/meshwizard"), _("Mesh Wizard"), 40)
-end
-
