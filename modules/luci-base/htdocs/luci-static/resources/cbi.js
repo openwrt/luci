openwrt/luci

commit 010102c2c24df1fb7826904933ab110f112852a2

luci-base: cbi.js: find dropdown and browser instances early
Gather dropdown and file browser widget nodes early, before initializing
dynamic list widgets as dynlists invoke a dependency update which in turn
might remove widget nodes from dom before they have been initialized.

Ref: https://github.com/openwrt/luci/issues/2997#issuecomment-524251601
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/cbi.js
@@ -736,7 +736,9 @@ function cbi_d_update() {
 }
 
 function cbi_init() {
-	var nodes;
+	var dropdowns = document.querySelectorAll('.cbi-dropdown'),
+	    browsers = document.querySelectorAll('[data-browser]'),
+	    nodes;
 
 	nodes = document.querySelectorAll('[data-strings]');
 
@@ -808,8 +810,8 @@ function cbi_init() {
 		                   node.getAttribute('data-type'));
 	}
 
-	document.querySelectorAll('.cbi-dropdown').forEach(cbi_dropdown_init);
-	document.querySelectorAll('[data-browser]').forEach(cbi_browser_init);
+	dropdowns.forEach(cbi_dropdown_init);
+	browsers.querySelectorAll('[data-browser]').forEach(cbi_browser_init);
 
 	document.querySelectorAll('.cbi-tooltip:not(:empty)').forEach(function(s) {
 		s.parentNode.classList.add('cbi-tooltip-container');

commit c0f3e3c6a297da4ec13955eef9dedb419fdc4894

luci-base: cbi.js: fix dropdown and browser initialization
We need to query the dropdowns before initializing dynamic lists but
after initializing combo boxes.

Also fix a copy-paste error from a previous commit.

Fixes: 010102c2c ("luci-base: cbi.js: find dropdown and browser instances early")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/cbi.js
@@ -736,9 +736,7 @@ function cbi_d_update() {
 }
 
 function cbi_init() {
-	var dropdowns = document.querySelectorAll('.cbi-dropdown'),
-	    browsers = document.querySelectorAll('[data-browser]'),
-	    nodes;
+	var dropdowns, browsers, nodes;
 
 	nodes = document.querySelectorAll('[data-strings]');
 
@@ -787,6 +785,9 @@ function cbi_init() {
 		                  node.getAttribute('data-manual'));
 	}
 
+	dropdowns = document.querySelectorAll('.cbi-dropdown');
+	browsers = document.querySelectorAll('[data-browser]');
+
 	nodes = document.querySelectorAll('[data-dynlist]');
 
 	for (var i = 0, node; (node = nodes[i]) !== undefined; i++) {
@@ -811,7 +812,7 @@ function cbi_init() {
 	}
 
 	dropdowns.forEach(cbi_dropdown_init);
-	browsers.querySelectorAll('[data-browser]').forEach(cbi_browser_init);
+	browsers.forEach(cbi_browser_init);
 
 	document.querySelectorAll('.cbi-tooltip:not(:empty)').forEach(function(s) {
 		s.parentNode.classList.add('cbi-tooltip-container');