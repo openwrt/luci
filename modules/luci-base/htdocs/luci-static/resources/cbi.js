openwrt/luci

commit 010102c2c24df1fb7826904933ab110f112852a2

luci-base: cbi.js: find dropdown and browser instances early
Gather dropdown and file browser widget nodes early, before initializing
dynamic list widgets as dynlists invoke a dependency update which in turn
might remove widget nodes from dom before they have been initialized.

Ref: https://github.com/openwrt/luci/issues/2997#issuecomment-524251601
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/cbi.js
@@ -736,7 +736,9 @@ function cbi_d_update() {
 }
 
 function cbi_init() {
-	var nodes;
+	var dropdowns = document.querySelectorAll('.cbi-dropdown'),
+	    browsers = document.querySelectorAll('[data-browser]'),
+	    nodes;
 
 	nodes = document.querySelectorAll('[data-strings]');
 
@@ -808,8 +810,8 @@ function cbi_init() {
 		                   node.getAttribute('data-type'));
 	}
 
-	document.querySelectorAll('.cbi-dropdown').forEach(cbi_dropdown_init);
-	document.querySelectorAll('[data-browser]').forEach(cbi_browser_init);
+	dropdowns.forEach(cbi_dropdown_init);
+	browsers.querySelectorAll('[data-browser]').forEach(cbi_browser_init);
 
 	document.querySelectorAll('.cbi-tooltip:not(:empty)').forEach(function(s) {
 		s.parentNode.classList.add('cbi-tooltip-container');

commit a3573524531f761cc6f2a9d46b96bb01f4ef027e

luci-base: cbi.js: also initialize comboxes embedded in dynlists
Fixes: #3062
Fixes: 010102c2c ("luci-base: cbi.js: find dropdown and browser instances early")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/cbi.js
@@ -802,6 +802,8 @@ function cbi_init() {
 		}
 
 		cbi_dynlist_init(node, choices[2], choices[3], options);
+
+		node.querySelectorAll('.cbi-dropdown').forEach(cbi_dropdown_init);
 	}
 
 	nodes = document.querySelectorAll('[data-type]');
@@ -1954,6 +1956,9 @@ function cbi_dropdown_init(sb) {
 	if (!(this instanceof cbi_dropdown_init))
 		return new cbi_dropdown_init(sb);
 
+	if (sb.classList.contains('initialized'))
+		return;
+
 	this.multi = sb.hasAttribute('multiple');
 	this.optional = sb.hasAttribute('optional');
 	this.placeholder = sb.getAttribute('placeholder') || '---';
@@ -2049,6 +2054,8 @@ function cbi_dropdown_init(sb) {
 		li.setAttribute('unselectable', '');
 		li.addEventListener('click', this.handleCreateClick.bind(this));
 	}
+
+	sb.classList.add('initialized');
 }
 
 cbi_dropdown_init.prototype = CBIDropdown;