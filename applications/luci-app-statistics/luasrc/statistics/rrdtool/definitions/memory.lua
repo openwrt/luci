00fc5548ffb35184e5915c130889ed8ad9912fa6
Florian Eckert <fe@dev.tdt.de>
2019-09-13 12:56:39 +0200

    luci-app-statistics: update license header in memory definition
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- applications/luci-app-statistics/luasrc/statistics/rrdtool/definitions/memory.lua
+++ applications/luci-app-statistics/luasrc/statistics/rrdtool/definitions/memory.lua
@@ -1,13 +1,5 @@
---[[
-
-(c) 2011 Manuel Munz <freifunk at somakoma dot de>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-]]--
+-- Copyright  2011 Manuel Munz <freifunk at somakoma dot de>
+-- Licensed to the public under the Apache License 2.0.
 
 module("luci.statistics.rrdtool.definitions.memory",package.seeall)
 
