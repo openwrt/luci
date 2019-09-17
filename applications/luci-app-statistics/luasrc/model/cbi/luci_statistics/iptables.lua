1ce1e7f83e160e1071e5037fce76775139328392
Florian Eckert <fe@dev.tdt.de>
2019-09-17 14:36:10 +0200

    luci-app-statistics: move iptables config to luci_collectd uci configuration
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- applications/luci-app-statistics/luasrc/model/cbi/luci_statistics/iptables.lua
+++ applications/luci-app-statistics/luasrc/model/cbi/luci_statistics/iptables.lua
@@ -13,24 +13,22 @@ for i, rule in ipairs( ip:find() ) do
 	end
 end
 
-
-m = Map("luci_statistics",
+m = Map("luci_collectd",
 	translate("Iptables Plugin Configuration"),
 	translate(
 		"The iptables plugin will monitor selected firewall rules and " ..
 		"collect information about processed bytes and packets per rule."
 	))
 
--- collectd_iptables config section
-s = m:section( NamedSection, "collectd_iptables", "luci_statistics" )
+-- iptables config section
+s = m:section( NamedSection, "iptables", "plugin" )
 
--- collectd_iptables.enable
+-- iptables.enable
 enable = s:option( Flag, "enable", translate("Enable this plugin") )
 enable.default = 0
 
-
--- collectd_iptables_match config section (Chain directives)
-rule = m:section( TypedSection, "collectd_iptables_match",
+-- iptables_match config section (Chain directives)
+rule = m:section( TypedSection, "iptables_match",
 	translate("Add matching rule"),
 	translate(
 		"Here you can define various criteria by which the monitored " ..
@@ -39,12 +37,11 @@ rule = m:section( TypedSection, "collectd_iptables_match",
 rule.addremove = true
 rule.anonymous = true
 
-
--- collectd_iptables_match.name
+-- iptables_match.name
 rule_table = rule:option( Value, "name",
 	translate("Name of the rule"), translate("max. 16 chars") )
 
--- collectd_iptables_match.table
+-- iptables_match.table
 rule_table = rule:option( ListValue, "table", translate("Table") )
 rule_table.default  = "filter"
 rule_table.rmempty  = true
@@ -54,30 +51,25 @@ rule_table:value("filter")
 rule_table:value("nat")
 rule_table:value("mangle")
 
-
--- collectd_iptables_match.chain
+-- iptables_match.chain
 rule_chain = rule:option( ListValue, "chain", translate("Chain") )
 rule_chain.rmempty  = true
 rule_chain.optional = true
 rule_chain:value("")
-
 for chain, void in pairs( chains ) do
 	rule_chain:value( chain )
 end
 
-
--- collectd_iptables_match.target
+-- The follwing values are not needed by the collectd but this is needed
+-- to find the rule index
 rule_target = rule:option( ListValue, "target", translate("Action (target)") )
 rule_target.rmempty  = true
 rule_target.optional = true
 rule_target:value("")
-
 for target, void in pairs( targets ) do
 	rule_target:value( target )
 end
 
-
--- collectd_iptables_match.protocol
 rule_protocol = rule:option( ListValue, "protocol", translate("Network protocol") )
 rule_protocol.rmempty  = true
 rule_protocol.optional = true
@@ -86,31 +78,26 @@ rule_protocol:value("tcp")
 rule_protocol:value("udp")
 rule_protocol:value("icmp")
 
--- collectd_iptables_match.source
 rule_source = rule:option( Value, "source", translate("Source ip range") )
 rule_source.default  = "0.0.0.0/0"
 rule_source.rmempty  = true
 rule_source.optional = true
 
--- collectd_iptables_match.destination
 rule_destination = rule:option( Value, "destination", translate("Destination ip range") )
 rule_destination.default  = "0.0.0.0/0"
 rule_destination.rmempty  = true
 rule_destination.optional = true
 
--- collectd_iptables_match.inputif
 rule_inputif = rule:option( Value, "inputif",
 	translate("Incoming interface"), translate("e.g. br-lan") )
 rule_inputif.rmempty  = true
 rule_inputif.optional = true
 
--- collectd_iptables_match.outputif
 rule_outputif = rule:option( Value, "outputif",
 	translate("Outgoing interface"), translate("e.g. br-ff") )
 rule_outputif.rmempty  = true
 rule_outputif.optional = true
 
--- collectd_iptables_match.options
 rule_options = rule:option( Value, "options",
 	translate("Options"), translate("e.g. reject-with tcp-reset") )
 rule_options.rmempty  = true
