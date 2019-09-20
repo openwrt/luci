openwrt/luci

commit 63f28fd0657c121541b1beaabc901dccdf26a534

luci-mod-network: default to ieee80211w=1 for OWE
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -1545,7 +1545,7 @@ return L.view.extend({
 							o.depends({ mode: 'sta-wds', encryption: 'owe' });
 							o.defaults = {
 								'2': [{ encryption: 'sae' }],
-								'1': [{ encryption: 'sae-mixed'}],
+								'1': [{ encryption: 'sae-mixed'}, { encryption: 'owe' }],
 								'':  []
 							};
 

commit ccd152361072c9e16dfe4a1fac2d9a5c8af092ba

luci-mod-network: default to ieee80211w=2 for OWE
Ref: https://git.openwrt.org/?p=openwrt/openwrt.git;a=blob;f=package/network/services/hostapd/files/hostapd.sh;h=46acbefeeeb771de879dd9926ad170042dff2473;hb=HEAD#l327
Fixes: 63f28fd06 luci-mod-network: default to ieee80211w=1 for OWE
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -1544,8 +1544,8 @@ return L.view.extend({
 							o.depends({ mode: 'sta-wds', encryption: 'sae-mixed' });
 							o.depends({ mode: 'sta-wds', encryption: 'owe' });
 							o.defaults = {
-								'2': [{ encryption: 'sae' }],
-								'1': [{ encryption: 'sae-mixed'}, { encryption: 'owe' }],
+								'2': [{ encryption: 'sae' }, { encryption: 'owe' }],
+								'1': [{ encryption: 'sae-mixed'}],
 								'':  []
 							};
 