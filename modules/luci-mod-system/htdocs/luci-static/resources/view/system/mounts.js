openwrt/luci

commit e39ab8725531bd390d57a037823b8546f68ce5be

luci-mod-system: mounts.js: implicitely create globals section
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-system/htdocs/luci-static/resources/view/system/mounts.js
@@ -161,6 +161,9 @@ return L.view.extend({
 			btrfs: results[6]
 		};
 
+		if (!uci.sections('fstab', 'global').length)
+			uci.add('fstab', 'global');
+
 		m = new form.Map('fstab', _('Mount Points'));
 
 		s = m.section(form.TypedSection, 'global', _('Global Settings'));

commit 4518f8f3428471aa44fe7d2bd6e2cfa5be23af8f

luci-mod-system: mounts.js: preload fstab uci
Preload the uci configuration on page load to avoid accidential double
rendering of the globals section and mount overview table.

Fixes: e39ab8725 ("luci-mod-system: mounts.js: implicitely create globals section")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-system/htdocs/luci-static/resources/view/system/mounts.js
@@ -137,7 +137,8 @@ return L.view.extend({
 			callFileStat('/usr/sbin/e2fsck'),
 			callFileStat('/usr/sbin/fsck.f2fs'),
 			callFileStat('/usr/sbin/dosfsck'),
-			callFileStat('/usr/bin/btrfsck')
+			callFileStat('/usr/bin/btrfsck'),
+			uci.load('fstab')
 		]);
 	},
 