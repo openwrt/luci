71e265a7f038f172eada95576496ef240a9c83e7
Jo-Philipp Wich <jo@mein.io>
2019-09-21 18:36:31 +0200

    luci-mod-system: remplement fstab settings as client side view
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-mod-system/luasrc/controller/admin/system.lua
+++ modules/luci-mod-system/luasrc/controller/admin/system.lua
@@ -23,9 +23,7 @@ function index()
 	entry({"admin", "system", "crontab"}, view("system/crontab"), _("Scheduled Tasks"), 46)
 
 	if fs.access("/sbin/block") and fs.access("/etc/config/fstab") then
-		entry({"admin", "system", "fstab"}, cbi("admin_system/fstab"), _("Mount Points"), 50)
-		entry({"admin", "system", "fstab", "mount"}, cbi("admin_system/fstab/mount"), nil).leaf = true
-		entry({"admin", "system", "fstab", "swap"},  cbi("admin_system/fstab/swap"),  nil).leaf = true
+		entry({"admin", "system", "mounts"}, view("system/mounts"), _("Mount Points"), 50)
 	end
 
 	local nodes, number = fs.glob("/sys/class/leds/*")
