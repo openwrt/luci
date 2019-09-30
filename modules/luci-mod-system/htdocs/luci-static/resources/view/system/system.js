f1db82399ae7a1ac6cb58a5489dad51dc3a22c52
Jo-Philipp Wich <jo@mein.io>
2019-09-30 10:27:35 +0200

    luci-mod-system: allow IPv6 addresses for external syslog server
    Fixes: #3126
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/system.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/system.js
@@ -141,7 +141,7 @@ return L.view.extend({
 		o = s.taboption('logging', form.Value, 'log_ip', _('External system log server'))
 		o.optional    = true
 		o.placeholder = '0.0.0.0'
-		o.datatype    = 'ip4addr'
+		o.datatype    = 'ipaddr'
 
 		o = s.taboption('logging', form.Value, 'log_port', _('External system log server port'))
 		o.optional    = true
