641c82cb21da3167e271045bc88c0db1110845c2
Jo-Philipp Wich <jo@mein.io>
2019-10-02 19:45:36 +0200

    luci-mod-system: crontab.js: use common fs.js class
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/crontab.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/crontab.js
@@ -1,36 +1,19 @@
 'use strict';
-'require rpc';
+'require fs';
 
 return L.view.extend({
-	callFileRead: rpc.declare({
-		object: 'file',
-		method: 'read',
-		params: [ 'path' ],
-		expect: { data: '' }
-	}),
-
-	callFileWrite: rpc.declare({
-		object: 'file',
-		method: 'write',
-		params: [ 'path', 'data' ]
-	}),
-
 	load: function() {
-		return this.callFileRead('/etc/crontabs/root');
+		return L.resolveDefault(fs.read('/etc/crontabs/root'), '');
 	},
 
 	handleSave: function(ev) {
 		var value = (document.querySelector('textarea').value || '').trim().replace(/\r\n/g, '\n') + '\n';
 
-		return this.callFileWrite('/etc/crontabs/root', value).then(function(rc) {
-			if (rc != 0)
-				throw rpc.getStatusText(rc);
-
+		return fs.write('/etc/crontabs/root', value).then(function(rc) {
 			document.querySelector('textarea').value = value;
 			L.ui.addNotification(null, E('p', _('Contents have been saved.')), 'info');
-
 		}).catch(function(e) {
-			L.ui.addNotification(null, E('p', _('Unable to save contents: %s').format(e)));
+			L.ui.addNotification(null, E('p', _('Unable to save contents: %s').format(e.message)));
 		});
 	},
 
