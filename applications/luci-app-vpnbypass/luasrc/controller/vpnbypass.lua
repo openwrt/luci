f6bf361ec50cad34b10b756793aabc1fc34baf39
Stan Grishin <stangri@melmac.net>
2019-10-28 16:35:10 -0700

    luci-app-vpnbypass: bugfix: call to action has no parent node
    Signed-off-by: Stan Grishin <stangri@melmac.net>

--- applications/luci-app-vpnbypass/luasrc/controller/vpnbypass.lua
+++ applications/luci-app-vpnbypass/luasrc/controller/vpnbypass.lua
@@ -1,12 +1,10 @@
 module("luci.controller.vpnbypass", package.seeall)
 function index()
 	if nixio.fs.access("/etc/config/vpnbypass") then
-		if luci.dispatcher.lookup("admin/vpn") then
-			entry({"admin", "vpn", "vpnbypass"}, cbi("vpnbypass"), _("VPN Bypass"))
-		else
-			entry({"admin", "services", "vpnbypass"}, cbi("vpnbypass"), _("VPN Bypass"))
-		end
-	entry({"admin", "services", "vpnbypass", "action"}, call("vpnbypass_action"), nil).leaf = true
+		local node = "services"
+		if luci.dispatcher.lookup("admin/vpn") then node = "vpn" end
+		entry({"admin", node, "vpnbypass"}, cbi("vpnbypass"), _("VPN Bypass"))
+		entry({"admin", node, "vpnbypass", "action"}, call("vpnbypass_action"), nil).leaf = true
 	end
 end
 
