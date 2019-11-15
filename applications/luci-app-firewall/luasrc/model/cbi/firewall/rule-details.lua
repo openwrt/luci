openwrt/luci

commit c048f23bad54b0a79449652380b317819e0ea978

luci-app-firewall: fix stored XSS in rule- and forward detail pages
Signed-off-by: Jo-Philipp Wich <jo@mein.io>
(cherry picked from commit 2a143f4777e5ec57dfc6c63d55bf80600486efd7)

applications/luci-app-firewall/luasrc/model/cbi/firewall/rule-details.lua
@@ -44,7 +44,7 @@ elseif rule_type == "redirect" then
 		name = "SNAT %s" % name
 	end
 
-	m.title = "%s - %s" %{ translate("Firewall - Traffic Rules"), name }
+	m.title = "%s - %s" %{ translate("Firewall - Traffic Rules"), luci.util.pcdata(name) }
 
 	local wan_zone = nil
 

applications/luci-app-firewall/luasrc/model/cbi/firewall/forward-details.lua
@@ -25,7 +25,7 @@ else
 	if not name or #name == 0 then
 		name = translate("(Unnamed Entry)")
 	end
-	m.title = "%s - %s" %{ translate("Firewall - Port Forwards"), name }
+	m.title = "%s - %s" %{ translate("Firewall - Port Forwards"), luci.util.pcdata(name) }
 end
 
 s = m:section(NamedSection, arg[1], "redirect", "")

commit 3961268597abba4c2b231790cb4aa7936e73cdf8

luci-app-firewall: fix stored XSS in rule-details.lua
A previous fix to this file failed to address a second instance of the
same issue.

Fixes: c048f23ba ("luci-app-firewall: fix stored XSS in rule- and forward detail pages")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

applications/luci-app-firewall/luasrc/model/cbi/firewall/rule-details.lua
@@ -190,7 +190,7 @@ else
 		name = translate("(Unnamed Rule)")
 	end
 
-	m.title = "%s - %s" %{ translate("Firewall - Traffic Rules"), name }
+	m.title = "%s - %s" %{ translate("Firewall - Traffic Rules"), luci.util.pcdata(name) }
 
 
 	s = m:section(NamedSection, arg[1], "rule", "")