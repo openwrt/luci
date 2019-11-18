29fdc2b014b05a4d6ae2324b36ea9ce1c956360b
Richard Yu <yurichard3839@gmail.com>
2019-11-18 20:46:48 +0800

    luci-app-nextdns: port to client side
    Signed-off-by: Richard Yu <yurichard3839@gmail.com>

--- applications/luci-app-nextdns/luasrc/controller/nextdns.lua
+++ applications/luci-app-nextdns/luasrc/controller/nextdns.lua
@@ -3,30 +3,12 @@
 
 module("luci.controller.nextdns", package.seeall)
 
-local util  = require("luci.util")
-local i18n  = require("luci.i18n")
-local templ = require("luci.template")
-local http  = require("luci.http")
-
 function index()
 	if not nixio.fs.access("/etc/config/nextdns") then
 		return
 	end
 
 	entry({"admin", "services", "nextdns"}, firstchild(), _("NextDNS"), 60).dependent = false
-	entry({"admin", "services", "nextdns", "overview"}, cbi("overview", {hideresetbtn=true, hidesavebtn=true}), _("Overview"), 10).leaf = true
-	entry({"admin", "services", "nextdns", "log"}, template("nextdns/logread"), _("Logs"), 30).leaf = true
-
-	entry({"admin", "services", "nextdns", "logread"}, call("logread"), nil).leaf = true
-
+	entry({"admin", "services", "nextdns", "overview"}, view("nextdns/overview"), _("Overview"), 10).leaf = true
+	entry({"admin", "services", "nextdns", "log"}, view("nextdns/logread"), _("Logs"), 30).leaf = true
 end
-
-function logread()
-	local content = util.trim(util.exec("logread -e 'nextdns'")) or ""
-
-	if content == "" then
-		content = "No nextdns related logs yet!"
-	end
-	http.write(content)
-end
-
