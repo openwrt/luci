277d4e3268dd47f2fa94046aa24bb990ea4d2c59
George Iv <57254463+zhoreeq@users.noreply.github.com>
2019-12-01 00:32:50 -0500

    luci-app-yggdrasil: Add application for Yggdrasil
    Signed-off-by: George Iv <57254463+zhoreeq@users.noreply.github.com>

---
+++ applications/luci-app-yggdrasil/htdocs/luci-static/resources/view/yggdrasil/keys.js
@@ -0,0 +1,22 @@
+'use strict';
+'require form';
+
+return L.view.extend({
+	render: function() {
+		var m, s, o;
+
+		m = new form.Map('yggdrasil', 'Yggdrasil');
+
+		s = m.section(form.TypedSection, "yggdrasil", _("Encryption keys")); 
+		s.anonymous = true;
+
+		s.option(form.Value, "EncryptionPublicKey", _("Encryption public key"));
+		s.option(form.Value, "EncryptionPrivateKey", _("Encryption private key"),
+			_("Keep this private. When compromised, generate a new keypair and IPv6."));
+		s.option(form.Value, "SigningPublicKey", _("Signing public key"));
+		s.option(form.Value, "SigningPrivateKey", _("Signing private key"),
+			_("Keep this private. When compromised, generate a new keypair and IPv6."));
+
+		return m.render();
+	}
+});
