43ddf0cd12ffdcbe6c5d760293cddcbe35968801
Stan Grishin <stangri@melmac.net>
2019-12-19 11:19:59 -0700

    luci-app-https-dns-proxy: package name change, support for RFC8484 binary
    Signed-off-by: Stan Grishin <stangri@melmac.net>

--- applications/luci-app-https_dns_proxy/luasrc/https_dns_proxy/providers.disabled/ch.digitale-gesellschaft.dns.lua
+++
@@ -1,6 +0,0 @@
-return {
-	name = "Digitale-Gesellschaft",
-	label = _("Digitale Gesellschaft"),
-	url_prefix = "https://dns.digitale-gesellschaft.ch/dns-query?",
-	bootstrap_dns = "185.95.218.42,185.95.218.43"
-}
