d3b5abcbcf2db7ba289170c87b66a4a6964908d5
Balázs Úr <balazs@urbalazs.hu>
2020-01-09 00:41:49 +0100

    luci-app-olsr: fix typo
    Signed-off-by: Balázs Úr <balazs@urbalazs.hu>

--- applications/luci-app-olsr/luasrc/model/cbi/olsr/olsrd.lua
+++ applications/luci-app-olsr/luasrc/model/cbi/olsr/olsrd.lua
@@ -224,8 +224,8 @@ i:tab("addrs",   translate("IP Addresses"))
 i:tab("timing",  translate("Timing and Validity"))
 
 mode = i:taboption("general", ListValue, "Mode", translate("Mode"),
-	translate("Interface Mode is used to prevent unnecessary packet forwarding on switched ethernet interfaces. "..
-	"valid Modes are \"mesh\" and \"ether\". Default is \"mesh\"."))
+	translate("Interface mode is used to prevent unnecessary packet forwarding on switched ethernet interfaces. "..
+	"Valid modes are \"mesh\" and \"ether\". Default is \"mesh\"."))
 mode:value("mesh")
 mode:value("ether")
 mode.optional = true
