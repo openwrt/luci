1787cd4a4c39e3ef8bccde4e601c6a1d7c531471
Jo-Philipp Wich <jo@mein.io>
2020-01-15 19:22:06 +0100

    luci-app-firewall: introduce support for "config nat" sections
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>
    (backported from commit c8b8ae6b8e0b7e249cf0392263f44e519025b426)

--- applications/luci-app-firewall/luasrc/controller/firewall.lua
+++ applications/luci-app-firewall/luasrc/controller/firewall.lua
@@ -14,6 +14,9 @@ function index()
 	entry({"admin", "network", "firewall", "rules"},
 		view("firewall/rules"), _("Traffic Rules"), 30)
 
+	entry({"admin", "network", "firewall", "snats"},
+		view("firewall/snats"), _("NAT Rules"), 40)
+
 	entry({"admin", "network", "firewall", "custom"},
-		view("firewall/custom"), _("Custom Rules"), 40).leaf = true
+		view("firewall/custom"), _("Custom Rules"), 50).leaf = true
 end
