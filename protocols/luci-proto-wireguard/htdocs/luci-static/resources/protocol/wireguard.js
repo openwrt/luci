c1405dd9c30fe8e6bb80733ac0869be20e378b0d
Jo-Philipp Wich <jo@mein.io>
2020-01-21 18:38:04 +0100

    luci-proto-wireguard: explicitely escape slashes in regex literals
    The unespaced slashes confuse xgettext and likely other source
    scanners as well.
    
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>
    (cherry picked from commit f1c89c103b6e5109d45a7573f707fb83d8365ce2)

--- protocols/luci-proto-wireguard/htdocs/luci-static/resources/protocol/wireguard.js
+++ protocols/luci-proto-wireguard/htdocs/luci-static/resources/protocol/wireguard.js
@@ -6,7 +6,7 @@ function validateBase64(section_id, value) {
 	if (value.length == 0)
 		return true;
 
-	if (value.length != 44 || !value.match(/^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/))
+	if (value.length != 44 || !value.match(/^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/))
 		return _('Invalid Base64 key string');
 
 	return true;
