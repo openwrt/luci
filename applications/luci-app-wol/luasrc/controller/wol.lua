40ee3fa3ce1742d625123be11f41df04cf353d7a
Jo-Philipp Wich <jo@mein.io>
2020-01-26 22:48:20 +0100

    luci-app-woll: convert to client side rendering
    Also make the interface option mandatory
    
    Fixes: #3557
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- applications/luci-app-wol/luasrc/controller/wol.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.wol", package.seeall)
-
-function index()
-	entry({"admin", "services", "wol"}, form("wol"), _("Wake on LAN"), 90)
-	entry({"mini", "services", "wol"}, form("wol"), _("Wake on LAN"), 90)
-end
