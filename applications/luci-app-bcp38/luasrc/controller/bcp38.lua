625abbfdbe94dada023ebd8ea69778173d1a7351
Jo-Philipp Wich <jo@mein.io>
2020-02-05 16:51:19 +0100

    treewide: convert simple Lua controllers to declarative JSON
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- applications/luci-app-bcp38/luasrc/controller/bcp38.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.bcp38", package.seeall)
-
-function index()
-	entry({"admin", "network", "firewall", "bcp38"},
-		cbi("bcp38"),
-		_("BCP38"), 50).dependent = false
-end
--- applications/luci-app-snmpd/luasrc/controller/snmpd.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.snmpd", package.seeall)
-
-function index()
-    entry({"admin", "services", "snmpd"}, cbi("snmpd"), "SNMPD")
-end
