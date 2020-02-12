9bb78b5cd842f94cbcaf00cc5b19c1e3b9669d93
Jo-Philipp Wich <jo@mein.io>
2020-02-12 08:16:06 +0100

    luci-base: form.js: rendering fixes for grid sections
     - Ensure that last header cell is rendered for grid sections without
       sorting or addremove actions
     - Don't skip header cells for optional options
    
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-base/htdocs/luci-static/resources/form.js
+++ modules/luci-base/htdocs/luci-static/resources/form.js
@@ -1104,7 +1104,7 @@ var CBITableSection = CBITypedSection.extend({
 		return sectionEl;
 	},
 
-	renderHeaderRows: function(max_cols) {
+	renderHeaderRows: function(max_cols, has_action) {
 		var has_titles = false,
 		    has_descriptions = false,
 		    max_cols = isNaN(this.max_cols) ? this.children.length : this.max_cols,
@@ -1113,7 +1113,7 @@ var CBITableSection = CBITypedSection.extend({
 		    trEls = E([]);
 
 		for (var i = 0, opt; i < max_cols && (opt = this.children[i]) != null; i++) {
-			if (opt.optional || opt.modalonly)
+			if (opt.modalonly)
 				continue;
 
 			has_titles = has_titles || !!opt.title;
@@ -1127,7 +1127,7 @@ var CBITableSection = CBITypedSection.extend({
 			});
 
 			for (var i = 0, opt; i < max_cols && (opt = this.children[i]) != null; i++) {
-				if (opt.optional || opt.modalonly)
+				if (opt.modalonly)
 					continue;
 
 				trEl.appendChild(E('div', {
@@ -1149,7 +1149,7 @@ var CBITableSection = CBITypedSection.extend({
 					L.dom.content(trEl.lastElementChild, opt.title);
 			}
 
-			if (this.sortable || this.extedit || this.addremove || has_more)
+			if (this.sortable || this.extedit || this.addremove || has_more || has_action)
 				trEl.appendChild(E('div', {
 					'class': 'th cbi-section-table-cell cbi-section-actions'
 				}));
@@ -1163,7 +1163,7 @@ var CBITableSection = CBITypedSection.extend({
 			});
 
 			for (var i = 0, opt; i < max_cols && (opt = this.children[i]) != null; i++) {
-				if (opt.optional || opt.modalonly)
+				if (opt.modalonly)
 					continue;
 
 				trEl.appendChild(E('div', {
@@ -1475,6 +1475,10 @@ var CBIGridSection = CBITableSection.extend({
 			'data-name': opt.option,
 			'data-widget': opt.typename || opt.__name__
 		}, (value != null) ? value : E('em', _('none')));
+	},
+
+	renderHeaderRows: function(section_id) {
+		return this.super('renderHeaderRows', [ NaN, true ]);
 	},
 
 	renderRowActions: function(section_id) {
