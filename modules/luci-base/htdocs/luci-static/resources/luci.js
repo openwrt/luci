308215ca156212ad2a632b31179c2284a59a8556
Jo-Philipp Wich <jo@mein.io>
2020-02-14 08:40:15 +0100

    luci-base: luci.js: L.dom.data(): gracefully handle non-node argument
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-base/htdocs/luci-static/resources/luci.js
+++ modules/luci-base/htdocs/luci-static/resources/luci.js
@@ -2554,6 +2554,9 @@
 			 * be found.
 			 */
 			data: function(node, key, val) {
+				if (!node || !node.getAttribute)
+					return null;
+
 				var id = node.getAttribute('data-idref');
 
 				/* clear all data */
