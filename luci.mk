b9292a6f575e5bd440b2011006489c06a3dcb833
Florian Eckert <fe@dev.tdt.de>
2020-02-14 09:12:31 +0100

    luci.mk: move /tmp/luci-modulecache remove to package postinst script
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- luci.mk
+++ luci.mk
@@ -226,6 +226,7 @@ define Package/$(PKG_NAME)/postinst
 [ -n "$${IPKG_INSTROOT}" ] || {$(foreach script,$(LUCI_DEFAULTS),
 	(. /etc/uci-defaults/$(script)) && rm -f /etc/uci-defaults/$(script))
 	rm -f /tmp/luci-indexcache
+	rm -rf /tmp/luci-modulecache/
 	exit 0
 }
 endef
@@ -233,6 +234,7 @@ else
 define Package/$(PKG_NAME)/postinst
 [ -n "$${IPKG_INSTROOT}" ] || {
 	rm -f /tmp/luci-indexcache
+	rm -rf /tmp/luci-modulecache/
 	exit 0
 }
 endef
