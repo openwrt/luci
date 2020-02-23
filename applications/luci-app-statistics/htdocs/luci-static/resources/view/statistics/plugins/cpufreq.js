0c3ce36ea88425876c089eae9205711957fa2975
Jo-Philipp Wich <jo@mein.io>
2020-02-23 12:10:35 +0100

    luci-app-statistics: cpufreq: re-add ExtraItems config
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- applications/luci-app-statistics/htdocs/luci-static/resources/view/statistics/plugins/cpufreq.js
+++ applications/luci-app-statistics/htdocs/luci-static/resources/view/statistics/plugins/cpufreq.js
@@ -5,7 +5,20 @@ return L.Class.extend({
 	title: _('CPU Frequency Plugin Configuration'),
 	description: _('This plugin collects statistics about the processor frequency scaling.'),
 
+	addFormOptions: function(s) {
+		var o;
+
+		o = s.option(form.Flag, 'enable', _('Enable this plugin'));
+		o.default = '0';
+
+		o = s.option(form.Flag, 'ExtraItems', _('Extra items'),
+			_('More details about frequency usage and transitions'));
+		o.depends('enable', '1');
+	},
+
 	configSummary: function(section) {
-		return _('CPU frequency monitoring enabled');
+		return (section.ExtraItems == '1')
+			? _('Detailled CPU frequency monitoring enabled')
+			: _('Simple CPU frequency monitoring enabled');
 	}
 });
