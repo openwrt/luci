be62595c5b44e2f4bc366bb3a2d2ee07d69a77dd
Jo-Philipp Wich <jo@mein.io>
2020-03-23 22:02:30 +0100

    luci-mod-status: tag DHCP lease tables with dedicated CSS classes
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/40_dhcp.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/40_dhcp.js
@@ -23,7 +23,7 @@ return L.Class.extend({
 		    leases6 = Array.isArray(data[0].dhcp6_leases) ? data[0].dhcp6_leases : [],
 		    machints = data[1].getMACHints(false);
 
-		var table = E('div', { 'class': 'table' }, [
+		var table = E('div', { 'class': 'table lases' }, [
 			E('div', { 'class': 'tr table-titles' }, [
 				E('div', { 'class': 'th' }, _('Hostname')),
 				E('div', { 'class': 'th' }, _('IPv4-Address')),
@@ -50,7 +50,7 @@ return L.Class.extend({
 			];
 		}), E('em', _('There are no active leases')));
 
-		var table6 = E('div', { 'class': 'table' }, [
+		var table6 = E('div', { 'class': 'table leases6' }, [
 			E('div', { 'class': 'tr table-titles' }, [
 				E('div', { 'class': 'th' }, _('Host')),
 				E('div', { 'class': 'th' }, _('IPv6-Address')),
