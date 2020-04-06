b3184860e1c4fac314bf505bf6e2c6e458b918bd
Stan Grishin <stangri@melmac.net>
2020-04-06 14:00:33 +0000

    luci-app-https-dns-proxy: Move help links to providers files, Add LibreDNS.
    Signed-off-by: Stan Grishin <stangri@melmac.net>

--- applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/net.quad9.dns9.lua
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/net.quad9.dns9.lua
@@ -2,5 +2,7 @@ return {
 	name = "Quad9-Secured",
 	label = _("Quad 9 (Secured)"),
 	resolver_url = "https://dns9.quad9.net/dns-query",
-	bootstrap_dns = "9.9.9.9,149.112.112.9"
+	bootstrap_dns = "9.9.9.9,149.112.112.9",
+	help_link = "https://www.quad9.net/doh-quad9-dns-servers/",
+	help_link_text = "Quad9.net"
 }
