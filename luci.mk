openwrt/luci

commit 9d8e99f9bed1402543e128c2543fdbd5e2f9c338

build: gracefully handle non-Git source trees
Fixes: #3911
Fixes: 59fe4086f ("build: luci.mk: drop support for non-Git SCMs")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

luci.mk
@@ -59,19 +59,30 @@ LUCI_LC_ALIAS.zh_Hant=zh-tw
 
 PKG_NAME?=$(LUCI_NAME)
 
-PKG_PO_VERSION?=$(if $(DUMP),x,$(strip $(shell \
-	set -- $$(git log -1 --format="%ct %h" --abbrev=7 -- po); \
-	secs="$$(($$1 % 86400))"; \
-	yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
-	printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2" \
-)))
 
-PKG_SRC_VERSION?=$(if $(DUMP),x,$(strip $(shell \
-	set -- $$(git log -1 --format="%ct %h" --abbrev=7 -- . ':(exclude)po'); \
-	secs="$$(($$1 % 86400))"; \
-	yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
-	printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2" \
-)))
+# 1: everything expect po subdir or only po subdir
+define findrev
+  $(shell \
+    if git log -1 >/dev/null 2>/dev/null; then \
+      set -- $$(git log -1 --format="%ct %h" --abbrev=7 -- '$(if $(1),:(exclude))po'); \
+      secs="$$(($$1 % 86400))"; \
+      yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
+      printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2"; \
+    else \
+      ts=$$(find . -type f $(if $(1),-not) -path './po/*' -printf '%T@\n' 2>/dev/null | sort -rn | head -n1 | cut -d. -f1); \
+      if [ -n "$$ts" ]; then \
+        secs="$$(($$ts % 86400))"; \
+        date="$$(date --utc --date="@$$ts" "+%y%m%d")"; \
+        printf '%s.%05d' "$$date" "$$secs"; \
+      else \
+        echo "unknown"; \
+      fi; \
+    fi \
+  )
+endef
+
+PKG_PO_VERSION?=$(if $(DUMP),x,$(strip $(call findrev)))
+PKG_SRC_VERSION?=$(if $(DUMP),x,$(strip $(call findrev,1)))
 
 PKG_GITBRANCH?=$(if $(DUMP),x,$(strip $(shell \
 	variant="LuCI"; \

commit e6f77d5d72c0e13ed3eb370419abab52c4920a32

build: luci.mk: gracefully handle missing or unversioned po subdirectories
Fixes: #3911
Fixes: 9d8e99f9b build: gracefully handle non-Git source trees
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

luci.mk
@@ -65,9 +65,13 @@ define findrev
   $(shell \
     if git log -1 >/dev/null 2>/dev/null; then \
       set -- $$(git log -1 --format="%ct %h" --abbrev=7 -- '$(if $(1),:(exclude))po'); \
-      secs="$$(($$1 % 86400))"; \
-      yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
-      printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2"; \
+      if [ -n "$$1" ]; then
+        secs="$$(($$1 % 86400))"; \
+        yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
+        printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2"; \
+      else \
+        echo "unknown"; \
+      fi; \
     else \
       ts=$$(find . -type f $(if $(1),-not) -path './po/*' -printf '%T@\n' 2>/dev/null | sort -rn | head -n1 | cut -d. -f1); \
       if [ -n "$$ts" ]; then \

modules/luci-base/htdocs/luci-static/resources/luci.js
@@ -2051,7 +2051,15 @@
 		 * methods are overwritten with `null`.
 		 */
 		addFooter: function() {
-			var footer = E([]);
+			var footer = E([]),
+			    vp = document.getElementById('view'),
+			    readonly = true;
+
+			vp.querySelectorAll('.cbi-map').forEach(function(map) {
+				var m = DOM.findClassInstance(map);
+				if (m && !m.readonly)
+					readonly = false;
+			});
 
 			var saveApplyBtn = this.handleSaveApply ? new L.ui.ComboButton('0', {
 				0: [ _('Save & Apply') ],
@@ -2061,7 +2069,8 @@
 					0: 'btn cbi-button cbi-button-apply important',
 					1: 'btn cbi-button cbi-button-negative important'
 				},
-				click: L.ui.createHandlerFn(this, 'handleSaveApply')
+				click: L.ui.createHandlerFn(this, 'handleSaveApply'),
+				disabled: readonly || null
 			}).render() : E([]);
 
 			if (this.handleSaveApply || this.handleSave || this.handleReset) {
@@ -2069,11 +2078,13 @@
 					saveApplyBtn, ' ',
 					this.handleSave ? E('button', {
 						'class': 'cbi-button cbi-button-save',
-						'click': L.ui.createHandlerFn(this, 'handleSave')
+						'click': L.ui.createHandlerFn(this, 'handleSave'),
+						'disabled': readonly || null
 					}, [ _('Save') ]) : '', ' ',
 					this.handleReset ? E('button', {
 						'class': 'cbi-button cbi-button-reset',
-						'click': L.ui.createHandlerFn(this, 'handleReset')
+						'click': L.ui.createHandlerFn(this, 'handleReset'),
+						'disabled': readonly || null
 					}, [ _('Reset') ]) : ''
 				]));
 			}
@@ -3060,6 +3071,35 @@
 							try { json = res.json() } catch(e) {}
 						cb(res.xhr, json, res.duration);
 					});
+		},
+
+		/**
+		 * Check whether the current session has been granted the given ACL
+		 * group permissions.
+		 *
+		 * @param {string} aclGroupName
+		 * The ACL group name to check.
+		 *
+		 * @return {boolean|null}
+		 * Returns `null` if the session does not have the specified grant,
+		 * returns `false` if the permissions are granted readonly or
+		 * `true` if they're granted read/write.
+		 */
+		hasAcl: function(aclGroupName) {
+			if (!this.isObject(this.env.accessgroups) ||
+			    !this.env.accessgroups.hasOwnProperty(aclGroupName) ||
+			    !Array.isArray(this.env.accessgroups[aclGroupName]))
+				return null;
+
+			for (var i = 0; i < this.env.accessgroups[aclGroupName].length; i++)
+				if (this.env.accessgroups[aclGroupName][i] == 'write')
+					return true;
+
+			for (var i = 0; i < this.env.accessgroups[aclGroupName].length; i++)
+				if (this.env.accessgroups[aclGroupName][i] == 'read')
+					return false;
+
+			return null;
 		},
 
 		/**