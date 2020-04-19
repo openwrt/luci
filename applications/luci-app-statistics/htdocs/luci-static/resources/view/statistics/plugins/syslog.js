8bc1d204ce5efc2866b802fa3ad8d3f376527424
Tobias Waldvogel <tobias.waldvogel@gmail.com>
2020-04-19 10:18:46 +0200

    luci_app_statistics: Add syslog plugin and activate by default in order to avoid spamming the syslog with collectd 'notice' entries
    Signed-off-by: Tobias Waldvogel <tobias.waldvogel@gmail.com>

---
+++ applications/luci-app-statistics/htdocs/luci-static/resources/view/statistics/plugins/syslog.js
@@ -0,0 +1,33 @@
+'use strict';
+'require baseclass';
+'require form';
+
+return baseclass.extend({
+	title: _('Syslog Plugin Configuration'),
+	description: _('The SysLog plugin receives log messages from the daemon and dispatches them to syslog.'),
+
+	addFormOptions: function(s) {
+		var o, ss;
+
+		o = s.option(form.Flag, 'enable', _('Enable this plugin'));
+		o.default = '0';
+
+		o = s.option(form.ListValue, 'LogLevel', _('Log level'), _('Sets the syslog log-level.'));
+		o.value('err');
+		o.value('warning');
+		o.value('notice');
+		o.value('info');
+		o.value('debug');
+		o.rmempty = false;
+
+		o = s.option(form.ListValue, 'NotifyLevel', _('Notify level'), _('Controls which notifications should be sent to syslog.'));
+		o.value('FAILURE');
+		o.value('WARNING');
+		o.value('OKAY');
+		o.rmempty = false;
+    },
+
+	configSummary: function(section) {
+		return _('Syslog enabled');
+	}
+});
