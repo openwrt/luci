openwrt/luci

commit 81effc111227bc65cb974829f17a45515fbe9807

luci-base: ui.js: assume select widget by default for ListValue widget
Fixes: #3973
Fixes: 2fb55e1ab ("luci-base: ui.js: fixes for radio widget type of select element")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/ui.js
@@ -712,7 +712,7 @@ var UISelect = UIElement.extend(/** @lends LuCI.ui.Select.prototype */ {
 		else if (Array.isArray(this.options.sort))
 			keys = this.options.sort;
 
-		if (this.options.widget == 'select') {
+		if (this.options.widget != 'radio' && this.options.widget != 'checkbox') {
 			frameEl.appendChild(E('select', {
 				'id': this.options.id ? 'widget.' + this.options.id : null,
 				'name': this.options.name,

commit 4972bc2ab1900377ae9e6f50301c2f80bb60677a

luci-base: ui.js: fix input value reading for select widgets
Fixes: #3989
Fixes: 81effc111 ("luci-base: ui.js: assume select widget by default for ListValue widget")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>
(cherry picked from commit e3ef463ccdd7d32eef1717203bd872272c57793d)

modules/luci-base/htdocs/luci-static/resources/ui.js
@@ -773,7 +773,7 @@ var UISelect = UIElement.extend(/** @lends LuCI.ui.Select.prototype */ {
 	bind: function(frameEl) {
 		this.node = frameEl;
 
-		if (this.options.widget == 'select') {
+		if (this.options.widget != 'radio' && this.options.widget != 'checkbox') {
 			this.setUpdateEvents(frameEl.firstChild, 'change', 'click', 'blur');
 			this.setChangeEvents(frameEl.firstChild, 'change');
 		}
@@ -792,7 +792,7 @@ var UISelect = UIElement.extend(/** @lends LuCI.ui.Select.prototype */ {
 
 	/** @override */
 	getValue: function() {
-		if (this.options.widget == 'select')
+		if (this.options.widget != 'radio' && this.options.widget != 'checkbox')
 			return this.node.firstChild.value;
 
 		var radioEls = this.node.querySelectorAll('input[type="radio"]');
@@ -805,7 +805,7 @@ var UISelect = UIElement.extend(/** @lends LuCI.ui.Select.prototype */ {
 
 	/** @override */
 	setValue: function(value) {
-		if (this.options.widget == 'select') {
+		if (this.options.widget != 'radio' && this.options.widget != 'checkbox') {
 			if (value == null)
 				value = '';
 