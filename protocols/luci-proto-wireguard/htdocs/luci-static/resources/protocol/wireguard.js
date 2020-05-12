dae15b8cf476fe761b2b6a7457c4d8e3e9950569
Florian Eckert <fe@dev.tdt.de>
2020-05-12 14:45:47 +0200

    luci-proto-wireguard: verify last base64 string symbol is an = sign
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- protocols/luci-proto-wireguard/htdocs/luci-static/resources/protocol/wireguard.js
+++ protocols/luci-proto-wireguard/htdocs/luci-static/resources/protocol/wireguard.js
@@ -10,6 +10,9 @@ function validateBase64(section_id, value) {
 	if (value.length != 44 || !value.match(/^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/))
 		return _('Invalid Base64 key string');
 
+	if (value[43] != "=" )
+		return _('Invalid Base64 key string');
+
 	return true;
 }
 
