b73ca28c427515b32516fbeafc2e6450d04737bd
Jo-Philipp Wich <jo@mein.io>
2020-05-19 12:27:44 +0200

    luci-app-vpn-policy-routing: don't clobber permissions of VPN menu
    Ref: https://github.com/openwrt/luci/issues/4066
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>
    (cherry picked from commit 4dcab51ca522407cf63a51883bc6505cf24fbd64)

--- applications/luci-app-vpn-policy-routing/luasrc/controller/vpn-policy-routing.lua
+++ applications/luci-app-vpn-policy-routing/luasrc/controller/vpn-policy-routing.lua
@@ -3,9 +3,8 @@ function index()
 	if nixio.fs.access("/etc/config/vpn-policy-routing") then
 		local e = entry({"admin", "vpn"}, firstchild(), _("VPN"), 60)
 		e.dependent = false
-		e.acl_depends = { "luci-app-vpn-policy-routing" }
 
-		entry({"admin", "vpn", "vpn-policy-routing"}, cbi("vpn-policy-routing"), _("VPN Policy Routing"))
+		entry({"admin", "vpn", "vpn-policy-routing"}, cbi("vpn-policy-routing"), _("VPN Policy Routing")).acl_depends = { "luci-app-vpn-policy-routing" }
 		entry({"admin", "vpn", "vpn-policy-routing", "action"}, call("vpn_policy_routing_action"), nil).leaf = true
 	end
 end
