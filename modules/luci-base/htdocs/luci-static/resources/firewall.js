openwrt/luci

commit 4052436d825e1e78d1734ddd4d06d2291fbc5684

luci-base: firewall.js: don't treat zone name as network fallback
Drop obsolete extra logic which treats the zone name as covered network
name in case the network list is unset. This behaviour applied to the
pre-fw3 uci firewall, but is not supported since fw3 anymore.

Ref: https://forum.openwrt.org/t/luci-zone-creation-bug/55921
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/firewall.js
@@ -362,13 +362,13 @@ Zone = AbstractFirewallItem.extend({
 		if (newNetworks.length > 0)
 			this.set('network', newNetworks.join(' '));
 		else
-			this.set('network', ' ');
+			this.set('network', null);
 
 		return (newNetworks.length < oldNetworks.length);
 	},
 
 	getNetworks: function() {
-		return L.toArray(this.get('network') || this.get('name'));
+		return L.toArray(this.get('network'));
 	},
 
 	clearNetworks: function() {

commit f6f4005d0baf70f04752747822cda9f1248c47ca

luci-base: firewall.js: remove further zone network/name fallback logic
Fixes: #4093
Fixes: 4052436d8 ("luci-base: firewall.js: don't treat zone name as network fallback")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/firewall.js
@@ -140,7 +140,7 @@ Firewall = L.Class.extend({
 			var sections = uci.sections('firewall', 'zone');
 
 			for (var i = 0; i < sections.length; i++)
-				if (L.toArray(sections[i].network || sections[i].name).indexOf(network) != -1)
+				if (L.toArray(sections[i].network).indexOf(network) != -1)
 					return new Zone(sections[i]['.name']);
 
 			return null;
@@ -201,9 +201,6 @@ Firewall = L.Class.extend({
 			for (var i = 0; i < sections.length; i++) {
 				if (sections[i].name != oldName)
 					continue;
-
-				if (L.toArray(sections[i].network).length == 0)
-					uci.set('firewall', sections[i]['.name'], 'network', oldName);
 
 				uci.set('firewall', sections[i]['.name'], 'name', newName);
 				found = true;