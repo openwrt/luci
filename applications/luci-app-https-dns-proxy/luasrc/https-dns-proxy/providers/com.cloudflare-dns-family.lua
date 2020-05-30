d373bd340a20aa25ba02ad1619820367d3e840c8
Cason Adams <casonadams@protonmail.com>
2020-05-30 00:01:26 -0600

    luci-app: https-dns-proxy adding cloudflare family and security
    Signed-off-by: Cason Adams <casonadams@protonmail.com>

---
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/com.cloudflare-dns-family.lua
@@ -0,0 +1,8 @@
+return {
+	name = "Cloudflare-Family",
+	label = _("Cloudflare (Family Protection)"),
+	resolver_url = "https://family.cloudflare-dns.com/dns-query/",
+	bootstrap_dns = "1.1.1.3,1.0.0.3"
+	help_link = "https://developers.cloudflare.com/1.1.1.1/1.1.1.1-for-families/setup-instructions/dns-over-https/",
+	help_link_text = "cloudflare.com"
+}
