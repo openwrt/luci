ba801aa40eb5862b041ff164e74b4f6e3d2bb919
Joe Nahmias <joe@nahmias.net>
2020-06-16 10:00:16 -0400

    luci-app-statistics: add ability to configure collectd-mod-sqm
    This patch makes it possible to enable and configure the SQM input
    plugin for collectd from the Luci front-end. It also handles the
    generation of the appropriate collectd configuration for the SQM
    plugin using the generic Exec plugin.
    
    Signed-off-by: Joe Nahmias <joe@nahmias.net>

---
+++ applications/luci-app-statistics/luasrc/model/cbi/luci_statistics/sqm.lua
@@ -0,0 +1,25 @@
+-- /usr/lib/lua/luci/model/cbi/luci_statistics/sqm.lua
+-- Copyright 2011 Jo-Philipp Wich <jow@openwrt.org>
+-- Copyright 2020 Joseph Nahmias <joe@nahmias.net>
+-- based on /usr/lib/lua/luci/model/cbi/luci_statistics/iwinfo.lua
+-- Licensed to the public under the Apache License 2.0.
+
+local m, s, o
+
+m = Map("luci_statistics",
+	translate("SQM Plugin Configuration"),
+	translate("The sqm plugin collects statistics about smart queue management QoS."))
+
+s = m:section(NamedSection, "collectd_sqm", "luci_statistics")
+
+o = s:option(Flag, "enable", translate("Enable this plugin"))
+o.default = 1
+
+o = s:option(DynamicList, "Interfaces", translate("Monitor interfaces"),
+	translate("Leave unselected to automatically determine interfaces to monitor."))
+o.template = "cbi/network_ifacelist"
+o.widget   = "checkbox"
+o.nocreate = true
+o:depends("enable", 1)
+
+return m
