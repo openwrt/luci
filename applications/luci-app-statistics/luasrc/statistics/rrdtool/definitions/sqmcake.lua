cf0d3580a313dba29cc2eb9e2a09db5de834af87
Joe Nahmias <joe@nahmias.net>
2020-06-16 11:58:54 -0400

    luci-app-statistics: add display of SQM and SQM-Cake graphs
    Now that we can configure the SQM collectd plugin to run, backport SQM &
    SQM-Cake graph creation and display code from [master 9c4f345],
    translating the javascript code to lua.
    
    Signed-off-by: Joe Nahmias <joe@nahmias.net>

---
+++ applications/luci-app-statistics/luasrc/statistics/rrdtool/definitions/sqmcake.lua
@@ -0,0 +1,80 @@
+-- /usr/lib/lua/luci/statistics/rrdtool/definitions/sqmcake.lua
+-- Licensed to the public under the Apache License 2.0.
+
+module("luci.statistics.rrdtool.definitions.sqmcake", package.seeall)
+
+function item()
+        return luci.i18n.translate("SQM-Cake")
+end
+
+function rrdargs( graph, plugin, plugin_instance, dtype )
+
+	local tindrops = {
+		per_instance = true,
+		title = "%H = CAKE %pi %di Drops/s & Backlog",
+		vlabel = "Bytes & Drops/s",
+		rrdopts = { "--logarithmic" },
+		number_format = "%5.0lf",
+		data = {
+			types = { "qdisct_backlog", "qdisct_drops" },
+			sources = { qdisct_drops = { "ack", "drops", "ecn" } },
+			options = {
+				qdisct_backlog = { title = "Backlog:", overlay = true, color = "0000ff" },
+				qdisct_drops__ack = { title = "Ack:", overlay = true, noarea = true, color = "ff00ff" },
+				qdisct_drops__drops = { title = "Drops:", overlay = true, noarea = true, color = "00ffff" },
+				qdisct_drops__ecn = { title = "Ecn:", overlay = true, noarea = true, color = "00ff00" }
+			}
+		}
+	}
+
+	local tinlatency = {
+		per_instance = true,
+		title = "%H = CAKE %pi %di Latency",
+		vlabel = "ms",
+		number_format = "%4.3lf",
+		data = {
+			types = { "qdisct_latencyus" },
+			sources = { qdisct_latencyus = { "tg", "pk", "av", "sp" } },
+			options = {
+				qdisct_latencyus__tg = { title = "Target:", overlay = true, noarea = true, color = "000000", transform_rpn = "1000,/" },
+				qdisct_latencyus__pk = { title = "Peak:", overlay = true, noarea = true, color = "ff0000", transform_rpn = "1000,/" },
+				qdisct_latencyus__av = { title = "Avg:", overlay = true, noarea = true, color = "00ff00", transform_rpn = "1000,/" },
+				qdisct_latencyus__sp = { title = "Sparse:", overlay = true, noarea = true, color = "0000ff", transform_rpn = "1000,/" }
+			}
+		}
+	};
+
+	local tinflows = {
+		per_instance = true,
+		title = "%H = CAKE %pi %di Flow Counts",
+		vlabel = "Flows",
+		number_format = "%4.0lf",
+		data = {
+			types = { "qdisct_flows" },
+			sources = { qdisct_flows = { "sp", "bu", "un" } },
+			options = {
+				qdisct_flows__sp = { title = "Sparse:", overlay = true, noarea = true, color = "00ff00" },
+				qdisct_flows__bu = { title = "Bulk:", overlay = true, noarea = true, color = "0000ff" },
+				qdisct_flows__un = { title = "Unresponsive:", overlay = true, noarea = true, color = "ff0000" }
+			}
+		}
+	};
+
+	local tinbytes = {
+		per_instance = true,
+		title = "%H = CAKE %pi %di Traffic",
+		vlabel = "Kb/s",
+		number_format = "%5.0lf",
+		rrdopts = { "--logarithmic" },
+		data = {
+			types = { "qdisct_bytes", "qdisct_thres" },
+			options = {
+				qdisct_bytes = { title = "Kb/s:", noarea = false, color = "0000ff", transform_rpn = "125,/" },
+				qdisct_thres = { title = "Thres:", overlay = true, noarea = true, color = "000000", transform_rpn = "125,/" }
+			}
+		}
+	};
+
+	return { tinbytes, tinlatency, tindrops, tinflows };
+
+end
