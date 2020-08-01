4dd9650489357e5c5d6f037a92b960b44e8fe61c
Sander Vanheule <sander@svanheule.net>
2020-08-01 20:18:34 +0200

    luci-app-dawn: shorten controller entry titles
    Menu entries in LuCI don't usually contain a verb, but just the
    description of that page's contents. Drop 'View' in the menu title for
    the Hearing Map and Network Overview.
    
    Signed-off-by: Sander Vanheule <sander@svanheule.net>

--- applications/luci-app-dawn/luasrc/controller/dawn.lua
+++ applications/luci-app-dawn/luasrc/controller/dawn.lua
@@ -6,6 +6,6 @@ function index()
     e.acl_depends = { "luci-app-dawn" }
 
     entry({ "admin", "dawn", "configure_daemon" }, cbi("dawn/dawn_config"), "Configure DAWN", 1)
-    entry({ "admin", "dawn", "view_network" }, cbi("dawn/dawn_network"), "View Network Overview", 2)
-    entry({ "admin", "dawn", "view_hearing_map" }, cbi("dawn/dawn_hearing_map"), "View Hearing Map", 3)
+    entry({ "admin", "dawn", "view_network" }, cbi("dawn/dawn_network"), "Network Overview", 2)
+    entry({ "admin", "dawn", "view_hearing_map" }, cbi("dawn/dawn_hearing_map"), "Hearing Map", 3)
 end
