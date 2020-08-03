400985d020bb7b44f472637e7cd4eea7f143045c
Florian Eckert <fe@dev.tdt.de>
2020-08-03 12:32:11 +0200

    luci-app-dockerman: set overview to first menu entry
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- applications/luci-app-dockerman/luasrc/controller/dockerman.lua
+++ applications/luci-app-dockerman/luasrc/controller/dockerman.lua
@@ -12,7 +12,7 @@ function index()
 	e.dependent = false
 	e.acl_depends = { "luci-app-dockerman" }
 
-	entry({"admin", "docker", "overview"},cbi("dockerman/overview"),_("Overview"),0).leaf=true
+	entry({"admin", "docker", "overview"},cbi("dockerman/overview"),_("Overview"), 1).leaf=true
 
 	local remote = luci.model.uci.cursor():get_bool("dockerd", "globals", "remote_endpoint")
 	if remote then
@@ -32,11 +32,11 @@ function index()
 		return
 	end
 
-	entry({"admin", "docker", "containers"}, form("dockerman/containers"), _("Containers"),1).leaf=true
-	entry({"admin", "docker", "images"}, form("dockerman/images"), _("Images"),2).leaf=true
-	entry({"admin", "docker", "networks"}, form("dockerman/networks"), _("Networks"),3).leaf=true
-	entry({"admin", "docker", "volumes"}, form("dockerman/volumes"), _("Volumes"),4).leaf=true
-	entry({"admin", "docker", "events"}, call("action_events"), _("Events"),5)
+	entry({"admin", "docker", "containers"}, form("dockerman/containers"), _("Containers"), 2).leaf=true
+	entry({"admin", "docker", "images"}, form("dockerman/images"), _("Images"), 3).leaf=true
+	entry({"admin", "docker", "networks"}, form("dockerman/networks"), _("Networks"), 4).leaf=true
+	entry({"admin", "docker", "volumes"}, form("dockerman/volumes"), _("Volumes"), 5).leaf=true
+	entry({"admin", "docker", "events"}, call("action_events"), _("Events"), 6)
 
 	entry({"admin", "docker", "newcontainer"}, form("dockerman/newcontainer")).leaf=true
 	entry({"admin", "docker", "newnetwork"}, form("dockerman/newnetwork")).leaf=true
