aabded28c56790dccc2e562e6f5e92f3deb414ec
Florian Eckert <fe@dev.tdt.de>
2020-08-04 15:45:42 +0200

    luci-app-mwan3: add missing familiy selection for rules
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- applications/luci-app-mwan3/luasrc/model/cbi/mwan/ruleconfig.lua
+++ applications/luci-app-mwan3/luasrc/model/cbi/mwan/ruleconfig.lua
@@ -18,6 +18,12 @@ s = m:section(NamedSection, arg[1], "rule", "")
 s.addremove = false
 s.dynamic = false
 
+o = s:option(ListValue, "family", translate("Internet Protocol"))
+o.default = ""
+o:value("", translate("IPv4 and IPv6"))
+o:value("ipv4", translate("only IPv4"))
+o:value("ipv6", translate("only IPv6"))
+
 o = s:option(Value, "src_ip", translate("Source address"),
 	translate("Supports CIDR notation (eg \"192.168.100.0/24\") without quotes"))
 o.datatype = ipaddr
