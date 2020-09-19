8401f59e036dda2b7927dfd7ab2b20f7435861ef
Hannu Nyman <hannu.nyman@iki.fi>
2020-09-19 08:46:16 +0300

    luci-base: tweak busybox top regex to accept 'm' in large VSZ
    Tweak the lua regex string for process list, so that processes
    with extremely large memory sizes (over 100 MB) are matched.
    For those large processes busybox top shows a MB value like '234m'
    instead of a normal kB value like 234000.
    
    Fixes #4425
    
    Reference to https://github.com/openwrt/luci/issues/4425#issuecomment-694073479
    
    Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>
    (cherry picked from commit 4f2ecd88fd02c1279041f45ccd758184b153c875)

--- modules/luci-base/luasrc/sys.lua
+++ modules/luci-base/luasrc/sys.lua
@@ -387,7 +387,7 @@ function process.list()
 
 	for line in ps do
 		local pid, ppid, user, stat, vsz, mem, cpu, cmd = line:match(
-			"^ *(%d+) +(%d+) +(%S.-%S) +([RSDZTW][<NW ][<N ]) +(%d+) +(%d+%%) +(%d+%%) +(.+)"
+			"^ *(%d+) +(%d+) +(%S.-%S) +([RSDZTW][<NW ][<N ]) +(%d+m?) +(%d+%%) +(%d+%%) +(.+)"
 		)
 
 		local idx = tonumber(pid)
