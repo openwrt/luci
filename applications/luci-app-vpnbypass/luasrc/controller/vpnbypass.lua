053e3952f02319ae5e0d8668cf67ce8c4a8c7f6d
Stan Grishin <stangri@melmac.net>
2020-09-21 18:33:51 +0000

    luci-app-vpnbypass: ACL update
    Signed-off-by: Stan Grishin <stangri@melmac.net>

--- applications/luci-app-vpnbypass/luasrc/controller/vpnbypass.lua
+++ applications/luci-app-vpnbypass/luasrc/controller/vpnbypass.lua
@@ -3,26 +3,32 @@ function index()
 	if nixio.fs.access("/etc/config/vpnbypass") then
 		local e = entry({"admin", "vpn"}, firstchild(), _("VPN"), 60)
 		e.dependent = false
-
-		entry({"admin", "vpn", "vpnbypass"}, cbi("vpnbypass"), _("VPN Bypass")).acl_depends = { "luci-app-vpnbypass" }
+		e.acl_depends = { "luci-app-vpnbypass" }
+		entry({"admin", "vpn", "vpnbypass"}, cbi("vpnbypass"), _("VPN Bypass"))
 		entry({"admin", "vpn", "vpnbypass", "action"}, call("vpnbypass_action"), nil).leaf = true
 	end
 end
 
 function vpnbypass_action(name)
 	local packageName = "vpnbypass"
+	local http = require "luci.http"
+	local sys = require "luci.sys"
+	local uci = require "luci.model.uci".cursor()
+	local util = require "luci.util"
 	if name == "start" then
-		luci.sys.init.start(packageName)
+		sys.init.start(packageName)
 	elseif name == "action" then
-		luci.util.exec("/etc/init.d/" .. packageName .. " restart >/dev/null 2>&1")
-		luci.util.exec("/etc/init.d/dnsmasq restart >/dev/null 2>&1")
+		util.exec("/etc/init.d/" .. packageName .. " restart >/dev/null 2>&1")
+		util.exec("/etc/init.d/dnsmasq restart >/dev/null 2>&1")
 	elseif name == "stop" then
-		luci.sys.init.stop(packageName)
+		sys.init.stop(packageName)
 	elseif name == "enable" then
-		luci.util.exec("uci set " .. packageName .. ".config.enabled=1; uci commit " .. packageName)
+		uci:set(packageName, "config", "enabled", "1")
+		uci:commit(packageName)
 	elseif name == "disable" then
-		luci.util.exec("uci set " .. packageName .. ".config.enabled=0; uci commit " .. packageName)
+		uci:set(packageName, "config", "enabled", "0")
+		uci:commit(packageName)
 	end
-	luci.http.prepare_content("text/plain")
-	luci.http.write("0")
+	http.prepare_content("text/plain")
+	http.write("0")
 end
