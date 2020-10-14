678c0b2d5ba77092ca4c422b6a3c3e3eefe86b01
Florian Eckert <fe@dev.tdt.de>
2020-10-14 10:07:52 +0200

    luci-app-mwan3: check rule name length on create
    fixes #13499
    
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- applications/luci-app-mwan3/luasrc/model/cbi/mwan/rule.lua
+++ applications/luci-app-mwan3/luasrc/model/cbi/mwan/rule.lua
@@ -61,9 +61,13 @@ mwan_rule.sortable = true
 mwan_rule.template = "cbi/tblsection"
 mwan_rule.extedit = dsp.build_url("admin", "network", "mwan", "rule", "%s")
 function mwan_rule.create(self, section)
-	TypedSection.create(self, section)
-	m.uci:save("mwan3")
-	luci.http.redirect(dsp.build_url("admin", "network", "mwan", "rule", section))
+	if #section > 15 then
+		self.invalid_cts = true
+	else
+		TypedSection.create(self, section)
+		m.uci:save("mwan3")
+		luci.http.redirect(dsp.build_url("admin", "network", "mwan", "rule", section))
+	end
 end
 
 src_ip = mwan_rule:option(DummyValue, "src_ip", translate("Source address"))
