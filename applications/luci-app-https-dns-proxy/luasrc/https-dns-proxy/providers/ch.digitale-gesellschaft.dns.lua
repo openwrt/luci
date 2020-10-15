5978c67cf762641a04bb02161a3896797bc508aa
n00bundercover <37903933+n00bundercover@users.noreply.github.com>
2020-10-16 01:59:42 +0200

    Create ch.digitale-gesellschaft.dns.lua

---
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/ch.digitale-gesellschaft.dns.lua
@@ -0,0 +1,8 @@
+return {
+	name = "Digitale Gesellschaft",
+	label = _("Digitale Gesellschaft"),
+	resolver_url = "https://dns.digitale-gesellschaft.ch/dns-query",
+	bootstrap_dns = "185.95.218.42,185.95.218.43,2a05:fc84::42,2a05:fc84::43",
+	help_link = "https://github.com/DigitaleGesellschaft/DNS-Resolver#technical-information--configuration-how-tos",
+	help_link_text = "digitale-gesellschaft.ch"
+}
