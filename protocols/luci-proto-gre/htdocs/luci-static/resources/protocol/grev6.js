openwrt/luci

commit 2b7fd1292f3c5055e9f5c431e411c53d3b84adf5

luci-proto-gre: improvement of LuCI interface
Better handling of Type of Service (IPv4), Traffic Class (IPv6) values
Optional value Local endpoint address is detected and pre-filled in the interface

Signed-off-by: Jan Bětík <jan.betik@svine.su>

protocols/luci-proto-gre/htdocs/luci-static/resources/protocol/grev6.js
@@ -46,6 +46,13 @@ return network.registerProtocol('grev6', {
 		o = s.taboption('general', form.Value, 'ip6addr', _("Local IPv6 address"), _("The local IPv6 address over which the tunnel is created (optional)."));
 		o.optional = true;
 		o.datatype = 'ip6addr("nomask")';
+		o.load = function(section_id) {
+			return network.getWAN6Networks().then(L.bind(function(nets) {
+				if (Array.isArray(nets) && nets.length)
+					this.placeholder = nets[0].getIP6Addr();
+				return form.Value.prototype.load.apply(this, [section_id]);
+			}, this));
+		};
 
 		o = s.taboption('general', widgets.NetworkSelect, 'weakif', _("Source interface"), _("Logical network from which to select the local endpoint if local IPv6 address is empty and no WAN IPv6 is available (optional)."));
 		o.exclude = s.section;
@@ -69,16 +76,19 @@ return network.registerProtocol('grev6', {
 		o.placeholder = 64;
 		o.datatype = 'min(1)';
 
-		o = s.taboption('advanced', form.Value, 'tos', _('Traffic Class'), _("Specify a Traffic Class. Can be either <code>inherit</code> (the outer header inherits the value of the inner header) or an hexadecimal value starting with <code>0x</code> (optional)."));
+		o = s.taboption('advanced', form.Value, 'tos', _("Traffic Class"), _("Specify a Traffic Class. Can be <code>inherit</code> (the outer header inherits the value of the inner header) or an hexadecimal value <code>00..FF</code> (optional)."));
 		o.optional = true;
 		o.validate = function(section_id, value) {
-			if (value.length > 0 && !value.match(/^0x[a-fA-F0-9]{1,2}$/) && !value.match(/^inherit$/i))
-				return _('Invalid value');
+			if (value.length > 0 && !value.match(/^[a-f0-9]{1,2}$/i) && !value.match(/^inherit$/i)))
+				return _("Invalid Traffic Class value, expected 00..FF or inherit");
 
 			return true;
 		};
 
 		o = s.taboption('advanced', form.Flag, 'nohostroute', _("No host route"), _("Do not create host route to peer (optional)."));
+		o.optional = true;
+
+		o = s.taboption('advanced', form.Flag, 'multicast', _("Multicast"), _("Enable support for multicast traffic (optional)."));
 		o.optional = true;
 
 		o = s.taboption('advanced', form.Value, 'ikey', _("Incoming key"), _("Key for incoming packets (optional)."));

protocols/luci-proto-gre/htdocs/luci-static/resources/protocol/gre.js
@@ -46,6 +46,13 @@ return network.registerProtocol('gre', {
 		o = s.taboption('general', form.Value, 'ipaddr', _("Local IPv4 address"), _("The local IPv4 address over which the tunnel is created (optional)."));
 		o.optional = true;
 		o.datatype = 'ip4addr("nomask")';
+		o.load = function(section_id) {
+			return network.getWANNetworks().then(L.bind(function(nets) {
+				if (nets.length)
+					this.placeholder = nets[0].getIPAddr();
+				return form.Value.prototype.load.apply(this, [section_id]);
+			}, this));
+		};
 
 		// -- advanced ---------------------------------------------------------------------
 
@@ -64,11 +71,11 @@ return network.registerProtocol('gre', {
 		o.placeholder = 64;
 		o.datatype = 'min(1)';
 
-		o = s.taboption('advanced', form.Value, 'tos', _('Override TOS'), _("Specify a TOS (Type of Service). Can be either <code>inherit</code> (the outer      header inherits the value of the inner header) or an hexadecimal value starting with <code>0x</code> (optional)."));
+		o = s.taboption('advanced', form.Value, 'tos', _("Override TOS"), _("Specify a TOS (Type of Service). Can be <code>inherit</code> (the outer header inherits the value of the inner header), or an hexadecimal value <code>00..FF</code> (optional)."));
 		o.optional = true;
 		o.validate = function(section_id, value) {
-			if (value.length > 0 && !value.match(/^0x[a-fA-F0-9]{1,2}$/) && !value.match(/^inherit$/i))
-				return _('Invalid value');
+			if (value.length > 0 && !value.match(/^[a-f0-9]{1,2}$/i) && !value.match(/^inherit$/i))
+				return _("Invalid TOS value, expected 00..FF or inherit");
 
 			return true;
 		};
@@ -77,6 +84,9 @@ return network.registerProtocol('gre', {
 		o.default = o.enabled;
 
 		o = s.taboption('advanced', form.Flag, 'nohostroute', _("No host route"), _("Do not create host route to peer (optional)."));
+		o.optional = true;
+
+		o = s.taboption('advanced', form.Flag, 'multicast', _("Multicast"), _("Enable support for multicast traffic (optional)."));
 		o.optional = true;
 
 		o = s.taboption('advanced', form.Value, 'ikey', _("Incoming key"), _("Key for incoming packets (optional)."));

protocols/luci-proto-gre/htdocs/luci-static/resources/protocol/gretap.js
@@ -46,6 +46,13 @@ return network.registerProtocol('gretap', {
 		o = s.taboption('general', form.Value, 'ipaddr', _("Local IPv4 address"), _("The local IPv4 address over which the tunnel is created (optional)."));
 		o.optional = true;
 		o.datatype = 'ip4addr("nomask")';
+		o.load = function(section_id) {
+			return network.getWANNetworks().then(L.bind(function(nets) {
+				if (nets.length)
+					this.placeholder = nets[0].getIPAddr();
+				return form.Value.prototype.load.apply(this, [section_id]);
+			}, this));
+		};
 
 		o = s.taboption('general', widgets.NetworkSelect, 'network', _("Network interface"), _("Logical network to which the tunnel will be added (bridged) (optional)."));
 		o.exclude = s.section;
@@ -69,11 +76,11 @@ return network.registerProtocol('gretap', {
 		o.placeholder = 64;
 		o.datatype = 'min(1)';
 
-		o = s.taboption('advanced', form.Value, 'tos', _('Override TOS'), _("Specify a TOS (Type of Service). Can be either <code>inherit</code> (the outer      header inherits the value of the inner header) or an hexadecimal value starting with <code>0x</code> (optional)."));
+		o = s.taboption('advanced', form.Value, 'tos', _("Override TOS"), _("Specify a TOS (Type of Service). Can be <code>inherit</code> (the outer header inherits the value of the inner header) or an hexadecimal value <code>00..FF</code> (optional)."));
 		o.optional = true;
 		o.validate = function(section_id, value) {
-			if (value.length > 0 && !value.match(/^0x[a-fA-F0-9]{1,2}$/) && !value.match(/^inherit$/i))
-				return _('Invalid value');
+			if (value.length > 0 && !value.match(/^[a-f0-9]{1,2}$/i) && !value.match(/^inherit$/i))
+				return _("Invalid TOS value, expected 00..FF or inherit");
 
 			return true;
 		};
@@ -82,6 +89,9 @@ return network.registerProtocol('gretap', {
 		o.default = o.enabled;
 
 		o = s.taboption('advanced', form.Flag, 'nohostroute', _("No host route"), _("Do not create host route to peer (optional)."));
+		o.optional = true;
+
+		o = s.taboption('advanced', form.Flag, 'multicast', _("Multicast"), _("Enable support for multicast traffic (optional)."));
 		o.optional = true;
 
 		o = s.taboption('advanced', form.Value, 'ikey', _("Incoming key"), _("Key for incoming packets (optional)."));

protocols/luci-proto-gre/htdocs/luci-static/resources/protocol/grev6tap.js
@@ -42,6 +42,13 @@ return network.registerProtocol('grev6tap', {
 		o = s.taboption('general', form.Value, 'peer6addr', _("Remote IPv6 address or FQDN"), _("The IPv6 address or the fully-qualified domain name of the remote tunnel end."));
 		o.optional = false;
 		o.datatype = 'or(hostname,ip6addr("nomask"))';
+		o.load = function(section_id) {
+			return network.getWAN6Networks().then(L.bind(function(nets) {
+				if (Array.isArray(nets) && nets.length)
+					this.placeholder = nets[0].getIP6Addr();
+				return form.Value.prototype.load.apply(this, [section_id]);
+			}, this));
+		};
 
 		o = s.taboption('general', form.Value, 'ip6addr', _("Local IPv6 address"), _("The local IPv6 address over which the tunnel is created (optional)."));
 		o.optional = true;
@@ -74,16 +81,19 @@ return network.registerProtocol('grev6tap', {
 		o.placeholder = 64;
 		o.datatype = 'min(1)';
 
-		o = s.taboption('advanced', form.Value, 'tos', _('Traffic Class'), _("Specify a Traffic Class. Can be either <code>inherit</code> (the outer header inherits the value of the inner header) or an hexadecimal value starting with <code>0x</code> (optional)."));
+		o = s.taboption('advanced', form.Value, 'tos', _("Traffic Class"), _("Specify a Traffic Class. Can be <code>inherit</code> (the outer header inherits the value of the inner header) or an hexadecimal value <code>00..FF</code> (optional)."));
 		o.optional = true;
 		o.validate = function(section_id, value) {
-			if (value.length > 0 && !value.match(/^0x[a-fA-F0-9]{1,2}$/) && !value.match(/^inherit$/i))
-				return _('Invalid value');
+			if (value.length > 0 && !value.match(/^[a-f0-9]{1,2}$/i) && !value.match(/^inherit$/i))
+				return _("Invalid Traffic Class value, expected 00..FF or inherit");
 
 			return true;
 		};
 
 		o = s.taboption('advanced', form.Flag, 'nohostroute', _("No host route"), _("Do not create host route to peer (optional)."));
+		o.optional = true;
+
+		o = s.taboption('advanced', form.Flag, 'multicast', _("Multicast"), _("Enable support for multicast traffic (optional)."));
 		o.optional = true;
 
 		o = s.taboption('advanced', form.Value, 'ikey', _("Incoming key"), _("Key for incoming packets (optional)."));

commit 37f97b7859c8a601024ae6570e9d481c50ee8897

luci-proto-gre: remove extra parenthesis
fix the following error:
SyntaxError
Unexpected token ')'
  in http://192.168.122.131/luci-static/resources/protocol/grev6.js:?
  at http://192.168.122.131/luci-static/resources/luci.js:22
  at async Promise.all (index 4)
  at async Promise.all (index 5)

Fixes: 2b7fd1292 ("luci-proto-gre: improvement of LuCI interface")
Signed-off-by: Chuanhong Guo <gch981213@gmail.com>

protocols/luci-proto-gre/htdocs/luci-static/resources/protocol/grev6.js
@@ -79,7 +79,7 @@ return network.registerProtocol('grev6', {
 		o = s.taboption('advanced', form.Value, 'tos', _("Traffic Class"), _("Specify a Traffic Class. Can be <code>inherit</code> (the outer header inherits the value of the inner header) or an hexadecimal value <code>00..FF</code> (optional)."));
 		o.optional = true;
 		o.validate = function(section_id, value) {
-			if (value.length > 0 && !value.match(/^[a-f0-9]{1,2}$/i) && !value.match(/^inherit$/i)))
+			if (value.length > 0 && !value.match(/^[a-f0-9]{1,2}$/i) && !value.match(/^inherit$/i))
 				return _("Invalid Traffic Class value, expected 00..FF or inherit");
 
 			return true;