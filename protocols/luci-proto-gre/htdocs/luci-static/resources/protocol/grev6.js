0f3f464adc9e24646638aa5d71edb995a352dec5
Sergio E. Nemirowski <sergio@outerface.net>
2020-11-22 18:29:24 +0300

    optinal -> optional typo fix
    Signed-off-by: Sergio E. Nemirowski <sergio@outerface.net>

--- protocols/luci-proto-gre/htdocs/luci-static/resources/protocol/grev6.js
+++ protocols/luci-proto-gre/htdocs/luci-static/resources/protocol/grev6.js
@@ -95,7 +95,7 @@ return network.registerProtocol('grev6', {
 		o.optional = true;
 		o.datatype = 'integer';
 
-		o = s.taboption('advanced', form.Value, 'okey', _("Outgoing key"), _("Key for outgoing packets (optinal)."));
+		o = s.taboption('advanced', form.Value, 'okey', _("Outgoing key"), _("Key for outgoing packets (optional)."));
 		o.optional = true;
 		o.datatype = 'integer';
 
--- protocols/luci-proto-gre/htdocs/luci-static/resources/protocol/grev6tap.js
+++ protocols/luci-proto-gre/htdocs/luci-static/resources/protocol/grev6tap.js
@@ -100,7 +100,7 @@ return network.registerProtocol('grev6tap', {
 		o.optional = true;
 		o.datatype = 'integer';
 
-		o = s.taboption('advanced', form.Value, 'okey', _("Outgoing key"), _("Key for outgoing packets (optinal)."));
+		o = s.taboption('advanced', form.Value, 'okey', _("Outgoing key"), _("Key for outgoing packets (optional)."));
 		o.optional = true;
 		o.datatype = 'integer';
 
