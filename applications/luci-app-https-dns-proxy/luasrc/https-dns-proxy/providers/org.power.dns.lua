67aecfcb9e49eed0c91e9c8256f6266bed6a0a0c
dutchcodes <dutchcodes@users.noreply.github.com>
2020-12-01 21:43:14 +0100

    Revert "Merge branch 'master' into dutchcodes-patch-1"
    This reverts commit 46ea79815361e8051ca0bf8cc2a26700886e0f3f, reversing
    changes made to 1556545b230c593d4aaccb9ad9c4c480a98bdcfb.
    
    Signed-off-by: dutchcodes <dutchcodes@users.noreply.github.com>

--- applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/org.power.dns.lua
+++
@@ -1,8 +0,0 @@
-return {
-	name = "PowerDNS",
-	label = _("PowerDNS"),
-	resolver_url = " https://doh.powerdns.org/",
-	bootstrap_dns = "136.144.215.158,2a01:7c8:d002:1ef:5054:ff:fe40:3703",
-	help_link = "https://powerdns.org/",
-	help_link_text = "PowerDNS"
-}
--- applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/org.snopyta.dns.lua
+++
@@ -1,8 +0,0 @@
-return {
-	name = "Snopyta",
-	label = _("Snopyta"),
-	resolver_url = "https://fi.doh.dns.snopyta.org/dns-query",
-	bootstrap_dns = "95.216.229.153,2a01:4f9:2a:1919::21",
-	help_link = "https://snopyta.org/index.html#faq",
-	help_link_text = "Snopyta"
-}
