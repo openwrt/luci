5da8a0d23feee86baa129ef64b2f2d965cb593fe
Hannu Nyman <hannu.nyman@iki.fi>
2021-02-05 21:27:14 +0200

    luci-app-statistics: Fix ipstatistics plugin (fix 6ae138c2)
    Fix the newly added ipstatistics plugin
    
    * add config definition
    * add configSummary
    * remove unnecessary option config form
      (no plugin-specific options)
    
    Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

--- applications/luci-app-statistics/htdocs/luci-static/resources/view/statistics/plugins/ipstatistics.js
+++ applications/luci-app-statistics/htdocs/luci-static/resources/view/statistics/plugins/ipstatistics.js
@@ -7,10 +7,7 @@ return baseclass.extend({
 	title: _('IP-Statistics Plugin Configuration'),
 	description: _('The ipstatistics plugin collects IPv4 and IPv6 statistics to compare them.'),
 
-	addFormOptions: function(s) {
-		var o;
-
-		o = s.option(form.Flag, 'enable', _('Enable this plugin'));
-		o.default = '0';
+	configSummary: function(section) {
+		return _('IPv4/IPv6 Statistics monitoring enabled');
 	}
 });
