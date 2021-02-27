56b8a4064d4dac951af7f064ef50615f74bde8cb
Chan Chak Shing <cschanaj@users.noreply.github.com>
2021-02-27 14:26:31 +0100

    Update com.cloudflare-dns-mozilla.lua
    Signed-off-by: cschanaj <cschanaj@users.noreply.github.com>

--- applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/com.cloudflare-dns-mozilla.lua
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/com.cloudflare-dns-mozilla.lua
@@ -1,6 +1,6 @@
 return {
-	name = "Cloudflare-Firefox",
-	label = _("Cloudflare (Firefox)"),
+	name = "Cloudflare-Mozilla-TRR",
+	label = _("Cloudflare (Mozilla’s Trusted Recursive Resolver (TRR) program)"),
 	resolver_url = "https://mozilla.cloudflare-dns.com/dns-query",
 	bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001",
 	help_link = "https://developers.cloudflare.com/1.1.1.1/privacy/firefox",
