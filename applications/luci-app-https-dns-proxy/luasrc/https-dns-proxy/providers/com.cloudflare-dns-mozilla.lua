c88022ddde927466a88a52260a1c554e51c8f0d1
Chan Chak Shing <cschanaj@users.noreply.github.com>
2021-02-27 14:09:45 +0100

    luci-app-https-dns-proxy: add firefox provider
    Signed-off-by: cschanaj <cschanaj@users.noreply.github.com>

---
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/com.cloudflare-dns-mozilla.lua
@@ -0,0 +1,8 @@
+return {
+	name = "Cloudflare-Firefox",
+	label = _("Cloudflare (Firefox)"),
+	resolver_url = "https://mozilla.cloudflare-dns.com/dns-query",
+	bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001",
+	help_link = "https://developers.cloudflare.com/1.1.1.1/privacy/firefox",
+	help_link_text = "Cloudflare Resolver for Firefox"
+}
