openwrt/luci

commit 2bfd4908a9cfeac74c7abc31d8cef0bf1e58af52

luci-mod-network: restore DNS option semantics for proto static
The peerdns settings makes little practical sense for proto:static
interfaces, so revert to allow setting the DNS server list directly.

Fixes: faad7464a8 ("luci-mod-network: add support for network.device sections")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -596,9 +596,11 @@ return view.extend({
 				o.default = o.enabled;
 
 				o = nettools.replaceOption(s, 'advanced', form.Flag, 'peerdns', _('Use DNS servers advertised by peer'), _('If unchecked, the advertised DNS server addresses are ignored'));
+				o.depends({ 'proto': 'static', '!reverse': true });
 				o.default = o.enabled;
 
 				o = nettools.replaceOption(s, 'advanced', form.DynamicList, 'dns', _('Use custom DNS servers'));
+				o.depends('proto', 'static');
 				o.depends('peerdns', '0');
 				o.datatype = 'ipaddr';
 

commit 9a92f9c7615f710a7934cccc8ac23902247c154b

luci-mod-network: fix handling of peerdns/dns/dns_search options
The previous change didn't take dynamic dependency mangling into account.

Fixes: 2bfd4908a9 ("luci-mod-network: restore DNS option semantics for proto static")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -595,17 +595,19 @@ return view.extend({
 				o = nettools.replaceOption(s, 'advanced', form.Flag, 'defaultroute', _('Use default gateway'), _('If unchecked, no default route is configured'));
 				o.default = o.enabled;
 
-				o = nettools.replaceOption(s, 'advanced', form.Flag, 'peerdns', _('Use DNS servers advertised by peer'), _('If unchecked, the advertised DNS server addresses are ignored'));
-				o.depends({ 'proto': 'static', '!reverse': true });
-				o.default = o.enabled;
+				if (protoval != 'static') {
+					o = nettools.replaceOption(s, 'advanced', form.Flag, 'peerdns', _('Use DNS servers advertised by peer'), _('If unchecked, the advertised DNS server addresses are ignored'));
+					o.default = o.enabled;
+				}
 
 				o = nettools.replaceOption(s, 'advanced', form.DynamicList, 'dns', _('Use custom DNS servers'));
-				o.depends('proto', 'static');
-				o.depends('peerdns', '0');
+				if (protoval != 'static')
+					o.depends('peerdns', '0');
 				o.datatype = 'ipaddr';
 
 				o = nettools.replaceOption(s, 'advanced', form.DynamicList, 'dns_search', _('DNS search domains'));
-				o.depends('peerdns', '0');
+				if (protoval != 'static')
+					o.depends('peerdns', '0');
 				o.datatype = 'hostname';
 
 				o = nettools.replaceOption(s, 'advanced', form.Value, 'dns_metric', _('DNS weight'), _('The DNS server entries in the local resolv.conf are primarily sorted by the weight specified here'));