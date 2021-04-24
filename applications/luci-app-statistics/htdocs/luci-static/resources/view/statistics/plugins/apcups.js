openwrt/luci

commit e7d22dce50b4a0c50c9f8ae8c18eeb5a37a51b3e

luci-app-statistics: convert collectd configuration to client side views
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

applications/luci-app-statistics/htdocs/luci-static/resources/view/statistics/plugins/apcups.js
@@ -1,0 +1,33 @@
+'use strict';
+'require form';
+
+return L.Class.extend({
+	title: _('APCUPS Plugin Configuration'),
+	description: _('The APCUPS plugin collects statistics about the APC UPS.'),
+
+	addFormOptions: function(s) {
+		var o;
+
+		o = s.option(form.Flag, 'enable', _('Enable this plugin'));
+		o.default = '0';
+
+		o = s.option(form.DynamicList, 'Host', _('Monitor host'));
+		o.default = 'localhost';
+		o.datatype = 'host';
+		o.depends('enable', '1');
+
+		o = s.option(form.Value, 'Port', _('Port for apcupsd communication'));
+		o.default = '3551';
+		o.datatype = 'port';
+		o.depends('enable', '1');
+	},
+
+	configSummary: function(section) {
+		var hosts = L.toArray(section.Host);
+		if (hosts.length)
+			return N_(hosts.length,
+				'Monitoring APC UPS at host %s, port %d',
+				'Monitoring APC UPS at hosts %s, port %d'
+			).format(hosts.join(', '), section.Port || 3551);
+	}
+});

commit ab46d731784a98cebb59a6311271a96ae0a32b35

luci-app-statistics: fix broken apcups support
A previous commit arbitrarily changed the apcups parameter "Host" into a
list, and broke collectd config generation by /usr/bin/stat-genconfig:

  /usr/bin/lua: /usr/bin/stat-genconfig:254: attempt to call method
  'find' (a nil value) stack traceback:
        /usr/bin/stat-genconfig:254: in function '_string'
        /usr/bin/stat-genconfig:74: in function 'config_generic'
        /usr/bin/stat-genconfig:45: in function 'section'
        /usr/bin/stat-genconfig:321: in main chunk
        [C]: ?

Restore "Host" to be a simple option.

Fixes: e7d22dce50b4 ("luci-app-statistics: convert collectd configuration
to client side views")
Reported-by: Gabe Rodriguez <lifehacksback@gmail.com>
Signed-off-by: Tony Ambardar <itugrok@yahoo.com>

applications/luci-app-statistics/htdocs/luci-static/resources/view/statistics/plugins/apcups.js
@@ -11,7 +11,7 @@ return baseclass.extend({
 
 		o = s.option(form.Flag, 'enable', _('Enable this plugin'));
 
-		o = s.option(form.DynamicList, 'Host', _('Monitor host'));
+		o = s.option(form.Value, 'Host', _('Monitor host'));
 		o.default = 'localhost';
 		o.datatype = 'host';
 		o.depends('enable', '1');
@@ -23,11 +23,6 @@ return baseclass.extend({
 	},
 
 	configSummary: function(section) {
-		var hosts = L.toArray(section.Host);
-		if (hosts.length)
-			return N_(hosts.length,
-				'Monitoring APC UPS at host %s, port %d',
-				'Monitoring APC UPS at hosts %s, port %d'
-			).format(hosts.join(', '), section.Port || 3551);
+		return 'Monitoring APC UPS at host %s, port %d'.format(section.Host || 'localhost', section.Port || 3551);
 	}
 });