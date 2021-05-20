a1b62732b817be369f9ce183aef5f581624b78c7
Rafał Miłecki <rafal@milecki.pl>
2021-05-20 14:00:36 +0200

    luci-mod-network: don't hide "Device" on protocol change
    Device ("ifname" UCI option) doesn't depend on protocol so there is no
    need to hide / reset it on protocol change.
    
    While at it drop names of two removed inputs (dead code).
    
    Fixes: ec020cee0c44 ("luci-mod-network: drop support for *editing* legacy bridges")
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    (cherry picked from commit 907b4222f70b3351d590d8a2e35c21a4ae07db8d)

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -822,6 +822,7 @@ return view.extend({
 					o = s.children[i];
 
 					switch (o.option) {
+					case 'ifname':
 					case 'proto':
 					case 'auto':
 					case '_dhcp':
@@ -830,8 +831,6 @@ return view.extend({
 					case '_ifacestat_modal':
 						continue;
 
-					case 'ifname_multi':
-					case 'ifname_single':
 					case 'igmp_snooping':
 					case 'stp':
 					case 'type':
