openwrt/luci

commit 17af33ee48bb709aec31d09b90cfbd4cbece6d0d

luci-mod-network: migrate network config depending on netifd version
Checking netifd version is important for users of the most recent LuCI
that didn't update netifd (e.g. OpenWrt package).

Suggested-by: Jo-Philipp Wich <jo@mein.io>
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -294,6 +294,7 @@ return view.extend({
 			network.getDSLModemType(),
 			network.getDevices(),
 			fs.lines('/etc/iproute2/rt_tables'),
+			fs.read('/usr/lib/opkg/info/netifd.control'),
 			uci.changes()
 		]);
 	},
@@ -354,8 +355,11 @@ return view.extend({
 	},
 
 	render: function(data) {
-		if (this.interfaceWithIfnameSections().length ||
-		    this.deviceWithIfnameSections().length)
+		var netifdVersion = (data[3] || '').match(/Version: ([^\n]+)/);
+
+		if (netifdVersion && netifdVersion[1] >= "2021-05-20" &&
+		    (this.interfaceWithIfnameSections().length ||
+		     this.deviceWithIfnameSections().length))
 			return this.renderMigration();
 
 		var dslModemType = data[0],

commit 34dc351303bad74c8f753c37788814796888129a

luci-mod-network: allow netifd version query to fail
Fixes: 17af33ee48 ("luci-mod-network: migrate network config depending on netifd version")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -294,7 +294,7 @@ return view.extend({
 			network.getDSLModemType(),
 			network.getDevices(),
 			fs.lines('/etc/iproute2/rt_tables'),
-			fs.read('/usr/lib/opkg/info/netifd.control'),
+			L.resolveDefault(fs.read('/usr/lib/opkg/info/netifd.control')),
 			uci.changes()
 		]);
 	},