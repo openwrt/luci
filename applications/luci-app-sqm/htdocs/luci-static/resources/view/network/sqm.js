openwrt/luci

commit e76d9cc7673ffbebd8887bcfc14542c1269288e0

luci-app-sqm: fix JS error if service is disabled
* fix a 'resource not found error' if the sqm service has been disabled
  and the dir '/var/run/sqm/available_qdiscs' not exist
* inform the user about this issue and provide a button
  to enable & start the service

Signed-off-by: Dirk Brenken <dev@brenken.org>

applications/luci-app-sqm/htdocs/luci-static/resources/view/network/sqm.js
@@ -1,5 +1,6 @@
 'use strict';
 'require fs';
+'require ui';
 'require rpc';
 'require uci';
 'require view';
@@ -23,8 +24,8 @@ return view.extend({
 
 	load: function() {
 		return Promise.all([
-			fs.list("/var/run/sqm/available_qdiscs"),
-			fs.list("/usr/lib/sqm").then(L.bind(function(scripts) {
+			L.resolveDefault(fs.list('/var/run/sqm/available_qdiscs'), []),
+			L.resolveDefault(fs.list('/usr/lib/sqm'), []).then(L.bind(function(scripts) {
 				var tasks = [], scriptHelpTbl = {};
 
 				for (var i = 0; i < scripts.length; i++)
@@ -40,6 +41,21 @@ return view.extend({
 	render: function(data) {
 		var qdiscs = data[0],
 		    scripts = data[1];
+
+		if (qdiscs.length === 0) {
+			ui.addNotification(null, 
+				E('div', { 'class': 'left' }, [
+				E('p', _("The SQM service seems to be disabled. Please use the button below to activate this service.")),
+				E('button', {
+					'class': 'btn cbi-button-active',
+					'click': ui.createHandlerFn(this, function() {
+						fs.exec_direct('/etc/init.d/sqm', ['enable']);
+						fs.exec_direct('/etc/init.d/sqm', ['start']);
+						location.reload();
+					})
+				}, _('Enable SQM'))
+			]));
+		}
 
 		var m, s, o;
 
@@ -62,7 +78,7 @@ return view.extend({
 		o.write = L.bind(function(section, value) {
 			if (value == "1") {
 				this.handleEnableSQM();
-				L.ui.addNotification(null, E('p', _("The SQM GUI has just enabled the sqm initscript on your behalf. Remember to disable the sqm initscript manually under System Startup menu in case this change was not wished for.")));
+				ui.addNotification(null, E('p', _("The SQM GUI has just enabled the sqm initscript on your behalf. Remember to disable the sqm initscript manually under System Startup menu in case this change was not wished for.")));
 			}
 
 			return uci.set("sqm", section, "enabled", value);

commit 005c09a293d1d0f4a5a76e210e0fa23842f13cb3

luci-app-sqm: only reload view once sqm enable/start completed
Chain the exec calls to enable and start the SQM service and only reload
the view once these operations are complete and return the entire promise
chain from the handler function to ensure that the busy indicator remains
active throughout the entire duration of the operation.

Also replace exec_direct() with exec() as there is no need to bypass ubus
for the init script calls.

Fixes: e76d9cc767 ("luci-app-sqm: fix JS error if service is disabled")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

applications/luci-app-sqm/htdocs/luci-static/resources/view/network/sqm.js
@@ -43,15 +43,17 @@ return view.extend({
 		    scripts = data[1];
 
 		if (qdiscs.length === 0) {
-			ui.addNotification(null, 
+			ui.addNotification(null,
 				E('div', { 'class': 'left' }, [
 				E('p', _("The SQM service seems to be disabled. Please use the button below to activate this service.")),
 				E('button', {
 					'class': 'btn cbi-button-active',
 					'click': ui.createHandlerFn(this, function() {
-						fs.exec_direct('/etc/init.d/sqm', ['enable']);
-						fs.exec_direct('/etc/init.d/sqm', ['start']);
-						location.reload();
+						return fs.exec('/etc/init.d/sqm', ['enable']).then(function() {
+							return fs.exec('/etc/init.d/sqm', ['start']);
+						}).then(function() {
+							location.reload();
+						});
 					})
 				}, _('Enable SQM'))
 			]));