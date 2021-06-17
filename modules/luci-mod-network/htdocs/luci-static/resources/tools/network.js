openwrt/luci

commit 01eac366f69ba58c090ddc2cb70aeb069adbdd5a

luci-mod-network: fix tagging/pvid state parsing in bridge-vlan ports
The previous code naively looked for a `t` in the entire port spec,
wrongly matching untagged ports having a `t` in their name, such
as `eth0`.

Rework the logic to be more strict when parsing the port member
specification to avoid this issue.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/tools/network.js
@@ -264,15 +264,20 @@ var cbiTagValue = form.Value.extend({
 	},
 
 	cfgvalue: function(section_id) {
-		var pname = this.port,
-		    spec = L.toArray(uci.get('network', section_id, 'ports')).filter(function(p) { return p.replace(/:[ut*]+$/, '') == pname })[0];
+		var ports = L.toArray(uci.get('network', section_id, 'ports'));
 
-		if (spec && spec.match(/t/))
-			return spec.match(/\*/) ? ['t', '*'] : ['t'];
-		else if (spec)
-			return spec.match(/\*/) ? ['u', '*'] : ['u'];
-		else
-			return ['-'];
+		for (var i = 0; i < ports.length; i++) {
+			var s = ports[i].split(/:/);
+
+			if (s[0] != this.port)
+				continue;
+
+			var t = s[1].match(/t/) ? 't' : 'u';
+
+			return s[1].match(/\*/) ? [t, '*'] : [t];
+		}
+
+		return ['-'];
 	},
 
 	write: function(section_id, value) {

commit e57f8663afd53cc7421debcd63444e1d136f5dec

luci-mod-network: parse ports without tag/pvid annotation as well
Fixes: 01eac366f6 ("luci-mod-network: fix tagging/pvid state parsing in bridge-vlan ports")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/tools/network.js
@@ -272,9 +272,9 @@ var cbiTagValue = form.Value.extend({
 			if (s[0] != this.port)
 				continue;
 
-			var t = s[1].match(/t/) ? 't' : 'u';
+			var t = /t/.test(s[1] || '') ? 't' : 'u';
 
-			return s[1].match(/\*/) ? [t, '*'] : [t];
+			return /\*/.test(s[1] || '') ? [t, '*'] : [t];
 		}
 
 		return ['-'];