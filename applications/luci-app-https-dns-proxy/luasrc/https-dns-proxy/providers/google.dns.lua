756063c9f800b1bcd2ddc99a2d8bb570597b389c
Stan Grishin <stangri@melmac.net>
2021-07-31 10:47:39 +0000

    luci-app-https-dns-proxy: update to 2021-07-29-1
    * add HTTP/2-only supporting providers: Mullvad, Digitale-Gesellschaft, dns.sb and Rubyfish.cn
    * switch default provider from Google to Cloudflare
    * add IPv6 addresses for bootstrap resolvers for Google DNS
    * add secondary bootstrap resolver (Cloudflare's) to all providers with a single bootstrap resolver
    * modify model/cbi file to show HTTP/2-only providers (and help texts) on HTTP/2-supporting systems
    
    Signed-off-by: Stan Grishin <stangri@melmac.net>

--- applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/google.dns.lua
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/google.dns.lua
@@ -2,6 +2,5 @@ return {
 	name = "Google",
 	label = _("Google"),
 	resolver_url = "https://dns.google/dns-query",
-	bootstrap_dns = "8.8.8.8,8.8.4.4",
-	default = true
+	bootstrap_dns = "8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
