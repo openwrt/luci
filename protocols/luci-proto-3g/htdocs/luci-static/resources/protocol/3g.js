openwrt/luci

commit 1661ac6bf888e4563546cc63fe0e228af8889bf0

luci-proto-ncm: add APN and PIN validation
Signed-off-by: Nicholas Smith <nicholas@nbembedded.com>
[squash commits, fix whitespace]
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

protocols/luci-proto-ncm/htdocs/luci-static/resources/protocol/ncm.js
@@ -84,8 +84,17 @@ return network.registerProtocol('ncm', {
 		o.value('IPV4V6', _('IPv4+IPv6'));
 		o.value('IPV6', _('IPv6'));
 
-		s.taboption('general', form.Value, 'apn', _('APN'));
-		s.taboption('general', form.Value, 'pincode', _('PIN'));
+		o = s.taboption('general', form.Value, 'apn', _('APN'));
+		o.validate = function(section_id, value) {
+			if (!/^[a-zA-Z0-9\-.]*[a-zA-Z0-9]$/.test(value))
+					return _('Invalid APN provided');
+
+			return true;
+		};
+
+		o = s.taboption('general', form.Value, 'pincode', _('PIN'));
+		o.datatype = 'and(uinteger,minlength(4),maxlength(8))';
+
 		s.taboption('general', form.Value, 'username', _('PAP/CHAP username'));
 
 		o = s.taboption('general', form.Value, 'password', _('PAP/CHAP password'));

commit 5ff3ef7cbb9719d3476feb836cac86ee421f666d

protocols: make APN option optional again
Fixes: #5284
Fixes: 1661ac6bf8 ("luci-proto-ncm: add APN and PIN validation")
Fixes: 114dc38dc2 ("luci-proto-qmi: add APN and PIN validation")
Fixes: 6a8ad40b05 ("luci-proto-3g: add APN and PIN validation")
Fixes: ebb676ef7b ("luci-proto-modemmanager: add APN and PIN validation")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

protocols/luci-proto-3g/htdocs/luci-static/resources/protocol/3g.js
@@ -94,6 +94,9 @@ return network.registerProtocol('3g', {
 
 		o = s.taboption('general', form.Value, 'apn', _('APN'));
 		o.validate = function(section_id, value) {
+			if (value == null || value == '')
+				return true;
+
 			if (!/^[a-zA-Z0-9\-.]*[a-zA-Z0-9]$/.test(value))
 				return _('Invalid APN provided');
 

protocols/luci-proto-modemmanager/htdocs/luci-static/resources/protocol/modemmanager.js
@@ -93,6 +93,9 @@ return network.registerProtocol('modemmanager', {
 
 		o = s.taboption('general', form.Value, 'apn', _('APN'));
 		o.validate = function(section_id, value) {
+			if (value == null || value == '')
+				return true;
+
 			if (!/^[a-zA-Z0-9\-.]*[a-zA-Z0-9]$/.test(value))
 				return _('Invalid APN provided');
 

protocols/luci-proto-ncm/htdocs/luci-static/resources/protocol/ncm.js
@@ -86,6 +86,9 @@ return network.registerProtocol('ncm', {
 
 		o = s.taboption('general', form.Value, 'apn', _('APN'));
 		o.validate = function(section_id, value) {
+			if (value == null || value == '')
+				return true;
+
 			if (!/^[a-zA-Z0-9\-.]*[a-zA-Z0-9]$/.test(value))
 					return _('Invalid APN provided');
 

protocols/luci-proto-qmi/htdocs/luci-static/resources/protocol/qmi.js
@@ -67,6 +67,9 @@ return network.registerProtocol('qmi', {
 
 		o = s.taboption('general', form.Value, 'apn', _('APN'));
 		o.validate = function(section_id, value) {
+			if (value == null || value == '')
+				return true;
+
 			if (!/^[a-zA-Z0-9\-.]*[a-zA-Z0-9]$/.test(value))
 				return _('Invalid APN provided');
 