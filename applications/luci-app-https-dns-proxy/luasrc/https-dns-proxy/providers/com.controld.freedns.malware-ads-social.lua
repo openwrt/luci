cf364992fa7da5ea125f1a8b5ebb5cf7c2564364
Jo-Philipp Wich <jo@mein.io>
2021-09-24 20:08:59 +0200

    luci-app-https-dns-proxy: add ControlD as provider
    Signed-off-by: Aleem Hossain <aleem.hossain@tectonicinteractive.com>
    
    luci-app-https-dns-proxy: add ControlD as provider
    
    Signed-off-by: Aleem Hossain <aleem.hossain@tectonicinteractive.com>
    
    luci-app-transmission: grant access to group and passwd files
    
    This is required for the user and group ID dropdowns in the
    configuration.
    
    Fixes: #4697
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>
    
    luci-app-https-dns-proxy: add ControlD as provider
    
    Signed-off-by: Aleem Hossain <aleem.hossain@tectonicinteractive.com>
    
    luci-app-https-dns-proxy: add ControlD as provider
    
    Signed-off-by: Aleem Hossain <aleem.hossain@tectonicinteractive.com>
    
    luci-app-https-dns-proxy: add ControlD as provider
    
    Signed-off-by: Aleem Hossain <aleem@tectonic.it>
    
    luci-app-https-dns-proxy: add ControlD as provider
    
    Signed-off-by: Aleem Hossain <aleem.hossain@tectonicinteractive.com>
    
    luci-app-https-dns-proxy: add ControlD as provider
    
    Signed-off-by: Aleem Hossain <aleem@tectonic.it>

---
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/com.controld.freedns.malware-ads-social.lua
@@ -0,0 +1,8 @@
+return {
+	name = "ControlD-Malware-Ads-Social",
+	label = _("ControlD (Block Malware + Ads + Social)"),
+	resolver_url = "https://freedns.controld.com/p3",
+	bootstrap_dns = "76.76.2.3,2606:1a40::3",
+	help_link = "https://kb.controld.com/tutorials",
+	help_link_text = "ControlD"
+}
---
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/com.controld.freedns.malware-ads.lua
@@ -0,0 +1,8 @@
+return {
+	name = "ControlD-Malware-Ads",
+	label = _("ControlD (Block Malware + Ads)"),
+	resolver_url = "https://freedns.controld.com/p2",
+	bootstrap_dns = "76.76.2.2,2606:1a40::2",
+	help_link = "https://kb.controld.com/tutorials",
+	help_link_text = "ControlD"
+}
