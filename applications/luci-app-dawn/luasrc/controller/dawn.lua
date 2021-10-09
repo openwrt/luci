4a78122246d0991e4a952315c21e4c4e98538f43
Nick Hainke <vincent@systemli.org>
2021-10-09 09:57:49 +0200

    luci-app-dawn: remove page for steering options
    The luci-app-dawn is blocking the PR to be merged:
    https://github.com/openwrt/packages/pull/16299
    
    Remove the configuration page and set appropriate standard values.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>

--- applications/luci-app-dawn/luasrc/controller/dawn.lua
+++ applications/luci-app-dawn/luasrc/controller/dawn.lua
@@ -5,7 +5,6 @@ function index()
     e.dependent = false
     e.acl_depends = { "luci-app-dawn" }
 
-    entry({ "admin", "dawn", "configure_daemon" }, cbi("dawn/dawn_config"), "Configure DAWN", 1)
-    entry({ "admin", "dawn", "view_network" }, cbi("dawn/dawn_network"), "View Network Overview", 2)
-    entry({ "admin", "dawn", "view_hearing_map" }, cbi("dawn/dawn_hearing_map"), "View Hearing Map", 3)
+    entry({ "admin", "dawn", "view_network" }, cbi("dawn/dawn_network"), "View Network Overview", 1)
+    entry({ "admin", "dawn", "view_hearing_map" }, cbi("dawn/dawn_hearing_map"), "View Hearing Map", 2)
 end
