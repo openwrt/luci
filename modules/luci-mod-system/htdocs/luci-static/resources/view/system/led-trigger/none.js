7a677b1fdefb120c2628aec310f0508f411be9e8
Florian Eckert <fe@dev.tdt.de>
2021-10-28 15:43:50 +0200

    luci-mod-system: move default option from defaul-on trigger to none trigger
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>
    (cherry picked from commit 5b42cd5b46c32bf168b7156602bfa0ba841904a1)

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/none.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/none.js
@@ -6,5 +6,15 @@ return baseclass.extend({
 	kernel: true,
 	addFormOptions(s){
 		var o;
+
+		o = s.option(form.Flag, 'default', _('Default state'));
+		o.rmempty = false;
+		o.depends('trigger', 'none');
+		o.textvalue = function(section_id) {
+			var cval = this.cfgvalue(section_id);
+			if (cval == null)
+				cval = this.default;
+			return (cval == this.enabled) ? _('On') : _('Off');
+		};
 	}
 });
