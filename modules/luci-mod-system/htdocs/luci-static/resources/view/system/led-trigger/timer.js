fceabd74465f2aafc40edc084429f30837deff2c
Jo-Philipp Wich <jo@mein.io>
2021-10-31 22:26:42 +0100

    luci-mod-system: add missing require to `none` led trigger class
    Also convert ES6 to ES5 syntax while we're at it.
    
    Fixes: #5476
    Fixes: 5b42cd5b46 ("luci-mod-system: move default option from defaul-on trigger to none trigger")
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/timer.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/timer.js
@@ -6,7 +6,7 @@ return baseclass.extend({
 	trigger: _('Custom flash interval (kernel: timer)'),
 	description: _('The LED blinks with the configured on/off frequency'),
 	kernel: true,
-	addFormOptions(s){
+	addFormOptions: function(s) {
 		var o;
 
 		o = s.option(form.Value, 'delayon', _('On-State Delay'),
