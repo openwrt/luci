e1ccb6621c597c01ba4ab2187ffb49ed156566a0
Jo-Philipp Wich <jo@mein.io>
2021-11-10 11:51:42 +0100

    luci-base: network.js: ignore wireless ifname patterns on retrieving devices
    Only treat the given identifier as Linux netdev name if we can find a
    corresponding entry in the device info cache and do not consider strings
    starting with "wlan", "ath" or "wl" to be existing devices.
    
    This fixes incorrectly adding wireless sections as ifnames to network
    interfaces when the wifi-iface section name begins with one of the
    `iface_patterns_wireless` patterns.
    
    Fixes: #5069
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>
    (cherry picked from commit d4092b15ce92cda2d7711b57b2251ebebef32b5c)

--- modules/luci-base/htdocs/luci-static/resources/network.js
+++ modules/luci-base/htdocs/luci-static/resources/network.js
@@ -1149,7 +1149,7 @@ Network = baseclass.extend(/** @lends LuCI.network.prototype */ {
 			if (name == null)
 				return null;
 
-			if (_state.netdevs.hasOwnProperty(name) || isWifiIfname(name))
+			if (_state.netdevs.hasOwnProperty(name))
 				return this.instantiateDevice(name);
 
 			var netid = getWifiNetidBySid(name);
