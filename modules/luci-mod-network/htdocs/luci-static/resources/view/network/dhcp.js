b8d3ca6f826c3026f538c074e2a1a2cb843cb340
Jo-Philipp Wich <jo@mein.io>
2021-12-01 16:40:31 +0100

    luci-mod-network: fix unbalanced and confusing markup in DHCP translation
    The string `<%s>...<%s>` is invalid HTML and will confuse translation software
    like poedit as well as human translators. It is not clear that `<%s>...<%s>`
    is supposed to interpolate into both a start and end tag and `<%s>` might be
    mistaken as a quoted form of `%s` like `«%s»` or `〈%s〉`.
    
    Replace the string with `<a href="%s">...</a>` to make it immediately clear
    that the acronym is surrounded by an HTML anchor, only pass the destination
    URL via string formatting to avoid mistranslations there.
    
    Fixes: 3bfcf99a68 ("luci-mod-network: fix unbalanced and confusing markup in DHCP translation")
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/dhcp.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/dhcp.js
@@ -299,7 +299,7 @@ return view.extend({
 
 		o = s.taboption('general', form.Flag, 'rebind_protection',
 			_('Rebind protection'),
-			_('Discard upstream responses containing <%s>RFC1918<%s> addresses.').format('a href="https://datatracker.ietf.org/doc/html/rfc1918" target="_blank" rel="noreferrer noopener"', '/a'));
+			_('Discard upstream responses containing <a href="%s">RFC1918</a> addresses.').format('https://datatracker.ietf.org/doc/html/rfc1918'));
 		o.rmempty = false;
 
 		o = s.taboption('general', form.Flag, 'rebind_localhost',
