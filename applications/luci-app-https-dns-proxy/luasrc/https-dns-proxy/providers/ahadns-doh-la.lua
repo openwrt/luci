8b8fff90da07ad6b51ff7f3f5281c787d18db328
Jefe777 <82453643+Jefe777@users.noreply.github.com>
2021-12-21 13:23:09 -0600

    ahadns-doh-la
    A zero logging DNS with support for DNS-over-HTTPS (DoH) & DNS-over-TLS (DoT). Blocks ads, malware, trackers, viruses, ransomware, telemetry and more. No persistent logs. DNSSEC. Hosted in Los Angeles, USA. By https://ahadns.com/
    Server statistics can be seen at: https://statistics.ahadns.com/?server=la

---
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/ahadns-doh-la.lua
@@ -0,0 +1,8 @@
+return {
+	name = "ahadns-doh-la",
+	label = _("AhaDNS.com - Los Angeles (Block Malware + Ads)"),
+	resolver_url = "https://doh.la.ahadns.net/dns-query",
+	bootstrap_dns = "45.67.219.208,2a04:bdc7:100:70::70",
+	help_link = "https://ahadns.com/dns-over-https/",
+	help_link_text = "AhaDNS.com"
+}
