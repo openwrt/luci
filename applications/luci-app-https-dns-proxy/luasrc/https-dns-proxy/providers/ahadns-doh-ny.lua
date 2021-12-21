fbde3c584a43f1f9fb3e1a38566a04b19930866b
Jefe777 <82453643+Jefe777@users.noreply.github.com>
2021-12-21 13:22:43 -0600

    ahadns-doh-ny
    A zero logging DNS with support for DNS-over-HTTPS (DoH) & DNS-over-TLS (DoT). Blocks ads, malware, trackers, viruses, ransomware, telemetry and more. No persistent logs. DNSSEC. Hosted in New York, USA. By https://ahadns.com/
    Server statistics can be seen at: https://statistics.ahadns.com/?server=ny

---
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/ahadns-doh-ny.lua
@@ -0,0 +1,8 @@
+return {
+	name = "ahadns-doh-ny",
+	label = _("AhaDNS.com - New York (Block Malware + Ads)"),
+	resolver_url = "https://doh.ny.ahadns.net/dns-query",
+	bootstrap_dns = "185.213.26.187,2a0d:5600:33:3::3",
+	help_link = "https://ahadns.com/dns-over-https/",
+	help_link_text = "AhaDNS.com"
+}
