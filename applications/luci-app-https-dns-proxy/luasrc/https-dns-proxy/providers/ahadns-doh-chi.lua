61239e2eebb0e63318540817f832f6f0c61d523a
Jefe777 <82453643+Jefe777@users.noreply.github.com>
2021-12-21 13:22:15 -0600

    ahadns-doh-chi
    A zero logging DNS with support for DNS-over-HTTPS (DoH) & DNS-over-TLS (DoT). Blocks ads, malware, trackers, viruses, ransomware, telemetry and more. No persistent logs. DNSSEC. Hosted in Chicago, USA. By https://ahadns.com/
    Server statistics can be seen at: https://statistics.ahadns.com/?server=chi

---
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/ahadns-doh-chi.lua
@@ -0,0 +1,8 @@
+return {
+	name = "ahadns-doh-chi",
+	label = _("AhaDNS.com - Chicago (Block Malware + Ads)"),
+	resolver_url = "https://doh.chi.ahadns.net/dns-query",
+	bootstrap_dns = "193.29.62.196,2605:4840:3:c4::c4",
+	help_link = "https://ahadns.com/dns-over-https/",
+	help_link_text = "AhaDNS.com"
+}
