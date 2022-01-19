37253b569390bba9f395c8253ad275710281b8da
Matthew Hagan <mnhagan88@gmail.com>
2022-01-19 23:09:00 +0000

    luci-proto-ipip: add nohostroute configurable
    Add configurable to explicitly disable creation of route to the peer
    address.
    
    Signed-off-by: Matthew Hagan <mnhagan88@gmail.com>

--- protocols/luci-proto-ipip/htdocs/luci-static/resources/protocol/ipip.js
+++ protocols/luci-proto-ipip/htdocs/luci-static/resources/protocol/ipip.js
@@ -66,5 +66,8 @@ return network.registerProtocol('ipip', {
 
 		o = s.taboption('advanced', form.Flag, 'df', _("Don't Fragment"), _("Enable the DF (Don't Fragment) flag of the encapsulating packets."));
 		o.optional = true;
+
+		o = s.taboption('advanced', form.Flag, 'nohostroute', _("No host route"), _("Do not create host route to peer (optional)."));
+		o.optional = true;
 	}
 });
