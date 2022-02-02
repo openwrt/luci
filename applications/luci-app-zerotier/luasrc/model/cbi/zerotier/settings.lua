a92a8e049ee95ee1afa6a20e8e21cf1c98c0962d
Andrew Innes <andrew.c12@gmail.com>
2022-02-03 00:30:25 +0800

    luci-app-zerotier: add a new luci app to manage zerotier vpn
    Based off of https://github.com/coolsnowwolf/lede/tree/master/package/lean/luci-app-zerotier
    - without the interface code and scripts for allowing NAT access to the local lan.
    - added missing btn class to Zerotier.com button which fixes button style in the openwrt2020 theme.
    - removed html from Zerotier.com button translation.
    - add es translation and template from https://github.com/openwrtdiy/openwrt-ipk/tree/main/luci-app-zerotier
    - regenerated translation template from source
    - normalised translation files
    - add luci-compat to package dependencies
    
    Signed-off-by: Andrew Innes <andrew.c12@gmail.com>

--- applications/luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
+++ applications/luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
@@ -1,26 +1,23 @@
+a = Map("zerotier")
+a.title = translate("ZeroTier")
+a.description = translate("Zerotier is an open source, cross-platform and easy to use virtual LAN")
 
-a=Map("zerotier",translate("ZeroTier"),translate("Zerotier is an open source, cross-platform and easy to use virtual LAN"))
-a:section(SimpleSection).template  = "zerotier/zerotier_status"
+a:section(SimpleSection).template = "zerotier/zerotier_status"
 
-t=a:section(NamedSection,"sample_config","zerotier")
-t.anonymous=true
-t.addremove=false
+t = a:section(NamedSection, "sample_config", "zerotier")
+t.anonymous = true
+t.addremove = false
 
-e=t:option(Flag,"enabled",translate("Enable"))
-e.default=0
+e = t:option(Flag, "enabled", translate("Enable"))
+e.default = 0
 e.rmempty=false
 
-e=t:option(DynamicList,"join",translate('ZeroTier Network ID'))
-e.password=true
-e.rmempty=false
+e = t:option(DynamicList, "join", translate('ZeroTier Network ID'))
+e.password = true
+e.rmempty = false
 
-e=t:option(Flag,"nat",translate("Auto NAT Clients"))
-e.default=0
-e.rmempty=false
-e.description = translate("Allow zerotier clients access your LAN network")
 
-e=t:option(DummyValue,"opennewwindow" , 
-	translate("<input type=\"button\" class=\"btn cbi-button cbi-button-apply\" value=\"Zerotier.com\" onclick=\"window.open('https://my.zerotier.com/network')\" />"))
+e = t:option(DummyValue, "opennewwindow", "<input type=\"button\" class=\"btn cbi-button cbi-button-apply\" value=\"" .. translate('Zerotier.com') .. "\" onclick=\"window.open('https://my.zerotier.com/network')\" />")
 e.description = translate("Create or manage your zerotier network, and auth clients who could access")
 
 return a
