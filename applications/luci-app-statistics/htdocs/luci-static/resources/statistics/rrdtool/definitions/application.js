8081a06d0613831625a962266978b25bde995a30
bizzbyster <bizzbyster@gmail.com>
2022-03-10 07:02:03 -0500

    using total_bytes instead

--- applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/application.js
+++ applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/application.js
@@ -15,38 +15,38 @@ return baseclass.extend({
 		if (plugin_instance != '')
 			title = "Category: %pi";
 
-		var if_octets = {
+		var total_bytes = {
 			title: title,
 			y_min: "0",
 			alt_autoscale_max: true,
 			vlabel: "Bytes/s",
 			data: {
 				instances: {
-					if_octets: [ "3g_wwan", "eth1"]
+					total_bytes: [ "3g_wwan", "eth1"]
 				},
 				sources: {
-					if_octets: [ "tx", "rx" ],
-					if_octets: [ "tx", "rx" ]
+					total_bytes: [ "tx", "rx" ],
+					total_bytes: [ "tx", "rx" ]
 				},
 				options: {
-					if_octets_eth1_tx: {
+					total_bytes_eth1_tx: {
 						total: true,		/* report total amount of bytes */
 						color: "0000ff",	/* eth1 is blue */
 						title: "Viasat Bytes (TX)"
 					},
-					if_octets_eth1_rx: {
+					total_bytes_eth1_rx: {
 						flip : true,		/* flip rx line */
 						total: true,		/* report total amount of bytes */
 						color: "0000ff",	/* eth1 is blue */
 						title: "Viasat Bytes (RX)"
 					},
 
-					if_octets_3g_wwan_tx: {
+					total_bytes_3g_wwan_tx: {
 						total: true,		/* report total amount of bytes */
 						color: "00ff00",	/* 3g_wwan is green */
 						title: "TMobile LTEBytes (TX)"
 					},
-					if_octets_3g_wwan_rx: {
+					total_bytes_3g_wwan_rx: {
 						flip : true,		/* flip rx line */
 						total: true,		/* report total amount of bytes */
 						color: "00ff00",	/* 3g_wwan is green */
@@ -61,8 +61,8 @@ return baseclass.extend({
 		for (var i = 0; i < types.length; i++)
 			if (types[i] == 'cpu')
 				p.push(cpu);
-			else if (types[i] == 'if_octets')
-				p.push(if_octets);
+			else if (types[i] == 'total_bytes')
+				p.push(total_bytes);
 
 		return p;
 	}
