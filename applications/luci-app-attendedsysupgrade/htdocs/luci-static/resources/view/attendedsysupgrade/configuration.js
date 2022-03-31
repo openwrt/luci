fa9fb2f9552034f7e4229bbb35eb3524e5557185
Paul Spooren <mail@aparcar.org>
2022-03-31 16:01:31 +0100

    luci-app-attendedsysupgrade: introduce rebuilders
    This adds automatic verification builds to shift trust on multiple
    server and multiple entities.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- applications/luci-app-attendedsysupgrade/htdocs/luci-static/resources/view/attendedsysupgrade/configuration.js
+++ applications/luci-app-attendedsysupgrade/htdocs/luci-static/resources/view/attendedsysupgrade/configuration.js
@@ -3,28 +3,53 @@
 'require form';
 
 return view.extend({
-	render: function() {
-		var m, s, o;
+	render: function () {
+		let m, s, o;
 
-		m = new form.Map('attendedsysupgrade', _('Attended Sysupgrade'),
-				 _('Attendedsysupgrade Configuration.'));
+		m = new form.Map(
+			'attendedsysupgrade',
+			_('Attended Sysupgrade'),
+			_('Attendedsysupgrade Configuration.')
+		);
 
 		s = m.section(form.TypedSection, 'server', _('Server'));
 		s.anonymous = true;
 
-		s.option(form.Value, 'url', _('Address'),
-			 _('Address of the sysupgrade server'));
+		s.option(
+			form.Value,
+			'url',
+			_('Address'),
+			_('Address of the sysupgrade server')
+		);
+
+		s.option(
+			form.DynamicList,
+			'rebuilder',
+			_('Rebuilders'),
+			_(
+				'Other ASU server instances that rebuild a requested image. ' +
+					'Allows to compare checksums and verify that the results are the same.'
+			)
+		);
 
 		s = m.section(form.TypedSection, 'client', _('Client'));
 		s.anonymous = true;
 
-		o = s.option(form.Flag, 'auto_search', _('Search on opening'),
-		 _('Search for new sysupgrades on opening the tab'));
+		o = s.option(
+			form.Flag,
+			'auto_search',
+			_('Search on opening'),
+			_('Search for new sysupgrades on opening the tab')
+		);
 		o.default = '1';
 		o.rmempty = false;
 
-		o = s.option(form.Flag, 'advanced_mode', _('Advanced Mode'),
-		 _('Show advanced options like package list modification'));
+		o = s.option(
+			form.Flag,
+			'advanced_mode',
+			_('Advanced Mode'),
+			_('Show advanced options like package list modification')
+		);
 		o.default = '0';
 		o.rmempty = false;
 
