4c263cff5cc2b707c770790a9c7ef87faf735550
Jo-Philipp Wich <jo@mein.io>
2022-04-27 12:18:29 +0200

    luci-mod-network: avoid needlessly writing `none` RA flags
    Fixes: #5725
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>
    (cherry picked from commit f04011f0683dbacb8b831733c708bad760ca61df)

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -805,7 +805,14 @@ return view.extend({
 						return flags.length ? flags : [ 'other-config' ];
 					};
 					so.remove = function(section_id) {
-						uci.set('dhcp', section_id, 'ra_flags', [ 'none' ]);
+						var existing = L.toArray(uci.get('dhcp', section_id, 'ra_flags'));
+						if (this.isActive(section_id)) {
+							if (existing.length != 1 || existing[0] != 'none')
+								uci.set('dhcp', section_id, 'ra_flags', [ 'none' ]);
+						}
+						else if (existing.length) {
+							uci.unset('dhcp', section_id, 'ra_flags');
+						}
 					};
 
 					so = ss.taboption('ipv6-ra', form.Value, 'ra_maxinterval', _('Max <abbr title="Router Advertisement">RA</abbr> interval'), _('Maximum time allowed  between sending unsolicited <abbr title="Router Advertisement, ICMPv6 Type 134">RA</abbr>. Default is 600 seconds.'));
