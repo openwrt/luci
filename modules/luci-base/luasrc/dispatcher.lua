e1932592c3e0804eec5d85fee989ceeed1e1050a
Jo-Philipp Wich <jo@mein.io>
2022-07-08 15:38:53 +0200

    luci-base: use different cookie names for HTTP and HTTPS
    Since HTTP cookies may not overwrite HTTPS ("secure") ones, users are
    frequently unable to log into LuCI when a stale, "secure" `sysauth` cookie
    is still present in the browser as it commonly happens after e.g. a
    sysupgrade operation or when frequently jumping between HTTP and HTTPS
    access.
    
    Rework the dispatcher to set either a `sysauth_http` or `sysauth_https`
    cookie, depending on the HTTPS state of the server connection and accept
    both cookie names when verifying the session ID.
    
    This allows users to log into a HTTP-only LuCI instance while a stale,
    "secure" HTTPS cookie is still present.
    
    Requires commit 2b0539ef9d ("lucihttp: update to latest Git HEAD") to
    function properly.
    
    Fixes: #5843
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-base/luasrc/dispatcher.lua
+++ modules/luci-base/luasrc/dispatcher.lua
@@ -343,12 +343,12 @@ local function tree_to_json(node, json)
 				if subnode.sysauth_authenticator == "htmlauth" then
 					spec.auth = {
 						login = true,
-						methods = { "cookie:sysauth" }
+						methods = { "cookie:sysauth_https", "cookie:sysauth_http" }
 					}
 				elseif subname == "rpc" and subnode.module == "luci.controller.rpc" then
 					spec.auth = {
 						login = false,
-						methods = { "query:auth", "cookie:sysauth" }
+						methods = { "query:auth", "cookie:sysauth_https", "cookie:sysauth_http" }
 					}
 				elseif subnode.module == "luci.controller.admin.uci" then
 					spec.auth = {
@@ -732,7 +732,7 @@ local function init_template_engine(ctx)
 	return tpl
 end
 
-local function is_authenticated(auth)
+function is_authenticated(auth)
 	if type(auth) == "table" and type(auth.methods) == "table" and #auth.methods > 0 then
 		local sid, sdat, sacl
 		for _, method in ipairs(auth.methods) do
@@ -929,7 +929,8 @@ function dispatch(request)
 				return tpl.render("sysauth", scope)
 			end
 
-			http.header("Set-Cookie", 'sysauth=%s; path=%s; SameSite=Strict; HttpOnly%s' %{
+			http.header("Set-Cookie", 'sysauth_%s=%s; path=%s; SameSite=Strict; HttpOnly%s' %{
+				http.getenv("HTTPS") == "on" and "https" or "http",
 				sid, build_url(), http.getenv("HTTPS") == "on" and "; secure" or ""
 			})
 
