b6b7da4ffa5fea45b72c7f99c482fdf9a9c35c9f
Jo-Philipp Wich <jo@mein.io>
2022-09-21 14:47:41 +0200

    luci-mod-system: sshkeys.js: prevent XSS through pubkey comments
    Ensure to not display public key comments verbatim in order to prevent
    injection of markup.
    
    Reported-by: Eric McDonald <ericmcdonald@protonmail.com>
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>
    (cherry picked from commit 944b55738e7f9685865d5298248b7fbd7380749e)

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/sshkeys.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/sshkeys.js
@@ -112,7 +112,7 @@ function renderKeyItem(pubkey) {
 		click: isReadonlyView ? null : removeKey,
 		'data-key': pubkey.src
 	}, [
-		E('strong', pubkey.comment || _('Unnamed key')), E('br'),
+		E('strong', [ pubkey.comment || _('Unnamed key') ]), E('br'),
 		E('small', [
 			'%s, %s'.format(pubkey.type, pubkey.curve || _('%d Bit').format(pubkey.bits)),
 			pubkey.options ? E([], [
