openwrt/luci

commit 45ab2cd6be0cac32ec49364280bbfa2c43c17eda

luci-mod-status: use network.getDSLModemType()
This matches what luci-mod-network does for the DSL modem configuration.

Since this is based on a common uci config, it may also prevent issues as
fixed with 111c551c "luci-base: fix DSL feature detection" in the future.

Signed-off-by: Andre Heider <a.heider@gmail.com>

modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/50_dsl.js
@@ -1,5 +1,6 @@
 'use strict';
 'require baseclass';
+'require network';
 'require rpc';
 
 var callDSLMetrics = rpc.declare({
@@ -44,7 +45,7 @@ return baseclass.extend({
 	title: _('DSL'),
 
 	load: function() {
-		if (!L.hasSystemFeature('dsl'))
+		if (!network.getDSLModemType())
 			return Promise.reject();
 
 		return L.resolveDefault(callDSLMetrics(), {});

commit b5f11bf6ee5ceb04c36d9f88ce82386430321741

luci-mod-status: fix determining DSL modem type
The `network.getDSLModemType()` function returns a promise, so handle it
accordingly.

Fixes: 45ab2cd6be ("luci-mod-status: use network.getDSLModemType()")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/50_dsl.js
@@ -45,10 +45,12 @@ return baseclass.extend({
 	title: _('DSL'),
 
 	load: function() {
-		if (!network.getDSLModemType())
-			return Promise.reject();
+		return network.getDSLModemType().then(function(type) {
+			if (!type)
+				return Promise.reject();
 
-		return L.resolveDefault(callDSLMetrics(), {});
+			return L.resolveDefault(callDSLMetrics(), {});
+		});
 	},
 
 	render: function(dsl) {