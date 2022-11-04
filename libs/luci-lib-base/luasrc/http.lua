openwrt/luci

commit cea2c3578efe7ab36219f06985d01a4771aeb070

luci-lib-base: forward luci.http.context.request.message to ucode
Some existing LuCI application code accesses the
`luci.http.context.request.message.params` HTTP parameter table directly.

Forward the `luci.http.context.request.message` object to the ucode
`luci.http.message` instance in order to properly support this.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

libs/luci-lib-base/luasrc/http.lua
@@ -140,5 +140,6 @@ context = {
 		content        = function(self, ...) return content(...)        end;
 		getcookie      = function(self, ...) return getcookie(...)      end;
 		setfilehandler = function(self, ...) return setfilehandler(...) end;
+		message        = L.http.message
 	}
 }

commit 911845231380e8e4ecd5151493a561b1a3bfb4ad

luci-lib-base: ensure that `luci.http` can be required standalone
Various existing Lua software is requiring the `luci.http` library for
URL encoding/decoding tasks so ensure that it can be loaded in a stand
alone manner even if the emulated Lua runtime environment is not
available.

Fixes: cea2c3578e ("luci-lib-base: forward luci.http.context.request.message to ucode")
Ref: https://forum.openwrt.org/t/x/141817
Ref: https://github.com/openwrt/luci/pull/5976#issuecomment-1296220586
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

libs/luci-lib-base/luasrc/http.lua
@@ -140,6 +140,6 @@ context = {
 		content        = function(self, ...) return content(...)        end;
 		getcookie      = function(self, ...) return getcookie(...)      end;
 		setfilehandler = function(self, ...) return setfilehandler(...) end;
-		message        = L.http.message
+		message        = L and L.http.message
 	}
 }