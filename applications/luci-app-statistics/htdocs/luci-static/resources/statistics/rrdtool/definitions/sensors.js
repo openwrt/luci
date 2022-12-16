fcb96f53d7ffd286b1c8f4b577638f5ec34cfb3e
Uwe Kleine-König <uwe@kleine-koenig.org>
2022-12-16 23:22:04 +0100

    luci-app-statistics: Generate graphs for humidity sensors
    This fixes graph generation for sensors that provide humidity data.
    
    Suggested-by: Jo-Philipp Wich <jo@mein.io>
    Signed-off-by: Uwe Kleine-König <uwe@kleine-koenig.org>
    (cherry picked from commit 9f321b831f41b73554819ac54e308dfa3677ab45)

--- applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/sensors.js
+++ applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/sensors.js
@@ -7,20 +7,44 @@ return baseclass.extend({
 	title: _('Sensors'),
 
 	rrdargs: function(graph, host, plugin, plugin_instance, dtype) {
-		return {
-			per_instance: true,
-			title: "%H: %pi - %di",
-			vlabel: "\xb0C",
-			number_format: "%4.1lf\xb0C",
-			data: {
-				types: [ "temperature" ],
-				options: {
-					temperature__value: {
-						color: "ff0000",
-						title: "Temperature"
+		var rv = [];
+		var types = graph.dataTypes(host, plugin, plugin_instance);
+
+		if (types.indexOf('temperature') > -1) {
+			rv.push({
+				per_instance: true,
+				title: "%H: %pi - %di",
+				vlabel: "\xb0C",
+				number_format: "%4.1lf\xb0C",
+				data: {
+					types: [ "temperature" ],
+					options: {
+						temperature__value: {
+							color: "ff0000",
+							title: "Temperature"
+						}
 					}
 				}
-			}
-		};
+			});
+		}
+		if (types.indexOf('humidity') > -1) {
+			rv.push({
+				per_instance: true,
+				title: "%H: %pi - %di",
+				vlabel: "%RH",
+				number_format: "%4.1lf %%RH",
+				data: {
+					types: [ "humidity" ],
+					options: {
+						humidity__value: {
+							color: "0000ff",
+							title: "Humidity"
+						}
+					}
+				}
+			});
+		}
+
+		return rv;
 	}
 });
