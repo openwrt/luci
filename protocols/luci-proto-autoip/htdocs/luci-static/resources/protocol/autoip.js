openwrt/luci

commit 2965e527f578abda1dc58dd540343422455be3ac

luci-proto-autoip: add protocol support for avahi-autoipd
Fixes: #6162
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

protocols/luci-proto-autoip/htdocs/luci-static/resources/protocol/autoip.js
@@ -1,0 +1,16 @@
+'use strict';
+'require network';
+
+return network.registerProtocol('3g', {
+	getI18n: function() {
+		return _('Avahi IPv4LL');
+	},
+
+	getOpkgPackage: function() {
+		return 'avahi-autoipd';
+	},
+
+	renderFormOptions: function(s) {
+
+	}
+});

protocols/luci-proto-autoip/Makefile
@@ -1,0 +1,14 @@
+#
+# Copyright (C) 2008-2014 The LuCI Team <luci@lists.subsignal.org>
+#
+# This is free software, licensed under the Apache License, Version 2.0 .
+#
+
+include $(TOPDIR)/rules.mk
+
+LUCI_TITLE:=Support for Avahi IPv4LL configuration
+LUCI_DEPENDS:=+avahi-autoipd
+
+include ../../luci.mk
+
+# call BuildPackage - OpenWrt buildroot signature

commit ae47caf383c6da53f2345bd225c49b875cb0d514

luci-proto-autoip: fix protocol name
Fixes: 2965e527f5 ("luci-proto-autoip: add protocol support for avahi-autoipd")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

protocols/luci-proto-autoip/htdocs/luci-static/resources/protocol/autoip.js
@@ -1,7 +1,7 @@
 'use strict';
 'require network';
 
-return network.registerProtocol('3g', {
+return network.registerProtocol('autoip', {
 	getI18n: function() {
 		return _('Avahi IPv4LL');
 	},