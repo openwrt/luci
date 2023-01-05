66a42c944d190fa2931acd28c649994e7c84459b
Zuev Aleksandr <A.Zuev@stdev.su>
2023-01-06 01:14:21 +0400

    luci-app-https-dns-proxy: Change files names and replace bootstrap_dns
    Signed-off-by: Zuev Aleksandr <A.Zuev@stdev.su>
    (cherry picked from commit 1bf53a1b9a0e5178db2be7fc86877b0713f35577)

--- applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/io.adguard-dns.dns-family.lua
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/io.adguard-dns.dns-family.lua
@@ -2,7 +2,7 @@ return {
 	name = "dns-family.adguard.com",
 	label = _("AdGuard (Family Protection)"),
 	resolver_url = "https://dns-family.adguard.com/dns-query",
-	bootstrap_dns = "94.140.14.15,94.140.15.16,2a10:50c0::bad1:ff,2a10:50c0::bad2:ff",
+	bootstrap_dns = "94.140.14.140,94.140.14.141,2a10:50c0::1:ff,2a10:50c0::2:ff",
 	help_link = "https://adguard-dns.io/en/public-dns.html",
 	help_link_text = "AdGuard.com"
 }
