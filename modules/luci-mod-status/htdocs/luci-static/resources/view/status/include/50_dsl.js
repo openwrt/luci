06724c5a9c108d4b562281b8752003218abf35de
Jan Hoffmann <jan@3e8.eu>
2023-01-28 19:13:44 +0100

    luci-mod-status: group DSL metrics into sections for more readability
    Signed-off-by: Jan Hoffmann <jan@3e8.eu>

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/50_dsl.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/50_dsl.js
@@ -33,23 +33,35 @@ function renderbox(dsl) {
 				_('Line State'), dsl.state || '-',
 				_('Line Mode'), dsl.mode || '-',
 				_('Line Uptime'), '%t'.format(dsl.uptime),
-				_('Annex'), dsl.annex || '-',
+				_('Annex'), dsl.annex || '-'
+			]),
+			L.itemlist(E('span'), [
 				_('Data Rate'), format_values('%1000.3mb/s', dsl.downstream.data_rate, dsl.upstream.data_rate),
 				_('Max. Attainable Data Rate (ATTNDR)'), format_values('%1000.3mb/s', dsl.downstream.attndr, dsl.upstream.attndr),
-				_('Latency'), format_values_func(format_latency, dsl.downstream.interleave_delay, dsl.upstream.interleave_delay),
+				_('Latency'), format_values_func(format_latency, dsl.downstream.interleave_delay, dsl.upstream.interleave_delay)
+			]),
+			L.itemlist(E('span'), [
 				_('Line Attenuation (LATN)'), format_values('%.1f dB', dsl.downstream.latn, dsl.upstream.latn),
 				_('Signal Attenuation (SATN)'), format_values('%.1f dB', dsl.downstream.satn, dsl.upstream.satn),
 				_('Noise Margin (SNR)'), format_values('%.1f dB', dsl.downstream.snr, dsl.upstream.snr),
-				_('Aggregate Transmit Power (ACTATP)'), format_values('%.1f dB', dsl.downstream.actatp, dsl.upstream.actatp),
+				_('Aggregate Transmit Power (ACTATP)'), format_values('%.1f dB', dsl.downstream.actatp, dsl.upstream.actatp)
+			]),
+			L.itemlist(E('span'), [
 				_('Forward Error Correction Seconds (FECS)'), format_values('%d', dsl.errors.near.fecs, dsl.errors.far.fecs),
 				_('Errored seconds (ES)'), format_values('%d', dsl.errors.near.es, dsl.errors.far.es),
 				_('Severely Errored Seconds (SES)'), format_values('%d', dsl.errors.near.ses, dsl.errors.far.ses),
 				_('Loss of Signal Seconds (LOSS)'), format_values('%d', dsl.errors.near.loss, dsl.errors.far.loss),
-				_('Unavailable Seconds (UAS)'), format_values('%d', dsl.errors.near.uas, dsl.errors.far.uas),
+				_('Unavailable Seconds (UAS)'), format_values('%d', dsl.errors.near.uas, dsl.errors.far.uas)
+			]),
+			L.itemlist(E('span'), [
 				_('Header Error Code Errors (HEC)'), format_values('%d', dsl.errors.near.hec, dsl.errors.far.hec),
 				_('Non Pre-emptive CRC errors (CRC_P)'), format_values('%d', dsl.errors.near.crc_p, dsl.errors.far.crc_p),
-				_('Pre-emptive CRC errors (CRCP_P)'), format_values('%d', dsl.errors.near.crcp_p, dsl.errors.far.crcp_p),
-				_('ATU-C System Vendor ID'), dsl.atu_c.vendor || dsl.atu_c.vendor_id,
+				_('Pre-emptive CRC errors (CRCP_P)'), format_values('%d', dsl.errors.near.crcp_p, dsl.errors.far.crcp_p)
+			]),
+			L.itemlist(E('span'), [
+				_('ATU-C System Vendor ID'), dsl.atu_c.vendor || dsl.atu_c.vendor_id
+			]),
+			L.itemlist(E('span'), [
 				_('Power Management Mode'), dsl.power_state
 			])
 		])
