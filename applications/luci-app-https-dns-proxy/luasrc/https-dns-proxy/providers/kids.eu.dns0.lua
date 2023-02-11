3b10e00f19c0da9aaa06f36247b4dae9d558a778
artioni81 <artionkarreci@gmail.com>
2023-02-11 09:49:24 +0100

    Add DNS0.EU filtered for kids support
    add support for the new dns0.eu filtered for kids service
    
    Signed-off-by: artioni81 <artionkarreci@gmail.com>

---
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/kids.eu.dns0.lua
@@ -0,0 +1,6 @@
+return {
+	name = "kids.dns0.eu",
+	label = _("DNS0.EU Filtered for Kids"),
+	resolver_url = "https://kids.dns0.eu",
+	bootstrap_dns = "193.110.81.1,185.253.5.1,2a0f:fc80::1,2a0f:fc81::1"
+}
