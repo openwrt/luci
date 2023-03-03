openwrt/luci

commit 8673aef8db90b2ae276b9323f23d078cc68f4189

luci-mod-network: remove uciname validation from wireless interface
Remove the 'uciname' validation rule, as it prevents using '-' that
is a valid character.

Fixes: 0c1be9ae5 luci-mod-network: Add length restriction to wireless

Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -1159,7 +1159,7 @@ return view.extend({
 
 					o = ss.taboption('advanced', form.Value, 'ifname', _('Interface name'), _('Override default interface name'));
 					o.optional = true;
-					o.datatype = 'and(uciname,maxlength(15))';
+					o.datatype = 'maxlength(15)';
 					o.placeholder = radioNet.getIfname();
 					if (/^radio\d+\.network/.test(o.placeholder))
 						o.placeholder = '';

commit f090d4e27870785c3e3bda042a667ea70110887c

luci-mod-network: add stricter wireless interface name validation
Linux wireless interface names have the following restrictions:

 * It must not be an empty string
 * It must not be '.' or '..'
 * It must not contain any /, : or space character ( , \t, \n, ...)
 * It must be less than 16 chars
 * It likely must not contain any % either

Also added is a placeholder text for the field which describes the interface
name constraints.

Fixes: 8673aef8db luci-mod-network: remove uciname validation from wireless interface
Signed-off-by: Glenn Washburn <development@efficientek.com>

modules/luci-base/htdocs/luci-static/resources/validation.js
@@ -426,6 +426,15 @@ var ValidatorFactory = baseclass.extend({
 			return this.assert(this.value.match(/^[a-zA-Z0-9_]+$/), _('valid UCI identifier'));
 		},
 
+		netdevname: function() {
+			var v = this.value;
+
+			if (v == '.' || v == '..')
+				return this.assert(false, _('Must be a valid network device name'));
+
+			return this.assert(v.match(/^[^:/%\s]{1,15}$/), _('Must be a valid network device name'));
+		},
+
 		range: function(min, max) {
 			var val = this.factory.parseDecimal(this.value);
 			return this.assert(val >= +min && val <= +max, _('value between %f and %f').format(min, max));

modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -1150,9 +1150,9 @@ return view.extend({
 					o.depends('mode', 'ap');
 					o.depends('mode', 'ap-wds');
 
-					o = ss.taboption('advanced', form.Value, 'ifname', _('Interface name'), _('Override default interface name'));
+					o = ss.taboption('advanced', form.Value, 'ifname', _('Interface name'), _('Override default interface name - the name can not be ".", "..", nor contain the characters :, /, %, or whitespace and must contain from 1 to 15 characters'));
 					o.optional = true;
-					o.datatype = 'maxlength(15)';
+					o.datatype = 'netdevname';
 					o.placeholder = radioNet.getIfname();
 					if (/^radio\d+\.network/.test(o.placeholder))
 						o.placeholder = '';