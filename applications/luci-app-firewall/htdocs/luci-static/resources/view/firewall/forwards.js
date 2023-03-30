openwrt/luci

commit 48086e1c7b3e7094b6fbe61335209189b1006bfb

luci-app-firewall: Add ipset field to snats
Signed-off-by: Paul Dee <itsascambutmailmeanyway@gmail.com>

applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/snats.js
@@ -313,6 +313,17 @@ return view.extend({
 		o.depends({ proto: 'tcp', '!contains': true });
 		o.depends({ proto: 'udp', '!contains': true });
 
+		var have_fw4 = L.hasSystemFeature('firewall4')
+		if (!have_fw4) {
+			o = s.taboption('advanced', form.Value, 'ipset', _('Use ipset'));
+			uci.sections('firewall', 'ipset', function(s) {
+				if (typeof(s.name) == 'string')
+					this.value(s.name, s.comment ? '%s (%s)'.format(s.name, s.comment) : s.name);
+			});
+			o.modalonly = true;
+			o.rmempty = true;
+		}
+
 		o = s.taboption('advanced', widgets.DeviceSelect, 'device', _('Outbound device'),
 			_('Matches forwarded traffic using the specified outbound network device.'));
 		o.noaliases = true;
@@ -323,7 +334,7 @@ return view.extend({
 		fwtool.addLimitOption(s);
 		fwtool.addLimitBurstOption(s);
 
-		if (!L.hasSystemFeature('firewall4')) {
+		if (!have_fw4) {
 			o = s.taboption('advanced', form.Value, 'extra', _('Extra arguments'),
 				_('Passes additional arguments to iptables. Use with care!'));
 			o.modalonly = true;

commit 7a4afdcb05ad69fd9c0e39de9b44a59ad31130a7

application/luci-app-firewall: fix null access
Fixes: 48086e1c7b ("luci-app-firewall: Add ipset field to snats")
Fixes: d0d891c23e ("luci-app-firewall: Add ipset field to forwards (redirects)")
Fixes: f407a013ba ("luci-app-firewall: Add ipset field to rules")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/forwards.js
@@ -243,7 +243,7 @@ return view.extend({
 		o = s.taboption('advanced', form.Value, 'ipset', _('Use ipset'));
 		uci.sections('firewall', 'ipset', function(s) {
 			if (typeof(s.name) == 'string')
-				this.value(s.name, s.comment ? '%s (%s)'.format(s.name, s.comment) : s.name);
+				o.value(s.name, s.comment ? '%s (%s)'.format(s.name, s.comment) : s.name);
 		});
 		o.modalonly = true;
 		o.rmempty = true;

applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/rules.js
@@ -374,7 +374,7 @@ return view.extend({
 		o = s.taboption('advanced', form.Value, 'ipset', _('Use ipset'));
 		uci.sections('firewall', 'ipset', function(s) {
 			if (typeof(s.name) == 'string')
-				this.value(s.name, s.comment ? '%s (%s)'.format(s.name, s.comment) : s.name);
+				o.value(s.name, s.comment ? '%s (%s)'.format(s.name, s.comment) : s.name);
 		});
 		o.modalonly = true;
 		o.rmempty = true;

applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/snats.js
@@ -318,7 +318,7 @@ return view.extend({
 			o = s.taboption('advanced', form.Value, 'ipset', _('Use ipset'));
 			uci.sections('firewall', 'ipset', function(s) {
 				if (typeof(s.name) == 'string')
-					this.value(s.name, s.comment ? '%s (%s)'.format(s.name, s.comment) : s.name);
+					o.value(s.name, s.comment ? '%s (%s)'.format(s.name, s.comment) : s.name);
 			});
 			o.modalonly = true;
 			o.rmempty = true;