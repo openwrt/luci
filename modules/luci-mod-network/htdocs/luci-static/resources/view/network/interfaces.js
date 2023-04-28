openwrt/luci

commit 2be01cbfcb34746d9713173a60c3f8a69c554a68

luci-mod-network: restrict peerdns option to protocols that implemenent it
Only some supported protocol extensions honour the `peerdns` options.

Fixes: #6093
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -228,6 +228,22 @@ function get_netmask(s, use_cfgvalue) {
 	return subnetmask;
 }
 
+function has_peerdns(proto) {
+	switch (proto) {
+	case 'dhcp':
+	case 'qmi':
+	case 'ppp':
+	case 'pppoe':
+	case 'pppoa':
+	case 'pptp':
+	case 'openvpn':
+	case 'sstp':
+		return true;
+	}
+
+	return false;
+}
+
 var cbiRichListValue = form.ListValue.extend({
 	renderWidget: function(section_id, option_index, cfgvalue) {
 		var choices = this.transformChoices();
@@ -936,13 +952,13 @@ return view.extend({
 				o = nettools.replaceOption(s, 'advanced', form.Flag, 'defaultroute', _('Use default gateway'), _('If unchecked, no default route is configured'));
 				o.default = o.enabled;
 
-				if (protoval != 'static') {
+				if (has_peerdns(protoval)) {
 					o = nettools.replaceOption(s, 'advanced', form.Flag, 'peerdns', _('Use DNS servers advertised by peer'), _('If unchecked, the advertised DNS server addresses are ignored'));
 					o.default = o.enabled;
 				}
 
 				o = nettools.replaceOption(s, 'advanced', form.DynamicList, 'dns', _('Use custom DNS servers'));
-				if (protoval != 'static')
+				if (has_peerdns(protoval))
 					o.depends('peerdns', '0');
 				o.datatype = 'ipaddr';
 

commit b2476ba71de964828def0967793556bdb141d7e2

luci-mod-network: expose peerdns option for dhcpv6
Ref: https://github.com/openwrt/luci/commit/2be01cbfcb34746d9713173a60c3f8a69c554a68#commitcomment-111035308
Fixes: 2be01cbfcb ("luci-mod-network: restrict peerdns option to protocols that implemenent it")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -231,6 +231,7 @@ function get_netmask(s, use_cfgvalue) {
 function has_peerdns(proto) {
 	switch (proto) {
 	case 'dhcp':
+	case 'dhcpv6':
 	case 'qmi':
 	case 'ppp':
 	case 'pppoe':