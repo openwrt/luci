e4e5cd04481da4b52ebbeb12e8fd26f343acb2e9
Hippi Viking <33314937+hippi-viking@users.noreply.github.com>
2023-05-03 20:52:07 +0000

    Rework memory.js
    Simplify memory graph definitions, plus make it FreeBSD compatible.

--- applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/memory.js
+++ applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/memory.js
@@ -10,89 +10,63 @@ return baseclass.extend({
 	title: _('Memory'),
 
 	rrdargs: function(graph, host, plugin, plugin_instance, dtype) {
-		var p = [];
+		var definitions = [];
+		var instances;
 
-		var memory = {
-			title: "%H: Memory usage",
-			vlabel: "MB",
-			number_format: "%5.1lf%s",
-			y_min: "0",
-			alt_autoscale_max: true,
-			data: {
-				instances: {
-					memory: [
-						"free",
-						"buffered",
-						"cached",
-						"used"
-					]
-				},
+		function find_instances(dtype, wanted) {
+			var matching = graph.dataInstances(host, plugin, plugin_instance, dtype).filter(function(instance) {
+				return wanted.indexOf(instance) > -1;
+			});
 
-				options: {
-					memory_buffered: {
-						color: "0000ff",
-						title: "Buffered"
-					},
-					memory_cached: {
-						color: "ff00ff",
-						title: "Cached"
-					},
-					memory_used: {
-						color: "ff0000",
-						title: "Used"
-					},
-					memory_free: {
-						color: "00ff00",
-						title: "Free"
+			return matching.length ? { [dtype]: matching } : null;
+		}
+
+		if ((instances = find_instances('memory', [ 'free', 'buffered', 'cached', 'used', 'active', 'cache', 'inactive', 'wired' ])) != null) {
+			definitions.push({
+				title: "%H: Memory usage",
+				vlabel: "MB",
+				number_format: "%5.1lf%s",
+				y_min: "0",
+				alt_autoscale_max: true,
+				data: {
+					instances: instances,
+					options: {
+						memory_free: { color: "00ff00", title: "Free" },
+						memory_buffered: { color: "0000ff", title: "Buffered" },
+						memory_cached: { color: "ff00ff", title: "Cached" },
+						memory_used: { color: "ff0000", title: "Used" },
+						memory_active: { color: "880000", title: "Active" },
+						memory_cache: { color: "ff00ff", title: "Cache" },
+						memory_inactive: { color: "0000ff", title: "Inactive" },
+						memory_wired: { color: "ff0000", title: "Wired" }
 					}
 				}
-			}
-		};
+			});
+		}
 
-		var percent = {
-			title: "%H: Memory usage",
-			vlabel: "Percent",
-			number_format: "%5.1lf%%",
-			y_min: "0",
-			alt_autoscale_max: true,
-			data: {
-				instances: {
-					percent: [
-						"free",
-						"buffered",
-						"cached",
-						"used"
-					]
-				},
-				options: {
-					percent_buffered: {
-						color: "0000ff",
-						title: "Buffered"
-					},
-					percent_cached: {
-						color: "ff00ff",
-						title: "Cached"
-					},
-					percent_used: {
-						color: "ff0000",
-						title: "Used"
-					},
-					percent_free: {
-						color: "00ff00",
-						title: "Free"
+		if ((instances = find_instances('percent', [ 'free', 'buffered', 'cached', 'used', 'active', 'cache', 'inactive', 'wired' ])) != null) {
+			definitions.push({
+				title: "%H: Memory usage",
+				vlabel: "MB",
+				number_format: "%5.1lf%s",
+				y_min: "0",
+				alt_autoscale_max: true,
+				data: {
+					instances: instances,
+					options: {
+						percent_free: { color: "00ff00", title: "Free" },
+						percent_buffered: { color: "0000ff", title: "Buffered" },
+						percent_cached: { color: "ff00ff", title: "Cached" },
+						percent_used: { color: "ff0000", title: "Used" },
+						percent_active: { color: "880000", title: "Active" },
+						percent_cache: { color: "ff00ff", title: "Cache" },
+						percent_inactive: { color: "0000ff", title: "Inactive" },
+						percent_wired: { color: "ff0000", title: "Wired" }
 					}
 				}
-			}
-		};
+			});
+		}
 
-		var types = graph.dataTypes(host, plugin, plugin_instance);
-
-		for (var i = 0; i < types.length; i++)
-			if (types[i] == 'percent')
-				p.push(percent);
-			else if (types[i] == 'memory')
-				p.push(memory);
-
-		return p;
+		return definitions;
 	}
 });
