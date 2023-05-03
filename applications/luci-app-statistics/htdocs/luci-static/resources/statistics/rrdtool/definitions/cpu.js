cd3ae55d304c163f5a941bc449b6e8c718744fb4
Hippi Viking <33314937+hippi-viking@users.noreply.github.com>
2023-05-03 21:27:19 +0000

    Rework cpu.js
    Simplify CPU definitions, plus make it FreeBSD compatible

--- applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/cpu.js
+++ applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/cpu.js
@@ -8,7 +8,8 @@ return baseclass.extend({
 	title: _('Processor'),
 
 	rrdargs: function(graph, host, plugin, plugin_instance, dtype) {
-		var p = [];
+		var definitions = [];
+		var instances;
 
 		var title = "%H: Processor usage";
 
@@ -17,130 +18,63 @@ return baseclass.extend({
 
 		var show_idle = uci.get("luci_statistics", "collectd_cpu", "ShowIdle") == "1" ? true : false;
 
+		function find_instances(dtype, wanted) {
+			var matching = graph.dataInstances(host, plugin, plugin_instance, dtype).filter(function(instance) {
+				return wanted.indexOf(instance) > -1;
+			});
+			
+			return matching.length ? { [dtype]: matching } : null;
+		}
+		
 		if (uci.get("luci_statistics", "collectd_cpu", "ReportByState") == "1") {
-			var cpu = {
-				title: title,
-				y_min: "0",
-				alt_autoscale_max: true,
-				vlabel: "Jiffies",
-				number_format: "%5.1lf",
-				data: {
-					instances: {
-						cpu: [
-							...(show_idle ? ["idle"] : []),
-							"nice",
-							"user",
-							"wait",
-							"system",
-							"softirq",
-							"interrupt",
-							"steal"
-						]
-					},
-					options: {
-						cpu_idle: {
-							color: "ffffff",
-							title: "Idle"
-						},
-						cpu_interrupt: {
-							color: "a000a0",
-							title: "Interrupt"
-						},
-						cpu_nice: {
-							color: "00e000",
-							title: "Nice"
-						},
-						cpu_softirq: {
-							color: "ff00ff",
-							title: "Softirq"
-						},
-						cpu_steal: {
-							color: "000000",
-							title: "Steal"
-						},
-						cpu_system: {
-							color: "ff0000",
-							title: "System"
-						},
-						cpu_user: {
-							color: "0000ff",
-							title: "User"
-						},
-						cpu_wait: {
-							color: "ffb000",
-							title: "Wait I/O"
+			if ((instances = find_instances('cpu', [...(show_idle ? ['idle'] : []), 'nice', 'user', 'wait', 'system', 'softirq', 'interrupt', 'steal'])) != null) {
+				definitions.push({
+					title: title,
+					y_min: "0",
+					alt_autoscale_max: true,
+					vlabel: "Jiffies",
+					number_format: "%5.1lf",
+					data: {
+						instances: instances,
+						options: {
+							cpu_idle: { color: "ffffff", title: "Idle"},
+							cpu_nice: { color: "00e000", title: "Nice"},
+							cpu_user: { color: "0000ff", title: "User"},
+							cpu_wait: { color: "ffb000", title: "Wait I/O"},
+							cpu_system: { color: "ff0000", title: "System"},
+							cpu_softirq: { color: "ff00ff", title: "Softirq"},
+							cpu_interrupt: { color: "a000a0", title: "Interrupt"},
+							cpu_steal: { color: "000000", title: "Steal"}
 						}
 					}
-				}
-			};
-
-			var percent = {
-				title: title,
-				y_min: "0",
-				y_max: "2",
-				alt_autoscale_max: true,
-				vlabel: "Percent",
-				number_format: "%5.1lf%%",
-				data: {
-					instances: {
-						percent: [
-							...(show_idle ? ["idle"] : []),
-							"nice",
-							"user",
-							"wait",
-							"system",
-							"softirq",
-							"interrupt",
-							"steal"
-						]
-					},
-					options: {
-						percent_idle: {
-							color: "ffffff",
-							title: "Idle"
-						},
-						percent_interrupt: {
-							color: "a000a0",
-							title: "Interrupt"
-						},
-						percent_nice: {
-							color: "00e000",
-							title: "Nice"
-						},
-						percent_softirq: {
-							color: "ff00ff",
-							title: "Softirq"
-						},
-						percent_steal: {
-							color: "000000",
-							title: "Steal"
-						},
-						percent_system: {
-							color: "ff0000",
-							title: "System"
-						},
-						percent_user: {
-							color: "0000ff",
-							title: "User"
-						},
-						percent_wait: {
-							color: "ffb000",
-							title: "Wait I/O"
-						}
-					}
-				}
-			};
-
-			var types = graph.dataTypes(host, plugin, plugin_instance);
-
-			for (var i = 0; i < types.length; i++)
-				if (types[i] == 'cpu')
-					p.push(cpu);
-				else if (types[i] == 'percent')
-					p.push(percent);
+				});
+			}
+			
+			if ((instances = find_instances('percent', [...(show_idle ? ['idle'] : []), 'nice', 'user', 'wait', 'system', 'softirq', 'interrupt', 'steal'])) != null) {
+				definitions.push({
+					title: title,
+                                        y_min: "0",
+                                        alt_autoscale_max: true,
+                                        vlabel: "Percent",
+                                        number_format: "%5.1lf%%",
+                                        data: {
+                                                instances: instances,
+                                                options: {
+                                                        percent_idle: { color: "ffffff", title: "Idle"},
+                                                        percent_nice: { color: "00e000", title: "Nice"},
+                                                        percent_user: { color: "0000ff", title: "User"},
+                                                        percent_wait: { color: "ffb000", title: "Wait I/O"},
+                                                        percent_system: { color: "ff0000", title: "System"},
+                                                        percent_softirq: { color: "ff00ff", title: "Softirq"},
+                                                        percent_interrupt: { color: "a000a0", title: "Interrupt"},
+                                                        percent_steal: { color: "000000", title: "Steal"}
+                                                }
+                                        }
+                                });
+                        }
 		}
 		else {
-			p = {
+			definitions = {
 				title: title,
 				y_min: "0",
 				y_max: "2",
@@ -163,6 +97,6 @@ return baseclass.extend({
 			};
 		}
 
-		return p;
+		return definitions;
 	}
 });
