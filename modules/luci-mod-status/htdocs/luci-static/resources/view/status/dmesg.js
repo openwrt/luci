b8ac00c3ebb0c2a37087ffdac7339abb476eaab8
Nik Mid <midaranik@gmail.com>
2023-06-06 12:40:50 +0100

    luci-mod-status: dmesg offsets as date/time
    Provide date/time in dmesg output instead of offsets. Resolution goes to
    millis. If that is not good enough then will look to extend with a
    toggle: offset/date-time.
    
    Signed-off-by: Nik Midara <midaranik@gmail.com>

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/dmesg.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/dmesg.js
@@ -2,19 +2,61 @@
 'require view';
 'require fs';
 'require ui';
+'require rpc';
+
+const monthAbbrevs = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",	"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+var callSystemInfo = rpc.declare({
+	object: 'system',
+	method: 'info'
+});
+
+function todatetime(timestamp) {
+	var date = new Date(timestamp * 1000);
+
+	return '%04d-%s-%02d %02d:%02d:%02d.%03d'.format(
+		date.getUTCFullYear(),
+		monthAbbrevs[date.getUTCMonth()],
+		date.getUTCDate(),
+		date.getUTCHours(),
+		date.getUTCMinutes(),
+		date.getUTCSeconds(),
+		date.getUTCMilliseconds()
+	);
+}
 
 return view.extend({
 	load: function() {
-		return fs.exec_direct('/bin/dmesg', [ '-r' ]).catch(function(err) {
+		return Promise.all([
+			L.resolveDefault(fs.exec_direct('/bin/dmesg', [ '-r' ]), {}),
+			L.resolveDefault(L.resolveDefault(callSystemInfo(), {}))
+		]).catch(function(err) {
 			ui.addNotification(null, E('p', {}, _('Unable to load log data: ' + err.message)));
 			return '';
 		});
 	},
 
-	render: function(logdata) {
-		var loglines = logdata.trim().split(/\n/).map(function(line) {
-			return line.replace(/^<\d+>/, '');
-		});
+	render: function(data) {
+		var logdata     = data[0],
+			systeminfo  = data[1];
+
+		var loglines = null;
+
+		if (systeminfo.localtime) {
+			var startTime = systeminfo.localtime - systeminfo.uptime;
+
+			loglines = logdata.trim().split(/\n/).map(function(line) {
+				return line.replace(/^<\d+>\[\ *(?<offset>\d+\.\d+)\](?<rest>.*)/, (...match) => {
+					let groups = match.pop();
+					return `${todatetime(startTime + Number.parseFloat(groups.offset))} ${groups.rest}`;
+				  });
+			});
+		}
+		else {
+			loglines = logdata.trim().split(/\n/).map(function(line) {
+				return line.replace(/^<\d+>/, '');
+			});
+		}
 
 		return E([], [
 			E('h2', {}, [ _('Kernel Log') ]),
