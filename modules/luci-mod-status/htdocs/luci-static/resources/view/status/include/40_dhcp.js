5c08a697bc4b05a4dccb260d1d816b8fa5c75cd0
CharlesMengCA <CharlesMeng@outlook.com>
2023-08-21 22:09:43 -0400

    luci-mod-status: Hide Active DHCP Leases table(s) if there are no active leases
    If the DHCP Server is disabled, showing empty lease table on status page will be redundant.
    
    Signed-off-by: CharlesMengCA <CharlesMeng@outlook.com>

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/40_dhcp.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/40_dhcp.js
@@ -66,6 +66,10 @@ return baseclass.extend({
 		    hosts = uci.sections('dhcp', 'host'),
 		    isReadonlyView = !L.hasViewPermission();
 
+		if (leases.length < 1 && leases6.length < 1) {
+			return E([]);
+		}
+
 		for (var i = 0; i < hosts.length; i++) {
 			var host = hosts[i];
 
@@ -178,12 +182,17 @@ return baseclass.extend({
 			return rows;
 		}, this)), E('em', _('There are no active leases')));
 
-		return E([
-			E('h3', _('Active DHCP Leases')),
-			table,
-			E('h3', _('Active DHCPv6 Leases')),
-			table6
-		]);
+		var result = [];
+
+		if (leases.length > 0) {
+			result.push(E('h3', _('Active DHCP Leases')), table)
+		}
+
+		if (leases6.length > 0) {
+			result.push(E('h3', _('Active DHCPv6 Leases')), table6)
+		}
+
+		return E(result);
 	},
 
 	render: function(data) {
