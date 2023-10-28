dcd77b30430532c35a8986b4bae885ed80ef5143
Ramon00 <github@flightfoil.com>
2023-10-28 22:26:10 +0200

    First version of luci-app-usteer
    This implements the 1st version of luci-app-usteer. 2 pages, 1 for settings 1 for status of connected clients.
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update status.js
    
    Update status.js
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update status.js
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update settings.js
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update luci-app-usteer.json
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update status.js
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update settings.js
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update settings.js
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update settings.js
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update settings.js
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update luci-app-usteer.json
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update status.js
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Rename Makefile.txt to Makefile
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update luci-app-usteer.json
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update settings.js
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Delete applications/luci-app-usteer/root/etc/uci-defaults/80_usteer.txt
    
    Signed-off-by: Ramon00 <github@flightfoil.com>
    
    Update Makefile.txt
    
    Signed-off-by: Ramon00 <github@flightfoil.com>

---
+++ applications/luci-app-usteer/htdocs/luci-static/resources/view/usteer/settings.js
@@ -0,0 +1,206 @@
+//'use strict';
+'require view';
+'require form';
+
+return view.extend({
+	render: function() {
+	var m, s, o;
+
+	m = new form.Map('usteer', _('usteer'), _('usteer Configuration.'));
+
+	s = m.section(form.TypedSection, 'usteer', _('options'), _('First four options are mandatory. Also be sure to enable rrm reports, 80211kvr, etc see: https://openwrt.org/docs/guide-user/network/wifi/usteer'));
+	s.anonymous = true;
+
+	s.option(form.Value, 'network', _('network'), _('The network interface for inter-AP communication)'));
+
+	o = s.option(form.Flag, 'syslog', _('Syslog'), _('Log messages to syslog (0/1)'));
+	o.default = '1';
+	o.rmempty = false;
+
+	o = s.option(form.Flag, 'ipv6', _('ipv6 mode'), _('Use IPv6 for remote exchange'));
+	o.rmempty = false;
+
+	o = s.option(form.ListValue, 'debug_level', _('debug level'), _('Debug level'));
+	o.placeholder = 'lan';
+	o.value('0','0 Fatal');
+	o.value('1','1 info');
+	o.value('2','2 Verbose');
+	o.value('3','3 Some debug');
+	o.value('4','4 network packet info');
+	o.value('5','5 all debug messages');
+	o.rmempty = false;
+	o.editable = true;
+
+	o = s.option(form.Value, 'max_neighbour_reports', _('max neighbour reports'), _('Maximum number of neighbor reports set for a node'));
+	o.optional    = true;
+	o.placeholder = 8;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'sta_block_timeout', _('sta block timeout'), _('Maximum amount of time (ms) a station may be blocked due to policy decisions'));
+	o.optional    = true;
+	o.placeholder = 30000;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'local_sta_timeout', _('local sta timeout'), _('Maximum amount of time (ms) a local unconnected station is tracked'));
+	o.optional    = true;
+	o.placeholder = 12000;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'measurement_report_timeout', _('measurement report timeout'), _('Maximum amount of time (ms) a measurement report is stored'));
+	o.optional    = true;
+	o.placeholder = 120000;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'local_sta_update', _('local_sta_update'), _('Local station information update interval (ms)'));
+	o.optional    = true;
+	o.placeholder = 1000;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'max_retry_band', _('max retry band'), _('Maximum number of consecutive times a station may be blocked by policy'));
+	o.optional    = true;
+	o.placeholder = 5;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'seen_policy_timeout', _('seen policy timeout'), _('Maximum idle time of a station entry (ms) to be considered for policy decisions'));
+	o.optional    = true;
+	o.placeholder = 30000;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'load_balancing_threshold', _('load_balancing_threshold'), _('Minimum number of stations delta between APs before load balancing policy is active'));
+	o.optional    = true;
+	o.placeholder = 5;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'band_steering_threshold', _('band_steering_threshold'), _('Minimum number of stations delta between bands before band steering policy is active'));
+	o.optional    = true;
+	o.placeholder = 5;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'remote_update_interval', _('remote_update_interval'), _('Interval (ms) between sending state updates to other APs'));
+	o.optional    = true;
+	o.placeholder = 1000;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'remote_node_timeout', _('remote_node_timeout'), _('Number of remote update intervals after which a remote-node is deleted'));
+	o.optional    = true;
+	o.placeholder = 10;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Flag, 'assoc_steering', _('assoc_steering'), _('Allow rejecting assoc requests for steering purposes (0/1)'));
+	o.optional    = true;
+
+	o = s.option(form.Value, 'min_connect_snr', _('min_connect_snr'), _('Minimum signal-to-noise ratio or signal level (dBm) to allow connections'));
+	o.optional    = true;
+	o.placeholder = 0;
+	o.datatype    = 'integer';
+
+	o = s.option(form.Value, 'min_snr', _('min_snr'), _('Minimum signal-to-noise ratio or signal level (dBm) to remain connected'));
+	o.optional    = true;
+	o.placeholder = 0;
+	o.datatype    = 'integer';
+
+	o = s.option(form.Value, 'min_snr_kick_delay', _('min_snr_kick_delay'), _('Timeout after which a station with snr < min_snr will be kicked'));
+	o.optional    = true;
+	o.placeholder = 5000;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'roam_process_timeout', _('roam_process_timeout'), _('Timeout (in ms) after which a association following a disassociation is not seen as a roam'));
+	o.optional    = true;
+	o.placeholder = 5000;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'roam_scan_snr', _('roam_scan_snr'), _('Minimum signal-to-noise ratio or signal level (dBm) before attempting to trigger client scans for roam'));
+	o.optional    = true;
+	o.placeholder = 0;
+	o.datatype    = 'integer';
+
+	o = s.option(form.Value, 'roam_scan_tries', _('roam_scan_tries'), _('Maximum number of client roaming scan trigger attempts'));
+	o.optional    = true;
+	o.placeholder = 3;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'roam_scan_timeout', _('roam_scan_timeout'), _('Retry scanning when roam_scan_tries is exceeded after this timeout (in ms). In case this option is set to 0, the client is kicked instead'));
+	o.optional    = true;
+	o.placeholder = 0;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'roam_scan_interval', _('roam_scan_interval'), _('Minimum time (ms) between client roaming scan trigger attempts'));
+	o.optional    = true;
+	o.placeholder = 10000;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'roam_trigger_snr', _('roam_trigger_snr'), _('Minimum signal-to-noise ratio or signal level (dBm) before attempting to trigger forced client roaming'));
+	o.optional    = true;
+	o.placeholder = 0;
+	o.datatype    = 'integer';
+
+	o = s.option(form.Value, 'roam_trigger_interval', _('roam_trigger_interval'), _('Minimum time (ms) between client roaming trigger attempts'));
+	o.optional    = true;
+	o.placeholder = 60000;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'roam_kick_delay', _('roam_kick_delay'), _('Timeout (in 100ms beacon intervals) for client roam requests'));
+	o.optional    = true;
+	o.placeholder = 100;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'signal_diff_threshold', _('signal_diff_threshold'), _('Minimum signal strength difference until AP steering policy is active'));
+	o.optional    = true;
+	o.placeholder = 0;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'initial_connect_delay', _('initial_connect_delay'), _('Initial delay (ms) before responding to probe requests (to allow other APs to see packets as well)'));
+	o.optional    = true;
+	o.placeholder = 0;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Flag, 'load_kick_enabled', _('load_kick_enabled'), _('Enable kicking client on excessive channel load (0/1)'));
+	o.optional    = true;
+
+	o = s.option(form.Value, 'load_kick_threshold', _('load_kick_threshold'), _('Minimum channel load (%) before kicking clients'));
+	o.optional    = true;
+	o.placeholder = 75;
+	o.datatype    = 'uinteger';
+	
+	o = s.option(form.Value, 'load_kick_delay', _('load_kick_delay'), _('Minimum amount of time (ms) that channel load is above threshold before starting to kick clients'));
+	o.optional    = true;
+	o.placeholder = 10000;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'load_kick_min_clients', _('load_kick_min_clients'), _('Minimum number of connected clients before kicking based on channel load'));
+	o.optional    = true;
+	o.placeholder = 10;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'load_kick_reason_code', _('load_kick_reason_code'), _('Reason code on client kick based on channel load (default: WLAN_REASON_DISASSOC_AP_BUSY)'));
+	o.optional    = true;
+	o.placeholder = 5;
+	o.datatype    = 'uinteger';
+
+	o = s.option(form.Value, 'node_up_script', _('node_up_script'), _('Script to run after bringing up a node'));
+	o.optional    = true;
+	o.datatype    = 'string';
+
+	o = s.option(form.MultiValue, 'event_log_types', _('event_log_types'), _('Message types to include in log. Available types: probe_req_accept probe_req_deny, auth_req_accept, auth_req_deny, assoc_req_accept, assoc_req_deny, load_kick_trigger, load_kick_reset, load_kick_min_clients, load_kick_no_client, load_kick_client, signal_kick'));
+	o.value('probe_req_accept');
+	o.value('probe_req_deny');
+	o.value('auth_req_accept');
+	o.value('auth_req_deny');
+	o.value('assoc_req_accept');
+	o.value('assoc_req_deny');
+	o.value('load_kick_trigger');
+	o.value('load_kick_reset');
+	o.value('load_kick_min_clients');
+	o.value('load_kick_no_client');
+	o.value('load_kick_client');
+	o.value('signal_kick');
+	o.optional    = true;
+	o.datatype    = 'list(string)';
+
+	o = s.option(form.DynamicList, 'ssid_list', _('ssid_list'), _('List of SSIDs to enable steering on'));
+	o.optional    = true;
+	o.datatype    = 'list(string)';
+
+	return m.render();
+	},
+});
---
+++ applications/luci-app-usteer/htdocs/luci-static/resources/view/usteer/status.js
@@ -0,0 +1,264 @@
+'use strict';
+'require view';
+'require rpc';
+'require poll';
+'require dom';
+'require ui';
+
+return view.extend({
+	
+	callHostHints: rpc.declare({
+		object: 'luci-rpc',
+		method: 'getHostHints',
+		expect: { '': {} }
+	}),
+	callGetRemotehosts: rpc.declare({
+		object: 'usteer',
+		method: 'remote_hosts',
+		expect: {  '': {}}
+	}),
+	callGetRemoteinfo: rpc.declare({
+		object: 'usteer',
+		method: 'remote_info',
+		expect: { '': {} }
+	}),
+	callGetLocalinfo: rpc.declare({
+		object: 'usteer',
+		method: 'local_info',
+		expect: { '': {} }
+	}),
+	callGetClients: rpc.declare({
+		object: 'usteer',
+		method: 'get_clients',
+		expect: { '': {} }
+	}),
+
+
+	load: function() {
+		return Promise.all([
+			this.callHostHints(),
+			this.callGetRemotehosts(),
+			this.callGetRemoteinfo(),
+			this.callGetLocalinfo(),
+			this.callGetClients()
+			
+		]);
+	},
+	handleReset: null,
+	handleSaveApply: null,
+	handleSave: null,
+	
+	render: function(data) {
+	
+		var hosts = data[0];
+		var Remotehosts=data[1];
+		var Remoteinfo=data[2];
+		var Localinfo=data[3];
+		var Clients=data[4];
+
+
+
+		var body = E([
+			E('h2', _('Usteer status'))
+		]);
+
+
+/////////////////////////
+		body.appendChild(
+			E('h3', 'Remotehosts')
+		);
+		var remotehost_table = E('table', { 'class': 'table cbi-section-table' }, [
+			E('tr', { 'class': 'tr table-titles' }, [
+				E('th', { 'class': 'th' }, _('IP address')),
+				E('th', { 'class': 'th' }, _('identifier'))
+			])
+		]);
+		
+		var remotehosttableentries =[];
+		for(var IPaddr in Remotehosts) {
+			remotehosttableentries.push([
+							IPaddr, Remotehosts[IPaddr]['id']
+						]);
+				}
+
+		cbi_update_table(remotehost_table, remotehosttableentries, E('em', _('No data')));
+		body.appendChild(remotehost_table);
+		
+/////////////////////////
+		body.appendChild(
+			E('h3', 'Client list')
+		);
+		var connectioninfo_table = E('table', { 'class': 'table cbi-section-table' }, [
+			E('tr', { 'class': 'tr table-titles' }, [
+				E('th', { 'class': 'th' }, _('wlan')),
+				E('th', { 'class': 'th' }, _('bssid')),
+				E('th', { 'class': 'th' }, _('ssid')),
+				E('th', { 'class': 'th' }, _('freq')),
+				E('th', { 'class': 'th' }, _('n assoc')),
+				E('th', { 'class': 'th' }, _('noise')),
+				E('th', { 'class': 'th' }, _('load')),
+				E('th', { 'class': 'th' }, _('max assoc')),
+				E('th', { 'class': 'th' }, _('roam src')),
+				E('th', { 'class': 'th' }, _('roam tgt')),
+				E('th', { 'class': 'th' }, _('rrm_nr mac')),
+				E('th', { 'class': 'th' }, _('rrm_nr hex'))
+				
+			])
+		]);
+	
+		var connectioninfo_table_entries =[];
+		for(var wlan in Localinfo) {
+			connectioninfo_table_entries.push([
+							'<nobr>'+wlan+'</nobr>', 
+							Localinfo[wlan]['bssid'],
+							Localinfo[wlan]['ssid'],
+							Localinfo[wlan]['freq'],
+							Localinfo[wlan]['n_assoc'],
+							Localinfo[wlan]['noise'],
+							Localinfo[wlan]['load'],
+							Localinfo[wlan]['max_assoc'],
+							Localinfo[wlan]['roam_events']['source'],
+							Localinfo[wlan]['roam_events']['target'],
+							Localinfo[wlan]['rrm_nr'][0],
+							Localinfo[wlan]['rrm_nr'][2]
+						]);
+				}	
+		for(var wlan in Remoteinfo) {
+			connectioninfo_table_entries.push([
+							'<nobr>'+wlan+'</nobr>', 
+							Remoteinfo[wlan]['bssid'],
+							Remoteinfo[wlan]['ssid'],
+							Remoteinfo[wlan]['freq'],
+							Remoteinfo[wlan]['n_assoc'],
+							Remoteinfo[wlan]['noise'],
+							Remoteinfo[wlan]['load'],
+							Remoteinfo[wlan]['max_assoc'],
+							Remoteinfo[wlan]['roam_events']['source'],
+							Remoteinfo[wlan]['roam_events']['target'],
+							Remoteinfo[wlan]['rrm_nr'][0],
+							Remoteinfo[wlan]['rrm_nr'][2]
+
+						]);
+				}	
+		cbi_update_table(connectioninfo_table, connectioninfo_table_entries, E('em', _('No data')));
+		body.appendChild(connectioninfo_table);
+/////////////////////////////////////////
+
+
+		var compactconnectioninfo_table = E('table', { 'class': 'table cbi-section-table' }, [
+				E('tr', { 'class': 'tr table-titles' }, [
+					E('th', { 'class': 'th' }, _('wlan')),
+					E('th', { 'class': 'th' }, _('ssid')),
+					E('th', { 'class': 'th' }, _('freq')),
+					E('th', { 'class': 'th' }, _('load')),
+					E('th', { 'class': 'th' }, _('n')),
+					E('th', { 'class': 'th' }, _('host'))
+					
+				])
+			]);
+		var compactconnectioninfo_table_entries =[];
+		for(var wlan in Localinfo) {
+			var hostl=''
+			for(var mac in Clients) {
+				if (typeof Clients[mac] !== 'undefined') 
+					if (typeof Clients[mac][wlan] !== 'undefined') 
+					if (String(Clients[mac][wlan]['connected']).valueOf()==String("true").valueOf()) {
+							var foundname=mac;
+							var macUp=String(mac).toUpperCase()
+							if (typeof hosts[macUp] !== 'undefined') {
+								if ((String(hosts[macUp]['ipaddrs'][0]).length>0) && (typeof hosts[macUp]['ipaddrs'][0] !== 'undefined')) {
+									foundname=hosts[macUp]['ipaddrs'][0];
+								}
+								if ((String(hosts[macUp]['name']).length>0) && (typeof hosts[macUp]['name'] !== 'undefined')) {
+									foundname=hosts[macUp]['name'];
+								}
+							}
+							hostl=hostl+ foundname+'&emsp;';
+				}
+			}		
+			compactconnectioninfo_table_entries.push([
+								'<nobr>'+wlan+'</nobr>', 
+								Localinfo[wlan]['ssid'],
+								Localinfo[wlan]['freq'],
+								Localinfo[wlan]['load'],
+								Localinfo[wlan]['n_assoc'],
+								hostl
+							]);
+		}
+		for(var wlan in Remoteinfo) {
+			var hostl=''
+			for(var mac in Clients) {
+				if (typeof Clients[mac] !== 'undefined') 
+					if (typeof Clients[mac][wlan] !== 'undefined') 
+					if (String(Clients[mac][wlan]['connected']).valueOf()==String("true").valueOf()) {
+							var foundname=mac;
+							var macUp=String(mac).toUpperCase()
+							if (typeof hosts[macUp] !== 'undefined') {
+								if ((String(hosts[macUp]['ipaddrs'][0]).length>0) && (typeof hosts[macUp]['ipaddrs'][0] !== 'undefined')) {
+									foundname=hosts[macUp]['ipaddrs'][0]; 
+								}
+								if ((String(hosts[macUp]['name']).length>0) &&	(typeof hosts[macUp]['name'] !== 'undefined')) {
+									foundname=hosts[macUp]['name'];
+								}
+							}
+							hostl=hostl+ foundname+'&emsp;';
+				}
+			}	
+			compactconnectioninfo_table_entries.push([
+								'<nobr>'+wlan+'</nobr>', 
+								Remoteinfo[wlan]['ssid'],
+								Remoteinfo[wlan]['freq'],
+								Remoteinfo[wlan]['load'],
+								Remoteinfo[wlan]['n_assoc'],
+								hostl
+							]);
+		}
+
+		cbi_update_table(compactconnectioninfo_table, compactconnectioninfo_table_entries, E('em', _('No data')));
+		body.appendChild(compactconnectioninfo_table);
+
+
+/////////////////////////
+		body.appendChild(
+			E('h3', 'Hearing map')
+		);
+		for(var mac in Clients) {
+			var maciphost='';
+			maciphost='Mac: '+mac;
+			var macUp=String(mac).toUpperCase()
+				if (typeof hosts[macUp] !== 'undefined') {
+					if (typeof hosts[macUp]['ipaddrs'] !== 'undefined') 
+						maciphost=maciphost+'&emsp;IP: '+hosts[macUp]['ipaddrs'];
+					if (typeof hosts[macUp]['name'] !== 'undefined') 
+						maciphost=maciphost+'&emsp;Host: '+hosts[macUp]['name'];
+				}
+			body.appendChild(
+				E('h4', maciphost)
+			);
+			var client_table = E('table', { 'class': 'table cbi-section-table' }, [
+				E('tr', { 'class': 'tr table-titles' }, [
+					E('th', { 'class': 'th' }, _('wlan')),
+					E('th', { 'class': 'th' }, _('connected')),
+					E('th', { 'class': 'th' }, _('signal'))
+				])
+			]);
+		
+			var client_table_entries =[];
+			for(var wlanc in Clients[mac]) {
+				client_table_entries.push([
+					'<nobr>'+wlanc+'</nobr>', 
+					(String(Clients[mac][wlanc]['connected']).valueOf()==String("true").valueOf()) ? "True" : "",
+					Clients[mac][wlanc]['signal']
+				]);
+			}	
+			cbi_update_table(client_table, client_table_entries, E('em', _('No data')));
+			body.appendChild(client_table);
+		}
+	
+		return body;
+
+	}
+
+
+});
+
