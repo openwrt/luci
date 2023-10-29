c5f6e2b9897a2b9d407d24d22b5812ff2713f7b7
Ramon00 <github@flightfoil.com>
2023-10-29 16:36:54 +0100

    Update settings.js
    Signed-off-by: Ramon00 <github@flightfoil.com>

--- applications/luci-app-usteer/htdocs/luci-static/resources/view/usteer/settings.js
+++ applications/luci-app-usteer/htdocs/luci-static/resources/view/usteer/settings.js
@@ -18,7 +18,6 @@ return view.extend({
 	o.rmempty = false;
 
 	o = s.option(form.Flag, 'ipv6', _('ipv6 mode'), _('Use IPv6 for remote exchange'));
-	o.default = '0';
 	o.rmempty = false;
 
 	o = s.option(form.ListValue, 'debug_level', _('debug level'), _('Debug level'));
@@ -31,7 +30,7 @@ return view.extend({
 	o.value('5','5 all debug messages');
 	o.rmempty = false;
 	o.editable = true;
-	
+
 	o = s.option(form.Value, 'max_neighbour_reports', _('max neighbour reports'), _('Maximum number of neighbor reports set for a node'));
 	o.optional    = true;
 	o.placeholder = 8;
@@ -66,12 +65,12 @@ return view.extend({
 	o.optional    = true;
 	o.placeholder = 30000;
 	o.datatype    = 'uinteger';
-	
+
 	o = s.option(form.Value, 'load_balancing_threshold', _('load_balancing_threshold'), _('Minimum number of stations delta between APs before load balancing policy is active'));
 	o.optional    = true;
 	o.placeholder = 5;
 	o.datatype    = 'uinteger';
-	
+
 	o = s.option(form.Value, 'band_steering_threshold', _('band_steering_threshold'), _('Minimum number of stations delta between bands before band steering policy is active'));
 	o.optional    = true;
 	o.placeholder = 5;
@@ -80,121 +79,120 @@ return view.extend({
 	o = s.option(form.Value, 'band_steering_threshold', _('band_steering_threshold'), _('Minimum number of stations delta between bands before band steering policy is active'));
 	o.optional    = true;
 	o.placeholder = 5;
-	o.datatype    = 'uinteger';	
-	
+	o.datatype    = 'uinteger';
+
 	o = s.option(form.Value, 'remote_update_interval', _('remote_update_interval'), _('Interval (ms) between sending state updates to other APs'));
 	o.optional    = true;
 	o.placeholder = 1000;
-	o.datatype    = 'uinteger';	
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'remote_node_timeout', _('remote_node_timeout'), _('Number of remote update intervals after which a remote-node is deleted'));
 	o.optional    = true;
 	o.placeholder = 10;
-	o.datatype    = 'uinteger';	
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Flag, 'assoc_steering', _('assoc_steering'), _('Allow rejecting assoc requests for steering purposes (0/1)'));
 	o.optional    = true;
-	
+
 	o = s.option(form.Value, 'min_connect_snr', _('min_connect_snr'), _('Minimum signal-to-noise ratio or signal level (dBm) to allow connections'));
 	o.optional    = true;
 	o.placeholder = 0;
-	o.datatype    = 'uinteger';	    
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'min_snr', _('min_snr'), _('Minimum signal-to-noise ratio or signal level (dBm) to remain connected'));
 	o.optional    = true;
 	o.placeholder = 0;
-	o.datatype    = 'uinteger';	 
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'min_snr_kick_delay', _('min_snr_kick_delay'), _('Timeout after which a station with snr < min_snr will be kicked'));
 	o.optional    = true;
 	o.placeholder = 5000;
-	o.datatype    = 'uinteger';	 
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'roam_process_timeout', _('roam_process_timeout'), _('Timeout (in ms) after which a association following a disassociation is not seen as a roam'));
 	o.optional    = true;
 	o.placeholder = 5000;
-	o.datatype    = 'uinteger';	 
-	
+	o.datatype    = 'uinteger';
+
 	o = s.option(form.Value, 'roam_scan_snr', _('roam_scan_snr'), _('Minimum signal-to-noise ratio or signal level (dBm) before attempting to trigger client scans for roam'));
 	o.optional    = true;
 	o.placeholder = 0;
-	o.datatype    = 'uinteger';	 
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'roam_scan_tries', _('roam_scan_tries'), _('Maximum number of client roaming scan trigger attempts'));
 	o.optional    = true;
 	o.placeholder = 0;
-	o.datatype    = 'uinteger';	 
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'roam_scan_timeout', _('roam_scan_timeout'), _('Retry scanning when roam_scan_tries is exceeded after this timeout (in ms). In case this option is set to 0, the client is kicked instead'));
 	o.optional    = true;
 	o.placeholder = 0;
-	o.datatype    = 'uinteger';	 
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'roam_scan_interval', _('roam_scan_interval'), _('Minimum time (ms) between client roaming scan trigger attempts'));
 	o.optional    = true;
 	o.placeholder = 10000;
-	o.datatype    = 'uinteger';	 
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'roam_trigger_snr', _('roam_trigger_snr'), _('Minimum signal-to-noise ratio or signal level (dBm) before attempting to trigger forced client roaming'));
 	o.optional    = true;
 	o.placeholder = 0;
-	o.datatype    = 'uinteger';	 
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'roam_trigger_interval', _('roam_trigger_interval'), _('Minimum time (ms) between client roaming trigger attempts'));
 	o.optional    = true;
 	o.placeholder = 60000;
-	o.datatype    = 'uinteger';	
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'roam_kick_delay', _('roam_kick_delay'), _('Timeout (in 100ms beacon intervals) for client roam requests'));
 	o.optional    = true;
 	o.placeholder = 100;
-	o.datatype    = 'uinteger';	
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'signal_diff_threshold', _('signal_diff_threshold'), _('Minimum signal strength difference until AP steering policy is active'));
 	o.optional    = true;
 	o.placeholder = 0;
-	o.datatype    = 'uinteger';	
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'initial_connect_delay', _('initial_connect_delay'), _('Initial delay (ms) before responding to probe requests (to allow other APs to see packets as well)'));
 	o.optional    = true;
 	o.placeholder = 0;
-	o.datatype    = 'uinteger';	
+	o.datatype    = 'uinteger';
 
-	o = s.option(form.Value, 'load_kick_enabled', _('load_kick_enabled'), _('Enable kicking client on excessive channel load (0/1)'));
+	o = s.option(form.Flag, 'load_kick_enabled', _('load_kick_enabled'), _('Enable kicking client on excessive channel load (0/1)'));
 	o.optional    = true;
-	o.placeholder = 0;
-	
+
 	o = s.option(form.Value, 'load_kick_threshold', _('load_kick_threshold'), _('Minimum channel load (%) before kicking clients'));
 	o.optional    = true;
 	o.placeholder = 75;
-	o.datatype    = 'uinteger';	
+	o.datatype    = 'uinteger';
 	
 	o = s.option(form.Value, 'load_kick_delay', _('load_kick_delay'), _('Minimum amount of time (ms) that channel load is above threshold before starting to kick clients'));
 	o.optional    = true;
 	o.placeholder = 10000;
-	o.datatype    = 'uinteger';	
-		
+	o.datatype    = 'uinteger';
+
 	o = s.option(form.Value, 'load_kick_min_clients', _('load_kick_min_clients'), _('Minimum number of connected clients before kicking based on channel load'));
 	o.optional    = true;
 	o.placeholder = 10;
-	o.datatype    = 'uinteger';		
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'load_kick_reason_code', _('load_kick_reason_code'), _('Reason code on client kick based on channel load (default: WLAN_REASON_DISASSOC_AP_BUSY)'));
 	o.optional    = true;
 	o.placeholder = 5;
-	o.datatype    = 'uinteger';	
+	o.datatype    = 'uinteger';
 
 	o = s.option(form.Value, 'node_up_script', _('node_up_script'), _('Script to run after bringing up a node'));
 	o.optional    = true;
-	o.datatype    = 'string';	
+	o.datatype    = 'string';
 
 	o = s.option(form.Value, 'event_log_types', _('event_log_types'), _('Message types to include in log. Available types: probe_req_accept probe_req_deny, auth_req_accept, auth_req_deny, assoc_req_accept, assoc_req_deny, load_kick_trigger, load_kick_reset, load_kick_min_clients, load_kick_no_client, load_kick_client, signal_kick'));
 	o.optional    = true;
-	o.datatype    = 'list(string)';	
+	o.datatype    = 'list(string)';
 
 	o = s.option(form.Value, 'ssid_list', _('ssid_list'), _('List of SSIDs to enable steering on'));
 	o.optional    = true;
-	o.datatype    = 'list(string)';	
+	o.datatype    = 'list(string)';
 
 	return m.render();
 	},
