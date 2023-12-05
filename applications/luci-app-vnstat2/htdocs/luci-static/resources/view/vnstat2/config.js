openwrt/luci

commit 3ac4f567dfdfd6ee989ceffb025241e605790b6d

luci-app-vnstat2: use more efficient way to get database interfaces
The currently used command "vnstat --json f 1" can be very slow when the
database contains many entries. This can result in an XHR timeout error
on the configuration page.

Use the "--dbiflist" option instead, which was added in vnStat 2.7.

Fixes: #6702
Signed-off-by: Jan Hoffmann <jan@3e8.eu>

applications/luci-app-vnstat2/htdocs/luci-static/resources/view/vnstat2/config.js
@@ -58,13 +58,10 @@ return view.extend({
 		o = s.option(form.DummyValue, '_database');
 
 		o.load = function(section_id) {
-			return fs.exec('/usr/bin/vnstat', ['--json', 'f', '1']).then(L.bind(function(result) {
+			return fs.exec('/usr/bin/vnstat', ['--dbiflist', '1']).then(L.bind(function(result) {
 				var databaseInterfaces = [];
 				if (result.code == 0) {
-					var vnstatData = JSON.parse(result.stdout);
-					for (var i = 0; i < vnstatData.interfaces.length; i++) {
-						databaseInterfaces.push(vnstatData.interfaces[i].name);
-					}
+					databaseInterfaces = result.stdout.trim().split('\n');
 				}
 
 				var configInterfaces = uci.get_first('vnstat', 'vnstat', 'interface') || [];

commit 526c6e811093749e893665e8c6cb057c30a0df47

luci-app-vnstat2: fix configuration page when database is empty
The output of the command "vnstat --dbiflist 1" is empty when there are
no interfaces in the database. Add a check to avoid a "result.stdout is
undefined" error in that case.

Ref: https://forum.openwrt.org/t/error-in-luci-vnstat2-configuration-menu/179700
Fixes: 3ac4f567dfdf ("luci-app-vnstat2: use more efficient way to get database interfaces")
Signed-off-by: Jan Hoffmann <jan@3e8.eu>

applications/luci-app-vnstat2/htdocs/luci-static/resources/view/vnstat2/config.js
@@ -60,7 +60,7 @@ return view.extend({
 		o.load = function(section_id) {
 			return fs.exec('/usr/bin/vnstat', ['--dbiflist', '1']).then(L.bind(function(result) {
 				var databaseInterfaces = [];
-				if (result.code == 0) {
+				if (result.code == 0 && result.stdout) {
 					databaseInterfaces = result.stdout.trim().split('\n');
 				}
 