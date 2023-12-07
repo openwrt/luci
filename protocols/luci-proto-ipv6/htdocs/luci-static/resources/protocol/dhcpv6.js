openwrt/luci

commit 2065ead4e86d2da8868dd88160af5b2fdcf3adde

luci-proto-ipv6: dhcpv6.js fix translation (#6751)
Signed-off-by: he ping <pinghejk@gmail.com>

protocols/luci-proto-ipv6/htdocs/luci-static/resources/protocol/dhcpv6.js
@@ -15,9 +15,9 @@ return network.registerProtocol('dhcpv6', {
 		var o;
 
 		o = s.taboption('general', form.ListValue, 'reqaddress', _('Request IPv6-address'));
-		o.value('try');
-		o.value('force');
-		o.value('none', 'disabled');
+		o.value('try, _('try'));
+		o.value('force, _('force'));
+		o.value('none', _('disabled'));
 		o.default = 'try';
 
 		o = s.taboption('general', form.Value, 'reqprefix', _('Request IPv6-prefix of length'));

modules/luci-base/po/templates/base.pot
@@ -7502,6 +7502,14 @@ msgstr ""
 msgid "Request IPv6-address"
 msgstr ""
 
+#: protocols/luci-proto-ipv6/htdocs/luci-static/resources/protocol/dhcpv6.js:18
+msgid "try"
+msgstr ""
+
+#: protocols/luci-proto-ipv6/htdocs/luci-static/resources/protocol/dhcpv6.js:19
+msgid "force"
+msgstr ""
+
 #: protocols/luci-proto-ipv6/htdocs/luci-static/resources/protocol/dhcpv6.js:23
 msgid "Request IPv6-prefix of length"
 msgstr ""
@@ -10682,6 +10690,7 @@ msgstr ""
 #: modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js:967
 #: modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js:1044
 #: modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js:91
+#: protocols/luci-proto-ipv6/htdocs/luci-static/resources/protocol/dhcpv6.js:20
 #: protocols/luci-proto-ipv6/htdocs/luci-static/resources/protocol/dhcpv6.js:25
 msgid "disabled"
 msgstr ""

commit c2fb90259cb2857db83130ee0c33edce9ba7f08e

luci-proto-ipv6: dhcpv6.js: make reqaddress modes translatable
Fixes: #6730, #6751
Fixes: 2065ead4e8 ("luci-proto-ipv6: dhcpv6.js fix translation (#6751)")
Suggested-by: he ping <pinghejk@gmail.com>
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

protocols/luci-proto-ipv6/htdocs/luci-static/resources/protocol/dhcpv6.js
@@ -15,9 +15,9 @@ return network.registerProtocol('dhcpv6', {
 		var o;
 
 		o = s.taboption('general', form.ListValue, 'reqaddress', _('Request IPv6-address'));
-		o.value('try');
-		o.value('force');
-		o.value('none', 'disabled');
+		o.value('try', _('try', 'DHCPv6 address request mode'));
+		o.value('force', _('force', 'DHCPv6 address request mode'));
+		o.value('none', _('disabled', 'DHCPv6 address request mode'));
 		o.default = 'try';
 
 		o = s.taboption('general', form.Value, 'reqprefix', _('Request IPv6-prefix of length'));