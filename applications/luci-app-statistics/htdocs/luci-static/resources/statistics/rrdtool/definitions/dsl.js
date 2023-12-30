afe703c2439f55372eae6d4d481ff87a27eae984
Thomas Aldrian <aldrian.thom@proton.me>
2023-12-30 19:11:52 +0000

    Formatting fixes

--- applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/dsl.js
+++ applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/dsl.js
@@ -5,189 +5,184 @@
 return L.Class.extend({
 	title: _('DSL'),
 
-	rrdargs:  function(graph, host, plugin, plugin_instance, dtype) {
+	rrdargs: function (graph, host, plugin, plugin_instance, dtype) {
 		var g = [];
 		var dtypes = graph.dataTypes(host, plugin, plugin_instance);
 
 		const d_snr = {
-			title: _("DSL Signal"),
-			vlabel: "dB",
+			title: _('DSL Signal'),
+			vlabel: 'dB',
 			data: {
-				types: ["snr"],
+				types: ['snr'],
 				options: {
 					snr_latn_up: {
-						title: _("Line Attenuation Up (LATN)"),
+						title: _('Line Attenuation Up (LATN)'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					snr_latn_down: {
-						title: _("Line Attenuation Down (LATN)"),
+						title: _('Line Attenuation Down (LATN)'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					snr_satn_up: {
-						title: _("Signal Attenuation Up (SATN)"),
+						title: _('Signal Attenuation Up (SATN)'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					snr_satn_down: {
-						title: _("Signal Attenuation Down (SATN)"),
+						title: _('Signal Attenuation Down (SATN)'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					snr_snr_up: {
-						title: _("Noise Margin Up (SNR)"),
+						title: _('Noise Margin Up (SNR)'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					snr_snr_down: {
-						title: _("Noise Margin Down (SNR)"),
+						title: _('Noise Margin Down (SNR)'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
-				}
-			}
+				},
+			},
 		};
 		const d_uptime = {
-			title: _("DSL Line Uptime"),
-			vlabel: "seconds",
+			title: _('DSL Line Uptime'),
+			vlabel: 'seconds',
 			data: {
-				types: ["uptime"],
+				types: ['uptime'],
 				options: {
 					uptime: {
-						title: _("Uptime"),
-						noarea: true
-					}
-				}
-			}
+						title: _('Uptime'),
+						noarea: true,
+					},
+				},
+			},
 		};
 		const d_flags = {
-			title: _("DSL Flags"),
+			title: _('DSL Flags'),
 			data: {
 				instances: {
-					bool: [
-						"bitswap_up",
-						"bitswap_down",
-						"vector_up",
-						"vector_down"
-					]
+					bool: ['bitswap_up', 'bitswap_down', 'vector_up', 'vector_down'],
 				},
 				options: {
 					bool_bitswap_up: {
-						title: _("Bitswap Up"),
+						title: _('Bitswap Up'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					bool_bitswap_down: {
-						title: _("Bitswap Down"),
+						title: _('Bitswap Down'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					bool_vector_up: {
-						title: _("Vectoring Up"),
+						title: _('Vectoring Up'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					bool_vector_down: {
-						title: _("Vectoring Down"),
+						title: _('Vectoring Down'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
-				}
-			}
+				},
+			},
 		};
 		const d_bitrate = {
-			title: _("Bitrate"),
-			vlabel: "b/s",
+			title: _('Bitrate'),
+			vlabel: 'b/s',
 			data: {
 				instances: {
 					bitrate: [
-						"attndr_up",
-						"attndr_down",
-						"data_rate_up",
-						"data_rate_down"
-					]
+						'attndr_up',
+						'attndr_down',
+						'data_rate_up',
+						'data_rate_down',
+					],
 				},
 				options: {
 					bitrate_attndr_up: {
-						title: _("Max. Attainable Data Rate (ATTNDR) Up"),
+						title: _('Max. Attainable Data Rate (ATTNDR) Up'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					bitrate_attndr_down: {
-						title: _("Max. Attainable Data Rate (ATTNDR) Down"),
+						title: _('Max. Attainable Data Rate (ATTNDR) Down'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					bitrate_data_rate_up: {
-						title: _("Data Rate Up"),
+						title: _('Data Rate Up'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					bitrate_data_rate_down: {
-						title: _("Data Rate Down"),
+						title: _('Data Rate Down'),
 						noarea: true,
-						overlay: true
-					}
-				}
-			}
+						overlay: true,
+					},
+				},
+			},
 		};
-			const d_count = {
-			title: _("Errors"),
-			vlabel: "count",
+		const d_count = {
+			title: _('Errors'),
+			vlabel: 'count',
 			data: {
-				types: ["errors"],
+				types: ['errors'],
 				options: {
 					errors_rx_corrupted_far: {
-						title: _("Rx Corrupted Far"),
+						title: _('Rx Corrupted Far'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					errors_rx_corrupted_near: {
-						title: _("Rx Corrupted Near"),
+						title: _('Rx Corrupted Near'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					errors_rx_retransmitted_far: {
-						title: _("Rx Retransmitted Far"),
+						title: _('Rx Retransmitted Far'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					errors_tx_retransmitted_far: {
-						title: _("Tx Retransmitted Far"),
+						title: _('Tx Retransmitted Far'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					errors_rx_retransmitted_near: {
-						title: _("Rx Retransmitted Near"),
+						title: _('Rx Retransmitted Near'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
 					errors_tx_retransmitted_near: {
-						title: _("Tx Retransmitted Near"),
+						title: _('Tx Retransmitted Near'),
 						noarea: true,
-						overlay: true
+						overlay: true,
 					},
-				}
-			}
+				},
+			},
 		};
 
-		if (dtypes.includes("snr")) {
+		if (dtypes.includes('snr')) {
 			g.push(d_snr);
 		}
-		if (dtypes.includes("uptime")) {
+		if (dtypes.includes('uptime')) {
 			g.push(d_uptime);
 		}
-		if (dtypes.includes("bool")) {
+		if (dtypes.includes('bool')) {
 			g.push(d_flags);
 		}
-		if (dtypes.includes("bitrate")) {
+		if (dtypes.includes('bitrate')) {
 			g.push(d_bitrate);
 		}
-		if (dtypes.includes("count")) {
+		if (dtypes.includes('count')) {
 			g.push(d_count);
 		}
 
 		return g;
-	}
+	},
 });
