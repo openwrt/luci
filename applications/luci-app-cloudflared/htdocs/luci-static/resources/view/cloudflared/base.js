b6a81fe86a11493ad3f39a54a4100ed10ea38702
Sergey Ponomarev <stokito@gmail.com>
2024-01-21 22:33:38 +0200

    luci-app-cloudflared: add more options
    The UCI supports
    config
    origincert
    region but it's always 'us'
    loglevel
    and logfile but it's not a good idea to change it
    
    Signed-off-by: Sergey Ponomarev <stokito@gmail.com>

--- applications/luci-app-cloudflared/htdocs/luci-static/resources/view/cloudflared/base.js
+++ applications/luci-app-cloudflared/htdocs/luci-static/resources/view/cloudflared/base.js
@@ -47,10 +47,14 @@ return view.extend({
 	},
 
 	render: function(data) {
+		var docsUrl = 'https://developers.cloudflare.com/cloudflare-one/connections/connect-networks/configure-tunnels/tunnel-run-parameters/';
 		var m, s, o;
 
 		m = new form.Map('cloudflared', _('Cloudflared'),
-			_('Zero Trust Security services from Cloudflare to help you get maximum security both from outside and within the network.'));
+			_('Zero Trust Security services from Cloudflare to help you get maximum security both from outside and within the network.') + '<br />' +
+			_('See <a %s>documentation</a> about the tunnel configuration parameters.')
+				.format('href="' + docsUrl + '" target="_blank"')
+		);
 
 		s = m.section(form.NamedSection, 'config', 'cloudflared');
 
@@ -69,7 +73,34 @@ return view.extend({
 		o.rmempty = false;
 
 		o = s.option(form.Value, 'token', _('Token'));
+		o.optional = true;
 		o.rmempty = false;
+
+		o = s.option(form.FileUpload, 'config', _('Config file'),
+			_('The path to a configuration file in YAML format.')
+		);
+		o.default = '/etc/cloudflared/config.yml';
+		o.root_directory = '/';
+
+		o = s.option(form.FileUpload, 'origincert', _('Certificate of Origin'),
+			_('The account certificate for your zones authorizing the client to serve as an origin for that zone') + '<br />' +
+			_('You can obtain a certificate <a %s>here</a>.').format('href="https://dash.cloudflare.com/argotunnel" target="_blank"')
+		);
+		o.default = '/etc/cloudflared/cert.pem';
+		o.root_directory = '/';
+
+		o = s.option(form.Value, 'region', _('Region'),
+			_('Choose the region to which connections are established.') + '<br />' +
+			_('Currently the only available is <code>us</code> e.g. United States.')
+		);
+
+		o = s.option(form.ListValue, 'loglevel', _('Debug level'));
+		o.value('fatal', _('Fatal'));
+		o.value('error', _('Error'));
+		o.value('warn', _('Warning'));
+		o.value('info', _('Info'));
+		o.value('debug', _('Debug'));
+		o.default = 'info';
 
 		o = s.option(form.Button, '_panel', _('Cloudflare Zero Trust'),
 			_('Create or manage your cloudflared network.'));
