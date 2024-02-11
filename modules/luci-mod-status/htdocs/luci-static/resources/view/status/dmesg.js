8b8db0b8f2f76b5245681cf0df45bd13810e8df1
Ramon Van Gorkom <Ramon00c00@gmail.com>
2024-02-11 11:51:45 +0100

    luci-mod-status: Adding scroll buttons on syslog and kernellog status pages
    Signed-off-by: Ramon Van Gorkom <Ramon00c00@gmail.com>
    (cherry picked from commit eb6b2bdaae9c6f8d6d8969b47e21895d6508f396)

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/dmesg.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/dmesg.js
@@ -16,16 +16,36 @@ return view.extend({
 			return line.replace(/^<\d+>/, '');
 		});
 
+		var scrollDownButton = E('button', { 
+				'id': 'scrollDownButton',
+				'class': 'cbi-button cbi-button-neutral',
+			}, _('Scroll to tail', 'scroll to bottom (the tail) of the log file')
+		);
+		scrollDownButton.addEventListener('click', function() {
+			window.scrollTo(0, document.body.scrollHeight);
+		});
+
+		var scrollUpButton = E('button', { 
+				'id' : 'scrollUpButton',
+				'class': 'cbi-button cbi-button-neutral',
+			}, _('Scroll to head', 'scroll to top (the head) of the log file')
+		);
+		scrollUpButton.addEventListener('click', function() {
+			window.scrollTo(0, 0);
+		});
+
 		return E([], [
 			E('h2', {}, [ _('Kernel Log') ]),
 			E('div', { 'id': 'content_syslog' }, [
+				E('div', {'style': 'padding-bottom: 20px'}, [scrollDownButton]),
 				E('textarea', {
 					'id': 'syslog',
 					'style': 'font-size:12px',
 					'readonly': 'readonly',
 					'wrap': 'off',
 					'rows': loglines.length + 1
-				}, [ loglines.join('\n') ])
+				}, [ loglines.join('\n') ]),
+				E('div', {'style': 'padding-bottom: 20px'}, [scrollUpButton])
 			])
 		]);
 	},
