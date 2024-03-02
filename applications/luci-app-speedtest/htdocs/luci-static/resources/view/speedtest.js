2768b826f13542e9a2dd2f8b1f0ef03636596553
Hilman Maulana <hilman0.0maulana@gmail.com>
2024-03-02 14:20:09 +0700

    luci-app-speedtest: Fix element
    I have changed the placement of several variables so that if there are any changes in the future, there is no need to change the language template too much.

--- applications/luci-app-speedtest/htdocs/luci-static/resources/view/speedtest.js
+++ applications/luci-app-speedtest/htdocs/luci-static/resources/view/speedtest.js
@@ -12,6 +12,10 @@ return view.extend({
 	handleSave: null,
 	handleReset: null,
 	render: function() {
+		var header = [
+			E('h2', {'class': 'section-title'}, _('Speedtest')),
+			E('div', {'class': 'cbi-map-descr'}, _('Here you can perform a speed test to measure the basic aspects of your network connection.'))
+		];
 		var select = [
 			E('label', { 'class': 'cbi-input-label', 'for': 'speedtest-type', 'style': 'margin-right: 8px;'}, _('Test')),
 			E('select', { 'id': 'speedtest-type', 'style': 'width:80%;'}, [
@@ -24,47 +28,6 @@ return view.extend({
 		var status = [
 			E('label', { 'class': 'cbi-input-label', 'style': 'margin-right: 8px;'}, _('Status')),
 			E('em', { 'id': 'speedtest-status'}, _('Available'))
-		];
-		var running = false;
-		var button = [
-			E('button', {
-				'class': 'btn cbi-button cbi-button-action',
-				'click': function() {
-					if (!running) {
-						running = true;
-						var dropdown = document.getElementById('speedtest-type');
-						var value = dropdown.options[dropdown.selectedIndex].value;
-						var command = ['--output', 'json'];
-						if (value !== 'all') {
-							command.push('--' + value);
-						}
-
-						var status = document.getElementById('speedtest-status');
-						status.textContent = _('Running');
-
-						fs.exec_direct('speedtest', command)
-						.then(function(response) {
-							var result = JSON.parse(response);
-							document.getElementById('client-ip').textContent = result.client.ip;
-							document.getElementById('client-isp').textContent = result.client.isp + ' [' + result.client.lat + ', ' + result.client.lon + ']';
-							document.getElementById('server-name').textContent = result.server.sponsor;
-							document.getElementById('server-id').textContent = result.server.id;
-							document.getElementById('server-location').textContent = result.server.name + ' (' + result.server.distance + ' km)';
-							document.getElementById('server-host').textContent = result.server.host;
-							document.getElementById('ping').textContent = result.ping + ' ms';
-							document.getElementById('jitter').textContent = result.jitter + ' ms';
-							document.getElementById('download').textContent = result.download_mbit ? result.download_mbit + ' Mbit/s' : '-';
-							document.getElementById('upload').textContent = result.upload_mbit ? result.upload_mbit + ' Mbit/s' : '-';
-							status.textContent = _('Finished');
-							running = false;
-						})
-						.catch(function(err) {
-							status.textContent = err;
-							running = false;
-						});
-					}
-				}
-			}, _('Run Speedtest'))
 		];
 		var table = [
 			E('h3', {'class': 'section-title'}, _('Results')),
@@ -114,9 +77,55 @@ return view.extend({
 				])
 			])
 		];
-		var body = E('div', {'class': 'cbi-map'}, [
-			E('h2', {'class': 'section-title'}, _('Speedtest')),
-			E('div', {'class': 'cbi-map-descr'}, _('Here you can perform a speedtest to measure the basic aspects of your network connection.')),
+		var running = false;
+		var button = [
+			E('button', {
+				'class': 'btn cbi-button cbi-button-action',
+				'click': function() {
+					if (!running) {
+						running = true;
+						var dropdown = document.getElementById('speedtest-type');
+						var value = dropdown.options[dropdown.selectedIndex].value;
+						var command = ['--output', 'json'];
+						if (value !== 'all') {
+							command.push('--' + value);
+						}
+
+						var status = document.getElementById('speedtest-status');
+						status.textContent = _('Running');
+
+						fs.exec_direct('speedtest', command)
+						.then(function(response) {
+							var result = JSON.parse(response);
+							document.getElementById('client-ip').textContent = result.client.ip;
+							document.getElementById('client-isp').textContent = result.client.isp + ' [' + result.client.lat + ', ' + result.client.lon + ']';
+							document.getElementById('server-name').textContent = result.server.sponsor;
+							document.getElementById('server-id').textContent = result.server.id;
+							document.getElementById('server-location').textContent = result.server.name + ' (' + result.server.distance + ' km)';
+							document.getElementById('server-host').textContent = result.server.host;
+							document.getElementById('ping').textContent = result.ping + ' ms';
+							document.getElementById('jitter').textContent = result.jitter + ' ms';
+							document.getElementById('download').textContent = result.download_mbit ? result.download_mbit + ' Mbit/s' : '-';
+							document.getElementById('upload').textContent = result.upload_mbit ? result.upload_mbit + ' Mbit/s' : '-';
+							status.textContent = _('Finished');
+							running = false;
+						})
+						.catch(function(err) {
+							if (!err.response) {
+								status.textContent = _('No response received from speedtest. Please check your internet connection or try again later.');
+							} else if (err.response.data && err.response.data.error === 'unable to retrieve your ip info') {
+								status.textContent = _('Unable to retrieve IP information. Please check your internet connection.');
+							} else {
+								status.textContent = err;
+							}
+							running = false;
+						});
+					}
+				}
+			}, _('Start'))
+		];
+		return E('div', {'class': 'cbi-map'}, [
+			E(header),
 			E('table', {'class': 'table'}, [
 				E('tr', {'class': 'tr'}, [
 					E('td', {'class': 'td', 'style': 'overflow:initial;'}, select),
@@ -127,7 +136,6 @@ return view.extend({
 			E('div', {'class': 'cbi-section'}, [
 				E('div', {'class': 'speedtest-info'}, table)
 			])
-		]);
-		return body
+		])
 	}
 })
