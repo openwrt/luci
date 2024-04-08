90493e08cd17243dc8779a9fcf9ef1c0b2a94347
James Haggerty <james.haggerty@morsemicro.com>
2024-04-08 02:37:42 +0000

    luci-base: don't render until luci-loaded is done
    Because the setupDOM/initDOM methods do strange things related
    to the old lua server rendered templates
    (like remove all the elements currently hidden by dependencies...),
    we want to be sure that these have finished before the view
    itself renders.
    
    This also ensures that any caching (e.g. from probeSystemFeatures)
    is finished before the view render.
    
    This feels like a hacky fix, but it's not clear to me what the best
    one is.
    
    Signed-off-by: James Haggerty <james.haggerty@morsemicro.com>
    (cherry picked from commit a2fce95248a260d9471efbff94ad0cde7a90700d)

--- modules/luci-base/htdocs/luci-static/resources/luci.js
+++ modules/luci-base/htdocs/luci-static/resources/luci.js
@@ -1892,6 +1892,15 @@
 			DOM.content(vp, E('div', { 'class': 'spinning' }, _('Loading view…')));
 
 			return Promise.resolve(this.load())
+				.then(function (...args) {
+					if (L.loaded) {
+						return Promise.resolve(...args);
+					} else {
+						return new Promise(function (resolve) {
+							document.addEventListener('luci-loaded', resolve.bind(null, ...args), { once: true });
+						});
+					}
+				})
 				.then(LuCI.prototype.bind(this.render, this))
 				.then(LuCI.prototype.bind(function(nodes) {
 					const vp = document.getElementById('view');
@@ -2687,8 +2696,11 @@
 		initDOM() {
 			originalCBIInit();
 			Poll.start();
+			L.loaded = true;
 			document.dispatchEvent(new CustomEvent('luci-loaded'));
 		},
+
+		loaded: false,
 
 		/**
 		 * The `env` object holds environment settings used by LuCI, such
