openwrt/luci

commit 61cef9baad6cf753d37bfa7fb7a661d26180f6b9

luci-mod-network: don't trigger uci save on removing bridge vlans
Do not trigger a uci save operation on removing bridge VLANs as that might
invalidate the ephemeral section ID of a just added network device.

Fixes: #6990
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/tools/network.js
@@ -919,6 +919,15 @@ return baseclass.extend({
 			}, this));
 		};
 
+		ss.handleRemove = function(section_id) {
+			this.map.data.remove('network', section_id);
+			s.map.addedVLANs = s.map.addedVLANs.filter(function(sid) {
+				return sid != section_id;
+			});
+
+			return this.redraw();
+		};
+
 		o = ss.option(form.Value, 'vlan', _('VLAN ID'));
 		o.datatype = 'range(1, 4094)';
 

commit ea04003657cc06ee69639aaede24b4c74071946c

luci-mod-network: fix potential null dereference on deleting VLANs
Fixes: #7074
Fixes: 61cef9baad ("luci-mod-network: don't trigger uci save on removing bridge vlans")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/tools/network.js
@@ -921,7 +921,7 @@ return baseclass.extend({
 
 		ss.handleRemove = function(section_id) {
 			this.map.data.remove('network', section_id);
-			s.map.addedVLANs = s.map.addedVLANs.filter(function(sid) {
+			s.map.addedVLANs = (s.map.addedVLANs || []).filter(function(sid) {
 				return sid != section_id;
 			});
 