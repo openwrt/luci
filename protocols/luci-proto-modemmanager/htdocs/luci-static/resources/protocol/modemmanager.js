7546249a27ab1f9d6e0035f9f600a3f2c1c9b772
Florian Eckert <fe@dev.tdt.de>
2024-05-03 12:43:06 +0200

    luci-proto-modemmanger: fix auth handling
    This has never worked because the option 'auth' is not known to the
    modemmanger proto handler. The correct uci name is 'allowedauth' and is
    also a uci list option, so that several options can be selected.
    
    This commit fixes this bug.
    
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- protocols/luci-proto-modemmanager/htdocs/luci-static/resources/protocol/modemmanager.js
+++ protocols/luci-proto-modemmanager/htdocs/luci-static/resources/protocol/modemmanager.js
@@ -76,11 +76,13 @@ return network.registerProtocol('modemmanager', {
 		o = s.taboption('general', form.Value, 'pincode', _('PIN'));
 		o.datatype = 'and(uinteger,minlength(4),maxlength(8))';
 
-		o = s.taboption('general', form.ListValue, 'auth', _('Authentication Type'));
-		o.value('both', _('PAP/CHAP (both)'));
+		o = s.taboption('general', form.DynamicList, 'allowedauth', _('Authentication Type'));
 		o.value('pap', 'PAP');
 		o.value('chap', 'CHAP');
-		o.value('none', _('None'));
+		o.value('mschap', 'MSCHAP');
+		o.value('mschapv2', 'MSCHAPv2');
+		o.value('eap', 'EAP');
+		o.value('', _('None'));
 		o.default = 'none';
 
 		o = s.taboption('general', form.ListValue, 'allowedmode', _('Allowed network technology'),
