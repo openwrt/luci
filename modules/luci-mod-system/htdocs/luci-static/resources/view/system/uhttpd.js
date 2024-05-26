3c83300fd94b66247e5f320a8c16d50c06ed46a6
Sergey Ponomarev <stokito@gmail.com>
2024-05-26 17:49:24 +0300

    luci-mod-system: uhttpd: add flag to enable HTTP(S) firewall rule
    The wan_https_allow firewall rule should be pre-configured but disabled by default.
    For the main uhttpd instance will be shown an additional flag to enable the rule.
    Once the rule is enabled, the only way to disable it is on the Firewall page.
    
    Signed-off-by: Sergey Ponomarev <stokito@gmail.com>

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/uhttpd.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/uhttpd.js
@@ -1,9 +1,18 @@
 'use strict';
 'require view';
 'require form';
+'require uci';
 
 return view.extend({
+	load: function() {
+		return Promise.all([
+			uci.load('firewall')
+		]);
+	},
+
 	render: function() {
+		var httpsFirewallRule = uci.get('firewall', 'wan_https_allow');
+
 		var m, s, o;
 
 		m = new form.Map('uhttpd', _('HTTP(S) Access'), _('uHTTPd offers <abbr title="Hypertext Transfer Protocol">HTTP</abbr> or <abbr title="Hypertext Transfer Protocol Secure">HTTPS</abbr> network access.'));
@@ -13,6 +22,19 @@ return view.extend({
 
 		o = s.option(form.Flag, 'redirect_https', _('Redirect to HTTPS'), _('Enable automatic redirection of <abbr title="Hypertext Transfer Protocol">HTTP</abbr> requests to <abbr title="Hypertext Transfer Protocol Secure">HTTPS</abbr> port.'));
 		o.rmempty = false;
+
+		if (httpsFirewallRule && httpsFirewallRule.enabled !== '1') {
+			o = s.option(form.Flag, '_wan_https_firewall_rule', _('Allow HTTP and HTTPS from WAN'),
+				_('Enable firewall rule to allow access to the 80 and 443 ports')
+			);
+			o.depends('enabled', '');
+			o.depends('enabled', '1');
+			o.write = function(section_id, value) {
+				if (value === '1') {
+					uci.set('firewall', 'wan_https_allow', 'enabled', '1');
+				}
+			};
+		}
 
 		return m.render();
 	}
