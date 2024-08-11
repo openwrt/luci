06037ad777ab0500008c49bcb2c06af309bc4e51
Dirk Brenken <dev@brenken.org>
2024-08-11 09:37:22 +0200

    luci-app-adblock: sync with adblock-4.2.1
    * added full 1Hosts support
    
    Signed-off-by: Dirk Brenken <dev@brenken.org>

--- applications/luci-app-adblock/htdocs/luci-static/resources/view/adblock/overview.js
+++ applications/luci-app-adblock/htdocs/luci-static/resources/view/adblock/overview.js
@@ -609,6 +609,22 @@ return view.extend({
 		o.optional = true;
 		o.rmempty = true;
 
+		o = s.taboption('sources', form.DummyValue, '_sub');
+		o.rawhtml = true;
+		o.default = '<em><b>1Hosts List Selection</b></em>';
+
+		o = s.taboption('sources', form.DynamicList, 'adb_hst_sources', _('Variants'));
+		for (var i = 0; i < categories.length; i++) {
+			code = categories[i].match(/^(\w+);/)[1].trim();
+			if (code === 'hst') {
+				list = categories[i].match(/^\w+;(.*);/)[1].trim();
+				path = categories[i].match(/^.*;(.*$)/)[1].trim();
+				o.value(path, list);
+			}
+		}
+		o.optional = true;
+		o.rmempty = true;
+
 		return m.render();
 	},
 	handleReset: null
