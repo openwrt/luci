e8910c7a3b70c2ea015129fe1811b732522feade
lakdif <175205139+lakdif@users.noreply.github.com>
2024-09-08 11:38:20 +0300

    Add support for WiFi7 802.11be modes
    80/160/240/320Mhz - EHT80,EHT160,EHT240,EHT320
    Hostapd must be updated as well to provide those values
    
    Signed-off-by: lakdif <175205139+lakdif@users.noreply.github.com>

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -338,7 +338,8 @@ var CBIWifiFrequencyValue = form.Value.extend({
 				'', 'Legacy', hwmodelist.a || hwmodelist.b || hwmodelist.g,
 				'n', 'N', hwmodelist.n,
 				'ac', 'AC', L.hasSystemFeature('hostapd', '11ac') && hwmodelist.ac,
-				'ax', 'AX', L.hasSystemFeature('hostapd', '11ax') && hwmodelist.ax
+				'ax', 'AX', L.hasSystemFeature('hostapd', '11ax') && hwmodelist.ax,
+				'be", 'BE', L.hasSystemFeature('hostapd', '11be') && hwmodelist.be
 			];
 
 			var htmodelist = L.toArray(data[0] ? data[0].getHTModes() : null)
@@ -362,6 +363,12 @@ var CBIWifiFrequencyValue = form.Value.extend({
 					'HE80', '80 MHz', htmodelist.HE80,
 					'HE160', '160 MHz', htmodelist.HE160
 				]
+				'be': [
+                                        'EHT80', '80 MHz', htmodelist.EHT80,
+                                        'EHT160', '160 MHz', htmodelist.EHT160,
+					'EHT240', '240 MHz', htmodelist.EHT240,
+					'EHT320', '320 MHz', htmodelist.EHT320
+                                ]
 			};
 
 			this.bands = {
@@ -382,6 +389,12 @@ var CBIWifiFrequencyValue = form.Value.extend({
 					'5g', '5 GHz', this.channels['5g'].length > 3,
 					'6g', '6 GHz', this.channels['6g'].length > 3
 				]
+				'be': [
+                                        '2g', '2.4 GHz', this.channels['2g'].length > 3,
+                                        '5g', '5 GHz', this.channels['5g'].length > 3,
+                                        '6g', '6 GHz', this.channels['6g'].length > 3
+                                ]
+
 			};
 		}, this));
 	},
@@ -445,7 +458,9 @@ var CBIWifiFrequencyValue = form.Value.extend({
 
 		this.setValues(mode, this.modes);
 
-		if (/HE20|HE40|HE80|HE160/.test(htval))
+		if (/EHT80|EHT160/EHT240/EHT320/.test(htval))
+                        mode.value = 'be';
+		else if (/HE20|HE40|HE80|HE160/.test(htval))
 			mode.value = 'ax';
 		else if (/VHT20|VHT40|VHT80|VHT160/.test(htval))
 			mode.value = 'ac';
