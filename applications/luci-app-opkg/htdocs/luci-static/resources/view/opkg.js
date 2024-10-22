openwrt/luci

commit 9b25031cb29bd59561e550daccb69935834d7d63

luci-app-opkg: rework backend operations
Introduce a new /usr/libexec/opkg-call helper and invoke it via cgi-io
instead of ubus. This is required to be able to reload rpcd without
timing out currently running opkg ubus calls.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

applications/luci-app-opkg/htdocs/luci-static/resources/view/opkg.js
@@ -872,7 +872,7 @@ function handleOpkg(ev)
 				_('Waiting for the <em>opkg %h</em> command to complete…').format(cmd))
 		]);
 
-		var argv = [ '--force-removal-of-dependent-packages' ];
+		var argv = [ cmd, '--force-removal-of-dependent-packages' ];
 
 		if (rem && rem.checked)
 			argv.push('--autoremove');
@@ -880,12 +880,10 @@ function handleOpkg(ev)
 		if (owr && owr.checked)
 			argv.push('--force-overwrite');
 
-		argv.push(cmd);
-
 		if (pkg != null)
 			argv.push(pkg);
 
-		fs.exec('/bin/opkg', argv).then(function(res) {
+		fs.exec_direct('/usr/libexec/opkg-call', argv, 'json').then(function(res) {
 			dlg.removeChild(dlg.lastChild);
 
 			if (res.stdout)
@@ -957,8 +955,8 @@ function downloadLists()
 {
 	return Promise.all([
 		callMountPoints(),
-		fs.exec_direct('/usr/libexec/opkg-list', [ 'available' ]),
-		fs.exec_direct('/usr/libexec/opkg-list', [ 'installed' ])
+		fs.exec_direct('/usr/libexec/opkg-call', [ 'list-available' ]),
+		fs.exec_direct('/usr/libexec/opkg-call', [ 'list-installed' ])
 	]);
 }
 

applications/luci-app-opkg/root/usr/libexec/opkg-call
@@ -1,0 +1,59 @@
+#!/bin/sh
+
+. /usr/share/libubox/jshn.sh
+
+action=$1
+shift
+
+case "$action" in
+	list-installed)
+		cat /usr/lib/opkg/status
+	;;
+	list-available)
+		lists_dir=$(sed -rne 's#^lists_dir \S+ (\S+)#\1#p' /etc/opkg.conf /etc/opkg/*.conf 2>/dev/null | tail -n 1)
+		find "${lists_dir:-/tmp/opkg-lists}" -type f '!' -name '*.sig' | xargs -r gzip -cd
+	;;
+	install|update|remove)
+		(
+			opkg="opkg"
+
+			while [ -n "$1" ]; do
+				case "$1" in
+					--autoremove|--force-overwrite|--force-removal-of-dependent-packages)
+						opkg="$opkg $1"
+						shift
+					;;
+					-*)
+						shift
+					;;
+					*)
+						break
+					;;
+				esac
+			done
+
+			if flock -x 200; then
+				$opkg $action "$@" </dev/null >/tmp/opkg.out 2>/tmp/opkg.err
+				code=$?
+				stdout=$(cat /tmp/opkg.out)
+				stderr=$(cat /tmp/opkg.err)
+			else
+				code=255
+				stderr="Failed to acquire lock"
+			fi
+
+			json_init
+			json_add_int code $code
+			[ -n "$stdout" ] && json_add_string stdout "$stdout"
+			[ -n "$stderr" ] && json_add_string stderr "$stderr"
+			json_dump
+		) 200>/tmp/opkg.lock
+
+		rm -f /tmp/opkg.lock /tmp/opkg.err /tmp/opkg.out
+	;;
+	*)
+		echo "Usage: $0 {list-installed|list-available}"       >&2
+		echo "       $0 {install|upgrade|remove} pkg[ pkg...]" >&2
+		exit 1
+	;;
+esac

applications/luci-app-opkg/root/usr/libexec/opkg-list
@@ -1,15 +1,0 @@
-#!/bin/sh
-
-case "$1" in
-	installed)
-		cat /usr/lib/opkg/status
-	;;
-	available)
-		lists_dir=$(sed -rne 's#^lists_dir \S+ (\S+)#\1#p' /etc/opkg.conf /etc/opkg/*.conf 2>/dev/null | tail -n 1)
-		find "${lists_dir:-/tmp/opkg-lists}" -type f '!' -name '*.sig' | xargs -r gzip -cd
-	;;
-	*)
-		echo "Usage: $0 {installed|available}" >&2
-		exit 1
-	;;
-esac

commit 9143a49d56bfa779e26bf31d690019e3e02dc953

luci-app-opkg: fix passing wrong option on opkg update/install
Fix passing wrong option on opkg update/install. While starting to
introduce support for APK in the opkg module, it was notice that
--force-removal-of-dependent-packages was always passed even with update
and install command.

This was probably a leftover/oversight of old one. To fix this, limit
this option only on remove and also update the acl.d to support single
call to update or install.

Fixes: 9b25031cb29b ("luci-app-opkg: rework backend operations")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

applications/luci-app-opkg/htdocs/luci-static/resources/view/opkg.js
@@ -984,7 +984,10 @@ function handleOpkg(ev)
 				_('Waiting for the <em>opkg %h</em> command to complete…').format(cmd))
 		]);
 
-		var argv = [ cmd, '--force-removal-of-dependent-packages' ];
+		var argv = [ cmd ];
+
+		if (cmd == 'remove')
+			argv.push('--force-removal-of-dependent-packages')
 
 		if (rem && rem.checked)
 			argv.push('--autoremove');