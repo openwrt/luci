03f37cff4c1cd73993284779f92c4982d30aa2bd
Christian Korber <ckorber@tdt.de>
2024-11-15 09:18:17 +0100

    luci-app-keepalived: add notification tab
    This commit adds a tab to modify keepalived.user file.
    With this commit it is possible to issue custom commands.
    
    Signed-off-by: Christian Korber <ckorber@tdt.de>

---
+++ applications/luci-app-keepalived/htdocs/luci-static/resources/view/keepalived/notification.js
@@ -0,0 +1,38 @@
+'use strict';
+'require view';
+'require fs';
+'require ui';
+
+var isReadonlyView = !L.hasViewPermission() || null;
+
+return view.extend({
+	load: function() {
+		return L.resolveDefault(fs.read('/etc/keepalived.user'), '');
+	},
+
+	handleSave: function(ev) {
+		var value = (document.querySelector('textarea').value || '').trim().replace(/\r\n/g, '\n') + '\n';
+
+		return fs.write('/etc/keepalived.user', value).then(function(rc) {
+			document.querySelector('textarea').value = value;
+			ui.addNotification(null, E('p', _('Contents have been saved.')), 'info');
+
+			return fs.exec('/etc/init.d/keepalived', [ 'reload' ]);
+		}).catch(function(e) {
+			ui.addNotification(null, E('p', _('Unable to save contents: %s').format(e.message)));
+		});
+	},
+
+	render: function(keepalived) {
+		return E([
+			E('h2', _('Keepalived.user')),
+			E('p', { 'class': 'cbi-section-descr' }, _('This is the /etc/keepalived.user file in which custom commands can be defined.')),
+			E('p', {}, E('textarea', { 'style': 'width:100%', 'rows': 25, 'disabled': isReadonlyView }, [ keepalived != null ? keepalived : '' ]))
+		]);
+	},
+
+	handleSaveApply: null,
+	handleReset: null
+});
+
+
