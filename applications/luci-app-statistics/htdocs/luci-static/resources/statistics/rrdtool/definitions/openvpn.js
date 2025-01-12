881d82152ff181328ad165058e5e7b5dd2702f84
j <j@Air13.lan>
2025-01-12 13:41:50 +0100

    Remove all formatting only code from the PR "Signed-off-by: Julien DIOUF <juliendiouf7@gmail.com>"

--- applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/openvpn.js
+++ applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/openvpn.js
@@ -26,16 +26,16 @@ return baseclass.extend({
 				vlabel: "Bytes/s",
 				data: {
 					instances: {
-						if_octets: ["traffic", "overhead"]
+						if_octets: [ "traffic", "overhead" ]
 					},
 					sources: {
-						if_octets: ["tx", "rx"]
+						if_octets: [ "tx", "rx" ]
 					},
 					options: {
-						if_octets_traffic_tx: { weight: 0, title: "Bytes    (TX)", total: true, color: "00ff00" },
+						if_octets_traffic_tx : { weight: 0, title: "Bytes    (TX)", total: true, color: "00ff00" },
 						if_octets_overhead_tx: { weight: 1, title: "Overhead (TX)", total: true, color: "ff9900" },
 						if_octets_overhead_rx: { weight: 2, title: "Overhead (RX)", total: true, flip: true, color: "ff00ff" },
-						if_octets_traffic_rx: { weight: 3, title: "Bytes    (RX)", total: true, flip: true, color: "0000ff" }
+						if_octets_traffic_rx : { weight: 3, title: "Bytes    (RX)", total: true, flip: true, color: "0000ff" }
 					}
 				}
 			});
@@ -53,10 +53,10 @@ return baseclass.extend({
 					vlabel: "Bytes/s",
 					data: {
 						instances: {
-							if_octets: [vpnuser]
+							if_octets: [ vpnuser ]
 						},
 						sources: {
-							if_octets: ["tx", "rx"]
+							if_octets: [ "tx", "rx" ]
 						},
 						options: opts
 					}
@@ -94,16 +94,16 @@ return baseclass.extend({
 				vlabel: "Bytes/s",
 				data: {
 					instances: {
-						compression: ["data_out", "data_in"]
+						compression: [ "data_out", "data_in" ]
 					},
 					sources: {
-						compression: ["uncompressed", "compressed"]
+						compression: [ "uncompressed", "compressed" ]
 					},
 					options: {
 						compression_data_out_uncompressed: { weight: 0, title: "Uncompressed (TX)", total: true, color: "00ff00" },
-						compression_data_out_compressed: { weight: 1, title: "Compressed   (TX)", total: true, color: "008800" },
-						compression_data_in_compressed: { weight: 2, title: "Compressed   (RX)", total: true, flip: true, color: "000088" },
-						compression_data_in_uncompressed: { weight: 3, title: "Uncompressed (RX)", total: true, flip: true, color: "0000ff" }
+						compression_data_out_compressed  : { weight: 1, title: "Compressed   (TX)", total: true, color: "008800" },
+						compression_data_in_compressed   : { weight: 2, title: "Compressed   (RX)", total: true, flip: true, color: "000088" },
+						compression_data_in_uncompressed : { weight: 3, title: "Uncompressed (RX)", total: true, flip: true, color: "0000ff" }
 					}
 				}
 			});
