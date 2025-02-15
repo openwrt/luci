3e45fc644f7a4a172d75015d65ddde4dc55a7453
Aleksander Jan Bajkowski <olek2@wp.pl>
2025-02-15 22:47:13 +0100

    luci-mod-system: add speed and duplex triggers
    Many devices have LEDs to signal a specific link speed.
    They are incorrectly displayed in LUCI when they are deﬁned.
    This commit adds the missing speed and duplex triggers.
    
    Signed-off-by: Aleksander Jan Bajkowski <olek2@wp.pl>

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/netdev.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/netdev.js
@@ -22,6 +22,14 @@ return baseclass.extend({
 		o.modalonly = true;
 		o.depends('trigger', 'netdev');
 		o.value('link', _('Link On'));
+		o.value('link_10', _('Link 10M On'));
+		o.value('link_100', _('Link 100M On'));
+		o.value('link_1000', _('Link 1G On'));
+		o.value('link_2500', _('Link 2.5G On'));
+		o.value('link_5000', _('Link 5G On'));
+		o.value('link_10000', _('Link 10G On'));
+		o.value('half_duplex', _('Half Duplex'));
+		o.value('full_duplex', _('Full Duplex'));
 		o.value('tx', _('Transmit'));
 		o.value('rx', _('Receive'));
 	}
