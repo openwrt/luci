31824936faf24f35683ca462be3413cd39f5bc9c
Florian Eckert <fe@dev.tdt.de>
2025-03-07 13:49:04 +0100

    luci-base: revert timeout function argument for addNotification
    The function signature changed and all function call to addNotification
    that add a CSS class does not work anymore. A direct revert is not
    possible.
    
    If this feature is needed than a new function musst be added, that does
    not break the function signature.
    
    Fixes: 53e36e3293bee2ea2ce0fbc1250074c44cfe8335
    
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>
    (cherry picked from commit 3f832f25120184a5794f593bafa762747d6cc693)

--- modules/luci-base/htdocs/luci-static/resources/ui.js
+++ modules/luci-base/htdocs/luci-static/resources/ui.js
@@ -3847,11 +3847,6 @@ const UI = baseclass.extend(/** @lends LuCI.ui.prototype */ {
 	 * to the `dom.content()` function - refer to its documentation for
 	 * applicable values.
 	 *	 
-	 * @param {int} [timeout]
-	 * A millisecond value after which the notification will disappear
-	 * automatically. If omitted, the notification will remain until it receives
-	 * the click event.
-	 *
 	 * @param {...string} [classes]
 	 * A number of extra CSS class names which are set on the notification
 	 * banner element.
@@ -3859,7 +3854,7 @@ const UI = baseclass.extend(/** @lends LuCI.ui.prototype */ {
 	 * @returns {Node}
 	 * Returns a DOM Node representing the notification banner element.
 	 */
-	addNotification(title, children, timeout, ...classes) {
+	addNotification(title, children, ...classes) {
 		const mc = document.querySelector('#maincontent') ?? document.body;
 		const msg = E('div', {
 			'class': 'alert-message fade-in',
@@ -3876,7 +3871,7 @@ const UI = baseclass.extend(/** @lends LuCI.ui.prototype */ {
 					'class': 'btn',
 					'style': 'margin-left:auto; margin-top:auto',
 					'click': function(ev) {
-						fadeOutNotification(ev.target);
+						dom.parent(ev.target, '.alert-message').classList.add('fade-out');
 					},
 
 				}, [ _('Dismiss') ])
@@ -3891,27 +3886,6 @@ const UI = baseclass.extend(/** @lends LuCI.ui.prototype */ {
 		msg.classList.add(...classes);
 
 		mc.insertBefore(msg, mc.firstElementChild);
-
-		function fadeOutNotification(element) {
-			const notification = dom.parent(element, '.alert-message');
-			if (notification) {
-				notification.classList.add('fade-out');
-				notification.classList.remove('fade-in');
-				setTimeout(() => {
-					if (notification.parentNode) {
-						notification.parentNode.removeChild(notification);
-					}
-				});
-			}
-		}
-
-		if (typeof timeout === 'number' && timeout > 0) {
-			setTimeout(() => {
-				if (msg && msg.parentNode) {
-					fadeOutNotification(msg);
-				}
-			}, timeout);
-		}
 
 		return msg;
 	},
