daa629ad50f83a62ec9ccb64552df44330553561
Florian Eckert <fe@dev.tdt.de>
2025-03-07 11:01:43 +0100

    luci-app-mwan3: fix addNotification API change
    Fixes: 53e36e3293bee2ea2ce0fbc1250074c44cfe8335
    
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- applications/luci-app-mwan3/htdocs/luci-static/resources/view/mwan3/network/notify.js
+++ applications/luci-app-mwan3/htdocs/luci-static/resources/view/mwan3/network/notify.js
@@ -15,7 +15,11 @@ return view.extend({
 
 		return fs.write('/etc/mwan3.user', value).then(function(rc) {
 			document.querySelector('textarea').value = value;
-				ui.addNotification(null, E('p', _('Contents have been saved.')), 'info');
+				ui.addNotification(null,
+					E('p', _('Contents have been saved.')),
+					0,
+					'info'
+				);
 			}).catch(function(e) {
 				ui.addNotification(null, E('p', _('Unable to save contents: %s').format(e.message)));
 			});
