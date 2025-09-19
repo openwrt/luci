2524e2c029b82cddb94062f47b0344800db42ff1
Paul Donald <newtwen+github@gmail.com>
2025-09-19 14:10:23 +0200

    treewide: string fixes
    Announced -> Announce
    
    Signed-off-by: Paul Donald <newtwen+github@gmail.com>

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -928,7 +928,7 @@ return view.extend({
 					so.datatype = 'range(1,62)';
 					so.depends('dhcpv6', 'server');
 
-					so = ss.taboption('ipv6', form.DynamicList, 'dns', _('Announced IPv6 DNS servers'),
+					so = ss.taboption('ipv6', form.DynamicList, 'dns', _('Announce IPv6 DNS servers'),
 						_('Specifies a fixed list of IPv6 DNS server addresses to announce via DHCPv6. If left unspecified, the device will announce itself as IPv6 DNS server unless the <em>Local IPv6 DNS server</em> option is disabled.'));
 					so.datatype = 'ip6addr("nomask")'; /* restrict to IPv6 only for now since dnsmasq (DHCPv4) does not honour this option */
 					so.depends('ra', 'server');
@@ -944,7 +944,7 @@ return view.extend({
 					so.depends({ dhcpv6: 'server', dns: /^$/ });
 					so.depends({ dhcpv6: 'hybrid', dns: /^$/, master: '0' });
 
-					so = ss.taboption('ipv6', form.DynamicList, 'domain', _('Announced DNS domains'),
+					so = ss.taboption('ipv6', form.DynamicList, 'domain', _('Announce DNS domains'),
 						_('Specifies a fixed list of DNS search domains to announce via DHCPv6. If left unspecified, the local device DNS search domain will be announced.'));
 					so.datatype = 'hostname';
 					so.depends('ra', 'server');
