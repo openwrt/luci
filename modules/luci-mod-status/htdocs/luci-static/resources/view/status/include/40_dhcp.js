cf655f98c78f4782fc87481a72bceb13274424a4
David Härdeman <david@hardeman.nu>
2025-11-09 16:44:47 +0100

    luci-mod-status: talk to odhcpd via ubus - pt1
    Instead of going through rpcd, which digs through statefiles to find out
    which leases are active, communicate directly with odhcpd over ubus.
    
    This allows us to expose information which isn't otherwise available,
    like which interface a given lease belongs to and whether force-reconf
    is supported by a given client.
    
    It's also more efficient, and provides a small step towards a future
    where the odhcpd-statefile parsing code can be removed from rpcd.
    
    Signed-off-by: David Härdeman <david@hardeman.nu>

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/40_dhcp.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/40_dhcp.js
@@ -11,10 +11,29 @@ const callLuciDHCPLeases = rpc.declare({
 	expect: { '': {} }
 });
  
+const callODHCPD6Leases = rpc.declare({
+	object: 'dhcp',
+	method: 'ipv6leases',
+	expect: { 'device': {} }
+});
+
 const callUfpList = rpc.declare({
 	object: 'fingerprint',
 	method: 'fingerprint',
 });
+
+function DUIDtoMAC(duid) {
+	if (duid == null || duid == '')
+		return null;
+
+	var mac = null;
+	if (duid.match(/^00010001[a-f0-9]{20}$/i))
+		mac = duid.substring(16,);
+	else if (duid.match(/^00030001[a-f0-9]{12}$/i))
+		mac = duid.substring(8,);
+
+	return mac ? mac.match(/.{1,2}/g).join(":").toUpperCase() : null;
+}
 
 return baseclass.extend({
 	title: '',
@@ -30,25 +49,26 @@ return baseclass.extend({
 	load() {
 		return Promise.all([
 			callLuciDHCPLeases(),
+			L.hasSystemFeature('odhcpd', 'dhcpv6') ? L.resolveDefault(callODHCPD6Leases()) : null,
 			network.getHostHints(),
 			L.hasSystemFeature('ufpd') ? callUfpList() : null,
 			L.resolveDefault(uci.load('dhcp'))
 		]);
 	},
 
-	render([dhcp_leases, host_hints, ufp_list]) {
+	render([dnsmasq_leases, odhcpd_leases6, host_hints, ufp_list]) {
 		this.mac_hints = host_hints.getMACHints(false);
 		this.ufp_list = ufp_list;
 
 		if (L.hasSystemFeature('dnsmasq') || L.hasSystemFeature('odhcpd'))
-			return this.renderLeases(dhcp_leases);
+			return this.renderLeases(dnsmasq_leases, odhcpd_leases6);
 
 		return E([]);
 	},
 
-	renderLeases(dhcp_leases) {
-		const leases4 = L.toArray(dhcp_leases.dhcp_leases);
-		const leases6 = L.toArray(dhcp_leases.dhcp6_leases);
+	renderLeases(dnsmasq_leases, odhcpd_leases6) {
+		const leases4 = L.toArray(dnsmasq_leases.dhcp_leases);
+		const leases6 = L.toArray(dnsmasq_leases.dhcp6_leases);
 
 		if (leases4.length == 0 && leases6.length == 0)
 			return E([]);
@@ -70,13 +90,15 @@ return baseclass.extend({
 		};
 
 		const table4 = this.createTable4(leases4);
-		const table6 = this.createTable6(leases6);
+		const dnsmasq_table6 = this.createDnsmasqTable6(leases6);
+		const odhcpd_table6 = this.createOdhcpdTable6(odhcpd_leases6);
 
 		return E([
 			E('h3', _('Active DHCPv4 Leases')),
 			table4,
 			E('h3', _('Active DHCPv6 Leases')),
-			table6
+			dnsmasq_table6,
+			odhcpd_table6
 		]);
 	},
 
@@ -136,7 +158,7 @@ return baseclass.extend({
 		return table4;
 	},
 
-	createTable6(leases6) {
+	createDnsmasqTable6(leases6) {
 		const table6 = E('table', { 'id': 'status_leases6', 'class': 'table leases6' }, [
 			E('tr', { 'class': 'tr table-titles' }, [
 				E('th', { 'class': 'th' }, _('Host')),
@@ -150,7 +172,6 @@ return baseclass.extend({
 
 		cbi_update_table(table6, leases6.map(L.bind(function(lease) {
 			let exp;
-
 			if (lease.expires === false)
 				exp = E('em', _('unlimited'));
 			else if (lease.expires <= 0)
@@ -203,6 +224,88 @@ return baseclass.extend({
 		return table6;
 	},
 
+	createOdhcpdTable6(iface_leases6) {
+		if (!iface_leases6)
+			return null;
+
+		const table6 = E('table', { 'id': 'status_leases6', 'class': 'table leases6' }, [
+			E('tr', { 'class': 'tr table-titles' }, [
+				E('th', { 'class': 'th' }, _('Interface')),
+				E('th', { 'class': 'th' }, _('Host')),
+				E('th', { 'class': 'th' }, _('IPv6 addresses')),
+				E('th', { 'class': 'th' }, _('DUID')),
+				E('th', { 'class': 'th' }, _('IAID')),
+				E('th', { 'class': 'th' }, _('FR')),
+				E('th', { 'class': 'th' }, _('Lease time remaining')),
+				this.isReadonlyView ? E([]) : E('th', { 'class': 'th cbi-section-actions' }, _('Static Lease'))
+			])
+		]);
+
+		const leases6 = Object.entries(iface_leases6).flatMap(([iface, { leases }]) =>
+			leases.map(lease => {
+				lease.iface = iface;
+				lease.duid = lease.duid.toUpperCase();
+				lease.iaid = lease.iaid.toString(16).toUpperCase();
+				lease.duid_iaid = `${lease.duid}%${lease.iaid}`;
+				lease.ip6addrs = lease['ipv6-addr'].map(addr => addr.address);
+				lease.reconf = lease['accept-reconf'];
+				lease.macaddr = DUIDtoMAC(lease.duid);
+				return lease;
+			})
+		);
+
+		cbi_update_table(table6, leases6.map(L.bind(function(lease) {
+			let exp;
+			if (lease.valid == 0xffffffff)
+				exp = E('em', _('unlimited'));
+			else if (lease.valid <= 0)
+				exp = E('em', _('expired'));
+			else
+				exp = '%t'.format(lease.valid);
+
+			const hint = lease.macaddr ? this.mac_hints.filter(function(h) { return h[0] == lease.macaddr })[0] : null;
+
+			let host = null;
+			if (hint && lease.hostname && lease.hostname != hint[1] && lease.ip6addrs[0] != hint[1])
+				host = '%s (%s)'.format(lease.hostname, hint[1]);
+			else if (lease.hostname)
+				host = lease.hostname;
+			else if (hint)
+				host = hint[1];
+
+			// Note: "disabled: false" doesn't work
+			let disabled = null;
+			if (this.isDUIDStatic[lease.duid])
+				disabled = true;
+			if (this.isDUIDIAIDStatic[lease.duid_iaid])
+				disabled = true;
+
+			const columns = [
+				lease.iface,
+				host || '-',
+				lease.ip6addrs.join('<br />'),
+				lease.duid,
+				lease.iaid,
+				lease.reconf ? _('Yes') : _('No'),
+				exp
+			];
+
+			if (!this.isReadonlyView) {
+				columns.push(E('button', {
+					'class': 'cbi-button cbi-button-apply',
+					'click': L.bind(this.handleCreateStaticLease6, this, lease),
+					'data-tooltip': _('Reserve a specific IP address for this device'),
+					'disabled': disabled
+				}, [ _('Reserve IP') ]));
+			}
+
+			return columns;
+
+		}, this)), E('em', _('There are no active leases')));
+
+		return table6;
+	},
+
 	handleCreateStaticLease4(lease, ev) {
 		ev.currentTarget.classList.add('spinning');
 		ev.currentTarget.disabled = true;
@@ -235,7 +338,8 @@ return baseclass.extend({
 
 		uci.set('dhcp', cfg, 'name', lease.hostname);
 		uci.set('dhcp', cfg, 'duid', [duid_iaid]);
-		uci.set('dhcp', cfg, 'mac', [lease.macaddr]);
+		if (lease.macaddr)
+			uci.set('dhcp', cfg, 'mac', [lease.macaddr]);
 		if (ip6arr)
 			uci.set('dhcp', cfg, 'hostid', (ip6arr[6] * 0xFFFF + ip6arr[7]).toString(16));
 
