bf0ce9c571a51947ebafc0b02d4594073b18f218
Dirk Brenken <dev@brenken.org>
2025-12-04 20:28:42 +0100

    luci-app-banip: sync with banIP-1.6.0-1
    Signed-off-by: Dirk Brenken <dev@brenken.org>

--- applications/luci-app-banip/htdocs/luci-static/resources/view/banip/allowlist.js
+++ applications/luci-app-banip/htdocs/luci-static/resources/view/banip/allowlist.js
@@ -3,25 +3,29 @@
 'require fs';
 'require ui';
 
-let localFile = '/etc/banip/banip.allowlist';
-let notMsg, errMsg;
+const localFile = '/etc/banip/banip.allowlist';
+let notMsg = false, errMsg = false;
+
+const resetScroll = () => {
+	document.body.scrollTop = document.documentElement.scrollTop = 0;
+};
 
 return view.extend({
 	load: function () {
 		return L.resolveDefault(fs.stat(localFile), "")
-			.then(function (stat) {
-				if (!stat) {
-					return fs.write(localFile, "");
-				}
-				return Promise.all([
-					L.resolveDefault(fs.stat(localFile), ""),
-					L.resolveDefault(fs.read_direct(localFile), "")
-				]);
-			});
+		.then(function (stat) {
+			if (!stat) {
+				return fs.write(localFile, "");
+			}
+			return Promise.all([
+				L.resolveDefault(fs.stat(localFile), ""),
+				L.resolveDefault(fs.read_direct(localFile), "")
+			]);
+		});
 	},
 	render: function (allowlist) {
 		if (allowlist[0].size >= 100000) {
-			document.body.scrollTop = document.documentElement.scrollTop = 0;
+			resetScroll();
 			ui.addNotification(null, E('p', _('The allowlist is too big, unable to save modifications.')), 'error');
 		}
 		return E('div', { 'class': 'cbi-section cbi-section-descr' }, [
@@ -36,22 +40,22 @@ return view.extend({
 		]);
 	},
 	handleSave: function (ev) {
-		let value = ((document.querySelector('textarea').value || '').trim().toLowerCase().replace(/\r\n/g, '\n')) + '\n';
-		return fs.write(localFile, value)
-			.then(function () {
-				document.querySelector('textarea').value = value;
-				document.body.scrollTop = document.documentElement.scrollTop = 0;
-				if (!notMsg) {
-					ui.addNotification(null, E('p', _('Allowlist modifications have been saved, reload banIP that changes take effect.')), 'info');
-					notMsg = true;
-				}
-			}).catch(function (e) {
-				document.body.scrollTop = document.documentElement.scrollTop = 0;
-				if (!errMsg) {
-					ui.addNotification(null, E('p', _('Unable to save modifications: %s').format(e.message)), 'error');
-					errMsg = true;
-				}
-			});
+		let value = ((document.querySelector('textarea').value || '').trim().toLowerCase().replace(/\r\n?/g, '\n'));
+		return fs.write(localFile, value + "\n")
+		.then(function () {
+			document.querySelector('textarea').value = value;
+			resetScroll();
+			if (!notMsg) {
+				ui.addNotification(null, E('p', _('Allowlist modifications have been saved, reload banIP that changes take effect.')), 'info');
+				notMsg = true;
+			}
+		}).catch(function (e) {
+			resetScroll();
+			if (!errMsg) {
+				ui.addNotification(null, E('p', _('Unable to save modifications: %s').format(e.message)), 'error');
+				errMsg = true;
+			}
+		});
 	},
 	handleSaveApply: null,
 	handleReset: null
