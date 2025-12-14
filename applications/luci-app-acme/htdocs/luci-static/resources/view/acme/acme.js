735163e034f219380b54218f0289b7a78cfecc15
zhusir <smallprogram@foxmail.com>
2025-12-15 06:33:37 +0800

    remove the check file

--- applications/luci-app-acme/htdocs/luci-static/resources/view/acme/acme.js
+++ applications/luci-app-acme/htdocs/luci-static/resources/view/acme/acme.js
@@ -12,7 +12,7 @@ return view.extend({
 			L.resolveDefault(fs.list('/etc/ssl/acme/'), []).then(files => {
 				let certs = [];
 				for (let f of files) {
-					if ((f.type === 'file' || f.type === 'symlink' || f.type === 'link') && f.name.match(/\.fullchain\.crt$/)) {
+					if (f.name.match(/\.fullchain\.crt$/)) {
 						certs.push(f);
 					}
 				}
