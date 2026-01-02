146f4da8547e7efb55637cbf644f3dbd5ca5e82e
Self-Hosting-Group <selfhostinggroup-git+openwrt@shost.ing>
2026-01-02 00:00:00 +0000

    luci-app-upnp: improve existing UI slightly
    - Change `Active Service Port Maps` to `Active Port Maps`, use the same
      wording for the table headings and ACL as on the overview status page
    - Change description field header to `Added via / description`, always
      include the protocol and clearer/less redundant protocol labels
    - Disable IPv6 mapping (`ipv6_disable`): UI option added, UCI exists
    - Don't clear dependent options if UPnP IGD/STUN temporarily disabled
    - Set `notify_interval` minimum to 900 s (default), as recommended by
      [UDA 1.1] (2x=1800 in the standard), because daemon/OpenWrt wrongly
      suggested 30x less in the past, and to reduce multicast traffic and
      power consumption in wireless networks, clearer help
    - Report system instead of service uptime, UUID and service lease file:
      UI options removed (disabled, to keep translations) as rarely used
    - Slightly improve some text strings
    - Show hint if no suitable configuration found (missing related update),
      link to package manager, and show form as read-only to prevent changes
    - Configure service lease file by default in rpcd ucode for full UI
      functionality without option set
    - Refactoring by moving ubus connection as jow-/ucode@a58fe47 was merged
    - Adapt to JavaScript ES6 and remove some line breaks and format files
    - A commit has been prepared that manually adjusts the translations,
      without losing them
    
    Pre-update to comming PR: https://redirect.github.com/openwrt/luci/pull/7822
    
    [UDA 1.1]: https://upnp.org/specs/arch/UPnP-arch-DeviceArchitecture-v1.1.pdf#page=30
    
    Signed-off-by: Self-Hosting-Group <selfhostinggroup-git+openwrt@shost.ing>

--- applications/luci-app-upnp/htdocs/luci-static/resources/view/upnp/upnp.js
+++ applications/luci-app-upnp/htdocs/luci-static/resources/view/upnp/upnp.js
@@ -3,27 +3,28 @@
 'require dom';
 'require poll';
 'require uci';
+'require ui';
 'require rpc';
 'require form';
 
 const callInitAction = rpc.declare({
 	object: 'luci',
 	method: 'setInitAction',
-	params: [ 'name', 'action' ],
+	params: ['name', 'action'],
 	expect: { result: false }
 });
 
 const callUpnpGetStatus = rpc.declare({
 	object: 'luci.upnp',
 	method: 'get_status',
-	expect: {  }
+	expect: {}
 });
 
 const callUpnpDeleteRule = rpc.declare({
 	object: 'luci.upnp',
 	method: 'delete_rule',
-	params: [ 'token' ],
-	expect: { result : "OK" },
+	params: ['token'],
+	expect: { result: 'OK' },
 });
 
 function handleDelRule(num, ev) {
@@ -44,10 +45,10 @@ return view.extend({
 
 	poll_status: function(nodes, data) {
 
-		var rules = Array.isArray(data[0].rules) ? data[0].rules : [];
+		const rules = Array.isArray(data[0].rules) ? data[0].rules : [];
 
-		var rows = rules.map(function(rule) {
-			const padnum = (num, length) => num.toString().padStart(length, "0");
+		const rows = rules.map(function(rule) {
+			const padnum = (num, length) => num.toString().padStart(length, '0');
 			const expires_sec = rule?.expires || 0;
 			const hour = Math.floor(expires_sec / 3600);
 			const minute = Math.floor((expires_sec % 3600) / 60);
@@ -68,8 +69,9 @@ return view.extend({
 				rule.descr,
 				E('button', {
 					'class': 'btn cbi-button-remove',
-					'click': L.bind(handleDelRule, this, rule.num)
-				}, [ _('Delete') ])
+					'click': L.bind(handleDelRule, this, rule.num),
+					'title': _('Delete')
+				}, [_('Delete')])
 			];
 		});
 
@@ -80,35 +82,42 @@ return view.extend({
 
 		let m, s, o;
 
-		var protocols = '%s & %s/%s'.format(
+		const protocols = _('%s & %s/%s', '%s & %s/%s (%s = UPnP IGD, %s = PCP, %s = NAT-PMP)').format(
 			'<a href="https://en.wikipedia.org/wiki/Internet_Gateway_Device_Protocol" target="_blank" rel="noreferrer"><abbr title="UPnP Internet Gateway Device (Control Protocol)">UPnP IGD</abbr></a>',
 			'<a href="https://en.wikipedia.org/wiki/Port_Control_Protocol" target="_blank" rel="noreferrer"><abbr title="Port Control Protocol">PCP</abbr></a>',
 			'<a href="https://en.wikipedia.org/wiki/NAT_Port_Mapping_Protocol" target="_blank" rel="noreferrer"><abbr title="NAT Port Mapping Protocol">NAT-PMP</abbr></a>');
-		m = new form.Map('upnpd', [_('UPnP IGD & PCP/NAT-PMP Service')],
+		m = new form.Map('upnpd', _('UPnP IGD & PCP/NAT-PMP Service'),
 			_('The %s protocols allow clients on the local network to configure port maps/forwards on the router autonomously.',
 				'The %s (%s = UPnP IGD & PCP/NAT-PMP) protocols allow clients on the local network to configure port maps/forwards on the router autonomously.')
-				.format(protocols)
+			.format(protocols)
 		);
+		if (!uci.get('upnpd', 'config')) {
+			ui.addNotification(null, E('div', '<h4>' + _('No suitable configuration was found!') + '</h4><p>' +
+				_('No suitable (LuCI app %s) config found in %s. Related package update (daemon or LuCI app) may be missing.').format('v1.0', '<code>/etc/config/upnpd</code>') + '<br />' +
+				_('Use the software package manager, update lists, and install the related update. Config is migrated on the daemon package update.') + '</p>' +
+				'<a class="btn" href="/cgi-bin/luci/admin/system/package-manager?query=UPnP%20IGD%20&%20PCP/NAT-PMP">' + _('Go to package manager…') + '</a>'), 'warning');
+			m.readonly = true;
+		}
 
 		s = m.section(form.GridSection, '_active_rules');
 
 		s.render = L.bind(function(view, section_id) {
-			var table = E('table', { 'class': 'table cbi-section-table', 'id': 'upnp_status_table' }, [
+			const table = E('table', { 'class': 'table cbi-section-table', 'id': 'upnp_status_table' }, [
 				E('tr', { 'class': 'tr table-titles' }, [
-					E('th', { 'class': 'th' }, _('Client Name')),
-					E('th', { 'class': 'th' }, _('Client Address')),
-					E('th', { 'class': 'th' }, _('Client Port')),
-					E('th', { 'class': 'th' }, _('External Port')),
+					E('th', { 'class': 'th' }, _('Hostname')),
+					E('th', { 'class': 'th' }, _('IP address')),
+					E('th', { 'class': 'th' }, _('Port')),
+					E('th', { 'class': 'th' }, _('External port')),
 					E('th', { 'class': 'th' }, _('Protocol')),
 					E('th', { 'class': 'th right' }, _('Expires')),
-					E('th', { 'class': 'th' }, _('Description')),
+					E('th', { 'class': 'th' }, _('Added via / description')),
 					E('th', { 'class': 'th cbi-section-actions' }, '')
 				])
 			]);
 
-			var rules = Array.isArray(data[0].rules) ? data[0].rules : [];
+			const rules = Array.isArray(data[0].rules) ? data[0].rules : [];
 
-			var rows = rules.map(function(rule) {
+			const rows = rules.map(function(rule) {
 				return [
 					rule.host_hint || _('Unknown'),
 					rule.intaddr,
@@ -118,15 +127,17 @@ return view.extend({
 					rule.descr,
 					E('button', {
 						'class': 'btn cbi-button-remove',
-						'click': L.bind(handleDelRule, this, rule.num)
-					}, [ _('Delete') ])
+						'click': L.bind(handleDelRule, this, rule.num),
+						'title': _('Delete')
+					}, [_('Delete')])
 				];
 			});
 
 			cbi_update_table(table, rows, E('em', _('There are no active port maps.')));
 
 			return E('div', { 'class': 'cbi-section cbi-tblsection' }, [
-					E('h3', _('Active Service Port Maps')), table ]);
+				E('h3', _('Active Port Maps')), table
+			]);
 		}, o, this);
 
 		s = m.section(form.NamedSection, 'config', 'upnpd', _('Service Settings'));
@@ -134,8 +145,8 @@ return view.extend({
 		s.tab('setup', _('Service Setup'));
 		s.tab('advanced', _('Advanced Settings'));
 
-		o = s.taboption('setup', form.Flag, 'enabled', _('Start service'),
-			_('Start autonomous port mapping service'));
+		o = s.taboption('setup', form.Flag, 'enabled', _('Enable service'),
+			_('Enable the autonomous port mapping service'));
 		o.rmempty = false;
 
 		o = s.taboption('setup', form.Flag, 'enable_upnp', _('Enable UPnP IGD protocol'));
@@ -149,86 +160,104 @@ return view.extend({
 		o.default = '1';
 		o.rmempty = false;
 		o.depends('enable_upnp', '1');
+		o.retain = true;
 
 		o = s.taboption('setup', form.Value, 'download', _('Download speed'),
 			_('Report maximum download speed in kByte/s'));
 		o.depends('enable_upnp', '1');
+		o.retain = true;
 
 		o = s.taboption('setup', form.Value, 'upload', _('Upload speed'),
 			_('Report maximum upload speed in kByte/s'));
 		o.depends('enable_upnp', '1');
+		o.retain = true;
 
 		s.taboption('advanced', form.Flag, 'use_stun', _('Use %s', 'Use %s (%s = STUN)')
-				.format('<a href="https://en.wikipedia.org/wiki/STUN" target="_blank" rel="noreferrer"><abbr title="Session Traversal Utilities for NAT">STUN</abbr></a>'),
+			.format('<a href="https://en.wikipedia.org/wiki/STUN" target="_blank" rel="noreferrer"><abbr title="Session Traversal Utilities for NAT">STUN</abbr></a>'),
 			_('To detect the public IPv4 address for unrestricted full-cone/one-to-one NATs'));
 
 		o = s.taboption('advanced', form.Value, 'stun_host', _('STUN host'));
-		o.depends('use_stun', '1');
 		o.datatype = 'host';
+		o.depends('use_stun', '1');
+		o.retain = true;
 
 		o = s.taboption('advanced', form.Value, 'stun_port', _('STUN port'));
-		o.depends('use_stun', '1');
 		o.datatype = 'port';
 		o.placeholder = '3478';
+		o.depends('use_stun', '1');
+		o.retain = true;
 
 		o = s.taboption('advanced', form.Flag, 'secure_mode', _('Enable secure mode'),
 			_('Allow adding port maps for requesting IP addresses only'));
 		o.default = '1';
 		o.depends('enable_upnp', '1');
+		o.retain = true;
+
+		s.taboption('advanced', form.Flag, 'ipv6_disable', _('Disable IPv6 mapping'));
 
 		o = s.taboption('advanced', form.Value, 'notify_interval', _('Notify interval'),
-			_('A 900s interval will result in %s notifications with the minimum max-age of 1800s', 'A 900s interval will result in %s (%s = SSDP) notifications with the minimum max-age of 1800s')
-				.format('<abbr title="Simple Service Discovery Protocol">SSDP</abbr>'));
-		o.datatype = 'uinteger';
+			_('A 900 s interval sends %s announcements with the min. %s header',
+				'A 900 s interval sends %s (%s = SSDP) announcements with the min. %s (%s = Cache-Control: max-age=1800) header')
+			.format('<abbr title="Simple Service Discovery Protocol">SSDP</abbr>', '<code>Cache-Control: max-age=1800</code>'));
+		o.datatype = 'min(900)';
 		o.placeholder = '900';
 		o.depends('enable_upnp', '1');
+		o.retain = true;
 
 		o = s.taboption('advanced', form.Value, 'port', _('SOAP/HTTP port'));
 		o.datatype = 'port';
 		o.placeholder = '5000';
 		o.depends('enable_upnp', '1');
+		o.retain = true;
 
-		o = s.taboption('advanced', form.Value, 'presentation_url', _('Presentation URL'),
-			_('Report custom router web interface (presentation) URL'));
+		o = s.taboption('advanced', form.Value, 'presentation_url', _('Router/presentation URL'),
+			_('Report custom router web interface URL'));
 		o.placeholder = 'http://192.168.1.1/';
 		o.depends('enable_upnp', '1');
+		o.retain = true;
 
 		o = s.taboption('advanced', form.Value, 'uuid', _('Device UUID'));
-		o.depends('enable_upnp', '1');
+		// o.depends('enable_upnp', '1');
+		o.depends('to-disable-as-rarely-used', '1');
+		o.retain = true;
 
 		o = s.taboption('advanced', form.Value, 'model_number', _('Announced model number'));
 		o.depends('enable_upnp', '1');
+		o.retain = true;
 
 		o = s.taboption('advanced', form.Value, 'serial_number', _('Announced serial number'));
 		o.depends('enable_upnp', '1');
+		o.retain = true;
 
 		o = s.taboption('advanced', form.Flag, 'system_uptime', _('Report system instead of service uptime'));
 		o.default = '1';
-		o.depends('enable_upnp', '1');
+		o.depends('to-disable-as-rarely-used', '1');
+		o.retain = true;
 
 		s.taboption('advanced', form.Flag, 'log_output', _('Enable additional logging'),
 			_('Puts extra debugging information into the system log'));
 
 		o = s.taboption('advanced', form.Value, 'upnp_lease_file', _('Service lease file'));
-		o.placeholder = '/var/run/miniupnpd.leases';
+		o.depends('to-disable-as-rarely-used', '1');
+		o.retain = true;
 
 		s = m.section(form.GridSection, 'perm_rule', _('Service Access Control List'),
 			_('ACL specify which client addresses and ports can be mapped, IPv6 always allowed.'));
-		s.sortable = true;
 		s.anonymous = true;
 		s.addremove = true;
+		s.sortable = true;
 
 		s.option(form.Value, 'comment', _('Comment'));
 
-		o = s.option(form.Value, 'int_addr', _('Client Address'));
+		o = s.option(form.Value, 'int_addr', _('IP address'));
 		o.datatype = 'ip4addr';
 		o.placeholder = '0.0.0.0/0';
 
-		o = s.option(form.Value, 'int_ports', _('Client Port'));
+		o = s.option(form.Value, 'int_ports', _('Port'));
 		o.datatype = 'portrange';
 		o.placeholder = '1-65535';
 
-		o = s.option(form.Value, 'ext_ports', _('External Port'));
+		o = s.option(form.Value, 'ext_ports', _('External port'));
 		o.datatype = 'portrange';
 		o.placeholder = '1-65535';
 
