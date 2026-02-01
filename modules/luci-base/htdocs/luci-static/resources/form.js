04a39dc9ce8ebffcd91317fc9642dacf97c8dcd3
Paul Donald <newtwen+github@gmail.com>
2026-02-01 19:44:11 +0100

    luci-base: render button for drag handle
    follow-up to 4d04877e0f013fd22bb184183bdaffe01c39a980
    
    divs look OK in all themes but for Openwrt2020, but button
    looks fine everywhere (as it was before above commit).
    
    Signed-off-by: Paul Donald <newtwen+github@gmail.com>
    (cherry picked from commit e365958f7d6009598c67658681f828cebd802328)

--- modules/luci-base/htdocs/luci-static/resources/form.js
+++ modules/luci-base/htdocs/luci-static/resources/form.js
@@ -2740,7 +2740,7 @@ const CBITableSection = CBITypedSection.extend(/** @lends LuCI.form.TableSection
 					this.handleTouchEnd(ev);
 				}, this) : null
 			};
-			const dragHandle = E('div', dragHandleProps, '☰');
+			const dragHandle = E('button', dragHandleProps, '☰');
 			dom.append(tdEl.lastElementChild, [ dragHandle ]);
 		}
 
