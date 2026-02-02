d9036435d5f65cb85ddd5ef43aa0e6234c2cb4d5
Paul Donald <newtwen+github@gmail.com>
2026-02-02 22:40:20 +0100

    luci-app-firewall: enable row filtering for firewall rules
    This is an area users seem to have most trouble searching
    (via the browser window search), and firewall tables
    tend to be the largest tables. Enable search.
    
    Signed-off-by: Paul Donald <newtwen+github@gmail.com>

--- applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/rules.js
+++ applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/rules.js
@@ -178,6 +178,7 @@ return view.extend({
 		s.anonymous = true;
 		s.sortable  = true;
 		s.cloneable = true;
+		s.filterrow = true;
 
 		s.tab('general', _('General Settings'));
 		s.tab('advanced', _('Advanced Settings'));
