3a9c5b4bad17bc340d882988c178541de5c289d0
Paul Donald <newtwen+github@gmail.com>
2026-02-23 22:03:39 +0100

    luci-app-bmx7: convert to JS
    migrate away from the old luci-app lua control system.
    
    See: https://github.com/openwrt/luci/issues/7310
    
    The lua app was relatively broken prior to conversion.
    
    Strangely, bmx7-uci-config package is what installs the
    init.d service and uci config.
    
    Signed-off-by: Paul Donald <newtwen+github@gmail.com>
    (cherry picked from commit f578786152c9e792202a8a9d08312a72849bf6d5)

--- applications/luci-app-bmx7/htdocs/luci-static/resources/bmx7/js/polling.js
+++ applications/luci-app-bmx7/htdocs/luci-static/resources/bmx7/js/polling.js
@@ -1,23 +1,23 @@
 /*
-    Copyright © 2011 Pau Escrich <pau@dabax.net>
-    Contributors Lluis Esquerda <eskerda@gmail.com>
+	Copyright © 2011 Pau Escrich <pau@dabax.net>
+	Contributors Lluis Esquerda <eskerda@gmail.com>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-    The full GNU General Public License is included in this distribution in
-    the file called "COPYING".
+	The full GNU General Public License is included in this distribution in
+	the file called "COPYING".
 */
 
 
@@ -25,20 +25,20 @@
 	Table pooler is a function to easy call XHR poller. 
 
 	new TablePooler(5,"/cgi-bin/bmx7-info", {'status':''}, "status_table", function(st){
-		var table = Array()
+		let table = Array()
 		table.push(st.first,st.second)
 		return table
 	}
 
 	The parameters are: 
-		polling_time: time between pollings
-		json_url: the json url to fetch the data
-		json_call: the json call
-		output_table_id: the table where javascript will put the data
+		polling_time: time between polls
+		json_url: the JSON URL to fetch the data
+		json_call: the JSON call
+		output_table_id: the table where JavaScript will put the data
 		callback_function: the function that will be executed each polling_time
 	
 	The callback_function must return an array of arrays (matrix).
-	In the code st is the data obtained from the json call
+	In the code st is the data obtained from the JSON call
 */
 
 function TablePooler (time, jsonurl, getparams, div_id, callback) {
@@ -51,9 +51,9 @@ function TablePooler (time, jsonurl, getparams, div_id, callback) {
 
 	this.start = function(){
 		XHR.poll(this.time, this.jsonurl, this.getparams, function(x, st){
-			var data = this.callback(st);
-			var content;
-			for (var i = 0; i < data.length; i++){
+			let data = this.callback(st);
+			let content, rowDiv, rowId, cellDiv, cellId;
+			for (let i = 0; i < data.length; i++){
 				rowId = "trDiv_" + this.div_id + i;
 				rowDiv = document.getElementById(rowId);
 				if (rowDiv === null) {
@@ -62,7 +62,7 @@ function TablePooler (time, jsonurl, getparams, div_id, callback) {
 					rowDiv.className = "tr";
 					this.div.appendChild(rowDiv);
 				}
-				for (var j = 0; j < data[i].length; j++){
+				for (let j = 0; j < data[i].length; j++){
 					cellId = "tdDiv_" + this.div_id + i + j;
 					cellDiv = document.getElementById(cellId);
 					if (cellDiv === null) {
