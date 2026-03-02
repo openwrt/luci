4bba32c88eec94b14e7eda8df5014c981859d3c7
Stan Grishin <stangri@melmac.ca>
2026-03-02 20:33:19 +0000

    luci-app-pbr: update to 1.2.2-r8
    * version bump to match the principal package
    * safer wait for long pbr operations
    
    Signed-off-by: Stan Grishin <stangri@melmac.ca>
    (cherry picked from commit 9c67861c6551ddf8b1b5fe6cb38a8e8d6c2d37e3)
    Signed-off-by: Stan Grishin <stangri@melmac.ca>

--- applications/luci-app-pbr/htdocs/luci-static/resources/pbr/status.js
+++ applications/luci-app-pbr/htdocs/luci-static/resources/pbr/status.js
@@ -131,12 +131,61 @@ var RPC = {
 		});
 	},
 	setInitAction: function (name, action) {
-		_setInitAction(name, action).then(
-			function (result) {
-				this.emit("setInitAction", result);
-			}.bind(this)
-		);
+		_setInitAction(name, action)
+			.then(
+				function (result) {
+					this.emit("setInitAction", { result: result, action: action });
+				}.bind(this),
+			)
+			.catch(
+				function (error) {
+					// Even if RPC call fails/times out, emit event to start polling
+					// This handles cases where the backend task starts but RPC times out
+					this.emit("setInitAction", { timeout: true, action: action });
+				}.bind(this),
+			);
 	},
+};
+
+// Poll service status until the expected running state is reached.
+// expectRunning: true for start/restart, false for stop.
+var pollServiceStatus = function (expectRunning, callback) {
+	var maxAttempts = 300; // Max 5 minutes of polling
+	var attempt = 0;
+
+	var checkStatus = function () {
+		attempt++;
+
+		L.resolveDefault(getInitStatus(pkg.Name), {})
+			.then(function (statusData) {
+				var isRunning =
+					statusData && statusData[pkg.Name] && statusData[pkg.Name].running;
+
+				// Check if the expected state has been reached
+				if (expectRunning ? isRunning === true : isRunning !== true) {
+					callback(true);
+				}
+				// Check if timed out
+				else if (attempt >= maxAttempts) {
+					callback(false, "timeout");
+				}
+				// Continue polling
+				else {
+					setTimeout(checkStatus, 1000);
+				}
+			})
+			.catch(function (err) {
+				// Retry on error unless timed out
+				if (attempt < maxAttempts) {
+					setTimeout(checkStatus, 1000);
+				} else {
+					callback(false, "error");
+				}
+			});
+	};
+
+	// Start polling after a delay to give the backend time to start the task
+	setTimeout(checkStatus, 3000);
 };
 
 var status = baseclass.extend({
@@ -680,8 +729,24 @@ var status = baseclass.extend({
 });
 
 RPC.on("setInitAction", function (reply) {
-	ui.hideModal();
-	location.reload();
+	var action = reply && reply.action;
+	if (action === "start" || action === "restart" || action === "reload") {
+		// Long-running: poll until service is running
+		pollServiceStatus(true, function () {
+			ui.hideModal();
+			location.reload();
+		});
+	} else if (action === "stop") {
+		// Poll until service has stopped
+		pollServiceStatus(false, function () {
+			ui.hideModal();
+			location.reload();
+		});
+	} else {
+		// enable/disable are fast, just reload immediately
+		ui.hideModal();
+		location.reload();
+	}
 });
 
 return L.Class.extend({
