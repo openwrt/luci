39c86fe3cdd0854bec7d2907a39f1c0e243bad57
S. Hoot <spoot_hoot@protonmail.com>
2026-05-25 10:13:00 -0400

    luci-base: disable CSSTIDY by default
    CSSTIDY relies on an outdated app that frequently causes build errors.
    Similar to the deprecated 660-fq-codel-buffer patch, this default is a
    relic from an era when SOCs had 32-64MB of memory. Modern hardware
    routinely has 128MB+, making this default minification unnecessary
    and problematic for current build environments.
    
    Signed-off-by: S. Hoot <spoot_hoot@protonmail.com>

--- luci.mk
+++ luci.mk
@@ -310,7 +310,7 @@ ifeq ($(PKG_NAME),luci-base)
 
    config LUCI_CSSTIDY
 	bool "Minify CSS files"
-	default y
+	default n
 
    menu "Translations"$(foreach lang,$(LUCI_LANGUAGES),$(if $(LUCI_LANG.$(lang)),
 
