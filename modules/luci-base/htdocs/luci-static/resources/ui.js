a9eb33135ed0a477cc414dad0057c79ac94b755f
Maxim Skokov <skokov.m020709@gmail.com>
2026-07-01 14:09:19 +0300

    luci-base: fix undefined View reference in instantiateView()
    instantiateView() guarded against invalid view modules with:
    
      if (!(view instanceof View))
    
    but View (capital V) was never declared or imported in ui.js, causing
    a ReferenceError whenever the guard was reached. Additionally,
    L.require() returns a class instance rather than a constructor, making
    instanceof conceptually incorrect here regardless.
    
    Replace with a duck-type check on the render() method, which all valid
    LuCI view modules are required to implement. This preserves the
    fail-fast behavior for invalid modules without introducing a circular
    dependency or relying on an undefined identifier.
    
    Also fixes two pre-existing no-useless-escape lint errors in the same
    file (lines 3365 and 5105) that were surfaced by ESLint when the file
    was touched.
    
    Signed-off-by: Maxim Skokov <skokov.m020709@gmail.com>

--- modules/luci-base/htdocs/luci-static/resources/ui.js
+++ modules/luci-base/htdocs/luci-static/resources/ui.js
@@ -3362,7 +3362,7 @@ const UIFileUpload = UIElement.extend(/** @lends LuCI.ui.FileUpload.prototype */
 	 * @returns {Promise}
 	 */
 	handleDelete(path, fileStat, ev) {
-		const parent = path.replace(/\/[^\/]+$/, '') ?? '/';
+		const parent = path.replace(/\/[^/]+$/, '') ?? '/';
 		const name = path.replace(/^.+\//, '');
 		let msg;
 
@@ -5102,7 +5102,7 @@ const UI = baseclass.extend(/** @lends LuCI.ui.prototype */ {
 				for (let i = 0; i < 2; i++)
 					for (let j = 0; j < ipaddrs.length; j++)
 						tasks.push(this.pingDevice(i ? 'https' : 'http', ipaddrs[j])
-							.then(ev => { reachable = ev.target.src.replace(/^(https?:\/\/[^\/]+).*$/, '$1/') }, () => {}));
+							.then(ev => { reachable = ev.target.src.replace(/^(https?:\/\/[^/]+).*$/, '$1/') }, () => {}));
 
 				return Promise.all(tasks).then(() => {
 					if (reachable) {
@@ -5715,7 +5715,7 @@ const UI = baseclass.extend(/** @lends LuCI.ui.prototype */ {
 		const className = 'view.%s'.format(path.replace(/\//g, '.'));
 
 		return L.require(className).then(view => {
-			if (!(view instanceof View))
+			if (typeof view?.render !== 'function')
 				throw new TypeError('Loaded class %s is not a descendant of View'.format(className));
 
 			return view;
