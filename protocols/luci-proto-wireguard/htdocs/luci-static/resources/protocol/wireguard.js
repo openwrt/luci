3eebcfead570fa39c67ae628d2fc990df1b085f0
BdN3504 <ein_armer_schlucker@gmx.de>
2026-07-07 14:36:03 +0200

    luci-proto-wireguard: fix peer IP allocation issues
    - Pass a client-side session reservation list to getNextPeerAddress so
      back-to-back peer creation within the same page session skips already-
      allocated octets that haven't reached committed UCI yet
    - Use uci.save() instead of parent.save() for the allowed_ips write-back;
      parent.save() re-reads the DOM (where the widget is still empty) and
      was overwriting the allocation
    - Repopulate the allowed_ips widget when returning from the Generate
      configuration dialog so a subsequent Save doesn't overwrite the value
    
    Signed-off-by: BdN3504 <ein_armer_schlucker@gmx.de>

--- protocols/luci-proto-wireguard/htdocs/luci-static/resources/protocol/wireguard.js
+++ protocols/luci-proto-wireguard/htdocs/luci-static/resources/protocol/wireguard.js
@@ -31,9 +31,13 @@ var generatePsk = rpc.declare({
 var getNextPeerAddress = rpc.declare({
 	object: 'luci.wireguard',
 	method: 'getNextPeerAddress',
-	params: ['interface'],
+	params: ['interface', 'reserved'],
 	expect: {}
 });
+
+// Octets allocated this page session but not yet committed to UCI,
+// so back-to-back peer creation doesn't produce duplicate addresses.
+var _sessionReserved = [];
 
 var qrIcon = '<svg viewBox="0 0 29 29" xmlns="http://www.w3.org/2000/svg"><path fill="#fff" d="M0 0h29v29H0z"/><path d="M4 4h1v1H4zM5 4h1v1H5zM6 4h1v1H6zM7 4h1v1H7zM8 4h1v1H8zM9 4h1v1H9zM10 4h1v1h-1zM12 4h1v1h-1zM13 4h1v1h-1zM14 4h1v1h-1zM15 4h1v1h-1zM16 4h1v1h-1zM18 4h1v1h-1zM19 4h1v1h-1zM20 4h1v1h-1zM21 4h1v1h-1zM22 4h1v1h-1zM23 4h1v1h-1zM24 4h1v1h-1zM4 5h1v1H4zM10 5h1v1h-1zM12 5h1v1h-1zM14 5h1v1h-1zM16 5h1v1h-1zM18 5h1v1h-1zM24 5h1v1h-1zM4 6h1v1H4zM6 6h1v1H6zM7 6h1v1H7zM8 6h1v1H8zM10 6h1v1h-1zM12 6h1v1h-1zM18 6h1v1h-1zM20 6h1v1h-1zM21 6h1v1h-1zM22 6h1v1h-1zM24 6h1v1h-1zM4 7h1v1H4zM6 7h1v1H6zM7 7h1v1H7zM8 7h1v1H8zM10 7h1v1h-1zM12 7h1v1h-1zM13 7h1v1h-1zM14 7h1v1h-1zM15 7h1v1h-1zM18 7h1v1h-1zM20 7h1v1h-1zM21 7h1v1h-1zM22 7h1v1h-1zM24 7h1v1h-1zM4 8h1v1H4zM6 8h1v1H6zM7 8h1v1H7zM8 8h1v1H8zM10 8h1v1h-1zM16 8h1v1h-1zM18 8h1v1h-1zM20 8h1v1h-1zM21 8h1v1h-1zM22 8h1v1h-1zM24 8h1v1h-1zM4 9h1v1H4zM10 9h1v1h-1zM12 9h1v1h-1zM13 9h1v1h-1zM15 9h1v1h-1zM18 9h1v1h-1zM24 9h1v1h-1zM4 10h1v1H4zM5 10h1v1H5zM6 10h1v1H6zM7 10h1v1H7zM8 10h1v1H8zM9 10h1v1H9zM10 10h1v1h-1zM12 10h1v1h-1zM14 10h1v1h-1zM16 10h1v1h-1zM18 10h1v1h-1zM19 10h1v1h-1zM20 10h1v1h-1zM21 10h1v1h-1zM22 10h1v1h-1zM23 10h1v1h-1zM24 10h1v1h-1zM13 11h1v1h-1zM14 11h1v1h-1zM15 11h1v1h-1zM16 11h1v1h-1zM4 12h1v1H4zM5 12h1v1H5zM8 12h1v1H8zM9 12h1v1H9zM10 12h1v1h-1zM13 12h1v1h-1zM15 12h1v1h-1zM19 12h1v1h-1zM21 12h1v1h-1zM22 12h1v1h-1zM23 12h1v1h-1zM24 12h1v1h-1zM5 13h1v1H5zM6 13h1v1H6zM8 13h1v1H8zM11 13h1v1h-1zM13 13h1v1h-1zM14 13h1v1h-1zM15 13h1v1h-1zM16 13h1v1h-1zM19 13h1v1h-1zM22 13h1v1h-1zM4 14h1v1H4zM5 14h1v1H5zM9 14h1v1H9zM10 14h1v1h-1zM11 14h1v1h-1zM15 14h1v1h-1zM18 14h1v1h-1zM19 14h1v1h-1zM20 14h1v1h-1zM21 14h1v1h-1zM22 14h1v1h-1zM23 14h1v1h-1zM7 15h1v1H7zM8 15h1v1H8zM9 15h1v1H9zM11 15h1v1h-1zM12 15h1v1h-1zM13 15h1v1h-1zM17 15h1v1h-1zM18 15h1v1h-1zM20 15h1v1h-1zM21 15h1v1h-1zM23 15h1v1h-1zM4 16h1v1H4zM6 16h1v1H6zM10 16h1v1h-1zM11 16h1v1h-1zM13 16h1v1h-1zM14 16h1v1h-1zM16 16h1v1h-1zM17 16h1v1h-1zM18 16h1v1h-1zM22 16h1v1h-1zM23 16h1v1h-1zM24 16h1v1h-1zM12 17h1v1h-1zM16 17h1v1h-1zM17 17h1v1h-1zM18 17h1v1h-1zM4 18h1v1H4zM5 18h1v1H5zM6 18h1v1H6zM7 18h1v1H7zM8 18h1v1H8zM9 18h1v1H9zM10 18h1v1h-1zM14 18h1v1h-1zM16 18h1v1h-1zM17 18h1v1h-1zM21 18h1v1h-1zM22 18h1v1h-1zM23 18h1v1h-1zM4 19h1v1H4zM10 19h1v1h-1zM12 19h1v1h-1zM13 19h1v1h-1zM15 19h1v1h-1zM16 19h1v1h-1zM19 19h1v1h-1zM21 19h1v1h-1zM23 19h1v1h-1zM24 19h1v1h-1zM4 20h1v1H4zM6 20h1v1H6zM7 20h1v1H7zM8 20h1v1H8zM10 20h1v1h-1zM12 20h1v1h-1zM13 20h1v1h-1zM15 20h1v1h-1zM18 20h1v1h-1zM19 20h1v1h-1zM20 20h1v1h-1zM22 20h1v1h-1zM23 20h1v1h-1zM24 20h1v1h-1zM4 21h1v1H4zM6 21h1v1H6zM7 21h1v1H7zM8 21h1v1H8zM10 21h1v1h-1zM13 21h1v1h-1zM15 21h1v1h-1zM16 21h1v1h-1zM19 21h1v1h-1zM21 21h1v1h-1zM23 21h1v1h-1zM24 21h1v1h-1zM4 22h1v1H4zM6 22h1v1H6zM7 22h1v1H7zM8 22h1v1H8zM10 22h1v1h-1zM13 22h1v1h-1zM15 22h1v1h-1zM18 22h1v1h-1zM19 22h1v1h-1zM20 22h1v1h-1zM21 22h1v1h-1zM22 22h1v1h-1zM4 23h1v1H4zM10 23h1v1h-1zM12 23h1v1h-1zM13 23h1v1h-1zM14 23h1v1h-1zM17 23h1v1h-1zM18 23h1v1h-1zM20 23h1v1h-1zM22 23h1v1h-1zM4 24h1v1H4zM5 24h1v1H5zM6 24h1v1H6zM7 24h1v1H7zM8 24h1v1H8zM9 24h1v1H9zM10 24h1v1h-1zM12 24h1v1h-1zM13 24h1v1h-1zM14 24h1v1h-1zM16 24h1v1h-1zM17 24h1v1h-1zM18 24h1v1h-1zM22 24h1v1h-1zM24 24h1v1h-1z"/></svg>';
 
@@ -718,15 +722,20 @@ return network.registerProtocol('wireguard', {
 				// peers (no IPs set) we fall through to next-address allocation.
 				let eips = L.toArray(peerSection.cfgvalue(section_id, 'allowed_ips'));
 
-				return (eips.length ? Promise.resolve(null) : getNextPeerAddress(s.section))
+				return (eips.length ? Promise.resolve(null) : getNextPeerAddress(s.section, _sessionReserved))
 					.then(function(next) {
 						if (next && !next.error) {
 							if (next.ipv4) eips.push(next.ipv4);
 							if (next.ipv6) eips.push(next.ipv6);
+							// Remember this octet for the lifetime of the page so a
+							// second peer allocation in the same session skips it.
+							_sessionReserved.push(next.octet);
 							// Write the allocated addresses back as allowed_ips so the
 							// server-side peer entry is complete without a manual edit.
+							// Use uci.save() directly: parent.save() re-reads the DOM
+							// (where allowed_ips is still empty) and would overwrite us.
 							uci.set('network', section_id, 'allowed_ips', eips);
-							return parent.save(null, true);
+							return uci.save();
 						}
 					}).then(function() {
 						const hostnames = [];
@@ -891,6 +900,10 @@ return network.registerProtocol('wireguard', {
 											mapNode.nextSibling.style = bRowStyle;
 											// Remove the H4 span (») title
 											headNode.removeChild(headNode.lastChild);
+											// Sync the allowed_ips widget with what we wrote
+											// to UCI so a subsequent Save doesn't overwrite it.
+											if (eips.length)
+												peerSection.getUIElement(section_id, 'allowed_ips')?.setValue(eips);
 										}
 									}, [ _('Back to peer configuration') ])
 								])
