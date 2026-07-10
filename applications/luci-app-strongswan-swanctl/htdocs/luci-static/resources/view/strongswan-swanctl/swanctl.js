e9491a4949b6b84e2bafc2b809ff75829ddf3154
Florian Eckert <fe@dev.tdt.de>
2026-07-10 13:53:06 +0200

    luci-app-strongswan-swanctl: split page into tunnel and service config
    Strongswan has many options that are not yet included in LuCI. For
    future expansion, it would make sense to split these across different
    domains.
    
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- applications/luci-app-strongswan-swanctl/htdocs/luci-static/resources/view/strongswan-swanctl/swanctl.js
+++ applications/luci-app-strongswan-swanctl/htdocs/luci-static/resources/view/strongswan-swanctl/swanctl.js
@@ -4,7 +4,6 @@
 'require rpc';
 'require uci';
 'require ui';
-'require tools.widgets as widgets';
 
 const callListAlgorithms = rpc.declare({
 	object: 'luci.swanctl',
@@ -67,24 +66,9 @@ return view.extend({
 		if (error)
 			ui.addNotification(null, E('p', _('Some options are unavailable because swanctl failed to load: %s').format(error)), 'warning');
 
-		m = new form.Map('ipsec', _('strongSwan Configuration'),
-			_('Configure strongSwan for secure VPN connections.'));
+		m = new form.Map('ipsec', _('Remote/Tunnel configuration'),
+			_('On this page, you can configure the IPsec tunnels and remotes.'));
 		m.tabbed = true;
-
-		// strongSwan General Settings
-		s = m.section(form.NamedSection, 'globals', 'ipsec', _('General Settings'));
-
-		o = s.option(widgets.NetworkSelect, 'interface', _('Listening Interfaces'),
-			_('Interfaces that accept VPN traffic') + '<br /> ' +
-			_('Select an interface or leave empty for all interfaces'));
-		o.multiple = true;
-		o.nocreate = true;
-		o.optional = true;
-
-		o = s.option(form.Value, 'debug', _('Debug Level'),
-			_('Trace level: 0 is least verbose, 4 is most'));
-		o.default = '0';
-		o.datatype = 'range(0,4)';
 
 		// Remote Configuration
 		s = m.section(form.GridSection, 'remote', _('Remote Configuration'),
