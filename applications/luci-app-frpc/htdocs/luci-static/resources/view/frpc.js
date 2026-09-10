7acf5d9b43d0422ffb89df897029eaf2fd6bf25a
Javen Li <j.wroiate@gmail.com>
2026-09-10 19:52:34 +0800

    luci-app-frp: avoid saving the same form twice
    The frpc and frps status sections render a nested cbi-map without a
    bound form instance. LuCI resolves it to the enclosing map, so save
    and reset invoke that map twice. Concurrent saves can submit duplicate
    UCI delete requests and report resource not found.
    
    Remove the form marker from both status wrappers while retaining their
    fieldsets and polling. This addresses the duplicate-save cause of
    issue #7679 on openwrt-25.12.
    
    Signed-off-by: Javen Li <j.wroiate@gmail.com>

--- applications/luci-app-frpc/htdocs/luci-static/resources/view/frpc.js
+++ applications/luci-app-frpc/htdocs/luci-static/resources/view/frpc.js
@@ -184,7 +184,7 @@ return view.extend({
 				});
 			});
 
-			return E('div', { class: 'cbi-map' },
+			return E('div', {},
 				E('fieldset', { class: 'cbi-section'}, [
 					E('p', { id: 'service_status' },
 						_('Collecting data ...'))
