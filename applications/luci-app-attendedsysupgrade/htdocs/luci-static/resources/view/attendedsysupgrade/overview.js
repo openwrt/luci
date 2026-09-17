openwrt/luci

commit 62382556d713725a13d2d322e21ed332deb1799c

luci-theme-openwrt-2020: use lighter color for checkboxes
When using browser extensions for automatic dark mode on pages,
the background is made too dark to see whether or not
the checkboxes are being toggled or their current state.

Use the brighter blue color for the check in the checkbox
which is suitable for either view as a light mode or dark mode,
and also makes the color of the check match the progress bar color.

This color change also affects "radio buttons"
which likely have the same problem as checkboxes,
although there are almost no "radio buttons" in LuCI at all.

Signed-off-by: Michael Pratt <mcpratt@pm.me>

themes/luci-theme-openwrt-2020/htdocs/luci-static/openwrt2020/cascade.css
@@ -981,7 +981,7 @@ button[disabled], button.disabled, .btn[disabled], .btn.disabled {
 input[type="checkbox"],
 input[type="radio"] {
 	--bd-color: var(--main-dark-color);
-	--fg-color: var(--main-dark-color);
+	--fg-color: var(--main-bright-color);
 	-webkit-appearance: none; /* nonstandard, should remove in future */
 	appearance: none;
 	width: 1em;

commit ebbc970353ee295a1d8588a49d05d9322dff5572

treewide: replace "btn" class in label elements with input
Interactive inputs like checkboxes and radio buttons
are commonly nested within a label element to manage the text and spacing
and to allow the text to be part of a larger click target for the input.

Because of that, label elements behave similarly to a generic button,
however, the styling for the generic button class is too broad for inputs,
as we have to be consistent with how checkboxes are presented
between the different pages and modals, while at the same time,
balancing the color of the check or radio button toggle with it's background,
which is usually not colored by the button class, in both light modes
and dark modes, whether programmed or third-party generated, for each theme.

This improves the consistency in how checkboxes and similar inputs are handled
by removing the "btn" class from the remaining use cases, but still using similar
style attributes in order to keep the critical padding and effects.
Use an existing but underutilized class "cbi-checkbox" instead.

Also, remove all style instances of label.btn, which, as explained
is slightly oxymoronic as the two should be treated separately,
and there should no longer be any instances of a label with "btn" class.

For styling with all themes, use the directive "label.cbi-checkbox"
so the styling for checkboxes with text after, especially the border or shadow,
does not also affect checkboxes in a div element on other configuration pages.
These elements should now be the only instances of a label with "cbi-checkbox" class.

In order to remain consistent with other checkboxes, add a blank label element
in the container with a "for" attribute referencing an id for the input element.
This is used for certain themes, like Footstrap, to replace the checkbox altogether
with a slide-style switch that is animated and colored according to the input state.
Because this is a new addition for the Footstrap theme, changes to the styling
for that theme is a special exception with much more modification than the rest.
Specifically, use "gap" instead of "margin-inline-end" and use "flex-shrink"
in order to properly space the text from the switch and prevent line wrapping
from shrinking the width of the background of the switch despite good spacing.

Finally, for all themes using inline-block display, add a right margin for inputs
after clearing inherited margins to space the following text, if not already defined.

Fixes: 62382556d713 ("luci-theme-openwrt-2020: use lighter color for checkboxes")

applications/luci-app-attendedsysupgrade/htdocs/luci-static/resources/view/attendedsysupgrade/overview.js
@@ -184,7 +184,7 @@ return view.extend({
 			this.sha256_unsigned = image.sha256_unsigned;
 			let sysupgrade_url = `${data.url}/store/${response.bin_dir}/${image.name}`;
 
-			let keep = E('input', { type: 'checkbox' });
+			let keep = E('input', { 'id': 'asu-keep', 'type': 'checkbox' });
 			keep.checked = true;
 
 			let fields = [
@@ -233,10 +233,11 @@ return view.extend({
 				E(
 					'p',
 					{ class: 'mt-2' },
-					E('label', { class: 'btn' }, [
+					E('label', { 'class': 'cbi-checkbox' }, [
 						keep,
 						' ',
-						_('Keep settings and retain the current configuration'),
+						E('label', { 'for': 'asu-keep' }),
+						_('Keep settings and retain the current configuration')
 					])
 				),
 				E('div', { class: 'right' }, [

modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
@@ -208,10 +208,10 @@ return view.extend({
 			.then(L.bind(function(btn, res) {
 				/* sysupgrade opts table  [0]:checkbox element [1]:check condition [2]:args to pass */
 				const opts = {
-				    keep : [ E('input', { type: 'checkbox' }), false, '-n' ],
-				    force : [ E('input', { type: 'checkbox' }), true, '--force' ],
-				    skip_orig : [ E('input', { type: 'checkbox' }), true, '-u' ],
-				    backup_pkgs : [ E('input', { type: 'checkbox' }), true, '-k' ],
+				    keep : [ E('input', { 'id': 'flash-keep', 'type': 'checkbox' }), false, '-n' ],
+				    force : [ E('input', { 'id': 'flash-force', 'type': 'checkbox' }), true, '--force' ],
+				    skip_orig : [ E('input', { 'id': 'flash-skip_orig', 'type': 'checkbox' }), true, '-u' ],
+				    backup_pkgs : [ E('input', { 'id': 'flash-backup_pkgs', 'type': 'checkbox' }), true, '-k' ],
 				    },
 				    is_valid = res[1].valid,
 				    is_forceable = res[1].forceable,
@@ -226,8 +226,10 @@ return view.extend({
 					res[0].sha256sum ? E('li', {}, '%s: %s'.format(_('SHA256'), res[0].sha256sum)) : ''
 				]));
 
-				body.push(E('p', {}, E('label', { 'class': 'btn' }, [
-					opts.keep[0], ' ', _('Keep settings and retain the current configuration')
+				body.push(E('p', {}, E('label', { 'class': 'cbi-checkbox' }, [
+					opts.keep[0], ' ',
+					E('label', { 'for': 'flash-keep' }),
+					_('Keep settings and retain the current configuration')
 				])));
 
 				if (!is_valid || is_too_big)
@@ -255,13 +257,17 @@ return view.extend({
 					opts.keep[0].checked = true;
 
 					if (has_rootfs_data) {
-						body.push(E('p', {}, E('label', { 'class': 'btn' }, [
-							opts.skip_orig[0], ' ', _('Skip from backup files that are equal to those in /rom')
+						body.push(E('p', {}, E('label', { 'class': 'cbi-checkbox' }, [
+							opts.skip_orig[0], ' ',
+							E('label', { 'for': 'flash-skip_orig' }),
+							_('Skip from backup files that are equal to those in /rom')
 						])));
 					}
 
-					body.push(E('p', {}, E('label', { 'class': 'btn' }, [
-						opts.backup_pkgs[0], ' ', _('Include in backup a list of current installed packages at /etc/backup/installed_packages.txt')
+					body.push(E('p', {}, E('label', { 'class': 'cbi-checkbox' }, [
+						opts.backup_pkgs[0], ' ',
+						E('label', { 'for': 'flash-backup_pkgs' }),
+						_('Include in backup a list of current installed packages at /etc/backup/installed_packages.txt')
 					])));
 				};
 
@@ -279,8 +285,10 @@ return view.extend({
 				};
 
 				if ((!is_valid || is_too_big || res[2].code != 0) && is_forceable) {
-					body.push(E('p', {}, E('label', { 'class': 'btn alert-message danger' }, [
-						opts.force[0], ' ', _('Force upgrade'),
+					body.push(E('p', {}, E('label', { 'class': 'cbi-checkbox alert-message danger' }, [
+						opts.force[0], ' ',
+						E('label', { 'for': 'flash-force' }),
+						_('Force upgrade'),
 						E('br'), E('br'),
 						_('Select \'Force upgrade\' to flash the image even if the image format check fails. Use only if you are sure that the firmware is correct and meant for your device!')
 					])));

themes/luci-theme-bootstrap/htdocs/luci-static/bootstrap/cascade.css
@@ -574,6 +574,7 @@ label > input[type="checkbox"],
 label > input[type="radio"] {
 	vertical-align: text-top;
 	margin: 0;
+	margin-right: .5em;
 }
 
 label[for] {
@@ -697,6 +698,16 @@ select,
 	height: 30px;
 	display: inline-flex;
 	align-items: center;
+}
+
+label.cbi-checkbox {
+	padding: 0 14px;
+	font-size: 13px;
+	line-height: 2em;
+	border: 1px solid var(--border-color-high);
+	border-radius: 4px;
+	display: inline-block;
+	cursor: pointer;
 }
 
 .cbi-radio {

themes/luci-theme-footstrap/htdocs/luci-static/footstrap/cascade.css
@@ -722,10 +722,10 @@ div.cbi-value var,.td.cbi-value-field var{color:var(--fs-accent)}
 .cbi-filebrowser > ul > li > div:last-child{flex:0 1 auto;min-width:0;display:flex;flex-wrap:wrap;gap:var(--fs-space-2);justify-content:flex-end}
 .cbi-filebrowser > ul > li > div:last-child > button{margin:0}
 .cbi-filebrowser .upload > div > input{width:100%}
-.btn > input[type="checkbox"],.btn > input[type="radio"]{margin-inline-end:.5em}
+label.cbi-checkbox{justify-content:center;width:auto;height:auto;min-height:var(--fs-ctl-h);padding:var(--fs-space-2) var(--fs-space-3-5);font-weight:var(--fs-weight);line-height:1.1;border:var(--fs-hairline);border-radius:var(--fs-radius);white-space:normal;overflow-wrap:anywhere;min-width:0;gap:.5em;cursor:pointer}
 .cbi-checkbox{display:inline-flex;align-items:center}
 .cbi-checkbox > input[type="checkbox"]{position:absolute;opacity:0;width:0;height:0;margin:0}
-.cbi-checkbox > label{--sw-w:calc(40px * var(--fs-density-box));--sw-h:calc(22px * var(--fs-density-box));--sw-knob:calc(16px * var(--fs-density-box));--sw-pad:calc(2px * var(--fs-density-box));display:inline-block;position:relative;margin:0;width:var(--sw-w);height:var(--sw-h);border-radius:var(--fs-radius-pill);background:var(--fs-track);border:var(--fs-hairline);cursor:pointer;transition:background var(--fs-dur),border-color var(--fs-dur);vertical-align:middle}
+.cbi-checkbox > label{--sw-w:calc(40px * var(--fs-density-box));--sw-h:calc(22px * var(--fs-density-box));--sw-knob:calc(16px * var(--fs-density-box));--sw-pad:calc(2px * var(--fs-density-box));display:inline-block;position:relative;margin:0;width:var(--sw-w);height:var(--sw-h);border-radius:var(--fs-radius-pill);background:var(--fs-track);border:var(--fs-hairline);cursor:pointer;transition:background var(--fs-dur),border-color var(--fs-dur);vertical-align:middle;flex-shrink:0}
 .cbi-checkbox > label::before{content:"";position:absolute;inset:-4px -2px}
 .cbi-checkbox > label::after{content:"";position:absolute;top:var(--sw-pad);inset-inline-start:var(--sw-pad);width:var(--sw-knob);height:var(--sw-knob);border-radius:50%;background:var(--fs-dim);transform:translateX(0);transition:transform var(--fs-dur-move),background var(--fs-dur)}
 .cbi-checkbox > input[type="checkbox"]:checked + label{background:var(--fs-accent);border-color:transparent}
@@ -881,7 +881,7 @@ input[type="range"]:focus{box-shadow:var(--fs-focus-ring-solo)}
 .modal > .alert-message{margin-bottom:var(--fs-space-2)}
 .modal > pre,.modal > textarea{white-space:pre-wrap;overflow:auto;width:100%}
 .modal:has(.cbi-map){max-width:min(1100px,94vw)}
-#modal_overlay .modal label.btn{white-space:normal;overflow-wrap:anywhere;min-width:0}
+#modal_overlay .modal{white-space:normal;overflow-wrap:anywhere;min-width:0}
 #modal_overlay .modal li{overflow-wrap:anywhere}
 #modal_overlay .uci-dialog div > ins,#modal_overlay .uci-dialog div > del,#modal_overlay .uci-dialog div > var{border-radius:var(--fs-radius-sm)}
 #modal_overlay .uci-change-legend-label > ins,#modal_overlay .uci-change-legend-label > del,#modal_overlay .uci-change-legend-label > var{border-radius:var(--fs-radius-sm);overflow:hidden}

themes/luci-theme-material/htdocs/luci-static/material/cascade.css
@@ -1780,7 +1780,7 @@ body:not(.Interfaces) .cbi-rowstyle-2:first-child {
 	word-break: break-word;
 }
 
-.modal label.btn {
+.modal {
 	display: flex;
 	align-items: center;
 	white-space: normal;
@@ -2250,6 +2250,22 @@ span[data-tooltip] .label {
 label > input[type="checkbox"],
 label > input[type="radio"] {
 	margin-right: 0.8rem;
+}
+
+label.cbi-checkbox {
+	background-color: var(--gray-color);
+	padding: .3rem .6rem;
+	padding-bottom: 0.2rem;
+	padding-top: 0.2rem;
+	line-height: normal;
+	align-items: center;
+	white-space: normal;
+	text-align: left;
+	text-transform: none;
+	border: 0;
+	border-radius: .2rem;
+	display: flex;
+	cursor: pointer;
 }
 
 label[data-index][data-depends] {

themes/luci-theme-openwrt-2020/htdocs/luci-static/openwrt2020/cascade.css
@@ -978,6 +978,15 @@ button[disabled], button.disabled, .btn[disabled], .btn.disabled {
 	background: var(--danger-color);
 }
 
+label.cbi-checkbox {
+	padding: 0 .5em;
+	line-height: 1.5em;
+	box-shadow: 0 0 5px var(--main-dark-color);
+	border-radius: .25em;
+	display: inline-block;
+	cursor: pointer;
+}
+
 input[type="checkbox"],
 input[type="radio"] {
 	--bd-color: var(--main-dark-color);
@@ -992,6 +1001,8 @@ input[type="radio"] {
 	cursor: pointer;
 	background: none;
 	border: none;
+	margin: 0;
+	margin-right: .5em;
 }
 
 input[type="checkbox"]::before,

themes/luci-theme-openwrt/htdocs/luci-static/openwrt.org/cascade.css
@@ -636,6 +636,19 @@ label > input[type="checkbox"],
 label > input[type="radio"] {
 	vertical-align: text-bottom;
 	margin: 0;
+	margin-right: .5em;
+}
+
+label.cbi-checkbox {
+	padding: 0 .5em;
+	font-size: inherit;
+	font-weight: bold;
+	border: 1px solid #aaa;
+	border-radius: 3px;
+	min-height: 22px;
+	line-height: 20px;
+	display: inline-block;
+	cursor: pointer;
 }
 
 .td select,